418ef586779081d3a8c8ce450e9c5c6ce2c98f4d
zotov <pavel-zotov>
2022-03-23 20:11:04 +0300

    removed dependency on separate .fbk file: got script which forces crash on FB 2.5.0 without it, but one need to re-connect. Put this inside test code.

--- tests/bugs/core_3557_test.py
+++ tests/bugs/core_3557_test.py
@@ -12,17 +12,24 @@ FBTEST:      bugs.core_3557
 import pytest
 from firebird.qa import *
 
-db = db_factory(from_backup='core3557.fbk')
+db = db_factory()
 
 test_script = """
+    create table test (id int primary key);
+    commit;
+
+    -- RECONNECT IS MANDATORY HERE IN ORDER TO GET CRASH:
+    -- ##################################################
+    connect '$(DSN)' user 'SYSDBA' password 'masterkey';
+
     -- Confirmed for 2.5.0 only: server crashes on running the following EB. 26.02.2015
     -- All subsequent releases should produce no stdout & stderr.
     set term ^;
     execute block as
     begin
-      execute statement 'drop table t';
+      execute statement 'drop table test';
       in autonomous transaction do
-        execute statement ('insert into t values (1)');
+        execute statement ('insert into test values (1)');
     end ^
 """
 
