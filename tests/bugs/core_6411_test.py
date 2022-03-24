ca8278c1695e44af9f5cc3888e195ab6e1071bdf
zotov <pavel-zotov>
2022-03-24 21:25:17 +0300

    Re-implemented: we have to create separate .sql file and run it using 'ISQL ... -I this_temporary_file.sql' - rather than use PIPE mechanism. Details see in the code.

--- tests/bugs/core_6411_test.py
+++ tests/bugs/core_6411_test.py
@@ -35,12 +35,24 @@ NOTES:
          + step: 1, FLD_COUNT: 8065, result: Dynamic SQL Error
          + step: 1, FLD_COUNT: 8065, result: -SQL error code = -804
          + step: 1, FLD_COUNT: 8065, result: -Count of read-write columns does not equal count of values
+[24.03.2022] pzotov
+  On WINDOWS excessive lines in STDERR (after first error) appear because ISQL ignores 'SET BAIL' command or '-bail' switch
+  in case when it executing commands that come via PIPE mechanism.
+  Discussed with FB-team, letter with subj "[new-qa] ISQL "SET BAIL ON" problem on Windows when commands come from PIPE", 12-mar-2022 16:04.
+  It looks strange but such behaviour was considered as DESIRED feature and requested long ago (in 2011).
+  This means that it is very unlikely that it will be fixed and ISQL will behave like on POSIX (where no such effect occurs).
+  Because of this, it was decided to change test: we create temporary SQL file and run ISQL using subprocess.call() with providing '-i' switch.
+
+
 JIRA:        CORE-6411
 FBTEST:      bugs.core_6411
 """
 
 import pytest
 import platform
+import subprocess
+import time
+from pathlib import Path
 from firebird.qa import *
 
 db = db_factory()
@@ -48,17 +60,29 @@ db = db_factory()
 act = python_act('db', substitutions=[('.*(-)?After line \\d+.*', ''), ('[ \t]+', ' ')])
 
 expected_stdout = """
-    step: 0, FLD_COUNT: 8064, result: FIELDS_TOTAL 32510016
-    step: 1, FLD_COUNT: 8065, result: Statement failed, SQLSTATE = 54000
-    step: 1, FLD_COUNT: 8065, result: unsuccessful metadata update
-    step: 1, FLD_COUNT: 8065, result: -new record size of 65536 bytes is too big
-    step: 1, FLD_COUNT: 8065, result: -TABLE TDATA
+    FLD_COUNT: 8064, ISQL output: MSG point-1
+    FLD_COUNT: 8064, ISQL output: MSG point-2
+    FLD_COUNT: 8064, ISQL output: MSG point-3
+    FLD_COUNT: 8064, ISQL output: RDB$RELATION_NAME TDATA
+    FLD_COUNT: 8064, ISQL output: Records affected: 1
+    FLD_COUNT: 8064, ISQL output: MSG point-4
+    FLD_COUNT: 8064, ISQL output: FIELDS_TOTAL 32510016
+    FLD_COUNT: 8064, ISQL output: MSG point-5
+    FLD_COUNT: 8065, ISQL output: MSG point-1
+    FLD_COUNT: 8065, ISQL output: Statement failed, SQLSTATE = 54000
+    FLD_COUNT: 8065, ISQL output: unsuccessful metadata update
+    FLD_COUNT: 8065, ISQL output: -new record size of 65536 bytes is too big
+    FLD_COUNT: 8065, ISQL output: -TABLE TDATA
 """
 
-@pytest.mark.skipif(platform.system() == 'Windows', reason='FIXME: see notes')
+isql_script = temp_file('test-script-6411.sql')
+isql_output = temp_file('test-script-6411.out')
+
+
+#@pytest.mark.skipif(platform.system() == 'Windows', reason='FIXME: see notes')
 @pytest.mark.version('>=3.0.7')
-def test_1(act: Action, capsys):
-    for step in range(2):
+def test_1(act: Action, isql_script: Path, isql_output: Path, capsys):
+    for step in range(0,2):
         FLD_COUNT = 8064 + step
         ddl_init = 'recreate table tdata (id bigint primary key'
         ddl_addi = '\n'.join([f',f{i} bigint' for i in range(1,FLD_COUNT)])
@@ -70,22 +94,51 @@ def test_1(act: Action, capsys):
         sel_addi = '+'.join([str(i) for i in range(0,FLD_COUNT)])
         sel_expr = ''.join([sel_init, sel_addi, ' as fields_total from tdata'])
         sql_expr=  f"""
-            set bail on ;
+            SET BAIL ON; -- !!! THIS FORCES TO RUN THIS SCRIPT ON WINDOWS VIA 'ISQL ... -I <SCRIPT>'. DO *NOT* USE PIPE MECHANISM !!!
+            set list on;
+            select 'point-1' as msg from rdb$database;
             {ddl_expr} ;
+            select 'point-2' as msg from rdb$database;
             commit;
             connect '{act.db.dsn}' user {act.db.user} password '{act.db.password}' ;
+            select 'point-3' as msg from rdb$database;
+            set count on;
+            select rdb$relation_name from rdb$relations where rdb$system_flag<>1;
+            set count off;
             {upd_expr} ;
-            set list on ;
+            select 'point-4' as msg from rdb$database;
             {sel_expr} ;
+            select 'point-5' as msg from rdb$database;
             quit ;
             """
+        
+        isql_script.write_text(sql_expr)
+        with open(isql_output, mode='w') as isql_out:
+            subprocess.call( [ act.vars['isql'],
+                               '-u', act.db.user, '-pas', act.db.password,
+                               act.db.dsn, '-i', str(isql_script)
+                             ],
+                             stdout=isql_out, stderr=subprocess.STDOUT
+                           )
+
+        # https://firebird-qa.readthedocs.io/en/latest/reference.html?highlight=RESET#firebird.qa.plugin.Action.reset
+        # It’s necessary to call this method between executions of external tools through isql(), ...
         act.reset()
-        act.isql(switches=[], input=sql_expr, combine_output=True)
-        for line in act.clean_stdout.splitlines():
-            if line.strip():
-                print(f'step: {step}, FLD_COUNT: {FLD_COUNT}, result: {line}')
-    # Check
+        with open(isql_output, mode='r') as f:
+            for line in f:
+                if line.strip():
+                    print(f'FLD_COUNT: {FLD_COUNT}, ISQL output: {line}')
+    
+    # https://firebird-qa.readthedocs.io/en/latest/reference.html?highlight=clean_stdout#firebird.qa.plugin.Action.clean_expected_stderr
+    # Value is cached once evaluated. Use reset() before new expected_stderr (expected_stdout) value is assigned to force re-evaluation.
+    ###########
     act.reset()
+    ###########
+
     act.expected_stdout = expected_stdout
     act.stdout = capsys.readouterr().out
+
+    # https://firebird-qa.readthedocs.io/en/latest/reference.html?highlight=clean_stdout#firebird.qa.plugin.Action.clean_expected_stdout
+    # Returns string with removed isql noise strings and leading and trailing whitespace, and after other substitutions.
+
     assert act.clean_stdout == act.clean_expected_stdout
