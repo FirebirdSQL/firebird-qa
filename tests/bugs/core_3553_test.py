e2363f5603ba9d10350a39801d6a859cafb5dc73
zotov <pavel-zotov>
2022-04-07 21:02:51 +0300

    Added/Updated bugs\core_3553_test.py: Expected stdout depends now on major FB version because of changes since 5.0.0.455 in PLAN HASH when data sources have the same cardinality.

--- tests/bugs/core_3553_test.py
+++ tests/bugs/core_3553_test.py
@@ -8,6 +8,11 @@ TITLE:       Nested loop plan is chosen instead of the sort merge for joining in
 DESCRIPTION:
 JIRA:        CORE-3553
 FBTEST:      bugs.core_3553
+NOTES:
+    [07.04.2022] pzotov
+    FB 5.0.0.455 and later: data sources with equal cardinality now present in the HASH plan in order they are specified in the query.
+    Reversed order was used before this build. Because of this, two cases of expected stdout must be taken in account, see variables
+    'fb3x_checked_stdout' and 'fb5x_checked_stdout'.
 """
 
 import pytest
@@ -15,25 +20,29 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """SET PLAN ON;
-select count(*)
-from rdb$database d1 join rdb$database d2
-  on cast(d1.rdb$relation_id as char(10)) = cast(d2.rdb$relation_id as char(20));
+test_script = """
+    set planonly;
+    select count(*)
+    from rdb$database d1
+    join rdb$database d2  on cast(d1.rdb$relation_id as char(10)) = cast(d2.rdb$relation_id as char(20));
 """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-PLAN HASH (D2 NATURAL, D1 NATURAL)
+fb3x_checked_stdout = """
+    PLAN HASH (D2 NATURAL, D1 NATURAL)
+"""
 
-                COUNT
-=====================
-                    1
+fb5x_checked_stdout = """
+    PLAN HASH (D1 NATURAL, D2 NATURAL)
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
+    with act.connect_server() as srv:
+        engine_major = int(srv.info.engine_version)
+
+    act.expected_stdout = fb3x_checked_stdout if engine_major < 5 else fb5x_checked_stdout
     act.execute()
     assert act.clean_stdout == act.clean_expected_stdout
 
