9e8aab15109cfdf20bbad7b4af150f11472a1614
zotov <pavel-zotov>
2022-05-08 07:50:43 +0300

    Added/Updated functional\monitoring\test_04.py: checked on 4.0.1.2692, 3.0.8.33535

--- tests/functional/monitoring/test_04.py
+++ tests/functional/monitoring/test_04.py
@@ -1,123 +1,105 @@
 #coding:utf-8
 
 """
-ID:          monitoring-tables-03
+ID:          monitoring-tables-04
 TITLE:       SYSDBA must see all attachments and their transactions, non-privileged user - only those which was of his login.
 DESCRIPTION:
 FBTEST:      functional.monitoring.04
 """
 
 import pytest
+import time
 from firebird.qa import *
+from firebird.driver import *
 
-init_script = """
-    set wng off;
-    create or alter user u01 password '123';
-    create or alter user u02 password '456';
-    commit;
-    create or alter view v_who as
-    select
-        current_user as who_am_i
-        ,a.mon$user who_else
-        ,dense_rank()over(partition by a.mon$user order by t.mon$transaction_id) tid_rown
-        ,t.mon$isolation_mode isol_mode
-        -- 15.01.2019: removed detailed info about read committed TIL because of read consistency TIL that 4.0 introduces.
-        -- Any record with t.mon$isolation_mode = 4 now is considered just as read committed, w/o any detalization (this not much needed here).
-        ,decode( t.mon$isolation_mode, 0,'CONSISTENCY', 1,'SNAPSHOT', 2, 'READ_COMMITTED', 3, 'READ_COMMITTED', 4, 'READ_COMMITTED', '??' ) as isol_descr
-    from
-        mon$attachments a
-        LEFT join mon$transactions t using(mon$attachment_id)
-    where
-        a.mon$attachment_id is distinct from current_connection
-        and a.mon$system_flag is distinct from 1 -- remove Cache Writer and Garbage Collector from resultset
-    order by a.mon$user, t.mon$transaction_id;
-    commit;
-
-    revoke all on all from u01;
-    revoke all on all from u02;
-    grant select on v_who to u01;
-    grant select on v_who to u02;
-    commit;
-  """
-
-db = db_factory(init=init_script)
+db = db_factory()
+test_usr1 = user_factory('db', name='u01', password='123')
+test_usr2 = user_factory('db', name='u02', password='456')
 
 act = python_act('db', substitutions=[('[=]{1,}', ''), ('[ \t]+', ' ')])
 
-expected_stdout = """
+
+test_expected_stdout = """
     CHECK_NO  WHO_AM_I     WHO_ELSE                  TID_ROWN ISOL_MODE  ISOL_DESCR
-    1         U01          U01                              1         2  READ_COMMITTED
-    1         U01          U01                              2         2  READ_COMMITTED
+    1         U01          U01                              1         1  SNAPSHOT
+    1         U01          U01                              2         1  SNAPSHOT
     Records affected: 2
 
     CHECK_NO  WHO_AM_I     WHO_ELSE                  TID_ROWN ISOL_MODE  ISOL_DESCR
-    2         SYSDBA       SYSDBA                           1         2  READ_COMMITTED
-    2         SYSDBA       U01                              1         2  READ_COMMITTED
-    2         SYSDBA       U01                              2         2  READ_COMMITTED
+    2         SYSDBA       SYSDBA                           1         1  SNAPSHOT
+    2         SYSDBA       U01                              1         1  SNAPSHOT
+    2         SYSDBA       U01                              2         1  SNAPSHOT
     2         SYSDBA       U02                              1         2  READ_COMMITTED
     2         SYSDBA       U02                              2         1  SNAPSHOT
     2         SYSDBA       U02                              3         0  CONSISTENCY
     Records affected: 6
 """
 
-@pytest.mark.skip('FIXME: Not IMPLEMENTED')
-@pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    pytest.fail("Not IMPLEMENTED")
+@pytest.mark.version('>=3')
+def test_1(act: Action, test_usr1: User, test_usr2: User):
 
-# test_script_1
-#---
-#
-#  db_conn.close()
-#
-#  #----------------------------------------------------------------------
-#
-#  con_1 = fdb.connect(dsn=dsn, user=user_name, password=user_password )
-#  con_2 = fdb.connect(dsn=dsn, user='U01', password='123')
-#  con_3 = fdb.connect(dsn=dsn, user='U01', password='123')
-#
-#  con_1.begin()
-#  con_2.begin()
-#  con_3.begin()
-#
-#  #----------------------------------------------------------------------
-#
-#  custom_tpb4 = fdb.TPB()
-#  custom_tpb4.isolation_level = (fdb.isc_tpb_read_committed, fdb.isc_tpb_rec_version)
-#
-#  custom_tpb5 = fdb.TPB()
-#  custom_tpb5.isolation_level = fdb.isc_tpb_concurrency
-#
-#  custom_tpb6 = fdb.TPB()
-#  custom_tpb6.isolation_level = fdb.isc_tpb_consistency
-#
-#  con_4 = fdb.connect(dsn=dsn, user='U02', password='456')
-#  con_5 = fdb.connect(dsn=dsn, user='U02', password='456')
-#  con_6 = fdb.connect(dsn=dsn, user='U02', password='456')
-#
-#  con_4.begin( tpb = custom_tpb4 )
-#  con_5.begin( tpb = custom_tpb5 )
-#  con_6.begin( tpb = custom_tpb6 )
-#
-#  sql_chk='''
-#      set width who_am_i 12;
-#      set width who_else 12;
-#      set width isol_descr 30;
-#      set count on;
-#
-#      connect '$(DSN)' user 'u01' password '123';
-#      select 1 as check_no, v.* from v_who v;
-#      commit;
-#
-#      connect '$(DSN)' user 'SYSDBA' password 'masterkey';
-#
-#      select 2 as check_no, v.* from v_who v;
-#      commit;
-#
-#      drop user u01;
-#      drop user u02;
-#      commit;
-#  '''
-#
-#  runProgram('isql',[  '-pag','99999','-q' ], sql_chk)
-#---
+    init_script = f"""
+        create or alter view v_who as
+        select
+            current_user as who_am_i
+            ,a.mon$user who_else
+            ,dense_rank()over(partition by a.mon$user order by t.mon$transaction_id) tid_rown
+
+            -- 07.05.2022: it is recommended to set ReadConsistency = 0 in the firebird.conf for FB 4.x+, see 'firebird-qa/README.rst'.
+            -- If it is so then mon$transactions.mon$isolation_mode will be 2 for transactions which were started as READ CONSISTENCY,
+            -- but otherwise this value withh be 4. Because of this, we have to check here engine version and if it is NOT '3.' (i.e. 4, 5 and later)
+            -- then we must REPLACE '4' with '2':
+            ,iif( not rdb$get_context('SYSTEM','ENGINE_VERSION') starting with '3.' and t.mon$isolation_mode = 4, 2, t.mon$isolation_mode) isol_mode
+
+            -- 15.01.2019: removed detailed info about read committed TIL because of read consistency TIL that 4.0 introduces.
+            -- Any record with t.mon$isolation_mode = 4 now is considered just as read committed, w/o any detalization (this not much needed here).
+            ,decode( t.mon$isolation_mode, 0,'CONSISTENCY', 1,'SNAPSHOT', 2, 'READ_COMMITTED', 3, 'READ_COMMITTED', 4, 'READ_COMMITTED', '??' ) as isol_descr
+        from
+            mon$attachments a
+            LEFT join mon$transactions t using(mon$attachment_id)
+        where
+            a.mon$attachment_id is distinct from current_connection
+            and a.mon$system_flag is distinct from 1 -- remove Cache Writer and Garbage Collector from resultset
+        order by a.mon$user, t.mon$transaction_id;
+        commit;
+        
+        grant select on v_who to public;
+        commit;
+    """
+    act.isql(switches=['-q'], input = init_script)
+    act.reset()
+
+    with act.db.connect() as con1 \
+         ,act.db.connect(user = test_usr1.name, password = test_usr1.password) as con2 \
+         ,act.db.connect(user = test_usr1.name, password = test_usr1.password) as con3 \
+         ,act.db.connect(user = test_usr2.name, password = test_usr2.password) as con4 \
+         ,act.db.connect(user = test_usr2.name, password = test_usr2.password) as con5 \
+         ,act.db.connect(user = test_usr2.name, password = test_usr2.password) as con6:
+
+        con1.begin()
+        con2.begin()
+        con3.begin()
+
+        con4.begin(TPB(isolation=Isolation.READ_COMMITTED_RECORD_VERSION).get_buffer())
+        con5.begin(TPB(isolation=Isolation.CONCURRENCY).get_buffer())
+        con6.begin(TPB(isolation=Isolation.CONSISTENCY).get_buffer())
+
+        test_script = \
+        f'''
+            set width who_am_i 12;
+            set width who_else 12;
+            set width isol_descr 30;
+            set count on;
+            connect '{act.db.dsn}' user '{test_usr1.name}' password '{test_usr1.password}';
+            select 1 as check_no, v.* from v_who v;
+            commit;
+
+            connect '{act.db.dsn}' user {act.db.user} password '{act.db.password}' ;
+
+            select 2 as check_no, v.* from v_who v;
+            commit;
+        '''
+
+        act.expected_stdout = test_expected_stdout
+        act.isql(switches=['-q'], input = test_script, connect_db = False )
+        assert act.clean_stdout == act.clean_expected_stdout
