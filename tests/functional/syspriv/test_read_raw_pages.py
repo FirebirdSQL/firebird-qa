73c917100c8711728a72c050f11d412230e853f6
zotov <pavel-zotov>
2022-05-20 22:05:12 +0300

    Added/Updated functional\syspriv\test_read_raw_pages.py: minor change: removed old code that was copied from FBT suite.

--- tests/functional/syspriv/test_read_raw_pages.py
+++ tests/functional/syspriv/test_read_raw_pages.py
@@ -96,36 +96,3 @@ def test_1(act: Action, tmp_hacker: User, tmp_reader: User, tmp_role: Role, caps
     act.expected_stdout = expected_stdout_main
     act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
-
-# test_script_1
-#---
-#
-#  import os
-#  from struct import unpack_from
-#
-#  db_file = db_conn.database_name
-#  db_conn.close()
-#
-#  for i in ( 'mike_adams_bad_hacker', 'john_smith_raw_reader'):
-#      print('User: %s' % i )
-#      con = fdb.connect(dsn = dsn, user = i, password = '123', role = 'role_for_read_raw_pages')
-#      try:
-#          page_buffer = con.get_page_contents( 1 )
-#          (page_type,) = unpack_from('<b',page_buffer)
-#          print('Successfully get content of page, its type: %d' % page_type )
-#      except Exception,e:
-#          print('Exception occured:')
-#          for x in e:
-#              print(x)
-#      finally:
-#          con.close()
-#
-#  #------------------------
-#
-#  con = fdb.connect(dsn = dsn, user = user_name, password = user_password)
-#  con.execute_immediate('drop user mike_adams_bad_hacker using plugin Srp')
-#  con.execute_immediate('drop user john_smith_raw_reader using plugin Srp')
-#  con.commit()
-#  con.close()
-#
-#---
