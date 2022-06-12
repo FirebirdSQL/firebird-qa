aa5d8fdf9d75693977cc3e3ac984ebea35a10733
zotov <pavel-zotov>
2022-06-12 18:55:09 +0300

    Added/Updated bugs\core_5808_test.py: Checked on 4.0.1.2692 - both on Linux and Windows.

--- tests/bugs/core_5808_test.py
+++ tests/bugs/core_5808_test.py
@@ -5,236 +5,159 @@ ID:          issue-6070
 ISSUE:       6070
 TITLE:       Support backup of encrypted databases
 DESCRIPTION:
-    THIS TEST USES IBSurgeon Demo Encryption package
-    ################################################
-    ( https://ib-aid.com/download-demo-firebird-encryption-plugin/ ; https://ib-aid.com/download/crypt/CryptTest.zip )
-    License file plugins\\dbcrypt.conf with unlimited expiration was provided by IBSurgeon to Firebird Foundation (FF).
-    This file was preliminary stored in FF Test machine.
-    Test assumes that this file and all neccessary libraries already were stored into FB_HOME and %FB_HOME%\\plugins.
 
-    After test database will be created, we try to encrypt it using 'alter database encrypt with <plugin_name> ...' command
-    (where <plugin_name> = dbcrypt - name of .dll in FB_HOME\\plugins\\ folder that implements encryption).
-    Then we allow engine to complete this job - take delay about 1..2 seconds BEFORE detach from database.
-    After this we make  backup of encrypted database + restore.
+    Test creates two tables, adds data, and drops one of them (thus there are deallocated pages in the database).
+    Then it creates lot of sequences with different initial values and increments.
+    Database is encrypted after this step.
 
-    Then we make snapshot of firebird.log, run 'gfix -v -full' of restored database and once again take snapshot of firebird.log.
-    Comparison of these two logs is result of validation. It should contain line about start and line with finish info.
-    The latter must look like this: "Validation finished: 0 errors, 0 warnings, 0 fixed"
+    After this, we get initial content of: firebird.log, metadata('isql -x') and sequences value and store them 
+    in variables fblog_1, meta_1 and sequ_1.
+    Then we run backup and restore, and validate database.
+    Further, we extract again metadata and sequences value of restored DB, and store them in fblog_2, meta_2 and sequ_2.
 
-    Checked on:
-        40sS, build 4.0.0.1487: OK, 6.552s.
-        40sC, build 4.0.0.1421: OK, 11.812s.
-        40Cs, build 4.0.0.1485: OK, 8.097s.
+    Difference between fblog_1 and fblog_2 must look like this: "Validation finished: 0 errors, 0 warnings, 0 fixed".
+    NO difference must be between meta_1 vs meta_2, and sequ_1 vs sequ_2
 
-    15.04.2021. Adapted for run both on Windows and Linux. Checked on:
-      Windows: 4.0.0.2416
-      Linux:   4.0.0.2416
 JIRA:        CORE-5808
 FBTEST:      bugs.core_5808
+NOTES:
+    [12.06.2022] pzotov
+    Checked on 4.0.1.2692 - both on Linux and Windows.
+    NB: duration on Linux ~40 s; on Windows ~22 s.
 """
+
+import re
+import time
+import datetime as py_dt
+from datetime import timedelta
+from io import BytesIO
+from difflib import unified_diff
 
 import pytest
 from firebird.qa import *
+from firebird.driver import DatabaseError
+from firebird.driver import SrvRestoreFlag, SrvRepairFlag
 
-db = db_factory()
+N_ROWS = 15
+init_script = f"""
+    create table test1(s varchar(784) unique, b blob);
+    create table test2(s varchar(784) unique, b blob);
+    commit;
+    insert into test1(s) select lpad('',784,uuid_to_char(gen_uuid())) from (select 1 i from rdb$types rows {N_ROWS}),(select 1 i from rdb$types rows {N_ROWS});
+    insert into test2(s) select lpad('',784,uuid_to_char(gen_uuid())) from (select 1 i from rdb$types rows {N_ROWS});
+    update test1 set b = (select list( (select gen_uuid() from rdb$database) ) from (select 1 i from rdb$types rows {N_ROWS}),(select 1 i from rdb$types rows {N_ROWS}));
+    update test2 set b = (select list( (select gen_uuid() from rdb$database) ) from (select 1 i from rdb$types rows {N_ROWS}),(select 1 i from rdb$types rows {N_ROWS}));
+    commit;
+    drop table test1;
+    commit;
+    set term ^;
+    execute block as
+        declare n int = 16000;
+        declare i bigint = -2147483648;
+        declare b bigint = 9223372036854775807;
+    begin
+        while (n > 0) do
+        begin
+            execute statement 'create sequence gen_' || n || ' start with ' || (b-abs(i)) || ' increment by ' || (i+1) ;
+            n = n - 1;
+            i = i + 1;
+        end
+    end ^
+    set term ;^
+    commit;
+"""
+
+db = db_factory(init = init_script)
 
 act = python_act('db')
 
-expected_stdout = """
-    + VALIDATION STARTED
-    + VALIDATION FINISHED: 0 ERRORS, 0 WARNINGS, 0 FIXED
-"""
+MAX_ENCRYPT_DECRYPT_MS = 5000
+ENCRYPTION_PLUGIN = 'fbSampleDbCrypt'
+ENCRYPTION_KEY = 'Red'
 
-@pytest.mark.skip('FIXME: encryption plugin')
+@pytest.mark.encryption
 @pytest.mark.version('>=4.0')
-def test_1(act: Action):
-    pytest.fail("Not IMPLEMENTED")
+def test_1(act: Action, capsys):
+    encryption_finished = False
+    with act.db.connect() as con:
 
-# test_script_1
-#---
-#
-#  import os
-#  import time
-#  import difflib
-#  import subprocess
-#  import re
-#  import fdb
-#
-#  os.environ["ISC_USER"] = user_name
-#  os.environ["ISC_PASSWORD"] = user_password
-#  db_conn.close()
-#
-#  #--------------------------------------------
-#
-#  def svc_get_fb_log( f_fb_log ):
-#
-#    global subprocess
-#
-#    subprocess.call( [ context['fbsvcmgr_path'],
-#                       "localhost:service_mgr",
-#                       "action_get_fb_log"
-#                     ],
-#                     stdout=f_fb_log, stderr=subprocess.STDOUT
-#                   )
-#    return
-#
-#  #--------------------------------------------
-#
-#  def flush_and_close( file_handle ):
-#      # https://docs.python.org/2/library/os.html#os.fsync
-#      # If you're starting with a Python file object f,
-#      # first do f.flush(), and
-#      # then do os.fsync(f.fileno()), to ensure that all internal buffers associated with f are written to disk.
-#      global os
-#
-#      file_handle.flush()
-#      if file_handle.mode not in ('r', 'rb') and file_handle.name != os.devnull:
-#          # otherwise: "OSError: [Errno 9] Bad file descriptor"!
-#          os.fsync(file_handle.fileno())
-#      file_handle.close()
-#
-#  #--------------------------------------------
-#
-#  def cleanup( f_names_list ):
-#      global os
-#      for i in range(len( f_names_list )):
-#         if type(f_names_list[i]) == file:
-#            del_name = f_names_list[i].name
-#         elif type(f_names_list[i]) == str:
-#            del_name = f_names_list[i]
-#         else:
-#            print('Unrecognized type of element:', f_names_list[i], ' - can not be treated as file.')
-#            print('type(f_names_list[i])=',type(f_names_list[i]))
-#            del_name = None
-#
-#         if del_name and os.path.isfile( del_name ):
-#             os.remove( del_name )
-#
-#  #--------------------------------------------
-#
-#  tmpfdb='$(DATABASE_LOCATION)'+'tmp_core_5808.fdb'
-#  tmpfbk='$(DATABASE_LOCATION)'+'tmp_core_5808.fbk'
-#
-#  f_list=( tmpfdb, tmpfbk )
-#  cleanup( f_list )
-#
-#
-#  # 14.04.2021.
-#  # Name of encryption plugin depends on OS:
-#  # * for Windows we (currently) use plugin by IBSurgeon, its name is 'dbcrypt';
-#  # * for Linux we use:
-#  #   ** 'DbCrypt_example' for FB 3.x
-#  #   ** 'fbSampleDbCrypt' for FB 4.x+
-#  #
-#  PLUGIN_NAME = 'dbcrypt' if os.name == 'nt' else '"fbSampleDbCrypt"'
-#
-#  con = fdb.create_database( dsn = 'localhost:'+tmpfdb )
-#  cur = con.cursor()
-#
-#  ##############################################
-#  # WARNING! Do NOT use 'connection_obj.execute_immediate()' for ALTER DATABASE ENCRYPT... command!
-#  # There is bug in FB driver which leads this command to fail with 'token unknown' message
-#  # The reason is that execute_immediate() silently set client dialect = 0 and any encryption statement
-#  # can not be used for such value of client dialect.
-#  # One need to to use only cursor_obj.execute() for encryption!
-#  # See letter from Pavel Cisar, 20.01.20 10:36
-#  ##############################################
-#  cur.execute('alter database encrypt with %(PLUGIN_NAME)s key Red' % locals())
-#
-#  con.commit()
-#
-#  time.sleep(2)
-#  #          ^
-#  #          +-------- !! ALLOW BACKGROUND ENCRYPTION PROCESS TO COMPLETE ITS JOB !!
-#
-#  con.close()
-#
-#  f_backup_log = open( os.path.join(context['temp_directory'],'tmp_backup_5808.log'), 'w')
-#  f_backup_err = open( os.path.join(context['temp_directory'],'tmp_backup_5808.err'), 'w')
-#
-#  subprocess.call( [ context['gbak_path'], "-v", "-b", 'localhost:' + tmpfdb, tmpfbk],
-#                   stdout = f_backup_log,
-#                   stderr = f_backup_err
-#                 )
-#  flush_and_close( f_backup_log )
-#  flush_and_close( f_backup_err )
-#
-#
-#  f_restore_log = open( os.path.join(context['temp_directory'],'tmp_restore_5808.log'), 'w')
-#  f_restore_err = open( os.path.join(context['temp_directory'],'tmp_restore_5808.err'), 'w')
-#
-#  subprocess.call( [ context['gbak_path'], "-v", "-rep", tmpfbk, 'localhost:'+tmpfdb],
-#                   stdout = f_restore_log,
-#                   stderr = f_restore_err
-#                 )
-#  flush_and_close( f_restore_log )
-#  flush_and_close( f_restore_err )
-#
-#  f_fblog_before=open( os.path.join(context['temp_directory'],'tmp_5808_fblog_before.txt'), 'w')
-#  svc_get_fb_log( f_fblog_before )
-#  flush_and_close( f_fblog_before )
-#
-#
-#  f_validate_log = open( os.path.join(context['temp_directory'],'tmp_validate_5808.log'), 'w')
-#  f_validate_err = open( os.path.join(context['temp_directory'],'tmp_validate_5808.err'), 'w')
-#
-#  subprocess.call( [ context['gfix_path'], "-v", "-full", tmpfdb ],
-#                   stdout = f_validate_log,
-#                   stderr = f_validate_err
-#                 )
-#  flush_and_close( f_validate_log )
-#  flush_and_close( f_validate_err )
-#
-#  f_fblog_after=open( os.path.join(context['temp_directory'],'tmp_5808_fblog_after.txt'), 'w')
-#  svc_get_fb_log( f_fblog_after )
-#  flush_and_close( f_fblog_after )
-#
-#
-#  # Compare firebird.log versions BEFORE and AFTER this test:
-#  ######################
-#
-#  oldfb=open(f_fblog_before.name, 'r')
-#  newfb=open(f_fblog_after.name, 'r')
-#
-#  difftext = ''.join(difflib.unified_diff(
-#      oldfb.readlines(),
-#      newfb.readlines()
-#    ))
-#  oldfb.close()
-#  newfb.close()
-#
-#
-#  with open( f_backup_err.name,'r') as f:
-#      for line in f:
-#          print("UNEXPECTED PROBLEM ON BACKUP, STDERR: "+line)
-#
-#  with open( f_restore_err.name,'r') as f:
-#      for line in f:
-#          print("UNEXPECTED PROBLEM ON RESTORE, STDERR: "+line)
-#
-#
-#  f_diff_txt=open( os.path.join(context['temp_directory'],'tmp_5808_diff.txt'), 'w')
-#  f_diff_txt.write(difftext)
-#  flush_and_close( f_diff_txt )
-#
-#  allowed_patterns = (
-#        re.compile( '\\+\\s+Validation\\s+started', re.IGNORECASE)
-#       ,re.compile( '\\+\\s+Validation\\s+finished:\\s+0\\s+errors,\\s+0\\s+warnings,\\s+0\\s+fixed', re.IGNORECASE)
-#  )
-#
-#
-#  with open( f_diff_txt.name,'r') as f:
-#      for line in f:
-#          match2some = filter( None, [ p.search(line) for p in allowed_patterns ] )
-#          if match2some:
-#              print( (' '.join( line.split()).upper() ) )
-#
-#  # CLEANUP:
-#  ##########
-#  # do NOT remove this pause otherwise some of logs will not be enable for deletion and test will finish with
-#  # Exception raised while executing Python test script. exception: WindowsError: 32
-#  time.sleep(1)
-#  cleanup( (f_backup_log, f_backup_err, f_restore_log, f_restore_err, f_validate_log, f_validate_err, f_fblog_before, f_fblog_after, f_diff_txt, tmpfdb, tmpfbk) )
-#
-#
-#---
+        t1=py_dt.datetime.now()
+        d1 = t1-t1
+        sttm = f'alter database encrypt with "{ENCRYPTION_PLUGIN}" key "{ENCRYPTION_KEY}"'
+        try:
+            con.execute_immediate(sttm)
+            con.commit()
+        except DatabaseError as e:
+            print( e.__str__() )
+
+        while True:
+            t2=py_dt.datetime.now()
+            d1=t2-t1
+            if d1.seconds*1000 + d1.microseconds//1000 > MAX_ENCRYPT_DECRYPT_MS:
+                print('TIMEOUT EXPIRATION: encryption took {d1.seconds*1000 + d1.microseconds//1000} ms which exceeds limit = {MAX_ENCRYPT_DECRYPT_MS} ms.')
+                break
+
+            # Possible output:
+            #     Database not encrypted
+            #     Database encrypted, crypt thread not complete
+            act.isql(switches=['-q'], input = 'show database;', combine_output = True)
+            if 'Database encrypted' in act.stdout:
+                if 'not complete' in act.stdout:
+                    pass
+                else:
+                    encryption_finished = True
+                    break
+            act.reset()
+
+
+        act.expected_stdout = ''
+        act.stdout = capsys.readouterr().out
+        assert act.clean_stdout == act.clean_expected_stdout
+        act.reset()
+    
+
+    if encryption_finished:
+        # Extract metadata from initial DB
+        act.isql(switches=['-x'])
+        meta_1 = act.stdout
+        act.reset()
+
+        act.isql(switches=['-q'], input = 'show sequ;', combine_output = True)
+        sequ_1 = act.stdout
+        act.reset()
+
+        # Snippet from core-1725:
+        backup = BytesIO()
+        with act.connect_server() as srv:
+            
+            srv.database.local_backup(database=act.db.db_path, backup_stream=backup)
+            backup.seek(0)
+            srv.database.local_restore(backup_stream=backup, database=act.db.db_path, flags = SrvRestoreFlag.REPLACE)
+     
+            # Get FB log before validation, run validation and get FB log after it:
+            fblog_1 = act.get_firebird_log()
+            srv.database.repair(database=act.db.db_path, flags=SrvRepairFlag.CORRUPTION_CHECK)
+            fblog_2 = act.get_firebird_log()
+
+        # Extract metadata from restored DB
+        act.isql(switches=['-x'])
+        meta_2 = act.stdout
+        act.reset()
+
+        act.isql(switches=['-q'], input = 'show sequ;', combine_output = True)
+        sequ_2 = act.stdout
+        act.reset()
+
+        diff_meta = ''.join(unified_diff(meta_1.splitlines(), meta_2.splitlines()))
+
+        p_diff = re.compile('Validation finished: \\d+ errors, \\d+ warnings, \\d+ fixed')
+        validation_result = ''
+        for line in unified_diff(fblog_1, fblog_2):
+            if line.startswith('+') and p_diff.search(line):
+                validation_result =line.strip().replace('\t', ' ')
+                break
+
+
+        assert diff_meta == ''
+        assert validation_result == '+ Validation finished: 0 errors, 0 warnings, 0 fixed'
+        assert sequ_1 == sequ_2
