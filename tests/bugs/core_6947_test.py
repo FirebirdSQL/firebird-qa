48fdb63cd88fd424318b21ea75f34fa0ea67fc9e
zotov <pavel-zotov>
2022-06-20 22:00:28 +0300

    Added/Updated bugs\core_6947_test.py. Checked on 5.0.0.509 - both Windows and Linux.

--- tests/bugs/core_6947_test.py
+++ tests/bugs/core_6947_test.py
@@ -6,175 +6,147 @@ ISSUE:       6947
 TITLE:       Query to mon$ tables does not return data when the encryption/decryption thread is running
 DESCRIPTION:
   Test creates table with wide indexed column and add some data to it.
-  Volume of data must be big enough enough so that the encryption process does not have time to end in 1 second.
+  Volume of data must be big enough so that the encryption thread will not complete instantly.
 
-  Then ALTER DATABASE ENCRYPT ... is called and we allow encryption process to work for 1 second.
-  After this we query MON$DATABASE and check that MON$CRYPT_STATE column has value = 3 ('is encrypting').
-  Before fix this value always was 1, i.e. 'fully encrypted' - because control returned to 'main' code only
-  after encryption process was completed.
+  Then 'ALTER DATABASE ENCRYPT...' is issued and we start loop 'select mon$crypt_page from mon$database'.
+  This loop must return NOT LESS than <MIN_DISTINCT_ENCRYPTED_PAGES> values greater than zero (i.e. it must
+  show progress of this process; zero values mean that encryption either did not start or already finished).
+  On each iteration we also check whether encryption completed (use ISQL call with 'show database' command)
+  or may be it is too slow and we have to cancel loop because of expiration <max_encrypt_decrypt_ms> limit.
 
-  Note: name for encryption plugin differs on Windows vs Linux:
-   PLUGIN_NAME = 'dbcrypt' if os.name == 'nt' else '"fbSampleDbCrypt"'
+  When this loop is finished, we check number of elements in the encrypted_pages_set (unique values returned
+  by query from mon$database) and show outcome message.
 
   Confirmed bug on 5.0.0.219 (Windows), 5.0.0.236 (Linux).
   Checked on 5.0.0.240 (Windows; SS/Cs), 5.0.0.241 (Linux; SS/CS).
 FBTEST:      bugs.core_6947
+NOTES:
+    [20.06.2022] pzotov
+    Settings for encryption are taken from act.files_dir/'test_config.ini' file.
+    Checked on 5.0.0.509 - both Windows and Linux.
 """
+
+import datetime as py_dt
+from datetime import timedelta
+import time
 
 import pytest
 from firebird.qa import *
+from firebird.driver import ShutdownMode, ShutdownMethod, DatabaseError
 
-db = db_factory()
+FLD_LEN = 500
+N_ROWS = 50000
+MIN_DISTINCT_ENCRYPTED_PAGES = 3
 
-act = python_act('db')
-
-expected_stdout = """
-    DB encryption state: is encrypting.
+init_ddl = f"""
+    recreate table test(s varchar({FLD_LEN}));
+    commit;
+    set term ^;
+    execute block as
+        declare n int = {N_ROWS};
+    begin
+        while (n>0) do
+        begin
+            insert into test(s) values(lpad('', {FLD_LEN}, uuid_to_char(gen_uuid())));
+            n = n -1;
+        end
+    end
+    ^
+    -- for debug, trace must be started with log_proc = true:
+    create procedure sp_tmp(a_page int) as
+    begin
+        -- nop --
+    end
+    ^
+    set term ;^
+    commit;
+    create index test_s on test(s);
 """
+db = db_factory(init = init_ddl)
+act = python_act('db', substitutions=[('[ \t]+', ' ')])
 
-@pytest.mark.skip('FIXME: Not IMPLEMENTED')
+expected_stdout = f"Expected: query to mon$database returned not less then {MIN_DISTINCT_ENCRYPTED_PAGES} encrypted pages."
+
+@pytest.mark.encryption
 @pytest.mark.version('>=5.0')
-def test_1(act: Action):
-    pytest.fail("Not IMPLEMENTED")
+def test_1(act: Action, capsys):
 
-# test_script_1
-#---
-#
-#  import os
-#  import time
-#  import subprocess
-#  import re
-#  import fdb
-#
-#  os.environ["ISC_USER"] = user_name
-#  os.environ["ISC_PASSWORD"] = user_password
-#  engine = db_conn.engine_version
-#  db_conn.close()
-#
-#  #--------------------------------------------
-#
-#  def flush_and_close( file_handle ):
-#      # https://docs.python.org/2/library/os.html#os.fsync
-#      # If you're starting with a Python file object f,
-#      # first do f.flush(), and
-#      # then do os.fsync(f.fileno()), to ensure that all internal buffers associated with f are written to disk.
-#      global os
-#
-#      file_handle.flush()
-#      if file_handle.mode not in ('r', 'rb') and file_handle.name != os.devnull:
-#          # otherwise: "OSError: [Errno 9] Bad file descriptor"!
-#          os.fsync(file_handle.fileno())
-#      file_handle.close()
-#
-#  #--------------------------------------------
-#
-#  def cleanup( f_names_list ):
-#      global os
-#      for i in range(len( f_names_list )):
-#         if type(f_names_list[i]) == file:
-#            del_name = f_names_list[i].name
-#         elif type(f_names_list[i]) == str:
-#            del_name = f_names_list[i]
-#         else:
-#            print('Unrecognized type of element:', f_names_list[i], ' - can not be treated as file.')
-#            print('type(f_names_list[i])=',type(f_names_list[i]))
-#            del_name = None
-#
-#         if del_name and os.path.isfile( del_name ):
-#             os.remove( del_name )
-#
-#  #--------------------------------------------
-#
-#  tmpfdb='$(DATABASE_LOCATION)'+'tmp_core_6947.fdb'
-#
-#  cleanup( (tmpfdb,) )
-#
-#  con = fdb.create_database( dsn = tmpfdb, page_size = 4096 )
-#  con.close()
-#  runProgram('gfix',[tmpfdb,'-w','async'])
-#
-#  con = fdb.connect( dsn = tmpfdb )
-#  con.execute_immediate( 'create table test(s varchar(1000))' )
-#  con.commit()
-#
-#  sql_init='''
-#  execute block as
-#      declare n int = 100000;
-#  begin
-#      while (n>0) do
-#      begin
-#          insert into test(s) values( lpad('', 1000, uuid_to_char(gen_uuid())) );
-#          n = n-1;
-#      end
-#  end
-#  '''
-#  con.execute_immediate(sql_init)
-#  con.commit()
-#  con.execute_immediate( 'create index test_s on test(s)' )
-#  con.commit()
-#
-#  con.close()
-#  runProgram('gfix',[tmpfdb,'-w','sync'])
-#
-#  #-----------------------------------------------------
-#
-#  # Name of encryption plugin depends on OS:
-#  # * for Windows we (currently) use plugin by IBSurgeon, its name is 'dbcrypt';
-#  # * for Linux we use:
-#  #   ** 'DbCrypt_example' for FB 3.x
-#  #   ** 'fbSampleDbCrypt' for FB 4.x+
-#  #
-#  PLUGIN_NAME = 'dbcrypt' if os.name == 'nt' else ( '"fbSampleDbCrypt"' if engine >= 4.0 else '"DbCrypt_example"')
-#
-#  customTPB = ( [ fdb.isc_tpb_read_committed, fdb.isc_tpb_rec_version, fdb.isc_tpb_nowait ] )
-#  con = fdb.connect( dsn = tmpfdb, isolation_level = customTPB )
-#  cur = con.cursor()
-#
-#  ##############################################
-#  # WARNING! Do NOT use 'connection_obj.execute_immediate()' for ALTER DATABASE ENCRYPT... command!
-#  # There is bug in FB driver which leads this command to fail with 'token unknown' message
-#  # The reason is that execute_immediate() silently set client dialect = 0 and any encryption statement
-#  # can not be used for such value of client dialect.
-#  # One need to to use only cursor_obj.execute() for encryption!
-#  # See letter from Pavel Cisar, 20.01.20 10:36
-#  ##############################################
-#  cur.execute('alter database encrypt with %(PLUGIN_NAME)s key Red' % locals())
-#  con.commit()
-#
-#  # Let encryption process start its job:
-#  time.sleep(1)
-#
-#  # Before this ticket was fixed query to mon$ tables did not return data until encryption fully completed.
-#  # This means that we could see only mon$crypt_state = 1 ("fully encrypted").
-#  # After fix, we must see that DB is currently encrypting:
-#  #
-#  crypt_state_map = {0: 'not encrypted', 1: 'fully encrypted', 2: 'is decrypting', 3: 'is encrypting'}
-#  cur.execute('select m.mon$crypt_state from mon$database m')
-#  for r in cur:
-#      db_crypt_state = crypt_state_map.get( r[0], 'UNKNOWN' )
-#
-#  cur.close()
-#  con.close()
-#
-#  print('DB encryption state: %s.' % db_crypt_state)
-#
-#  #---------------------------- shutdown temp DB --------------------
-#
-#  f_dbshut_log = open( os.path.join(context['temp_directory'],'tmp_dbshut_6947.log'), 'w')
-#  subprocess.call( [ context['gfix_path'], 'localhost:'+tmpfdb, "-shut", "full", "-force", "0" ],
-#                   stdout = f_dbshut_log,
-#                   stderr = subprocess.STDOUT
-#                 )
-#  flush_and_close( f_dbshut_log )
-#
-#  with open( f_dbshut_log.name,'r') as f:
-#      for line in f:
-#          print("Unexpected error on SHUTDOWN temp database: "+line)
-#
-#
-#  # CLEANUP
-#  #########
-#  time.sleep(1)
-#  cleanup( ( f_dbshut_log,tmpfdb )  )
-#
-#---
+    act.db.set_sync_write() # here we want DB be  encrypted for some *valuable* time
+
+    # test_cfg -- dict, is defined in qa/plugin.py, obtain settings
+    # from act.files_dir/'test_config.ini':
+    enc_settings = test_cfg['encryption']
+
+    max_encrypt_decrypt_ms = int(enc_settings['max_encrypt_decrypt_ms']) # 5000
+    encryption_plugin = enc_settings['encryption_plugin'] # fbSampleDbCrypt
+    encryption_holder  = enc_settings['encryption_holder'] # fbSampleKeyHolder
+    encryption_key = enc_settings['encryption_key'] # Red
+
+    encryption_started = False
+    encryption_finished = False
+    encrypted_pages_set = set()
+
+    with act.db.connect() as con:
+
+        t1=py_dt.datetime.now()
+        d1 = t1-t1
+        sttm = f'alter database encrypt with "{encryption_plugin}" key "{encryption_key}"'
+        try:
+            con.execute_immediate(sttm)
+            con.commit()
+            encryption_started = True
+        except DatabaseError as e:
+            # -ALTER DATABASE failed
+            # -Crypt plugin fbSampleDbCrypt failed to load
+            #  ==> no sense to do anything else, encryption_started remains False.
+            print( e.__str__() )
+
+
+        cur = con.cursor()
+        cu2 = con.cursor()
+
+        ps = cur.prepare('select mon$crypt_page from mon$database')
+
+        while encryption_started:
+            t2=py_dt.datetime.now()
+            d1=t2-t1
+            if d1.seconds*1000 + d1.microseconds//1000 > max_encrypt_decrypt_ms:
+                print(f'TIMEOUT EXPIRATION: encryption took {d1.seconds*1000 + d1.microseconds//1000} ms which exceeds limit = {max_encrypt_decrypt_ms} ms.')
+                break
+
+            cur.execute(ps)
+            p = cur.fetchone()[0]
+            cu2.callproc('sp_tmp', [ p, ] )
+            con.commit()
+
+            if p > 0:
+                encrypted_pages_set.add(p)
+                if len(encrypted_pages_set) >= MIN_DISTINCT_ENCRYPTED_PAGES:
+                    # We got enough data from mon$database to conclude that encryption is in PROGRESS.
+                    break
+
+            # Possible output:
+            #     Database not encrypted
+            #     Database encrypted, crypt thread not complete
+            act.isql(switches=['-q'], input = 'show database;', combine_output = True)
+            if 'Database encrypted' in act.stdout:
+                if 'not complete' in act.stdout:
+                    pass
+                else:
+                    encryption_finished = True
+                    break
+            act.reset()
+
+    if encryption_started:
+        if len(encrypted_pages_set) >= MIN_DISTINCT_ENCRYPTED_PAGES:
+            print(expected_stdout)
+        elif len(encrypted_pages_set) > 0:
+            print(f'PROBABLY ERROR OR TOO SLOW ENCRYPTION: found {len(encrypted_pages_set)} encrypted pages, minimal expected: {MIN_DISTINCT_ENCRYPTED_PAGES}')
+        else:
+            print(f'ERROR: query to mon$database was blocked during encryption.')
+    else:
+        print('ERROR: nothing was tested because encryption did not start.')
+
+    act.expected_stdout = expected_stdout
+    act.stdout = capsys.readouterr().out
+    assert act.clean_stdout == act.clean_expected_stdout
