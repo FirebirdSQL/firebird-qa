7198e3c695293c56f83f5476d5e59bc4fde6355e
zotov <pavel-zotov>
2022-07-31 10:35:14 +0300

    Added/Updated functional\gtcs\test_external_file_03_d.py. Checked on 3.0.8.33535, 4.0.1.2692, 5.0.0.591

--- tests/functional/gtcs/test_external_file_03_d.py
+++ tests/functional/gtcs/test_external_file_03_d.py
@@ -7,16 +7,33 @@ TITLE:       Test for external table with field of SMALLINT datatype
 DESCRIPTION:
   Original test see in:
   https://github.com/FirebirdSQL/fbtcs/blob/master/GTCS/tests/EXT_REL_0_3_D.script
+
+NOTES:
+  [31.07.2022] pzotov
+  FB config must allow creation of external tables, check parameter 'ExternalFileAccess'.
+  Otherwise: SQLSTATE = 28000 / Use of external file at location ... is not allowed ...
+  Checked on 3.0.8.33535, 4.0.1.2692, 5.0.0.591
 """
 
+from pathlib import Path
 import pytest
 from firebird.qa import *
 
 db = db_factory()
 
-act = python_act('db', substitutions=[('[ \t]+', ' ')])
+act = python_act('db', substitutions=[('[ \t]+', ' '), ('(INPUT|OUTPUT)\\s+message .*', ''), (':\\s+(name:|table:)\\s+.*', '') ])
+
+tmp_ext_file = temp_file('tmp_gtcs_external_file_03_d.dat')
 
 expected_stderr = """
+    Statement failed, SQLSTATE = 22003
+    arithmetic exception, numeric overflow, or string truncation
+    -numeric value is out of range
+
+    Statement failed, SQLSTATE = 22003
+    arithmetic exception, numeric overflow, or string truncation
+    -numeric value is out of range
+
     Statement failed, SQLSTATE = 22003
     arithmetic exception, numeric overflow, or string truncation
     -numeric value is out of range
@@ -27,6 +44,7 @@ expected_stderr = """
 """
 
 expected_stdout = """
+    01: sqltype: 500 SHORT Nullable scale: 0 subtype: 0 len: 2
     F01 -32768
     F01 -1
     F01 0
@@ -35,51 +53,37 @@ expected_stdout = """
     Records affected: 5
 """
 
-@pytest.mark.skip('FIXME: Not IMPLEMENTED')
 @pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    pytest.fail("Not IMPLEMENTED")
+def test_1(act: Action, tmp_ext_file: Path):
+    
+    sql_cmd = f'''
+        create table ext_table external file '{str(tmp_ext_file)}' (f01 smallint);
+        commit;
+        insert into ext_table (f01) values ( 32767);
+        insert into ext_table (f01) values (-32768);
+        insert into ext_table (f01) values (1);
+        insert into ext_table (f01) values (-1);
+        insert into ext_table (f01) values (0);
 
-# test_script_1
-#---
-#
-#  import os
-#  import sys
-#  import subprocess
-#  import time
-#
-#  tmp_file = os.path.join(context['temp_directory'],'tmp_ext_03_d.tmp')
-#  if os.path.isfile( tmp_file):
-#       os.remove( tmp_file )
-#
-#  this_fdb = db_conn.database_name
-#
-#  sql_cmd='''
-#      connect 'localhost:%(this_fdb)s' user '%(user_name)s' password '%(user_password)s';
-#      create table ext_table external file '%(tmp_file)s' (f01 smallint);
-#      commit;
-#      insert into ext_table (f01) values ( 32767);
-#      insert into ext_table (f01) values (-32768);
-#      insert into ext_table (f01) values (1);
-#      insert into ext_table (f01) values (-1);
-#      insert into ext_table (f01) values (0);
-#      insert into ext_table (f01) values ( 32768);
-#      insert into ext_table (f01) values ( -32769);
-#      commit;
-#      set list on;
-#      set count on;
-#      select * from ext_table order by f01;
-#  ''' % dict(globals(), **locals())
-#
-#  runProgram('isql', [ '-q' ], sql_cmd)
-#
-#  f_sql_chk = open( os.path.join(context['temp_directory'],'tmp_ext_03_d.sql'), 'w')
-#  f_sql_chk.write(sql_cmd)
-#  f_sql_chk.close()
-#
-#  time.sleep(1)
-#
-#  os.remove(f_sql_chk.name)
-#  os.remove( tmp_file )
-#
-#---
+        -- All subsequent statements must FAIL:
+        insert into ext_table (f01) values ( 32768);
+        insert into ext_table (f01) values (-32769);
+        insert into ext_table (f01) values (0xF0000000);  -- -268435456
+        insert into ext_table (f01) values (0x0F0000000); -- 4026531840
+        commit;
+        set list on;
+        set count on;
+        set sqlda_display on;
+        select * from ext_table order by f01;
+        commit;
+        drop table ext_table;
+        exit;
+    '''
+
+    act.expected_stdout = expected_stdout
+    act.expected_stderr = expected_stderr
+
+    act.isql(switches=['-q'], input = sql_cmd )
+
+    assert (act.clean_stderr == act.clean_expected_stderr and
+            act.clean_stdout == act.clean_expected_stdout)
