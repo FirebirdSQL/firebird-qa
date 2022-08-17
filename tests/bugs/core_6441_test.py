5f6abfae33ca40f550367a0bf35df1c035974555
zotov <pavel-zotov>
2022-08-17 15:29:46 +0300

    Added/Updated bugs\core_6441_test.py. Adapted for work with pre-created [QA_ROOT]/files/qa-databases.conf. Discussed with pcisar in may-jun 2022. See details in the source.

--- tests/bugs/core_6441_test.py
+++ tests/bugs/core_6441_test.py
@@ -2,7 +2,7 @@
 
 """
 ID:          issue-2367
-ISSUE:       2367
+ISSUE:       https://github.com/FirebirdSQL/firebird/issues/2367
 TITLE:       Srp plugin keeps connection after database has been removed for ~10 seconds (SS and SC)
 DESCRIPTION:
     Test is based on CORE-6412, but instead of complex DDL it only creates DB and makes connection.
@@ -13,7 +13,6 @@ DESCRIPTION:
     Confirmed bug on 4.0.0.2265:
        Statement failed, SQLSTATE = 08006
        Error occurred during login, please check server firebird.log for details
-       Error occurred during login, please check server firebird.log for details
 
     NB. Content of firebird.log will be added with following lines:
        Srp Server
@@ -23,157 +22,121 @@ DESCRIPTION:
 
    Checked on 3.0.8.33392 SS/SC, 4.0.0.2269 SC/SS - all fine.
 
-   03-mar-2021: fixed call syntax in runProgram() macros.
-   Checked on:
-        * WINDOWS: 335544344 : I/O error during "CreateFile (open)" operation ...
-        * LINUX:   335544344 : I/O error during "open" operation ...
 JIRA:        CORE-6441
 FBTEST:      bugs.core_6441
+NOTES:
+    [17.08.2022] pzotov
+    1. One need to be sure that firebird.conf does NOT contain DatabaseAccess = None.
+    2. Test uses pre-created databases.conf which has alias (see variable REQUIRED_ALIAS)
+       and SecurityDatabase in its details which points to that alias, thus making such
+       database be self-security. Database file for that alias must NOT exist in the
+       QA_root/files/qa/ subdirectory: it will be created here.
+       Content of databases.conf must be taken from $QA_ROOT/files/qa-databases.conf (one need to replace
+       it before every test session).
+       Discussed with pcisar, letters since 30-may-2022 13:48, subject:
+       "new qa, core_4964_test.py: strange outcome when use... shutil.copy() // comparing to shutil.copy2()"
+    3. Value of REQUIRED_ALIAS must be EXACTLY the same as alias specified in the pre-created databases.conf
+       (for LINUX this equality is case-sensitive, even when aliases are compared!)
+
+    Confirmed again problem: 4.0.0.2265 (SS), got SQLSTATE = 08006 on second iteration.
+    Checked on 5.0.0.623, 4.0.1.2692, 3.0.8.33535 - both on Windows and Linux; SC and SC.
 """
+import re
+import locale
+from pathlib import Path
 
 import pytest
 from firebird.qa import *
+
+# Name of alias for self-security DB in the QA_root/files/qa-databases.conf.
+# This file must be copied manually to each testing FB homw folder, with replacing
+# databases.conf there:
+#
+REQUIRED_ALIAS = 'tmp_core_6441_alias'
 
 db = db_factory()
 
 act = python_act('db')
 
-expected_stdout = """
-    ITERATION_NO                    0
-    MSG                             DB creation completed OK.
-    ITERATION_NO                    0
-    MSG                             Remote connection established OK.
+ITER_NUM=3
 
-    ITERATION_NO                    1
-    MSG                             DB creation completed OK.
-    ITERATION_NO                    1
-    MSG                             Remote connection established OK.
-
-    ITERATION_NO                    2
-    MSG                             DB creation completed OK.
-    ITERATION_NO                    2
-    MSG                             Remote connection established OK.
-"""
-
-@pytest.mark.skip('FIXME: databases.conf')
 @pytest.mark.version('>=3.0.8')
-def test_1(act: Action):
-    pytest.fail("Not IMPLEMENTED")
+def test_1(act: Action, capsys):
+    # Scan line-by-line through databases.conf, find line starting with REQUIRED_ALIAS and extract name of file that
+    # must be created in the $(dir_sampleDb)/qa/ folder. This name will be used further as target database (tmp_fdb).
+    # NOTE: we have to SKIP lines which are commented out, i.e. if they starts with '#':
+    p_required_alias_ptn =  re.compile( '^(?!#)((^|\\s+)' + REQUIRED_ALIAS + ')\\s*=\\s*\\$\\(dir_sampleDb\\)/qa/', re.IGNORECASE )
+    fname_in_dbconf = None
 
-# test_script_1
-#---
-#
-#  import os
-#  import sys
-#  import time
-#  import subprocess
-#  import shutil
-#  from fdb import services
-#
-#  from fdb import services
-#
-#  svc = services.connect(host='localhost', user = user_name, password = user_password)
-#  fb_home=svc.get_home_directory()
-#  svc.close()
-#
-#  dbconf = os.path.join(fb_home,'databases.conf')
-#  dbcbak = os.path.join(fb_home,'databases.bak')
-#
-#  try:
-#      del os.environ["ISC_USER"]
-#  except KeyError as e:
-#      pass
-#
-#  #--------------------------------------------
-#
-#  def flush_and_close( file_handle ):
-#      # https://docs.python.org/2/library/os.html#os.fsync
-#      # If you're starting with a Python file object f,
-#      # first do f.flush(), and
-#      # then do os.fsync(f.fileno()), to ensure that all internal buffers associated with f are written to disk.
-#      global os
-#
-#      file_handle.flush()
-#      if file_handle.mode not in ('r', 'rb') and file_handle.name != os.devnull:
-#          # otherwise: "OSError: [Errno 9] Bad file descriptor"!
-#          os.fsync(file_handle.fileno())
-#      file_handle.close()
-#
-#  #--------------------------------------------
-#
-#  def cleanup( f_names_list ):
-#      global os
-#      for i in range(len( f_names_list )):
-#         if type(f_names_list[i]) == file:
-#            del_name = f_names_list[i].name
-#         elif type(f_names_list[i]) == str:
-#            del_name = f_names_list[i]
-#         else:
-#            print('Unrecognized type of element:', f_names_list[i], ' - can not be treated as file.')
-#            del_name = None
-#
-#         if del_name and os.path.isfile( del_name ):
-#             os.remove( del_name )
-#
-#  #--------------------------------------------
-#
-#
-#  fdb_test = os.path.join(context['temp_directory'],'tmp_6441.fdb')
-#
-#  shutil.copy2( dbconf, dbcbak )
-#  tmp_alias = 'self_security_6441'
-#
-#  alias_data='''
-#
-#  # Added temporarily for executing test core_6441.fbt
-#  %(tmp_alias)s = %(fdb_test)s {
-#      # RemoteAccess = true
-#      SecurityDatabase = %(tmp_alias)s
-#  }
-#  ''' % locals()
-#
-#  f_dbconf=open( dbconf, 'a')
-#  f_dbconf.seek(0, 2)
-#  f_dbconf.write( alias_data )
-#  flush_and_close( f_dbconf )
-#
-#  cleanup( (fdb_test,) )
-#
-#  sql_init='''
-#      set bail on;
-#      set list on;
-#
-#      create database '%(tmp_alias)s';
-#
-#      select %(i)s as iteration_no, 'DB creation completed OK.' as msg from rdb$database;
-#
-#      alter database set linger to 0;
-#      create user SYSDBA password 'QweRty' using plugin Srp;
-#      commit;
-#
-#      connect 'localhost:%(tmp_alias)s' user sysdba password 'QweRty';
-#
-#      select %(i)s as iteration_no, 'Remote connection established OK.' as msg from rdb$database;
-#
-#      set term ^;
-#      execute block as
-#          declare v_dummy varchar(20);
-#      begin
-#          select left(a.mon$remote_protocol,3) as mon_protocol
-#          from mon$attachments a
-#          where a.mon$attachment_id = current_connection
-#          into v_dummy;
-#      end
-#      ^
-#      set term ;^
-#      quit;
-#  '''
-#
-#  for i in range(0,3):
-#      runProgram('isql',[ '-q' ], sql_init % locals() )
-#      runProgram('gfix',[ '-shut', 'full', '-force', '0', 'localhost:' + fdb_test, '-user', 'SYSDBA', '-pas', 'QweRty' ])
-#      cleanup( (fdb_test,) )
-#
-#  shutil.move( dbcbak, dbconf )
-#
-#---
+    with open(act.home_dir/'databases.conf', 'r') as f:
+        for line in f:
+            if p_required_alias_ptn.search(line):
+                # If databases.conf contains line like this:
+                #     tmp_6147_alias = $(dir_sampleDb)/qa/tmp_core_6147.fdb
+                # - then we extract filename: 'tmp_core_6147.fdb' (see below):
+                fname_in_dbconf = Path(line.split('=')[1].strip()).name
+                break
+
+    # if 'fname_in_dbconf' remains undefined here then propably REQUIRED_ALIAS not equals to specified in the databases.conf!
+    #
+    assert fname_in_dbconf
+
+    # Full path + filename of database to which we will try to connect:
+    #
+    tmp_fdb = Path( act.vars['sample_dir'], 'qa', fname_in_dbconf )
+
+    tmp_dba_pswd = 'QweRty6412'
+    for i in range(0,ITER_NUM):
+        init_sql = f'''
+           set bail on;
+           set list on;
+
+           create database '{REQUIRED_ALIAS}' user {act.db.user};
+
+           select {i} as iteration_no, 'DB creation completed OK.' as msg from rdb$database;
+           alter database set linger to 0;
+           create user {act.db.user} password '{tmp_dba_pswd}' using plugin Srp;
+           commit;
+
+           connect 'localhost:{REQUIRED_ALIAS}' user {act.db.user} password '{tmp_dba_pswd}';
+
+           select {i} as iteration_no, 'Remote connection established OK.' as msg from rdb$database;
+
+           set term ^;
+           execute block as
+               declare v_dummy varchar(20);
+           begin
+               select left(a.mon$remote_protocol,3) as mon_protocol
+               from mon$attachments a
+               where a.mon$attachment_id = current_connection
+               into v_dummy;
+           end
+           ^
+           set term ;^
+           quit;
+        '''
+
+        try:
+            act.expected_stdout = f"""
+                ITERATION_NO                    {i}
+                MSG                             DB creation completed OK.
+                ITERATION_NO                    {i}
+                MSG                             Remote connection established OK.
+            """
+            act.isql(switches = ['-q'], input = init_sql, connect_db=False, credentials = False, combine_output = True, io_enc = locale.getpreferredencoding())
+            assert act.clean_stdout == act.clean_expected_stdout
+            act.reset()
+
+            act.stdout = capsys.readouterr().out
+            assert act.clean_stdout == act.clean_expected_stdout
+            act.reset()
+
+            # Change DB state to full shutdown in order to have ability to drop database file.
+            # This is needed because when DB is self-security then it will be kept opened for 10s
+            # (as it always occurs for common security.db). Set linger to 0 does not help.
+            act.gfix(switches=['-shut', 'full', '-force', '0', f'localhost:{REQUIRED_ALIAS}', '-user', act.db.user, '-pas', tmp_dba_pswd], io_enc = locale.getpreferredencoding(), credentials = False, combine_output = True)
+            act.stdout = capsys.readouterr().out
+            assert act.clean_stdout == act.clean_expected_stdout
+            act.reset()
+        finally:
+            tmp_fdb.unlink()
