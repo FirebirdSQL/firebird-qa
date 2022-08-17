5479e498d4c581ebf2e69dfd3cb9a8c9a7e165b4
zotov <pavel-zotov>
2022-08-17 15:05:26 +0300

    Added/Updated bugs\core_6412_test.py. Adapted for work with pre-created [QA_ROOT]/files/qa-databases.conf. Discussed with pcisar in may-jun 2022. See details in the source.

--- tests/bugs/core_6412_test.py
+++ tests/bugs/core_6412_test.py
@@ -2,29 +2,57 @@
 
 """
 ID:          issue-6650
-ISSUE:       6650
+ISSUE:       https://github.com/FirebirdSQL/firebird/issues/6650
 TITLE:       Firebird is freezing when trying to manage users via triggers
 DESCRIPTION:
-  Confirmed hang on 4.0.0.2214
+    Confirmed hang on 4.0.0.2214
 
-  Checked on 4.0.0.2249 - no hang, but if this test runs in loop, w/o pauses for at
-  least ~4s, then starting from 2nd run following fail raises:
-   Statement failed, SQLSTATE = 08006
-   Error occurred during login, please check server firebird.log for details
-   Error occurred during login, please check server firebird.log for details
-  This was because of: http://tracker.firebirdsql.org/browse/CORE-6441 (fixed).
-
-  Content of firebird.log will be added with following lines:
-   Srp Server
-   connection shutdown
-   Database is shutdown.
-  Sent report to Alex et al, 09.11.2020.
+    Checked on 4.0.0.2249 - no hang, but if this test runs in loop, w/o pauses for at
+    least ~4s, then starting from 2nd run following fail raises:
+        Statement failed, SQLSTATE = 08006
+        Error occurred during login, please check server firebird.log for details
+        Error occurred during login, please check server firebird.log for details
+    This was because of: http://tracker.firebirdsql.org/browse/CORE-6441 (fixed).
+    Content of firebird.log will be added with following lines:
+        Srp Server
+        connection shutdown
+        Database is shutdown.
+    Sent report to Alex et al, 09.11.2020.
 JIRA:        CORE-6412
 FBTEST:      bugs.core_6412
+NOTES:
+    [17.08.2022] pzotov
+    1. One need to be sure that firebird.conf does NOT contain DatabaseAccess = None.
+    2. Test uses pre-created databases.conf which has alias (see variable REQUIRED_ALIAS)
+       and SecurityDatabase in its details which points to that alias, thus making such
+       database be self-security. Database file for that alias must NOT exist in the
+       QA_root/files/qa/ subdirectory: it will be created here.
+       Content of databases.conf must be taken from $QA_ROOT/files/qa-databases.conf (one need to replace
+       it before every test session).
+       Discussed with pcisar, letters since 30-may-2022 13:48, subject:
+       "new qa, core_4964_test.py: strange outcome when use... shutil.copy() // comparing to shutil.copy2()"
+    3. Value of REQUIRED_ALIAS must be EXACTLY the same as alias specified in the pre-created databases.conf
+       (for LINUX this equality is case-sensitive, even when aliases are compared!)
+
+    Confirmed again problem: 4.0.0.2214 hangs.
+    Checked on 5.0.0.623, 4.0.1.2692 - both on Windows and Linux.
 """
+import os
+import re
+import locale
+import subprocess
+from pathlib import Path
+import time
 
 import pytest
 from firebird.qa import *
+
+# Name of alias for self-security DB in the QA_root/files/qa-databases.conf.
+# This file must be copied manually to each testing FB homw folder, with replacing
+# databases.conf there:
+#
+REQUIRED_ALIAS = 'tmp_core_6412_alias'
+
 
 db = db_factory()
 
@@ -36,166 +64,112 @@ expected_stdout = """
     RESULT                          Completed
 """
 
-@pytest.mark.skip('FIXME: databases.conf')
 @pytest.mark.version('>=4.0')
-def test_1(act: Action):
-    pytest.fail("Not IMPLEMENTED")
+def test_1(act: Action, capsys):
+    # Scan line-by-line through databases.conf, find line starting with REQUIRED_ALIAS and extract name of file that
+    # must be created in the $(dir_sampleDb)/qa/ folder. This name will be used further as target database (tmp_fdb).
+    # NOTE: we have to SKIP lines which are commented out, i.e. if they starts with '#':
+    p_required_alias_ptn =  re.compile( '^(?!#)((^|\\s+)' + REQUIRED_ALIAS + ')\\s*=\\s*\\$\\(dir_sampleDb\\)/qa/', re.IGNORECASE )
+    fname_in_dbconf = None
 
-# test_script_1
-#---
-#
-#  import os
-#  import sys
-#  import time
-#  import subprocess
-#  import shutil
-#  import uuid
-#  from fdb import services
-#
-#  svc = services.connect(host='localhost', user = user_name, password = user_password)
-#  fb_home=svc.get_home_directory()
-#  svc.close()
-#
-#  dbconf = os.path.join(fb_home,'databases.conf')
-#  dbcbak = os.path.join(fb_home,'databases.bak')
-#
-#  try:
-#      del os.environ["ISC_USER"]
-#  except KeyError as e:
-#      pass
-#
-#  #--------------------------------------------
-#
-#  def svc_get_fb_log( fb_home, f_fb_log ):
-#
-#    global subprocess
-#    subprocess.call( [ fb_home + "fbsvcmgr",
-#                       "localhost:service_mgr",
-#                       "action_get_fb_log"
-#                     ],
-#                     stdout=f_fb_log, stderr=subprocess.STDOUT
-#                   )
-#    return
-#
-#  #--------------------------------------------
-#
-#  def flush_and_close(file_handle):
-#      # https://docs.python.org/2/library/os.html#os.fsync
-#      # If you're starting with a Python file object f,
-#      # first do f.flush(), and
-#      # then do os.fsync(f.fileno()), to ensure that all internal buffers associated with f are written to disk.
-#      global os
-#
-#      file_handle.flush()
-#      if file_handle.mode not in ('r', 'rb'):
-#          # otherwise: "OSError: [Errno 9] Bad file descriptor"!
-#          os.fsync(file_handle.fileno())
-#      file_handle.close()
-#
-#  #--------------------------------------------
-#
-#  def cleanup( f_names_list ):
-#      global os
-#      for i in range(len( f_names_list )):
-#         if os.path.isfile( f_names_list[i]):
-#              os.remove( f_names_list[i] )
-#              if os.path.isfile( f_names_list[i]):
-#                  print('ERROR: can not remove file ' + f_names_list[i])
-#
-#  #--------------------------------------------
-#
-#  fdb_test = os.path.join(context['temp_directory'],'tmp_6412.fdb')
-#
-#
-#  # NB: fb_home is full path to FB instance home (with trailing slash).
-#  shutil.copy2( dbconf, dbcbak )
-#
-#  tmp_alias = 'self_security_6412'
-#
-#  alias_data='''
-#
-#  # Added temporarily for executing test core_6412.fbt
-#  %(tmp_alias)s = %(fdb_test)s {
-#      # RemoteAccess = true
-#      SecurityDatabase = %(tmp_alias)s
-#  }
-#  ''' % locals()
-#
-#  f_dbconf=open(fb_home+'databases.conf','a')
-#  f_dbconf.seek(0, 2)
-#  f_dbconf.write( alias_data )
-#  flush_and_close( f_dbconf )
-#
-#  cleanup( (fdb_test,) )
-#
-#  sql_init='''
-#      set bail on;
-#      create database '%(tmp_alias)s';
-#      alter database set linger to 0;
-#      create user SYSDBA password 'QweRty' using plugin Srp;
-#      commit;
-#
-#      connect 'localhost:%(tmp_alias)s' user sysdba password 'QweRty';
-#
-#      set list on;
-#      select a.mon$remote_protocol,
-#             d.mon$sec_database
-#      from mon$attachments a cross join mon$database d
-#      where a.mon$attachment_id = current_connection;
-#
-#      CREATE TABLE USERS
-#      (
-#        AUTHENTICATION VARCHAR(32) CHARACTER SET WIN1252 COLLATE WIN_PTBR
-#      );
-#      COMMIT;
-#
-#      SET TERM ^ ;
-#      CREATE TRIGGER USERS_AI_AU_AD FOR USERS
-#      ACTIVE AFTER INSERT OR UPDATE OR DELETE POSITION 0
-#      AS
-#      BEGIN
-#      	IF ((OLD.AUTHENTICATION IS NOT NULL) AND ((NEW.AUTHENTICATION IS NULL) OR (OLD.AUTHENTICATION<>NEW.AUTHENTICATION))) THEN
-#      	BEGIN
-#             EXECUTE STATEMENT 'REVOKE RDB$ADMIN FROM "' || OLD.AUTHENTICATION || '" GRANTED BY "SYSDBA"';
-#             EXECUTE STATEMENT 'DROP USER "' || OLD.AUTHENTICATION || '" USING PLUGIN SRP';
-#          END
-#
-#      	IF ((NEW.AUTHENTICATION IS NOT NULL) AND ((OLD.AUTHENTICATION IS NULL) OR (OLD.AUTHENTICATION<>NEW.AUTHENTICATION))) THEN
-#      	BEGIN
-#      		   EXECUTE STATEMENT 'GRANT RDB$ADMIN TO "' || NEW.AUTHENTICATION || '" GRANTED BY "SYSDBA"';
-#      		   EXECUTE STATEMENT 'CREATE OR ALTER USER "' || NEW.AUTHENTICATION || '" SET PASSWORD ''123456'' USING PLUGIN SRP GRANT ADMIN ROLE';
-#      	END
-#      END^
-#      SET TERM ; ^
-#      COMMIT;
-#
-#      INSERT INTO USERS (AUTHENTICATION) VALUES ('AAA');
-#      COMMIT;
-#
-#
-#      UPDATE USERS SET AUTHENTICATION='BBB' WHERE AUTHENTICATION='AAA';
-#      COMMIT;
-#
-#      set list on;
-#      select 'Completed' as result from rdb$database;
-#      quit;
-#
-#  ''' % locals()
-#
-#  runProgram( 'isql',[ '-q' ], sql_init)
-#  runProgram( 'gfix',[ '-shut', 'full', '-force', '0', 'localhost:' + fdb_test, '-user', 'SYSDBA', '-pas', 'QweRty' ])
-#
-#  shutil.move( dbcbak, dbconf )
-#
-#  cleanup( (fdb_test,) )
-#
-#  # This delay is necessary for running this test multiple times (in loop) on SS and SC.
-#  # Otherwise we get runtime error "SQLSTATE = 08006 / Error occurred during login, please check server firebird.log"
-#  # and firebird.log will be filled with: "Srp Server / connection shutdown / Database is shutdown."
-#  # Delay must be not less than 4 seconds. Tested on 4.0.0.2249.
-#  #############
-#  # 03-mar-2021: can be deleted (related to CORE-6441) >>> time.sleep(4)
-#  #############
-#
-#
-#---
+    with open(act.home_dir/'databases.conf', 'r') as f:
+        for line in f:
+            if p_required_alias_ptn.search(line):
+                # If databases.conf contains line like this:
+                #     tmp_6147_alias = $(dir_sampleDb)/qa/tmp_core_6147.fdb
+                # - then we extract filename: 'tmp_core_6147.fdb' (see below):
+                fname_in_dbconf = Path(line.split('=')[1].strip()).name
+                break
+
+    # if 'fname_in_dbconf' remains undefined here then propably REQUIRED_ALIAS not equals to specified in the databases.conf!
+    #
+    assert fname_in_dbconf
+
+    # Full path + filename of database to which we will try to connect:
+    #
+    tmp_fdb = Path( act.vars['sample_dir'], 'qa', fname_in_dbconf )
+
+    tmp_dba_pswd = 'QweRty6412'
+
+    init_sql = f'''
+       set bail on;
+       set list on;
+       create database '{REQUIRED_ALIAS}' user {act.db.user};
+       create user {act.db.user} password '{tmp_dba_pswd}' using plugin Srp;
+       alter database set linger to 0;
+       commit;
+
+       connect 'localhost:{REQUIRED_ALIAS}' user {act.db.user} password '{tmp_dba_pswd}';
+
+       set list on;
+       select a.mon$remote_protocol,
+              d.mon$sec_database
+       from mon$attachments a cross join mon$database d
+       where a.mon$attachment_id = current_connection;
+
+       create table t_users
+       (
+         authentication varchar(32) character set win1252 collate win_ptbr
+       );
+       commit;
+
+       set term ^ ;
+       create trigger t_users_ai_au_ad for t_users
+       active after insert or update or delete position 0
+       as
+       begin
+        if ((old.authentication is not null) and ((new.authentication is null) or (old.authentication<>new.authentication))) then
+        begin
+              execute statement 'revoke rdb$admin from ' || old.authentication || ' granted by {act.db.user}';
+              execute statement 'drop user ' || old.authentication || ' using plugin srp';
+           end
+
+        if ((new.authentication is not null) and ((old.authentication is null) or (old.authentication<>new.authentication))) then
+        begin
+               execute statement 'grant rdb$admin to ' || new.authentication || ' granted by {act.db.user}';
+               execute statement 'create or alter user ' || new.authentication || ' set password ''123456'' using plugin srp grant admin role';
+        end
+       end^
+       set term ; ^
+       commit;
+
+       insert into t_users (authentication) values ('aaa');
+       commit;
+
+       update t_users set authentication='bbb' where upper(authentication) = upper('aaa');
+       commit;
+
+       set list on;
+       select sec$user_name,sec$admin,sec$plugin from sec$users; -- [WinError 22] here for recent FB 4.x and 5.x!
+       quit;
+    '''
+
+    try:
+        act.expected_stdout = f"""
+            MON$REMOTE_PROTOCOL             TCP
+            MON$SEC_DATABASE                Self
+
+            SEC$USER_NAME                   SYSDBA
+            SEC$ADMIN                       <true>
+            SEC$PLUGIN                      Srp
+            SEC$USER_NAME                   BBB
+            SEC$ADMIN                       <true>
+            SEC$PLUGIN                      Srp
+        """
+        act.isql(switches = ['-q'], input = init_sql, connect_db=False, credentials = False, combine_output = True, io_enc = locale.getpreferredencoding())
+        assert act.clean_stdout == act.clean_expected_stdout
+        act.reset()
+
+        act.stdout = capsys.readouterr().out
+        assert act.clean_stdout == act.clean_expected_stdout
+        act.reset()
+
+        # Change DB state to full shutdown in order to have ability to drop database file.
+        # This is needed because when DB is self-security then it will be kept opened for 10s
+        # (as it always occurs for common security.db). Set linger to 0 does not help.
+        act.gfix(switches=['-shut', 'full', '-force', '0', f'localhost:{REQUIRED_ALIAS}', '-user', act.db.user, '-pas', tmp_dba_pswd], io_enc = locale.getpreferredencoding(), credentials = False, combine_output = True)
+        act.stdout = capsys.readouterr().out
+        assert act.clean_stdout == act.clean_expected_stdout
+        act.reset()
+    finally:
+        tmp_fdb.unlink()
