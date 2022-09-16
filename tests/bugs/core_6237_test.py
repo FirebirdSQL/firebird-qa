20fee7b1b8656688a9a1f6c518279985cec99780
zotov <pavel-zotov>
2022-09-16 09:52:36 +0300

    1) adjusted MIN_RATIO_THRESHOLD. 2) median ratios must be ignored if both medians have proximity to zero, usually this is ~20...30 ms.    This is tuned by setting MAX_MEDIAN_TO_IGNORE. 3) Added notes.

--- tests/bugs/core_6237_test.py
+++ tests/bugs/core_6237_test.py
@@ -11,23 +11,25 @@ DESCRIPTION:
   Slow time of attach establishing can NOT be seen otherwise; rather almost no difference will be in that case.
   :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-  Test creates two users: one usingLegacy plugin and second using Srp.
+  Test creates two users: one using Legacy plugin and second using Srp.
   Then we make ~20...30 pairs of attach/detach by each of these users and get total time difference for these actions.
   Ratio between these total differences must be limited with threshold. Its value was determined after dozen of runs
-  and it seems to be reasonable assign to it value 1.25 (see MIN_RATIO_THRESHOLD in the code).
+  and it seems to be reasonable assign to it value 1.5 (see MIN_RATIO_THRESHOLD in the code).
 
   Test output will contain ALERT if total time of <attaches_using_Srp> vs <attaches_using_Legacy>
   will be greater than MIN_RATIO_THRESHOLD.
+  NB: median ratios must be ignored if both medians have proximity to zero, usually this is ~20...30 ms.
+  This is tuned by setting MAX_MEDIAN_TO_IGNORE.
 
-  Reproduced on on several builds 4.x before 17.01.2020 (tested: 4.0.0.1712 CS, 4.0.0.1731 CS - got ratio = ~1.95).
+  Reproduced on several builds 4.x before 17.01.2020 (tested: 4.0.0.1712 CS, 4.0.0.1731 CS - got ratio = ~1.95).
   Reproduced also on 3.0.5.33221 Classic - got ratio ~1.50 ... 1.70; could NOT reproduce on 3.0.5 SuperClassic / SuperServer.
-[09.02.2022] pcisar
-  Fails on Windows 4.0.1 with ratio 1.98 - raw iron W10, does not fail with Linux on the same HW
 JIRA:        CORE-6237
 FBTEST:      bugs.core_6237
 NOTES:
-    [30.07.2022] pzotov
-    Checked on 5.0.0.591, 4.0.1.2692, 3.0.8.33535 (windows only; pisix will be checked later).
+    [09.02.2022] pcisar
+        Fails on Windows 4.0.1 with ratio 1.98 - raw iron W10, does not fail with Linux on the same HW
+    [16.09.2022] pzotov
+        Checked on Windows and Linux, 4.0.1.2692, 3.0.8.33535.
 """
 
 import pytest
@@ -42,6 +44,19 @@ srp_user = user_factory('db', name='tmp_c6237_srp', password='123', plugin='Srp'
 
 act = python_act('db')
 
+###########################
+###   S E T T I N G S   ###
+###########################
+# Number of measurements:
+N_COUNT = 51
+
+# Minimal ratio between medians of time for alerting:
+MIN_RATIO_THRESHOLD = 1.50
+
+# Maximal value for both medians of time (Srp and Legacy) when
+# ratio between them can be ignored because of proximity to zero:
+MAX_MEDIAN_TO_IGNORE = 50
+
 #------------------
 def median(lst):
     n = len(lst)
@@ -49,17 +64,10 @@ def median(lst):
     return (sum(s[n//2-1:n//2+1])/2.0, s[n//2])[n % 2] if n else None
 #------------------
 
-
-expected_stdout = """
-    EXPECTED. Ratio of total elapsed time when use Srp vs Legacy is less then threshold.
-"""
-
 @pytest.mark.version('>=3.0.5')
 def test_1(act: Action, leg_user: User, srp_user: User, capsys):
-    N_COUNT = 100
-    MIN_RATIO_THRESHOLD = 1.41
+    
     sp_time = {}
-
 
     for user in [leg_user, srp_user]:
         for i in range(N_COUNT):
@@ -74,15 +82,23 @@ def test_1(act: Action, leg_user: User, srp_user: User, capsys):
     leg_user_conn_time = [round(v,2) for k,v in sp_time.items() if k[0] == leg_user.name]
     srp_user_conn_time = [round(v,2) for k,v in sp_time.items() if k[0] == srp_user.name]
 
-
     conn_time_ratio = median(srp_user_conn_time) / median(leg_user_conn_time)
-    if conn_time_ratio < MIN_RATIO_THRESHOLD:
-        print('EXPECTED. Ratio of total elapsed time when use Srp vs Legacy is less then threshold.')
+
+    expected_txt = 'EXPECTED. Ratio median(Srp) / madian(Legacy) is less then threshold or medians are about zero.'
+    if conn_time_ratio < MIN_RATIO_THRESHOLD or max( median(srp_user_conn_time), median(leg_user_conn_time) ) < MAX_MEDIAN_TO_IGNORE:
+        print(expected_txt)
     else:
         print(f'Ratio Srp/Legacy: {conn_time_ratio} - is GREATER than threshold = {MIN_RATIO_THRESHOLD}.')
         print(f'    Median for Srp: {median(srp_user_conn_time)}')
         print(f'    Median for Legacy: {median(leg_user_conn_time)}')
 
-    act.expected_stdout = expected_stdout
+        print('Check srp_user_conn_time:')
+        print(srp_user_conn_time)
+
+        print('\\nCheck leg_user_conn_time:')
+        print(leg_user_conn_time)
+        
+
+    act.expected_stdout = expected_txt
     act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
