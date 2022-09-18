01c28b9a3b007bc8029aa82b37154f10ef15dcdc
zotov <pavel-zotov>
2022-09-18 14:37:28 +0300

    Added/Updated bugs\core_4841_test.py. Checked on 3.0.8.33535 (SS/CS), 4.0.1.2692 (SS/CS), 5.0.0.730 (SS/CS).

--- tests/bugs/core_4841_test.py
+++ tests/bugs/core_4841_test.py
@@ -3,16 +3,19 @@
 """
 ID:          issue-5137
 ISSUE:       5137
-TITLE:       Make message about missing password being always displayed as reply on attempt
-  to issue CREATE new login without PASSWORD clause
+TITLE:       Make message about missing password being always displayed as reply on attempt to issue CREATE new login without PASSWORD clause
 DESCRIPTION:
-NOTES:
-[04.02.2022] pcisar
-  Test fails with 3.0.8, because command
-    create or alter user u01 tags (password = 'foo');
-  does not produce any error.
 JIRA:        CORE-4841
 FBTEST:      bugs.core_4841
+NOTES:
+  [04.02.2022] pcisar
+    Test fails with 3.0.8, because command
+      create or alter user u01 tags (password = 'foo');
+    does not produce any error.
+
+  [18.09.2022] pzotov
+  Could NOT find any problems on both Windows and Linux. Test works FINE.
+  Checked on 3.0.8.33535 (SS/CS), 4.0.1.2692 (SS/CS), 5.0.0.730 (SS/CS).
 """
 
 import pytest
@@ -69,7 +72,6 @@ unsuccessful metadata update
 -Password must be specified when creating user
 """
 
-@pytest.mark.skip("FIXME: see notes")
 @pytest.mark.version('>=3.0.8')
 def test_1(act: Action):
     act.expected_stderr = expected_stderr
