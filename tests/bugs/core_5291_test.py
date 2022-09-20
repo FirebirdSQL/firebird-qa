d3758011849a6c3f2211034cfbcd938648930f35
zotov <pavel-zotov>
2022-09-20 11:15:10 +0300

    Added/Updated bugs\core_5291_test.py. adjusted code. Fixed 'substitutions' because of non-default port that is displayed in error message after 'localhost

--- tests/bugs/core_5291_test.py
+++ tests/bugs/core_5291_test.py
@@ -3,11 +3,15 @@
 """
 ID:          issue-5569
 ISSUE:       5569
-TITLE:       Error messages differ when regular user tries to RESTORE database, depending
-  on his default role and (perhaps) system privilege USE_GBAK_UTILITY
+TITLE:       Error messages differ when regular user tries to RESTORE database, depending on his default role and (perhaps) system privilege USE_GBAK_UTILITY
 DESCRIPTION:
 JIRA:        CORE-5291
 FBTEST:      bugs.core_5291
+NOTES:
+    [20.09.2022] pzotov
+    One need to take in account that gbak produces error message with specifying PORT number after localhost,
+    i.e.: "failed to create database localhost/NNNNN:<dbname>" (perhaps, only for non-default ports?).
+    Checked on 5.0.0.591, 4.0.1.2692.
 """
 
 import pytest
@@ -18,30 +22,14 @@ substitutions = [('gbak: ERROR:no permission for CREATE access to DATABASE.*',
                   'gbak: ERROR:no permission for CREATE access to DATABASE'),
                  ('gbak: ERROR:    failed to create database.*',
                   'gbak: ERROR:    failed to create database'),
-                 ('gbak: ERROR:failed to create database localhost:.*',
+                 ('gbak: ERROR:failed to create database localhost(:|/).*',
                   'gbak: ERROR:failed to create database localhost')]
 
-init_script = """
-    -- This is not needed anymore for new test implementation
-    --
-    -- set wng off;
-    -- create or alter user tmp$c5291_1 password '123' revoke admin role;
-    -- create or alter user tmp$c5291_2 password '456' revoke admin role;
-    -- commit;
-    -- revoke all on all from tmp$c5291_1;
-    -- revoke all on all from tmp$c5291_2;
-    -- commit;
-    -- create role role_for_use_gbak_utility set system privileges to USE_GBAK_UTILITY, SELECT_ANY_OBJECT_IN_DATABASE;
-    -- commit;
-    -- grant default role_for_use_gbak_utility to user tmp$c5291_2;
-    -- commit;
-"""
+db = db_factory()
 
-db = db_factory(init=init_script)
-
-user_1 = user_factory('db', name='tmp$c5291_1', password='123')
-user_2 = user_factory('db', name='tmp$c5291_2', password='456')
-test_role = role_factory('db', name='role_for_use_gbak_utility')
+tmp_user_1 = user_factory('db', name='tmp$c5291_1', password='123')
+tmp_user_2 = user_factory('db', name='tmp$c5291_2', password='456')
+tmp_role = role_factory('db', name='role_for_use_gbak_utility')
 
 act = python_act('db', substitutions=substitutions)
 
@@ -70,40 +58,37 @@ expected_stderr_1 = """
 """
 
 @pytest.mark.version('>=4.0')
-def test_1(act: Action, user_1: User, user_2: User, fbk_file: Path, fdb_file_1: Path,
-           fdb_file_2: Path, test_role: Role, capsys):
+def test_1(act: Action, tmp_user_1: User, tmp_user_2: User, fbk_file: Path, fdb_file_1: Path, fdb_file_2: Path, tmp_role: Role, capsys):
     with act.db.connect() as con:
-        con.execute_immediate('alter role role_for_use_gbak_utility set system privileges to USE_GBAK_UTILITY, SELECT_ANY_OBJECT_IN_DATABASE')
-        con.commit()
-        con.execute_immediate('grant default role_for_use_gbak_utility to user tmp$c5291_2')
-        con.commit()
+        con.execute_immediate( f'alter role {tmp_role.name} set system privileges to USE_GBAK_UTILITY, SELECT_ANY_OBJECT_IN_DATABASE' )
+        con.execute_immediate( f'grant default {tmp_role.name} to user {tmp_user_2.name}' )
     #
     act.gbak(switches=['-b', act.db.dsn, str(fbk_file)])
     # User 1
     act.reset()
     act.expected_stderr = "We expect errors"
     act.gbak(switches=['-se', 'localhost:service_mgr', '-rep', str(fbk_file),
-                       str(fdb_file_1), '-user', user_1.name, '-pas', user_1.password],
+                       str(fdb_file_1), '-user', tmp_user_1.name, '-pas', tmp_user_1.password],
              credentials=False)
     print(act.stderr)
     # User 1
     act.reset()
     act.expected_stderr = "We expect errors"
     act.gbak(switches=['-rep', str(fbk_file), act.get_dsn(fdb_file_2),
-                       '-user', user_1.name, '-pas', user_1.password], credentials=False)
+                       '-user', tmp_user_1.name, '-pas', tmp_user_1.password], credentials=False)
     print(act.stderr)
     # User 2
     act.reset()
     act.expected_stderr = "We expect errors"
     act.gbak(switches=['-se', f'{act.host}:service_mgr', '-rep', str(fbk_file),
-                       str(fdb_file_1), '-user', user_2.name, '-pas', user_2.password],
+                       str(fdb_file_1), '-user', tmp_user_2.name, '-pas', tmp_user_2.password],
              credentials=False)
     print(act.stderr)
     # User 2
     act.reset()
     act.expected_stderr = "We expect errors"
     act.gbak(switches=['-rep', str(fbk_file), act.get_dsn(fdb_file_2),
-                       '-user', user_2.name, '-pas', user_2.password], credentials=False)
+                       '-user', tmp_user_2.name, '-pas', tmp_user_2.password], credentials=False)
     print(act.stderr)
     #
     act.reset()
