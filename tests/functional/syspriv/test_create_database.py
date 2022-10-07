9f12466262ee656629c9af23db01d2331d445794
zotov <pavel-zotov>
2022-10-07 20:48:09 +0300

    Added/Updated D:\FB\firebird-qa\tests\functional\syspriv\test_create_database.py: adjusted expected data for Linux

--- tests/functional/syspriv/test_create_database.py
+++ tests/functional/syspriv/test_create_database.py
@@ -22,7 +22,7 @@ import pytest
 from firebird.qa import *
 from firebird.driver.types import DatabaseError
 
-substitutions = [('[ \\t]+', ' '), ('MON\\$DATABASE_NAME.*TMP4TEST.TMP', 'MON$DATABASE_NAME TMP4TEST.TMP')]
+substitutions = [('[ \\t]+', ' '), ('DB_NAME.*TMP4TEST.TMP', 'DB_NAME TMP4TEST.TMP'), ('TCPv(4|6)', 'TCP')]
 db_main = db_factory()
 tmp_user = user_factory('db_main', name='tmp_syspriv_user', password='123')
 tmp_role = role_factory('db_main', name='tmp_role_for_create_database')
@@ -32,8 +32,11 @@ db_temp = db_factory(filename = 'tmp4test.tmp', do_not_create=True, do_not_drop=
 
 
 expected_stdout_isql = """
-    MON$OWNER         TMP_SYSPRIV_USER
-    MON$DATABASE_NAME TMP4TEST.TMP
+    DB_OWNER TMP_SYSPRIV_USER
+    DB_NAME  TMP4TEST.TMP
+    ATT_USER TMP_SYSPRIV_USER
+    ATT_PROT TCP
+    ATT_AUTH Srp
 """
 
 @pytest.mark.version('>=4.0')
@@ -42,6 +45,7 @@ def test_1(act: Action, tmp_user: User, tmp_role:Role, db_temp: Database, capsys
     f"""
         set wng off;
         set bail on;
+        set list on;
         connect '{act.db.dsn}' user '{act.db.user}' password '{act.db.password}';
         alter user {tmp_user.name} grant admin role; -- this must be specified!
         commit;
@@ -49,9 +53,27 @@ def test_1(act: Action, tmp_user: User, tmp_role:Role, db_temp: Database, capsys
         commit;
         grant default {tmp_role.name} to user {tmp_user.name};
         commit;
+
+        /*
+        connect 'localhost:security.db' user '{act.db.user}' password '{act.db.password}';
+        select 
+             upper(d.mon$database_name) as security_db_name
+            ,r.rdb$role_name
+            ,rdb$role_in_use(r.rdb$role_name) as RDB_ROLE_IN_USE
+            ,r.rdb$system_privileges
+        from mon$database d cross join rdb$roles r;
+        commit;
+        */
+
         create database '{db_temp.dsn}' user {tmp_user.name} password '{tmp_user.password}';
-        set list on;
-        select mon$owner, mon$database_name from mon$database;
+        select d.mon$owner as db_owner
+            ,upper(d.mon$database_name) as db_name
+            ,a.mon$user as att_user
+            ,a.mon$remote_protocol as att_prot
+            ,a.mon$auth_method as att_auth
+        from mon$database d
+        cross join mon$attachments a
+        where a.mon$attachment_id = current_connection;
         commit;
         drop database;
     """
