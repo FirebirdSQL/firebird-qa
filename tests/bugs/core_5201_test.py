cfcadbb989a1dbea54bec06a4afff9d6bfafab89
zotov <pavel-zotov>
2023-02-07 14:08:48 +0300

    Added/Updated D:\FB\firebird-qa\tests\bugs\core_5201_test.py: adjusted tail of restore log - see notes.

--- tests/bugs/core_5201_test.py
+++ tests/bugs/core_5201_test.py
@@ -5,23 +5,30 @@ ID:          issue-5482
 ISSUE:       5482
 TITLE:       Return nonzero result code when restore fails on activating and creating deferred user index
 DESCRIPTION:
-  According to Alex response on letter 25-apr-2016 19:15, zero retcode returned ONLY when restore
-  was done WITH '-verbose' switch, and this was fixed. When restoring was done without additional
-  switches, retcode was NON zero and its value was 1.
+    According to Alex response on letter 25-apr-2016 19:15, zero retcode returned ONLY when restore
+    was done WITH '-verbose' switch, and this was fixed.
+    When restoring is performed without additional switches then retcode is 1.
 
-  We create table with UNIQUE computed-by index which expression refers to other table (Firebird allows this!).
-  Because other table (test_2) initially is empty, index _can_ be created. But after this we insert record into
-  this table and do commit. Since that moment backup of this database will have table test_1 but its index will
-  NOT be able to restore (unless '-i' switch specified).
-  We will use this inability of restore index by checking 'gbak -rep -v ...' return code: it should be NON zero.
- If code will skip exception then this will mean FAIL of test.
+    We create table with UNIQUE computed-by index which expression refers to other table (Firebird allows this!).
+    Because other table (test_2) initially is empty, index _can_ be created. But after this we insert record into
+    this table and do commit. Since that moment backup of this database will have table test_1 but its index will
+    NOT be able to restore (unless '-i' switch specified).
+    We will use this inability of restore index by checking 'gbak -rep -v ...' return code: it should be NON zero.
+    If code will skip exception then this will mean FAIL of test.
 JIRA:        CORE-5201
 FBTEST:      bugs.core_5201
+NOTES:
+    [07.02.2023] pzotov
+    Adjusted tail of restore log: added messages
+        gbak: ERROR:Database is not online due to failure to activate one or more indices.
+        gbak: ERROR:    Run gfix -online to bring database online without active indices.
+   (actual since 5.0.0.932; will be soon also for FB 3.x and 4.x - see letter from Alex, 07.02.2023 11:53).
 """
 
 import pytest
 from pathlib import Path
 from firebird.qa import *
+import re
 
 init_script = """
     create table test_1(x int);
@@ -40,9 +47,11 @@ db = db_factory(init=init_script)
 
 act = python_act('db')
 
-expected_stdout = """
+gbak_expected_stdout = """
     gbak: ERROR:attempt to store duplicate value (visible to active transactions) in unique index "TEST_1_UNQ"
     gbak: ERROR:    Problematic key value is (<expression> = 1)
+    gbak: ERROR:Database is not online due to failure to activate one or more indices.
+    gbak: ERROR:    Run gfix -online to bring database online without active indices.
 """
 
 fbk_file = temp_file('core_5201.fbk')
@@ -54,10 +63,11 @@ def test_1(act: Action, fbk_file: Path, tmp_db_file: Path):
         srv.database.backup(database=act.db.db_path, backup=fbk_file)
         assert srv.readlines() == []
     #
-    act.expected_stderr = 'We expect error'
-    act.expected_stdout = expected_stdout
-    act.gbak(switches=['-rep', '-v', str(fbk_file), str(tmp_db_file)])
+    act.expected_stdout = gbak_expected_stdout
+    act.gbak(switches=['-rep', '-v', str(fbk_file), str(tmp_db_file)], combine_output = True)
+    p_gbak_err = re.compile('^gbak:\s?ERROR:', re.IGNORECASE)
+
     # filter stdout
-    act.stdout = '\n'.join([line for line in act.stdout.splitlines() if ' ERROR:' in line])
+    act.stdout = '\n'.join([line for line in act.stdout.splitlines() if p_gbak_err.search(line)])
     assert act.return_code == 2
     assert act.clean_stdout == act.clean_expected_stdout
