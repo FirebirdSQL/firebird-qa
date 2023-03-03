f4fa586ddf5b6b92f78e0ce84a739ed26c4bfd35
pavel-zotov <p519446@yandex.ru>
2023-03-03 15:06:17 +0300

    Added/Updated tests\bugs\gh_7482_test.py: see notes in the test

--- tests/bugs/gh_7482_test.py
+++ tests/bugs/gh_7482_test.py
@@ -7,6 +7,13 @@ TITLE:       Result of blob_append(null, null) (literal '<null>') is not shown
 NOTES:
     [14.02.2023] pzotov
     Checked on 5.0.0.958, intermediate build of 24-feb-2023. All OK.
+
+    [03.03.2023] pzotov
+    Waiting for patch in FB 4.x that fixes problem with <null> literal (4.0.3.2904 still fails).
+    Added substitution for suppressing 'Nullable' flags in the SQLDA output: it is sufficient for this test
+    to check only datatypes of result.
+    Discussed with Vlad, letters 02-mar-2023 16:01 and 03-mar-2023 14:43.
+    Checked on 5.0.0.967.
 """
 
 import pytest
@@ -21,7 +28,7 @@ test_script = """
     select blob_append(null, null) as blob_result from rdb$database;
 """
 
-act = isql_act('db', test_script, substitutions = [('^((?!sqltype:|BLOB_RESULT).)*$', ''), ('[ \t]+', ' ')])
+act = isql_act('db', test_script, substitutions = [('^((?!sqltype:|BLOB_RESULT).)*$', ''), ('BLOB Nullable', 'BLOB'), ('[ \t]+', ' ')])
 
 expected_stdout = """
     01: sqltype: 520 BLOB Nullable scale: 0 subtype: 0 len: 8
