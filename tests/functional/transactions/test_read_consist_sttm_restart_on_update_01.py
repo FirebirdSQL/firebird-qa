19b19eaced680a4eb6b8a3e493eed05107bdb17e
pavel-zotov <p519446@yandex.ru>
2023-03-19 01:44:14 +0300

    Added/Updated tests\functional\transactions\test_read_consist_sttm_restart_on_update_01.py: TEMPORARY DISABLED. DISCUSSION WITH FB-TEAM REQUIRED

--- tests/functional/transactions/test_read_consist_sttm_restart_on_update_01.py
+++ tests/functional/transactions/test_read_consist_sttm_restart_on_update_01.py
@@ -208,6 +208,7 @@ expected_stdout = """
 """
 
 @pytest.mark.version('>=4.0')
+@pytest.mark.skip("TEMPORARY DISABLED. DISCUSSION WITH FB-TEAM REQUIRED!")
 def test_1(act: Action, fn_worker_sql: Path, fn_worker_log: Path, fn_worker_err: Path, capsys):
     sql_init = (act.files_dir / 'read-consist-sttm-restart-DDL.sql').read_text()
 
