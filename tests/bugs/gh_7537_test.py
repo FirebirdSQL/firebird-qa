0a94fe1442ca450845e8b5f0a523e80ebb8569eb
pavel-zotov <p519446@yandex.ru>
2023-04-05 09:15:51 +0300

    Added/Updated tests\bugs\gh_7537_test.py: added more checks, changed expected output after fix gh-7539

--- tests/bugs/gh_7537_test.py
+++ tests/bugs/gh_7537_test.py
@@ -16,33 +16,79 @@ db = db_factory()
 
 test_script = """
     set list on;
-    select rdb$set_context('NS', 'VARNAME', 2) from rdb$database;
+    -- 1. Trying to set non-existent, empty or non-textual namespace:
+    select rdb$set_context('NO_SUCH_NS', 'VARNAME', 2) from rdb$database;
     select rdb$set_context('', 'VARNAME', 2) from rdb$database;
-    select rdb$set_context('FOO', '', 2) from rdb$database;
+    select rdb$set_context(null, 'VARNAME', 2) from rdb$database;
+    select rdb$set_context(false, 'VARNAME', 2) from rdb$database;
+    select rdb$set_context(date '05.04.2023', 'VARNAME', 2) from rdb$database;
+    select rdb$set_context(0E-6176, 'VARNAME', 2) from rdb$database;
 
-    select rdb$get_context('NS', 'VARNAME') from rdb$database;
+    -- 2. Trying to get smth non-existent, empty or non-textual namespace:
+    select rdb$get_context('NO_SUCH_NS', 'VARNAME') from rdb$database;
     select rdb$get_context('', 'VARNAME') from rdb$database;
-    select rdb$get_context('FOO', '') from rdb$database;
+    select rdb$get_context(null, 'VARNAME') from rdb$database;
+    select rdb$get_context(false, 'VARNAME') from rdb$database;
+    select rdb$get_context(date '05.04.2023', 'VARNAME') from rdb$database;
+    select rdb$get_context(0E-6176, 'VARNAME') from rdb$database;
+    
+    -- 3. Trying to set smth in the READ-ONLY name space 'SYSTEM':
+    select rdb$set_context('SYSTEM', 'NO_SUCH_VAR', 2) from rdb$database;
+    select rdb$set_context('SYSTEM', '', 2) from rdb$database;
+
+    -- 4. Trying to get non-existent or empty variable from 'SYSTEM':
+    select rdb$get_context('SYSTEM', 'NO_SUCH_VAR') from rdb$database;
+    select rdb$get_context('SYSTEM', '') from rdb$database;
 """
 
 expected_stdout = """
     Statement failed, SQLSTATE = HY000
-    Invalid namespace name NS passed to RDB$SET_CONTEXT
+    Invalid namespace name 'NO_SUCH_NS' passed to RDB$SET_CONTEXT
 
     Statement failed, SQLSTATE = HY000
-    Invalid namespace name  passed to RDB$SET_CONTEXT
+    Invalid namespace name '' passed to RDB$SET_CONTEXT
 
     Statement failed, SQLSTATE = HY000
-    Invalid namespace name FOO passed to RDB$SET_CONTEXT
+    Invalid argument passed to RDB$SET_CONTEXT
 
     Statement failed, SQLSTATE = HY000
-    Invalid namespace name NS passed to RDB$GET_CONTEXT
+    Invalid namespace name 'FALSE' passed to RDB$SET_CONTEXT
 
     Statement failed, SQLSTATE = HY000
-    Invalid namespace name  passed to RDB$GET_CONTEXT
+    Invalid namespace name '2023-04-05' passed to RDB$SET_CONTEXT
 
     Statement failed, SQLSTATE = HY000
-    Invalid namespace name FOO passed to RDB$GET_CONTEXT
+    Invalid namespace name '0E-6176' passed to RDB$SET_CONTEXT
+
+    Statement failed, SQLSTATE = HY000
+    Invalid namespace name 'NO_SUCH_NS' passed to RDB$GET_CONTEXT
+
+    Statement failed, SQLSTATE = HY000
+    Invalid namespace name '' passed to RDB$GET_CONTEXT
+
+    Statement failed, SQLSTATE = HY000
+    Invalid argument passed to RDB$GET_CONTEXT
+
+    Statement failed, SQLSTATE = HY000
+    Invalid namespace name 'FALSE' passed to RDB$GET_CONTEXT
+
+    Statement failed, SQLSTATE = HY000
+    Invalid namespace name '2023-04-05' passed to RDB$GET_CONTEXT
+
+    Statement failed, SQLSTATE = HY000
+    Invalid namespace name '0E-6176' passed to RDB$GET_CONTEXT
+
+    Statement failed, SQLSTATE = HY000
+    Invalid namespace name 'SYSTEM' passed to RDB$SET_CONTEXT
+
+    Statement failed, SQLSTATE = HY000
+    Invalid namespace name 'SYSTEM' passed to RDB$SET_CONTEXT
+
+    Statement failed, SQLSTATE = HY000
+    Context variable 'NO_SUCH_VAR' is not found in namespace 'SYSTEM'
+
+    Statement failed, SQLSTATE = HY000
+    Context variable '' is not found in namespace 'SYSTEM'
 """
 
 act = isql_act('db', test_script)
