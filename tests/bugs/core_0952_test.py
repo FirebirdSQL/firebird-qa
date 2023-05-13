d0f341561a760500b6fdb69117bd4b82a0ba902a
pavel-zotov <p519446@yandex.ru>
2023-05-13 07:11:18 +0300

    Added/Updated tests\bugs\core_0952_test.py: Made SQL code be executed after DB initialization, i.e. using 'test_script' instead of init.

--- tests/bugs/core_0952_test.py
+++ tests/bugs/core_0952_test.py
@@ -7,45 +7,48 @@ TITLE:       AV when blob is used in expression index
 DESCRIPTION:
 JIRA:        CORE-952
 FBTEST:      bugs.core_0952
+NOTES:
+    [13.05.2023] pzotov
+    Made SQL code be executed after DB initialization, i.e. using 'test_script' instead of init.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """create table T (
-     ID integer not null,
-     N1 blob sub_type 1 segment size 80,
-     N2 varchar(10)
-);
-commit;
+db = db_factory()
 
-insert into T (ID, N1, N2) values (1, 'www', 'qwer');
-commit;
+test_script = """
+    set bail on;
+    set list on;
+    recreate table test (
+         id int primary key,
+         b1 blob sub_type 1 segment size 80,
+         c1 varchar(10)
+    );
+    commit;
 
-alter table T add constraint PK_T primary key (ID);
-create index T_IDX on T computed by (cast(substring(N1 from 1 for 100) as varchar(100)));
-commit;
+    insert into test (id, b1, c1) values (1, 'www', 'qwer');
+    commit;
 
-update T set T.N2 = 'asdf' where T.ID = 1;
-commit;
+    create index test_blob_substr_idx on test computed by (cast(substring(b1 from 1 for 100) as varchar(100)));
+    commit;
+
+    update test set test.c1 = 'asdf' where test.id = 1;
+    commit; -- AV here
+    select id, b1 as blob_id, c1 from test;
 """
 
-db = db_factory(init=init_script)
+act = isql_act('db', test_script, substitutions = [ ('BLOB_ID.*', ''), ] )
 
-test_script = """SELECT ID,N2 FROM T;
+expected_stdout = """
+    ID                              1
+    BLOB_ID                         81:0
+    www
+    C1                              asdf
 """
 
-act = isql_act('db', test_script)
-
-expected_stdout = """ID N2
-============ ==========
-           1 asdf
-
-"""
-
-@pytest.mark.version('>=3')
+@pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
