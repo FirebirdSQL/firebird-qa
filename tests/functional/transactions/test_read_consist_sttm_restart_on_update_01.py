50d446bb0ca22594a40ed77177daeca6857d8039
pavel-zotov <p519446@yandex.ru>
2023-09-23 22:43:26 +0300

    Added/Updated tests\functional\transactions\test_read_consist_sttm_restart_on_update_01.py: Replaced verification method of worker attachment presense (which tries DML and waits for resource). Loop with query to mon$statements is used instead. Maximal duration of this loop is limited - see notes.

--- tests/functional/transactions/test_read_consist_sttm_restart_on_update_01.py
+++ tests/functional/transactions/test_read_consist_sttm_restart_on_update_01.py
@@ -115,8 +115,16 @@ DESCRIPTION:
       24.09.2020: added for-loop in order to check different target objects: TABLE ('test') and VIEW ('v_test'), see 'checked_mode'.
 FBTEST:      functional.transactions.read_consist_sttm_restart_on_update_01
 NOTES:
-[29.07.2022] pzotov
-    Checked on 4.0.1.2692, 5.0.0.591
+    [29.07.2022] pzotov
+        Checked on 4.0.1.2692, 5.0.0.591
+    [23.09.2023] pzotov
+        Replaced verification method of worker attachment presense (which tries DML and waits for resource).
+        This attachment is created by *asynchronously* launched ISQL thus using of time.sleep(1) is COMPLETELY wrong.
+        Loop with query to mon$statements is used instead (we search for record which SQL_TEXT contains 'special tag', see variable SQL_TAG_THAT_WE_WAITING_FOR).
+        Maximal duration of this loop is limited by variable 'MAX_WAIT_FOR_WORKER_START_MS'.
+        Many thanks to Vlad for suggestions.
+
+        Checked on WI-T6.0.0.48, WI-T5.0.0.1211, WI-V4.0.4.2988.
 """
 import pytest
 from firebird.qa import *
@@ -126,19 +134,12 @@ from pathlib import Path
 import time
 import datetime as py_dt
 
-# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
-# from act_src.files_dir/'test_config.ini':
-#
-enc_settings = QA_GLOBALS['wait_for_async_pid']
-
-# ACHTUNG: this must be carefully tuned on every new host:
-#
-MAX_WAIT_FOR_PID_APPEAR_MS = int(enc_settings['MAX_WAIT_FOR_PID_APPEAR_WIN' if os.name == 'nt' else 'MAX_WAIT_FOR_PID_APPEAR_NIX'])
-assert MAX_WAIT_FOR_PID_APPEAR_MS > 0
-
 db = db_factory()
-
 act = python_act('db', substitutions=[('=', ''), ('[ \t]+', ' ')])
+
+MAX_WAIT_FOR_WORKER_START_MS = 10000;
+SQL_TAG_THAT_WE_WAITING_FOR = 'SQL_TAG_THAT_WE_WAITING_FOR'
+# SQL_TO_BE_RESTARTED -- will be defined inside loop, see below!
 
 fn_worker_sql = temp_file('tmp_worker.sql')
 fn_worker_log = temp_file('tmp_worker.log')
@@ -207,13 +208,38 @@ expected_stdout = """
     checked_mode: view, STDLOG: Records affected: 12
 """
 
+def wait_for_attach_showup_in_monitoring(con_monitoring, sql_text_tag):
+    chk_sql = f"select 1 from mon$statements s where s.mon$attachment_id != current_connection and s.mon$sql_text containing '{sql_text_tag}'"
+    attach_with_sql_tag = None
+    t1=py_dt.datetime.now()
+    cur_monitoring = con_monitoring.cursor()
+    while True:
+        cur_monitoring.execute(chk_sql)
+        for r in cur_monitoring:
+            attach_with_sql_tag = r[0]
+        if not attach_with_sql_tag:
+            t2=py_dt.datetime.now()
+            d1=t2-t1
+            if d1.seconds*1000 + d1.microseconds//1000 >= MAX_WAIT_FOR_WORKER_START_MS:
+                break
+            else:
+                con_monitoring.commit()
+                time.sleep(0.2)
+        else:
+            break
+            
+    assert attach_with_sql_tag, f"Could not find attach statement containing '{sql_text_tag}' for {MAX_WAIT_FOR_WORKER_START_MS} ms. ABEND."
+    return
+
 @pytest.mark.version('>=4.0')
-@pytest.mark.skip("TEMPORARY DISABLED. DISCUSSION WITH FB-TEAM REQUIRED!")
+# @pytest.mark.skip("TEMPORARY DISABLED. DISCUSSION WITH FB-TEAM REQUIRED!")
 def test_1(act: Action, fn_worker_sql: Path, fn_worker_log: Path, fn_worker_err: Path, capsys):
     sql_init = (act.files_dir / 'read-consist-sttm-restart-DDL.sql').read_text()
 
     for checked_mode in('table', 'view'):
         target_obj = 'test' if checked_mode == 'table' else 'v_test'
+
+        SQL_TO_BE_RESTARTED = f"update /* {SQL_TAG_THAT_WE_WAITING_FOR} */ {target_obj} set id = -id order by id"
 
         # add rows with ID = 1, 2, ..., 5:
         sql_addi = f'''
@@ -236,7 +262,7 @@ def test_1(act: Action, fn_worker_sql: Path, fn_worker_log: Path, fn_worker_err:
         assert act.stderr == ''
         act.reset()
 
-        with act.db.connect() as con_lock_1, act.db.connect() as con_lock_2:
+        with act.db.connect() as con_lock_1, act.db.connect() as con_lock_2, act.db.connect() as con_monitoring:
             for i,c in enumerate((con_lock_1,con_lock_2)):
                 sttm = f"execute block as begin rdb$set_context('USER_SESSION', 'WHO', 'LOCKER #{i+1}'); end"
                 c.execute_immediate(sttm)
@@ -262,17 +288,15 @@ def test_1(act: Action, fn_worker_sql: Path, fn_worker_log: Path, fn_worker_err:
                 ^
                 set term ;^
                 commit;
-                --set echo on;
+
                 SET KEEP_TRAN_PARAMS ON;
                 set transaction read committed read consistency;
-                --select current_connection, current_transaction from rdb$database;
                 set list off;
                 set wng off;
-                --set plan on;
                 set count on;
 
                  -- THIS MUST BE LOCKED:
-                {worker_dml};
+                {SQL_TO_BE_RESTARTED};
 
                 -- check results:
                 -- ###############
@@ -308,37 +332,9 @@ def test_1(act: Action, fn_worker_sql: Path, fn_worker_log: Path, fn_worker_err:
                                               stderr = hang_err
                                            )
 
-                ##########################################################################################################
-                # added 18.03.2023: we have to be sure that ISQL process really establishes connection
-                # and start execution of <worker_dml> (and will be paused because of locked record):
-                #
-                t1=py_dt.datetime.now()
-                d1 = 0
-                with act.db.connect() as con_checker:
-                    cur_checker = con_checker.cursor()
-                    stm = f"""
-                        select a.mon$remote_pid
-                        from mon$attachments a join mon$statements s using(mon$attachment_id)
-                        where a.mon$remote_pid = ? and cast(s.mon$sql_text as varchar(8190)) = ? and ? >= 0
-                    """
-                    ps = cur_checker.prepare(stm)
 
-                    found_worker_pid = False
-                    while not found_worker_pid:
-                        t2=py_dt.datetime.now()
-                        d1=t2-t1
-                        if d1.seconds*1000 + d1.microseconds//1000 > MAX_WAIT_FOR_PID_APPEAR_MS:
-                            break
-
-                        cur_checker.execute(ps, (p_worker.pid, worker_dml, d1.seconds*1000 + d1.microseconds//1000))
-                        for r in cur_checker:
-                            found_worker_pid = True
-                        con_checker.commit()
-
-                assert found_worker_pid, f'TIMEOUT EXPIRATION: could not find ISQL pid for {d1.seconds*1000 + d1.microseconds//1000} ms which exceeds limit = {MAX_WAIT_FOR_PID_APPEAR_MS} ms.'
-                # 4debug assert not found_worker_pid, f'ISQL pid found for {d1.seconds*1000 + d1.microseconds//1000} ms.'
-                ##########################################################################################################
-
+                wait_for_attach_showup_in_monitoring(con_monitoring, SQL_TAG_THAT_WE_WAITING_FOR)
+                
                 #########################
                 ###  L O C K E R - 2  ###
                 #########################
@@ -390,4 +386,3 @@ def test_1(act: Action, fn_worker_sql: Path, fn_worker_log: Path, fn_worker_err:
     act.expected_stdout = expected_stdout
     act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
-
