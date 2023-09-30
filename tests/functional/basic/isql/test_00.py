89ac37919ccf83838035c16320bc05fcaa5b7ec7
pavel-zotov <p519446@yandex.ru>
2023-09-30 10:35:17 +0300

    Updated tests\functional\basic\isql\test_00.py: adjust expected stdout/stderr to current FB version.

--- tests/functional/basic/isql/test_00.py
+++ tests/functional/basic/isql/test_00.py
@@ -106,8 +106,53 @@ fb5x_checked_stdout = """
     All commands may be abbreviated to letters in CAPitals
 """
 
+fb6x_checked_stdout = """
+    Frontend commands:
+    BLOBDUMP <blobid> <file>   -- dump BLOB to a file
+    BLOBVIEW <blobid>          -- view BLOB in text editor
+    EDIT     [<filename>]      -- edit SQL script file and execute
+    EDIT                       -- edit current command buffer and execute
+    EXPLAIN                    -- explain a query access plan
+    HELP                       -- display this menu
+    INput    <filename>        -- take input from the named SQL file
+    OUTput   [<filename>]      -- write output to named file
+    OUTput                     -- return output to stdout
+    SET      <option>          -- (Use HELP SET for complete list)
+    SHELL    <command>         -- execute Operating System command in sub-shell
+    SHOW     <object> [<name>] -- display system information
+    <object> = CHECK, COLLATION, DATABASE, DOMAIN, EXCEPTION, FILTER, FUNCTION,
+    GENERATOR, GRANT, INDEX, PACKAGE, PROCEDURE, ROLE, SQL DIALECT,
+    SYSTEM, TABLE, TRIGGER, VERSION, USERS, VIEW
+    EXIT                       -- exit and commit changes
+    QUIT                       -- exit and roll back changes
+    All commands may be abbreviated to letters in CAPitals
+    Set commands:
+    SET                    -- display current SET options
+    SET AUTOddl            -- toggle autocommit of DDL statements
+    SET BAIL               -- toggle bailing out on errors in non-interactive mode
+    SET BLOB [ALL|<n>]     -- display BLOBS of subtype <n> or ALL
+    SET BLOB               -- turn off BLOB display
+    SET COUNT              -- toggle count of selected rows on/off
+    SET MAXROWS [<n>]      -- limit select stmt to <n> rows, zero is no limit
+    SET ECHO               -- toggle command echo on/off
+    SET EXPLAIN            -- toggle display of query access plan in the explained form
+    SET HEADING            -- toggle display of query column titles
+    SET KEEP_TRAN_params   -- toggle to keep or not to keep text of following successful SET TRANSACTION statement
+    SET LIST               -- toggle column or table display format
+    SET NAMES <csname>     -- set name of runtime character set
+    SET PER_TABle_stats    -- toggle display of detailed per-table statistics
+    SET PLAN               -- toggle display of query access plan
+    SET PLANONLY           -- toggle display of query plan without executing
+    SET SQL DIALECT <n>    -- set sql dialect to <n>
+    SET STATs              -- toggle display of performance statistics
+    SET TIME               -- toggle display of timestamp with DATE values
+    SET TERM <string>      -- change statement terminator string
+    SET WIDTH <col> [<n>]  -- set/unset print width to <n> for column <col>
+    All commands may be abbreviated to letters in CAPitals
+"""
+
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = fb3x_checked_stdout if act.is_version('<5') else fb5x_checked_stdout
-    act.execute()
+    act.expected_stdout = fb3x_checked_stdout if act.is_version('<5') else fb5x_checked_stdout if act.is_version('<6') else fb6x_checked_stdout
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
