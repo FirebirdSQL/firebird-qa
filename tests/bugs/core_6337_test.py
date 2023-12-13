75ab5fc3a0c662b0618f1769c297075a8a84580f
pavel-zotov <p519446@yandex.ru>
2023-12-13 20:04:01 +0300

    Added/Updated tests\bugs\core_6337_test.py: Added 'SQLSTATE' in substitutions: runtime error must not be suppressed. Added 'combine_output = True' in order to see SQLSTATE if any error occurs.

--- tests/bugs/core_6337_test.py
+++ tests/bugs/core_6337_test.py
@@ -5,11 +5,15 @@ ID:          issue-6578
 ISSUE:       6578
 TITLE:       SubType information is lost when calculating arithmetic expressions
 DESCRIPTION:
-NOTES:
-[25.06.2020]
-  4.0.0.2076: changed types in SQLDA from numeric to int128 // after discuss with Alex about CORE-6342.
 JIRA:        CORE-6337
 FBTEST:      bugs.core_6337
+NOTES:
+    [25.06.2020]
+        4.0.0.2076: changed types in SQLDA from numeric to int128 // after discuss with Alex about CORE-6342.
+    [13.12.2023] pzotov
+        Added 'SQLSTATE' in substitutions: runtime error must not be filtered out by '?!(...)' pattern
+        ("negative lookahead assertion", see https://docs.python.org/3/library/re.html#regular-expression-syntax).
+        Added 'combine_output = True' in order to see SQLSTATE if any error occurs.
 """
 
 import pytest
@@ -23,7 +27,7 @@ test_script = """
     select cast(1 as numeric(18,2)) * cast(1 as numeric(18,2)) from rdb$database;
 """
 
-act = isql_act('db', test_script, substitutions=[('^((?!sqltype).)*$', ''), ('[ \t]+', ' ')])
+act = isql_act('db', test_script, substitutions = [ ('^((?!SQLSTATE|sqltype).)*$', ''), ('[ \t]+', ' ') ] )
 
 expected_stdout = """
     01: sqltype: 32752 INT128 scale: -4 subtype: 1 len: 16
@@ -32,5 +36,5 @@ expected_stdout = """
 @pytest.mark.version('>=4.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
