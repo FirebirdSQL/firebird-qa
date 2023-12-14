4b1dea4ad4a086a70cc3c0fadf3889d620e8d6b6
pavel-zotov <p519446@yandex.ru>
2023-12-14 19:20:23 +0300

    Added/Updated tests\bugs\gh_6812_test.py: Added 'SQLSTATE' in substitutions: runtime error must not be suppressed. Added 'combine_output = True' in order to see SQLSTATE if any error occurs.

--- tests/bugs/gh_6812_test.py
+++ tests/bugs/gh_6812_test.py
@@ -6,6 +6,11 @@ ISSUE:       6812
 TITLE:       BASE64_ENCODE and HEX_ENCODE can exceed maximum widths for VARCHAR
 DESCRIPTION:
 FBTEST:      bugs.gh_6812
+NOTES:
+    [14.12.2023] pzotov
+        Added 'SQLSTATE' in substitutions: runtime error must not be filtered out by '?!(...)' pattern
+        ("negative lookahead assertion", see https://docs.python.org/3/library/re.html#regular-expression-syntax).
+        Added 'combine_output = True' in order to see SQLSTATE if any error occurs.
 """
 
 import pytest
@@ -19,8 +24,7 @@ test_script = """
     select base64_encode(cast('' as char(32767))) as "enc_02" from rdb$database where 1 <> 1;
 """
 
-act = isql_act('db', test_script, substitutions=[('^((?!(sqltype|enc)).)*$', ''),
-                                                 ('[ \t]+', ' ')])
+act = isql_act('db', test_script, substitutions=[('^((?!(SQLSTATE|sqltype|enc)).)*$', ''), ('[ \t]+', ' ')])
 
 expected_stdout = """
     01: sqltype: 520 BLOB scale: 0 subtype: 1 len: 8 charset: 2 ASCII
@@ -32,5 +36,5 @@ expected_stdout = """
 @pytest.mark.version('>=4.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
