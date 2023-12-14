dc20c186b3c063ae6cddf82b334e02a6990f55ac
pavel-zotov <p519446@yandex.ru>
2023-12-14 19:29:30 +0300

    Added/Updated tests\bugs\gh_6874_test.py: Added 'SQLSTATE' in substitutions: runtime error must not be suppressed. Added 'combine_output = True' in order to see SQLSTATE if any error occurs.

--- tests/bugs/gh_6874_test.py
+++ tests/bugs/gh_6874_test.py
@@ -7,6 +7,11 @@ TITLE:       Literal 65536 (interpreted as int) can not be multiplied by itself 
 DESCRIPTION:
   Confirmed need to explicitly cast literal 65536 on: 5.0.0.88, 4.0.1.2523 (otherwise get SQLSTATE = 22003).
 FBTEST:      bugs.gh_6874
+NOTES:
+    [14.12.2023] pzotov
+        Added 'SQLSTATE' in substitutions: runtime error must not be filtered out by '?!(...)' pattern
+        ("negative lookahead assertion", see https://docs.python.org/3/library/re.html#regular-expression-syntax).
+        Added 'combine_output = True' in order to see SQLSTATE if any error occurs.
 """
 
 import pytest
@@ -21,8 +26,7 @@ test_script = """
     select -65536*-65536*-65536*-65536 as "multiply_result_2" from rdb$database;
 """
 
-act = isql_act('db', test_script, substitutions=[('^((?!sqltype:|multiply_result).)*$', ''),
-                                                 ('[ \t]+', ' '), ('.*alias:.*', '')])
+act = isql_act('db', test_script, substitutions=[('^((?!SQLSTATE|sqltype:|multiply_result).)*$', ''), ('[ \t]+', ' '), ('.*alias:.*', '')])
 
 expected_stdout = """
     01: sqltype: 32752 INT128 scale: 0 subtype: 0 len: 16
@@ -35,5 +39,5 @@ expected_stdout = """
 @pytest.mark.version('>=4.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
