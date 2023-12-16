c0530f06b94e612652e9edd9920f774ac7c387c9
pavel-zotov <p519446@yandex.ru>
2023-12-16 13:48:45 +0300

    Added/Updated tests\functional\intfunc\avg\test_06.py: see notes

--- tests/functional/intfunc/avg/test_06.py
+++ tests/functional/intfunc/avg/test_06.py
@@ -4,14 +4,19 @@
 ID:          intfunc.avg-06
 TITLE:       AVG - Integer OverFlow
 DESCRIPTION:
-NOTES:
-[14.10.2019] Refactored: adjusted expected_stdout/stderr
-[25.06.2020] 4.0.0.2076: changed types in SQLDA from numeric to int128 // after discuss with Alex about CORE-6342.
-[09.07.2020], 4.0.0.2091:
-  NO more overflow since INT128 was introduced. AVG() is evaluated successfully.
-  Removed error message from expected_stderr, added result into expected_stdout.
-[27.07.2021] changed sqltype in FB 4.x+ to 580 INT64: this is needed since fix #6874.
 FBTEST:      functional.intfunc.avg.06
+NOTES:
+    [25.06.2020] 4.0.0.2076: changed types in SQLDA from numeric to int128 // after discuss with Alex about CORE-6342.
+    [09.07.2020], 4.0.0.2091:
+        NO more overflow since INT128 was introduced. AVG() is evaluated successfully.
+        Removed error message from expected_stderr, added result into expected_stdout.
+    [27.07.2021]
+        Changed sqltype in FB 4.x+ to 580 INT64: this is needed since fix #6874.
+    [16.12.2023]
+        Replaced splitted code with assigning appropiate expected text using if-else depending on act.is_version result.
+        Adjusted substitutions: runtime error must not be filtered out by '?!(...)' pattern
+        ("negative lookahead assertion", see https://docs.python.org/3/library/re.html#regular-expression-syntax).
+        Added 'combine_output = True' in order to see SQLSTATE if any error occurs.
 """
 
 import pytest
@@ -36,43 +41,26 @@ test_script = """
     select * from v_test;
 """
 
-act = isql_act('db', test_script, substitutions=[('^((?!sqltype|AVG_RESULT).)*$', ''), ('[ \t]+', ' ')])
+# Statement failed, SQLSTATE = 22003
+# Integer overflow.  The result of an integer operation caused the most significant bit of the result to carry.
 
-# version: 3.0
+act = isql_act('db', test_script, substitutions=[('^((?!SQLSTATE|sqltype|AVG_RESULT|[Ii]nteger|overflow).)*$', ''), ('[ \t]+', ' ')])
 
-expected_stdout_1 = """
-    INPUT message field count: 0
-    OUTPUT message field count: 1
+expected_fb3x = """
     01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
     : name: AVG_RESULT alias: AVG_RESULT
-    : table: V_TEST owner: SYSDBA
-"""
-
-expected_stderr_1 = """
     Statement failed, SQLSTATE = 22003
-    Integer overflow.  The result of an integer operation caused the most significant bit of the result to carry.
+    Integer overflow. The result of an integer operation caused the most significant bit of the result to carry.
 """
 
-@pytest.mark.version('>=3.0,<4.0')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout_1
-    act.expected_stderr = expected_stderr_1
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
-
-# version: 4.0
-
-expected_stdout_2 = """
-      01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
-      :  name: AVG_RESULT  alias: AVG_RESULT
-      : table: V_TEST  owner: SYSDBA
-
-    AVG_RESULT                                                4410000000000000000
+expected_fb4x = """
+    01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+    : name: AVG_RESULT alias: AVG_RESULT
+    AVG_RESULT 4410000000000000000
 """
 
-@pytest.mark.version('>=4.0')
+@pytest.mark.version('>=3.0')
 def test_2(act: Action):
-    act.expected_stdout = expected_stdout_2
-    act.execute()
+    act.expected_stdout = expected_fb3x if act.is_version('<4') else expected_fb4x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
