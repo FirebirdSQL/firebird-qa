d1345a1cb6bc27fb629b770ea1368f58b8313985
pavel-zotov <p519446@yandex.ru>
2023-12-16 13:20:35 +0300

    Added/Updated tests\functional\gtcs\test_computed_fields_12.py: Added 'SQLSTATE' in substitutions: runtime error must not be suppressed. Added 'combine_output = True' in order to see SQLSTATE if any error occurs.

--- tests/functional/gtcs/test_computed_fields_12.py
+++ tests/functional/gtcs/test_computed_fields_12.py
@@ -5,14 +5,18 @@ ID:          computed-fields-12
 FBTEST:      functional.gtcs.computed_fields_12
 TITLE:       Computed fields
 DESCRIPTION:
-  Original test see in:
-  https://github.com/FirebirdSQL/fbtcs/blob/master/GTCS/tests/CF_ISQL_12.script
-  SQL script for creating test database ('gtcs_sp1.fbk') and fill it with some data:
-  https://github.com/FirebirdSQL/fbtcs/blob/master/GTCS/tests/PROCS_QA_INIT_ISQL.script
+    Original test see in:
+    https://github.com/FirebirdSQL/fbtcs/blob/master/GTCS/tests/CF_ISQL_12.script
+    SQL script for creating test database ('gtcs_sp1.fbk') and fill it with some data:
+    https://github.com/FirebirdSQL/fbtcs/blob/master/GTCS/tests/PROCS_QA_INIT_ISQL.script
 NOTES:
-[25.09.2021]
-  moved code for 4.0+ into separate secion because of fixed gh-6845. Use SET_SQLDA_DISPLAY ON for check datatypes.
-  (seel also commit for apropriate GTCS-tests: e617f3d70be5018de6e6ee8624da6358d52a9ce0, 20-aug-2021 14:11)
+    [25.09.2021]
+        splitted output for 3.x and 4.x+ because of fixed gh-6845. Use SET_SQLDA_DISPLAY ON for check datatypes.
+        Seel commit for apropriate GTCS-tests: e617f3d70be5018de6e6ee8624da6358d52a9ce0, 20-aug-2021 14:11
+    [16.12.2023] pzotov
+        Added 'SQLSTATE' in substitutions: runtime error must not be filtered out by '?!(...)' pattern
+        ("negative lookahead assertion", see https://docs.python.org/3/library/re.html#regular-expression-syntax).
+        Added 'combine_output = True' in order to see SQLSTATE if any error occurs.
 """
 
 import pytest
@@ -20,37 +24,7 @@ from firebird.qa import *
 
 db = db_factory()
 
-# version: 3.0
-
-test_script_1 = """
-    set heading off;
-    set list on;
-    /*---------------------------------------------*/
-    /* Computed field using another computed field */
-    /*---------------------------------------------*/
-    create table t3 (a integer, af computed by (a*3), afaf computed by (af*2));
-    insert into t3(a) values(10);
-
-    select * from t3;
-"""
-
-act_1 = isql_act('db', test_script_1, substitutions=[('[ \t]+', ' ')])
-
-expected_stdout_1 = """
-    A                               10
-    AF                              30
-    AFAF                            60
-"""
-
-@pytest.mark.version('>=3.0,<4.0')
-def test_1(act_1: Action):
-    act_1.expected_stdout = expected_stdout_1
-    act_1.execute()
-    assert act_1.clean_stdout == act_1.clean_expected_stdout
-
-# version: 4.0
-
-test_script_2 = """
+test_script = """
     set list on;
     recreate table test (fld_source integer, fld_comp_based_on_source computed by ( fld_source*3 ), fld_comp_based_on_comp computed by ( fld_comp_based_on_source * 2 ) );
     insert into test(fld_source) values(10);
@@ -64,20 +38,29 @@ test_script_2 = """
     select * from test;
 """
 
-act_2 = isql_act('db', test_script_2, substitutions=[('^((?!(sqltype|FLD_)).)*$', ''),
+act = isql_act('db', test_script, substitutions=[('^((?!(SQLSTATE|sqltype|FLD_)).)*$', ''),
                                                      ('[ \t]+', ' '), ('.*alias.*', '')])
 
-expected_stdout_2 = """
+expected_fb3x = """
+    01: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
+    02: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+    03: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+    FLD_SOURCE               10
+    FLD_COMP_BASED_ON_SOURCE 30
+    FLD_COMP_BASED_ON_COMP   60
+"""
+
+expected_fb4x = """
     01: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
     02: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
     03: sqltype: 32752 INT128 Nullable scale: 0 subtype: 0 len: 16
-    FLD_SOURCE                      10
-    FLD_COMP_BASED_ON_SOURCE        30
-    FLD_COMP_BASED_ON_COMP                                                     60
+    FLD_SOURCE               10
+    FLD_COMP_BASED_ON_SOURCE 30
+    FLD_COMP_BASED_ON_COMP   60
 """
 
-@pytest.mark.version('>=4.0')
-def test_2(act_2: Action):
-    act_2.expected_stdout = expected_stdout_2
-    act_2.execute()
-    assert act_2.clean_stdout == act_2.clean_expected_stdout
+@pytest.mark.version('>=3.0')
+def test(act: Action):
+    act.expected_stdout = expected_fb3x if act.is_version('<4') else expected_fb4x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
