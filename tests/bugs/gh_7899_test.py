34704fbf0dcef97567fecd6412419bf59b5d218f
pavel-zotov <p519446@yandex.ru>
2024-01-12 18:33:29 +0300

    Added/Updated tests\bugs\gh_7899_test.py: removed SKIP directive for FB 5.x. Checked on intermediate snapshot 5.0.0.1307.

--- tests/bugs/gh_7899_test.py
+++ tests/bugs/gh_7899_test.py
@@ -9,8 +9,8 @@ NOTES:
     Confirmed bug on 6.0.0.180.
     Checked on intermediate builds:
          6.0.0.186,  commit 305c40a05b1d64c14dbf5f25f36c42c44c6392d9
+         5.0.0.1307, commit e35437e00687db9ed6add279cecb816dcdf8b07a
          4.0.5.3042, commit f7b090043e8886ab6286f8d626dd1684dc09e3b8
-    NB: execution is currently SKIPPED for FB 5.x. Waiting for backport.
 """
 
 import pytest
@@ -34,10 +34,6 @@ expected_stdout = """
 
 @pytest.mark.version('>=4.0.5')
 def test_1(act: Action):
-
-    if act.is_version('>=5.0,<6.0'):
-        # TEMPORARY, TO BE DELETED LATER:
-        pytest.skip('Waiting for backport from FB 6.x')
 
     test_script = f"""
         set list on;
