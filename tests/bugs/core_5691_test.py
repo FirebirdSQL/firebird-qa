efe36e44139eaec9f0be6091d06c66cdbd8632a4
pavel-zotov <p519446@yandex.ru>
2024-03-28 19:52:36 +0300

    Added/Updated tests\bugs\core_5691_test.py: Removed loop with search for 'file description' attribute because it is useless in case when system locale differs from English (e.g. russian etc). Defined constant with value = 34.

--- tests/bugs/core_5691_test.py
+++ tests/bugs/core_5691_test.py
@@ -20,10 +20,8 @@ NOTES:
     Checked on 6.0.0.157, 5.0.0.1280, 4.0.5.3031
 
     [28.03.2024] pzotov
-    Added call os.system("chcp 65001") before invocation of VBS.
-    This is needed on machine with non-ascii system locale, e.g. ru-ru etc (i.e. when chcp output is 866 or 1251).
-    Otherwise VBS will not able to find attribute with name 'file description' (all of attributes will be returned in
-    localized form).
+    Removed loop with search for 'file description' attribute because it is useless in case when system locale
+    differs from English (e.g. russian etc). Defined constant with value = 34.
 """
 import os
 from pathlib import Path
@@ -77,6 +75,10 @@ def test_1(act: Action, tmp_vbs: Path, tmp_log: Path, capsys):
 
             on error resume next
 
+            '################################
+            const FILE_DESCRIPTION_INDEX = 34
+            '################################
+
             dim fso
             dim objFile
 
@@ -108,40 +110,16 @@ def test_1(act: Action, tmp_vbs: Path, tmp_log: Path, capsys):
             set objShell = CreateObject("Shell.Application")
             set objFolder = objShell.NameSpace(folderName)
 
-            dim i
-            dim fdescr_idx
-            dim propertyName
-
-            i = 0
-            fdescr_idx = 0
-            do
-                propertyName = vbNullString
-                rem https://docs.microsoft.com/en-us/windows/win32/shell/folder-getdetailsof
-                rem retrieves details about an item in a folder. for example, its size, type, or the time of its last modification.
-                propertyName = objFolder.GetDetailsOf(objFolder.Items, i)
-                if LCase(propertyName) = LCase("file description") then
-                    fdescr_idx = i
-                    exit do
-                end if
-
-                if propertyName = vbNullString then
-                    exit do
-                end if
-                i = i + 1
-            loop
-            propertyName = Nothing
-            rem wscript.echo "fdescr_idx=",fdescr_idx
-
             dim objFolderItem
             set objFolderItem = objFolder.ParseName(fileName)
 
             if (not objFolderItem Is Nothing) then
                 dim attribName
                 dim objInfo
-                attribName = objFolder.GetDetailsOf(objFolder.Items, fdescr_idx)
-                objInfo = objFolder.GetDetailsOf(objFolderItem, fdescr_idx)
+                attribName = objFolder.GetDetailsOf(objFolder.Items, FILE_DESCRIPTION_INDEX)
+                objInfo = objFolder.GetDetailsOf(objFolderItem, FILE_DESCRIPTION_INDEX)
 
-                wscript.echo "'" & fileName & "' " & LCase(attribName) & ":", LCase(objInfo)
+                wscript.echo "'" & fileName & "' : ", LCase(objInfo)
 
                 attribName = Nothing
                 objInfo = Nothing
@@ -176,15 +154,6 @@ def test_1(act: Action, tmp_vbs: Path, tmp_log: Path, capsys):
                'plugins/fbtrace.dll',
              )
 
-
-    #for x in sorted(f_list):
-    #    print(act.vars['bin-dir'] / x)
-
-    # Following is needed on machine with non-ascii system locale, e.g. ru-ru etc (i.e. when chcp output is 866 or 1251).
-    # Otherwise VBS will not able to find attribute with name 'file description' (all of attributes will be returned in
-    # localized form).
-    #
-    os.system("chcp 65001")
     with open(tmp_log,'w') as vbs_log:
         for x in sorted(f_list):
             subprocess.call( [ 'cscript', '//nologo', str(tmp_vbs), act.vars['bin-dir'] / x  ], stdout = vbs_log, stderr = subprocess.STDOUT)
@@ -195,24 +164,24 @@ def test_1(act: Action, tmp_vbs: Path, tmp_log: Path, capsys):
             print( line.lower() )
 
     expected_stdout = f"""
-        'fb_lock_print.exe' 		file description: firebird lock print tool
-        'fbclient.dll' 				file description: firebird client library
-        'fbguard.exe' 				file description: firebird guardian
-        'fbsvcmgr.exe' 				file description: firebird services management tool
-        'fbtracemgr.exe' 			file description: firebird trace management tool
-        'firebird.exe' 				file description: firebird server executable
-        'gbak.exe' 					file description: firebird gbak tool
-        'gfix.exe' 					file description: firebird gfix tool
-        'gstat.exe' 				file description: firebird gstat tool
-        'isql.exe' 					file description: firebird interactive query tool
-        'nbackup.exe' 				file description: firebird physical backup management tool
-        'chacha.dll' 				file description: firebird wire encryption plugin using chacha cypher
-        '{engine_version}.dll'		file description: firebird engine plugin
-        'fbtrace.dll' 				file description: firebird trace plugin
-        'legacy_auth.dll' 			file description: firebird legacy auth plugin
-        'legacy_usermanager.dll' 	file description: firebird legacy user manager plugin
-        'srp.dll' 					file description: firebird srp user manager plugin
-        'udr_engine.dll' 			file description: firebird user defined routines engine
+        'fb_lock_print.exe' 		: firebird lock print tool
+        'fbclient.dll' 				: firebird client library
+        'fbguard.exe' 				: firebird guardian
+        'fbsvcmgr.exe' 				: firebird services management tool
+        'fbtracemgr.exe' 			: firebird trace management tool
+        'firebird.exe' 				: firebird server executable
+        'gbak.exe' 					: firebird gbak tool
+        'gfix.exe' 					: firebird gfix tool
+        'gstat.exe' 				: firebird gstat tool
+        'isql.exe' 					: firebird interactive query tool
+        'nbackup.exe' 				: firebird physical backup management tool
+        'chacha.dll' 				: firebird wire encryption plugin using chacha cypher
+        '{engine_version}.dll'		: firebird engine plugin
+        'fbtrace.dll' 				: firebird trace plugin
+        'legacy_auth.dll' 			: firebird legacy auth plugin
+        'legacy_usermanager.dll' 	: firebird legacy user manager plugin
+        'srp.dll' 					: firebird srp user manager plugin
+        'udr_engine.dll' 			: firebird user defined routines engine
     """
 
     act.expected_stdout = expected_stdout
