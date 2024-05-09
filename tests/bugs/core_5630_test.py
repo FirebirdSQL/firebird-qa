bc76e00aa23b2dc122b0131f27055f1e12f7eaba
pavel-zotov <p519446@yandex.ru>
2024-05-10 01:13:53 +0300

    Added/Updated tests\bugs\core_5630_test.py: Refactored, plus added combine_output in order to see STDERR content

--- tests/bugs/core_5630_test.py
+++ tests/bugs/core_5630_test.py
@@ -9,6 +9,7 @@ DESCRIPTION:
 JIRA:        CORE-5630
 FBTEST:      bugs.core_5630
 """
+import locale
 
 import pytest
 from pathlib import Path
@@ -43,37 +44,38 @@ shd_file = temp_file('core_5630.shd')
 @pytest.mark.version('>=3.0.3')
 def test_1(act: Action, fdb_file: Path, fbk_file: Path, shd_file: Path):
     init_ddl = f"""
-    set bail on;
-    set list on;
+        set bail on;
+        set list on;
 
-    create database 'localhost:{fdb_file}' user '{act.db.user}' password '{act.db.password}';
+        create database 'localhost:{fdb_file}' user {act.db.user} password '{act.db.password}';
 
-    recreate table test(s varchar(30));
-    commit;
+        recreate table test(s varchar(30));
+        commit;
 
-    create or alter view v_shadow_info as
-    select
-         rdb$file_sequence  --           0
-        ,rdb$file_start     --           0
-        ,rdb$file_length    --           0
-        ,rdb$file_flags     --           1
-        ,rdb$shadow_number  --           1
-    from rdb$files
-    where lower(rdb$file_name) containing lower('core_5630.shd')
-    ;
+        create or alter view v_shadow_info as
+        select
+             rdb$file_sequence  --           0
+            ,rdb$file_start     --           0
+            ,rdb$file_length    --           0
+            ,rdb$file_flags     --           1
+            ,rdb$shadow_number  --           1
+        from rdb$files
+        where lower(rdb$file_name) containing lower('core_5630.shd')
+        ;
 
-    insert into test select 'line #' || lpad(row_number()over(), 3, '0' ) from rdb$types rows 200;
-    commit;
+        insert into test select 'line #' || lpad(row_number()over(), 3, '0' ) from rdb$types rows 200;
+        commit;
 
-    create shadow 1 '{shd_file}';
-    commit;
-    set list on;
-    select * from v_shadow_info;
-    select hash( list(s) ) as s_hash_before from test;
-    quit;
+        create shadow 1 '{shd_file}';
+        commit;
+        set list on;
+        select * from v_shadow_info;
+        select hash( list(s) ) as s_hash_before from test;
+        quit;
     """
+
     act.expected_stdout = expected_stdout_a
-    act.isql(switches=['-q'], input=init_ddl)
+    act.isql(switches=['-q'], input = init_ddl, connect_db=False, combine_output = True, io_enc = locale.getpreferredencoding())
     assert act.clean_stdout == act.clean_expected_stdout
     #
     with act.connect_server() as srv:
@@ -84,6 +86,9 @@ def test_1(act: Action, fdb_file: Path, fbk_file: Path, shd_file: Path):
     shd_file.unlink()
     #
     act.reset()
+
+    #-----------------------------------------------------------------------------------
+
     act.gbak(switches=['-c', '-use_all_space', str(fbk_file), act.get_dsn(fdb_file)])
     # Check that we have the same data in DB tables
     sql_text = """
@@ -92,6 +97,9 @@ def test_1(act: Action, fdb_file: Path, fbk_file: Path, shd_file: Path):
         select hash( list(s) ) as s_hash_after from test;
         """
     act.reset()
+
+    #-----------------------------------------------------------------------------------
     act.expected_stdout = expected_stdout_b
-    act.isql(switches=['-q', act.get_dsn(fdb_file)], input=sql_text, connect_db=False)
+    act.isql(switches=['-q', act.get_dsn(fdb_file)], input=sql_text, connect_db=False, combine_output = True, io_enc = locale.getpreferredencoding())
+
     assert act.clean_stdout == act.clean_expected_stdout
