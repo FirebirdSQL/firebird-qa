f01cc06f40f7553ca895894dc3780e6219e6f6fe
pavel-zotov <p519446@yandex.ru>
2024-05-10 01:19:51 +0300

    Added/Updated tests\bugs\core_6517_test.py: Refactored, plus added combine_output in order to see STDERR content

--- tests/bugs/core_6517_test.py
+++ tests/bugs/core_6517_test.py
@@ -3,15 +3,15 @@
 """
 ID:          issue-6746
 ISSUE:       6746
-TITLE:       Regression: CREATE DATABASE fails with 'Token unknown' error when DB name is
-  enclosed in double quotes and 'DEFAULT CHARACTER SET' is specified after DB name
+TITLE:       Regression: CREATE DATABASE fails with 'Token unknown' error when DB name is enclosed in double quotes and 'DEFAULT CHARACTER SET' is specified after DB name
 DESCRIPTION:
 JIRA:        CORE-6517
 FBTEST:      bugs.core_6517
 """
+import locale
+from pathlib import Path
 
 import pytest
-from pathlib import Path
 from firebird.qa import *
 
 db = db_factory()
@@ -22,5 +22,7 @@ test_db = temp_file('tmp_core_6517.fdb')
 
 @pytest.mark.version('>=3.0.8')
 def test_1(act: Action, test_db: Path):
-    act.isql(switches=[], input=f'create database "{act.get_dsn(test_db)}" default character set utf8;',
-               connect_db=False)
+    test_sql = f"""
+        create database "{act.get_dsn(test_db)}" default character set utf8;
+    """
+    act.isql(switches = ['-q'], input = test_sql, connect_db = False, combine_output = True, io_enc = locale.getpreferredencoding())
