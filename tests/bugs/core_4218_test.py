21b880feae185229b30580e555355bc1cff8258b
pavel-zotov <p519446@yandex.ru>
2024-05-10 00:44:18 +0300

    Added/Updated tests\bugs\core_4218_test.py: Refactored, plus added combine_output in order to see STDERR content

--- tests/bugs/core_4218_test.py
+++ tests/bugs/core_4218_test.py
@@ -15,37 +15,37 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_user: User = user_factory('db', name='TMP_U4218', password='123')
+tmp_user: User = user_factory('db', name='TMP_U4218', password='123')
 
-act = python_act('db', substitutions=[('Commit current transaction \\(y/n\\)\\?', '')])
-
-expected_stdout = """
-    WHO_AM_I                        TMP_U4218
-    WHO_IS_OWNER                    TMP_U4218
-    WHO_AM_I                        SYSDBA
-    WHO_IS_OWNER                    TMP_U4218
-"""
+act = python_act('db', substitutions = [('[ \t]+', ' ')])
 
 test_db = temp_file('owner-db.fdb')
 
 @pytest.mark.version('>=3.0')
-def test_1(act: Action, test_user: User, test_db: Path):
+def test_1(act: Action, tmp_user: User, test_db: Path):
     with act.db.connect() as con:
         c = con.cursor()
         c.execute('grant create database to user TMP_U4218')
         con.commit()
+
     test_script = f"""
-    create database 'localhost:{test_db}' user 'TMP_U4218' password '123';
-    set list on;
-    set list on; -- Needed on Windows to really set list ON.
-    select current_user as who_am_i, mon$owner as who_is_owner from mon$database;
-    commit;
-    connect 'localhost:{test_db}';
-    select current_user as who_am_i, mon$owner as who_is_owner from mon$database;
-    commit;
-    drop database;
-    quit;
+        create database 'localhost:{test_db}' user {tmp_user.name} password '{tmp_user.password}';
+        set list on;
+        select current_user as who_am_i, mon$owner as who_is_owner from mon$database;
+        commit;
+        connect 'localhost:{test_db}';
+        select current_user as who_am_i, mon$owner as who_is_owner from mon$database;
+        commit;
+        drop database;
+        quit;
     """
+    expected_stdout = f"""
+        WHO_AM_I                        {tmp_user.name.upper()}
+        WHO_IS_OWNER                    {tmp_user.name.upper()}
+        WHO_AM_I                        SYSDBA
+        WHO_IS_OWNER                    {tmp_user.name.upper()}
+    """
+
     act.expected_stdout = expected_stdout
-    act.isql(switches=['-q'], input=test_script)
+    act.isql(switches=['-q'], input=test_script, connect_db=False, combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
