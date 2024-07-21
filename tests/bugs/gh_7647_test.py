764caf92a14afa3adf7ee64a9743f7028e477fdc
pavel-zotov <p519446@yandex.ru>
2024-07-21 11:03:56 +0300

    Added/Updated tests\bugs\gh_7647_test.py: Problem with 6.x has been fixed in 6.0.0.346, commit 17b007d1. Removed upper limit restriction for major version.

--- tests/bugs/gh_7647_test.py
+++ tests/bugs/gh_7647_test.py
@@ -12,6 +12,11 @@ NOTES:
     "ValueError: Incorrect ARRAY field value." raises on firebird.driver: 1.10.1 / firebird-qa-0.19.2
     Added restriction for major versions that are allowed to be checked: currently only 3.x and 4.x.
     See also: https://github.com/FirebirdSQL/firebird/issues/7862
+
+    [21.07.2024] pzotov
+    Problem with 6.x has been fixed in 6.0.0.346, commit date 07.05.2024:
+    https://github.com/FirebirdSQL/firebird/commit/17b007d14f8ccc6cfba0d63a3b2f21622ced20d0
+    Removed upper limit restriction for major version.
 """
 
 import pytest
@@ -25,7 +30,7 @@ expected_stdout = """
 
 act = python_act('db')
 
-@pytest.mark.version('>=3.0.12,<5.0')
+@pytest.mark.version('>=3.0.12')
 def test_1(act: Action, capsys):
     with act.db.connect() as con:
         cur = con.cursor()
