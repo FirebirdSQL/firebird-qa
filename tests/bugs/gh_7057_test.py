cb5a75c52862ff83cbcc10d0eb9524710be6645e
pavel-zotov <p519446@yandex.ru>
2024-07-29 19:41:54 +0300

    Added/Updated tests\bugs\gh_7057_test.py: Raised min_version after check on fresh 5.0.1 -- see notes. Checked on 6.0.0.401-a7d10a4, 5.0.1.1453-62ee5f1.

--- tests/bugs/gh_7057_test.py
+++ tests/bugs/gh_7057_test.py
@@ -18,11 +18,16 @@ NOTES:
     2. Test caused crash of server on snapshots before 6.0.0.401-a7d10a4.
        Problem related to MaxStatementCacheSize which default value > 0
        (explained by dimitr, letter 19-JUL-2024 12:52).
-       It seems that bug was fixed in https://github.com/FirebirdSQL/firebird/commit/a7d10a40147d326e56540498b50e40b2da0e5850
-       ("Fix #8185 - SIGSEGV with WHERE CURRENT OF statement with statement cache turned on.")
-    3. Currently this test can run only in 6.0.0.401-a7d10a4 and later.
+       
+       It seems that bug was fixed in:
+           FB 5.x: https://github.com/FirebirdSQL/firebird/commit/08dc25f8c45342a73c786bc60571c8a5f2c8c6e3 (27.07.2024 14:55)
+           FB 6.x: https://github.com/FirebirdSQL/firebird/commit/a7d10a40147d326e56540498b50e40b2da0e5850 (29.07.2024 03:53)
+           ("Fix #8185 - SIGSEGV with WHERE CURRENT OF statement with statement cache turned on.")
 
-    Checked on 6.0.0.401-a7d10a4.
+    3. Attempt to run this test on FB 4.0.5.3127 (10-JUL-2024) raises:
+           "E firebird.driver.types.DatabaseError: feature is not supported"
+
+    Checked on 6.0.0.401-a7d10a4, 5.0.1.1453-62ee5f1.
 """
 import pytest
 from firebird.qa import *
@@ -47,7 +52,7 @@ def print_row(row, cur = None):
 
 
 @pytest.mark.scroll_cur
-@pytest.mark.version('>=6.0.0')
+@pytest.mark.version('>=5.0.1')
 def test_1(act: Action, capsys):
     with act.db.connect() as con:
         con.execute_immediate('recreate table ts(id int)')
