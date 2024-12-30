6c5b05bfdc76a5d13465afab8e0abe6164f050e3
pavel-zotov <p519446@yandex.ru>
2024-12-30 12:13:18 +0300

    Added/Updated tests\bugs\gh_6915_cs_cz_test.py: added comments

--- tests/bugs/gh_6915_cs_cz_test.py
+++ tests/bugs/gh_6915_cs_cz_test.py
@@ -5,6 +5,8 @@ ID:          issue-6915-cs-cz
 ISSUE:       https://github.com/FirebirdSQL/firebird/issues/6915
 TITLE:       Performance effect of applying 'DISABLE-COMPRESSIONS=1' in UNICODE collation for LOCALE=cs_CZ
 DESCRIPTION:
+    Original discussion:
+    https://sourceforge.net/p/firebird/mailman/firebird-devel/thread/9361c612-d720-eb76-d412-7101518ca60d%40ibphoenix.cz/
     Test verifies only PERFORMANCE issues referred to in the ticket #6915. Correctness of ORDER BY is not checked.
     A pre-build database is used for check, see: files/gh_6915_cs_cz.zip (it was created in FB 4.x with date ~aug-2021).
     SQL script that was used to fulfill test DB see in the end of this file.
