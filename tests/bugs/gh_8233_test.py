df454c615b3147906c1be14e76014341e91043a6
pavel-zotov <p519446@yandex.ru>
2025-01-18 14:49:03 +0300

    Added/Updated tests\bugs\gh_8233_test.py: Resultset of cursor that executes using instance of selectable PreparedStatement must be stored in some variable in order to have ability close it EXPLICITLY, before PS will be freed. Otherwise access violation can raises when Python runs garbage collection.

--- tests/bugs/gh_8233_test.py
+++ tests/bugs/gh_8233_test.py
@@ -13,6 +13,13 @@ NOTES:
        Because of that, testing version are limited only for 5.0.2. FB 6.x currently is NOT tested.
     3. Table 't1' must have more than one row for bug reproducing. Query must be enclosed in execute block.
     4. Custom driver config objects are created here, one with SubQueryConversion = true and second with false.
+
+    [18.01.2025] pzotov
+    Resultset of cursor that executes using instance of selectable PreparedStatement must be stored
+    in some variable in order to have ability close it EXPLICITLY (before PS will be freed).
+    Otherwise access violation raises during Python GC and pytest hangs at final point (does not return control to OS).
+    This occurs at least for: Python 3.11.2 / pytest: 7.4.4 / firebird.driver: 1.10.6 / Firebird.Qa: 0.19.3
+    The reason of that was explained by Vlad, 26.10.24 17:42 ("oddities when use instances of selective statements").
     
     Checked on 5.0.2.1487-6934878 -- all ok.
     Thanks to dimitr for the advice on implementing the test.
@@ -71,6 +78,7 @@ def test_1(act: Action, capsys):
         """
 
         with connect(db_cfg_name, user = act.db.user, password = act.db.password) as con:
+            ps, rs =  None, None
             try:
                 cur = con.cursor()
                 cur.execute("select g.rdb$config_name, g.rdb$config_value from rdb$database r left join rdb$config g on g.rdb$config_name = 'SubQueryConversion'")
@@ -82,12 +90,23 @@ def test_1(act: Action, capsys):
                 # Print explained plan with padding eash line by dots in order to see indentations:
                 print( '\n'.join([replace_leading(s) for s in ps.detailed_plan.split('\n')]) )
 
-                # Print data:
-                for r in cur.execute(ps):
+                # ::: NB ::: 'ps' returns data, i.e. this is SELECTABLE expression.
+                # We have to store result of cur.execute(<psInstance>) in order to
+                # close it explicitly.
+                # Otherwise AV can occur during Python garbage collection and this
+                # causes pytest to hang on its final point.
+                # Explained by hvlad, email 26.10.24 17:42
+                rs = cur.execute(ps)
+                for r in rs:
                     print(r[0])
             except DatabaseError as e:
                 print(e.__str__())
                 print(e.gds_codes)
+            finally:
+                if rs:
+                    rs.close() # <<< EXPLICITLY CLOSING CURSOR RESULTS
+                if ps:
+                    ps.free()
 
     act.expected_stdout = f"""
         SubQueryConversion true
