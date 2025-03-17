4b9d42ca4309404ae3edf1676271c4f8cfcbb960
pavel-zotov <p519446@yandex.ru>
2025-03-18 00:40:47 +0300

    Added/Updated tests\bugs\gh_8437_test.py: Confirmed crash on 3.0.13.33798; checked on 3.0.13.33804 after fix #e02eaffb. Reduced min_version from 4.0.6 to 3.0.13.

--- tests/bugs/gh_8437_test.py
+++ tests/bugs/gh_8437_test.py
@@ -9,6 +9,10 @@ NOTES:
     [24.02.2025] pzotov
     Confirmed bug on 6.0.0.647-9fccb55; 5.0.3.1619-81c5f17; 4.0.6.3188-8ee1ca8
     Checked on 6.0.0.652-226622f; 5.0.3.1622-c1a518f; 4.0.6.3189-3fb0bbf - all fine.
+
+    [18.03.2025] pzotov
+    Confirmed crash on 3.0.13.33798; checked on 3.0.13.33804 after fix #e02eaffb.
+    Reduced min_version from 4.0.6 to 3.0.13.
 """
 
 import pytest
@@ -27,7 +31,7 @@ expected_stdout = """
     1
 """
 
-@pytest.mark.version('>=4.0.6')
+@pytest.mark.version('>=3.0.13')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
     act.execute(combine_output = True)
