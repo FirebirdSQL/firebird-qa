9575b90d82ba39bd7015df3419c23a43ca3bc683
pavel-zotov <p519446@yandex.ru>
2025-03-19 16:58:45 +0300

    Added/Updated tests\bugs\core_6438_test.py: changed SQL: names of any objects, including cursor columns, can not contain only spaces, see #8473

--- tests/bugs/core_6438_test.py
+++ tests/bugs/core_6438_test.py
@@ -2,11 +2,16 @@
 
 """
 ID:          issue-2366
-ISSUE:       2366
+ISSUE:       https://github.com/FirebirdSQL/firebird/issues/2366
 TITLE:       ISQL: bad headers when text columns has >= 80 characters
 DESCRIPTION:
 JIRA:        CORE-6438
 FBTEST:      bugs.core_6438
+NOTES:
+    [19.03.2025] pzotov
+    Fix https://github.com/FirebirdSQL/firebird/commit/37a42a6093077c9156a5853cfe69bba1ea92a468 (08-nov-2020)
+    Confirmed bug on 3.0.7.33388 (07-nov-2020), 4.0.0.2240 (04-nov-2020).
+    Checked on 4.0.0.2249 (09-nov-2020)
 """
 
 import pytest
@@ -16,25 +21,31 @@ db = db_factory()
 
 act = python_act('db')
 
-expected_stdout = """
-hdr_len: 65533
-txt_len: 65533
-"""
-
-@pytest.mark.version('>=4.0')
+PAD_TO_WIDTH = 65533
+@pytest.mark.version('>=3.0')
 def test_1(act: Action, capsys):
-    data = '1' * 65533
-    act.isql(switches=[], input=f'''select '{data}' as " ", 1 as "  " from rdb$database;''', combine_output = True)
+    data = '1' * PAD_TO_WIDTH
+    test_sql = f"""
+        set list off;
+        select '{data}' as col_1, 1 as col_2 from rdb$database;
+    """
+    act.isql(switches=[], input = test_sql, combine_output = True)
     hdr_len = txt_len = 0
     for line in act.stdout.splitlines():
         if line.startswith('='):
+            # before fix: hdr_len=79 instead of expected {PAD_TO_WIDTH}
             hdr_len = len(line.split()[0])
         elif line.startswith('1'):
             txt_len = len(line.split()[0])
+    act.reset()
+
     print('hdr_len:', hdr_len)
     print('txt_len:', txt_len)
-    #
-    act.reset()
+
+    expected_stdout = f"""
+        hdr_len: {PAD_TO_WIDTH}
+        txt_len: {PAD_TO_WIDTH}
+    """
     act.expected_stdout = expected_stdout
     act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
