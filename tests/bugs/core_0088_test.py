17e5277ddea1a3271b8b16c174e344f24da70b9c
pavel-zotov <p519446@yandex.ru>
2025-03-23 09:59:12 +0300

    Added/Updated tests\bugs\core_0088_test.py: Removed PLAN output because it differs on 6.x vs previous versions since commit fc12c0ef39. This test must check only returned data.

--- tests/bugs/core_0088_test.py
+++ tests/bugs/core_0088_test.py
@@ -2,79 +2,73 @@
 
 """
 ID:          issue-413
-ISSUE:       413
+ISSUE:       https://github.com/FirebirdSQL/firebird/issues/413
 TITLE:       Join on diffrent datatypes
 DESCRIPTION:
 JIRA:        CORE-88
 FBTEST:      bugs.core_0088
+NOTES:
+    [23.03.2025] pzotov
+    Removed PLAN output because it differs on 6.x vs previous versions since commit fc12c0ef39
+    ("Unnest IN/ANY/EXISTS subqueries and optimize them using semi-join algorithm (#8061)").
+    This test must check only returned data.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE TEST_A (
-    ID INTEGER NOT NULL PRIMARY KEY,
-    SNUM CHAR(10) UNIQUE
-);
+init_script = """
+    recreate table test1 (
+        id integer not null primary key,
+        snum char(10) unique using index test1_snum_unq
+    );
 
-CREATE TABLE TEST_B (
-    ID INTEGER NOT NULL PRIMARY KEY,
-    NUM NUMERIC(15,2) UNIQUE
-);
+    recreate table test2 (
+        id integer not null primary key,
+        inum numeric(15,2) unique using index test2_inum_unq
+    );
+    commit;
 
-commit;
+    insert into test1 (id, snum) values (1, '01');
+    insert into test1 (id, snum) values (2, '02');
+    insert into test1 (id, snum) values (3, '03');
+    insert into test1 (id, snum) values (5, '05');
+    commit;
 
-INSERT INTO TEST_A (ID, SNUM) VALUES (1, '01');
-INSERT INTO TEST_A (ID, SNUM) VALUES (2, '02');
-INSERT INTO TEST_A (ID, SNUM) VALUES (3, '03');
-INSERT INTO TEST_A (ID, SNUM) VALUES (5, '05');
-
-commit;
-
-INSERT INTO TEST_B (ID, NUM) VALUES (1, 1);
-INSERT INTO TEST_B (ID, NUM) VALUES (2, 2);
-INSERT INTO TEST_B (ID, NUM) VALUES (3, 3);
-INSERT INTO TEST_B (ID, NUM) VALUES (4, 4);
-
-commit;
+    insert into test2 (id, inum) values (1, 1);
+    insert into test2 (id, inum) values (2, 2);
+    insert into test2 (id, inum) values (3, 3);
+    insert into test2 (id, inum) values (4, 4);
+    commit;
 """
 
 db = db_factory(init=init_script)
 
-test_script = """SET PLAN ON;
-
-SELECT * FROM test_b WHERE num NOT IN (SELECT snum FROM test_a) ;
-
-SELECT * FROM test_b WHERE num IN (SELECT snum FROM test_a) ;
-
+test_script = """
+    set list on;
+    set count on;
+    select * from test2 where inum not in (select snum from test1) order by id;
+    select * from test2 where inum in (select snum from test1) order by id;
 """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-PLAN (TEST_A NATURAL)
-PLAN (TEST_A NATURAL)
-PLAN (TEST_B NATURAL)
-
-          ID                   NUM
-============ =====================
-           4                  4.00
-
-
-PLAN (TEST_A NATURAL)
-PLAN (TEST_B NATURAL)
-
-          ID                   NUM
-============ =====================
-           1                  1.00
-           2                  2.00
-           3                  3.00
-
-"""
-
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
+    expected_stdout = """
+        ID                              4
+        INUM                            4.00
+        Records affected: 1
 
+        ID                              1
+        INUM                            1.00
+        ID                              2
+        INUM                            2.00
+        ID                              3
+        INUM                            3.00
+        Records affected: 3
+    """
+
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
