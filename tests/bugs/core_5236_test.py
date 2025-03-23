d0d509a666008106e08164e77b0646aefc6c6405
pavel-zotov <p519446@yandex.ru>
2025-03-23 11:01:08 +0300

    Added/Updated tests\bugs\core_5236_test.py: Separated expected_out because plans differ on 6.x vs previous versions since commit fc12c0ef, see gh-8061. Checked on 6.0.0.687-730aa8f; 5.0.3.1633-25a0817

--- tests/bugs/core_5236_test.py
+++ tests/bugs/core_5236_test.py
@@ -29,6 +29,11 @@ NOTES:
     It was adjusted after talk with dimitr: must be the same for all FB versions
     (with only difference: 'Sub-query' at the top line for FB 5.x+).
     Changed filler character for show padding ('#' --> '.').
+
+    [23.03.2025] pzotov
+    Separated expected_out because plans differ on 6.x vs previous versions since commit fc12c0ef
+    ("Unnest IN/ANY/EXISTS subqueries and optimize them using semi-join algorithm (#8061)").
+    Checked on 6.0.0.687-730aa8f; 5.0.3.1633-25a0817 
 """
 
 import pytest
@@ -126,12 +131,26 @@ def test_1(act: Action, capsys):
         ....................-> Index "UNQ1_DP_RECIBO" Range Scan (partial match: 1/2)
     """
 
+    fb6x_expected_out = """
+        Select Expression
+        ....-> Nested Loop Join (semi)
+        ........-> Nested Loop Join (inner)
+        ............-> Table "DP_REGISTRO" Full Scan
+        ............-> Filter
+        ................-> Table "DP_RECIBO" Access By ID
+        ....................-> Bitmap
+        ........................-> Index "UNQ1_DP_RECIBO" Range Scan (partial match: 1/2)
+        ........-> Filter
+        ............-> Table "DP_REGISTRO_OEST" Access By ID
+        ................-> Bitmap
+    """
+
     with act.db.connect() as con:
         cur = con.cursor()
         ps = cur.prepare(test_sql)
         print( '\n'.join([replace_leading(s) for s in ps.detailed_plan.split('\n')]) )
 
-    act.expected_stdout = fb4x_expected_out if act.is_version('<5') else fb5x_expected_out
+    act.expected_stdout = fb4x_expected_out if act.is_version('<5') else fb5x_expected_out if act.is_version('<6') else fb6x_expected_out
     act.execute(combine_output = True)
     act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
