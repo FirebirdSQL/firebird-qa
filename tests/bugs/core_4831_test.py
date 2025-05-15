4de38396e113ee8183e84598304f4124f93bcc8d
pavel-zotov <p519446@yandex.ru>
2025-05-15 23:52:35 +0300

    Added/Updated tests\bugs\core_4831_test.py: Removed 'show grants', use query to rdb$ tables instead.

--- tests/bugs/core_4831_test.py
+++ tests/bugs/core_4831_test.py
@@ -14,29 +14,39 @@ from firebird.qa import *
 
 db = db_factory()
 
-role = role_factory('db', name='r_20150608_20h03m')
-
-test_script = """
-    set wng off;
-    -- create role r_20150608_20h03m;
-    -- commit;
-    revoke all on all from role r_20150608_20h03m; -- this was failed, possibly due to: http://sourceforge.net/p/firebird/code/61729
-    commit;
-    show grants;
-    -- commit;
-    -- drop role r_20150608_20h03m;
-    --commit;
-"""
-
-act = isql_act('db', test_script)
-
-expected_stderr = """
-There is no privilege granted in this database
-"""
+tmp_role = role_factory('db', name='r_20150608_20h03m')
+act = isql_act('db')
 
 @pytest.mark.version('>=3.0')
-def test_1(act: Action, role: Role):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stdout == act.clean_expected_stdout and
-            act.clean_stderr == act.clean_expected_stderr)
+def test_1(act: Action, tmp_role: Role):
+
+    test_sql = f"""
+        set list on;
+        set count on;
+        set wng off;
+
+        -- this failed, possibly by: http://sourceforge.net/p/firebird/code/61729
+        -- alexpeshkoff 2015-06-04
+        -- Postfix for CORE-4821: fixed segfault in REVOKE ALL ON ALL
+        revoke all on all from role {tmp_role.name};
+
+        commit;
+        select
+            g.rdb$privilege
+            ,g.rdb$grant_option
+            ,g.rdb$relation_name
+            ,g.rdb$user
+            ,g.rdb$object_type
+        from rdb$user_privileges g
+        join sec$users u on g.rdb$user = u.sec$user_name
+        where g.rdb$user = '{tmp_role.name.upper()}'
+        ;
+    """
+ 
+    act.expected_stdout = """
+        Records affected: 0
+    """
+
+    act.isql(input = test_sql, combine_output = True)
+
+    assert act.clean_stdout == act.clean_expected_stdout
