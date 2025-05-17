77274449bc9cec6e44b5fbb646ef46151e785efb
pavel-zotov <p519446@yandex.ru>
2025-05-17 10:35:56 +0300

    Added/Updated tests\functional\gtcs\test_minimum_grant_test.py: replaced hard-coded user names with values provided by user_factory.

--- tests/functional/gtcs/test_minimum_grant_test.py
+++ tests/functional/gtcs/test_minimum_grant_test.py
@@ -19,72 +19,51 @@ substitutions = [('no permission for (read/select|SELECT) access.*', 'no permiss
 
 db = db_factory()
 
-test_script = """
-    set list on;
+tmp_user1 = user_factory('db', name='tmp_gtcs_34_a', password='123')
+tmp_user2 = user_factory('db', name='tmp_gtcs_34_b', password='456')
 
-    set term ^;
-    execute block as
-    begin
-        begin
-            execute statement 'drop user tmp$qa_user1' with autonomous transaction;
-            when any do begin end
-        end
-
-        begin
-            execute statement 'drop user tmp$qa_user2' with autonomous transaction;
-            when any do begin end
-        end
-
-    end^
-    set term ;^
-    commit;
-
-    create user tmp$qa_user1 password '123';
-    create user tmp$qa_user2 password '456';
-    commit;
-
-    create table test (c1 int);
-    commit;
-
-    grant insert on test to tmp$qa_user1;
-    grant select on test to tmp$qa_user2;
-    commit;
-
-    -------------------------------------------------
-    connect '$(DSN)' user tmp$qa_user1 password '123';
-    select current_user as whoami from rdb$database;
-    insert into test values(1); -- should pass
-    select * from test;    -- should fail
-    commit;
-
-    -------------------------------------------------
-    connect '$(DSN)' user tmp$qa_user2 password '456';
-    select current_user as whoami from rdb$database;
-    insert into test values(2);    -- should fail
-    select * from test; -- should pass
-    commit;
-"""
-
-act = isql_act('db', test_script, substitutions=substitutions)
-
-expected_stdout = """
-    WHOAMI                          TMP$QA_USER1
-    WHOAMI                          TMP$QA_USER2
-    C1                              1
-"""
-
-expected_stderr = """
-    Statement failed, SQLSTATE = 28000
-    no permission for read/select access to TABLE TEST
-
-    Statement failed, SQLSTATE = 28000
-    no permission for insert/write access to TABLE TEST
-"""
+act = python_act('db', substitutions = substitutions)
 
 @pytest.mark.version('>=3')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+def test_1(act: Action, tmp_user1: User, tmp_user2: User):
+
+    test_sql = f"""
+        set list on;
+
+        create table test (f01 int);
+        commit;
+
+        grant insert on test to {tmp_user1.name};
+        grant select on test to {tmp_user2.name};
+        commit;
+
+        -------------------------------------------------
+        connect '{act.db.dsn}' user {tmp_user1.name} password '{tmp_user1.password}';
+        select current_user as whoami from rdb$database;
+        insert into test values(1); -- should pass
+        select * from test; -- should fail
+        commit;
+
+        -------------------------------------------------
+        connect '{act.db.dsn}' user {tmp_user2.name} password '{tmp_user2.password}';
+        select current_user as whoami from rdb$database;
+        insert into test values(2); -- should fail
+        select * from test; -- should pass
+        commit;
+    """
+
+    act.expected_stdout = f"""
+        WHOAMI {tmp_user1.name.upper()}
+        
+        Statement failed, SQLSTATE = 28000
+        no permission for read access
+        
+        WHOAMI {tmp_user2.name.upper()}
+        
+        Statement failed, SQLSTATE = 28000
+        no permission for write access
+        
+        F01 1
+    """
+    act.isql(input = test_sql, combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
