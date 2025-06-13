2bbf6a819e61b5b602766cfc4282ba6164f02bf8
pavel-zotov <p519446@yandex.ru>
2025-06-13 15:48:38 +0300

    Added/Updated tests\bugs\core_0058_test.py: Increased the 'subsitutions' list to suppress PUBLIC schema prefix and remove single/double quotes from object names. Need since 6.0.0.834. SEE NOTES.

--- tests/bugs/core_0058_test.py
+++ tests/bugs/core_0058_test.py
@@ -5,14 +5,26 @@ ID:          issue-383
 ISSUE:       383
 TITLE:       WHERE CURRENT OF doesn't work
 DESCRIPTION:
-JIRA:        CORE-58
-FBTEST:      bugs.core_0058
+NOTES:
+    [13.06.2025] pzotov
+    1. Increased the 'subsitutions' list to suppress "PUBLIC" schema prefix and remove single/double quotes from object names. Need since 6.0.0.834.
+       ::: NB :::
+       File act.files_dir/'test_config.ini' must contain section:
+           [schema_n_quotes_suppress]
+           addi_subst="PUBLIC". " '
+       (thi file is used in qa/plugin.py, see QA_GLOBALS dictionary).
+
+       Value of parameter 'addi_subst' is splitted on tokens using space character and we add every token to 'substitutions' list which
+       eventually will be like this:
+           substitutions = [ ( <previous tuple(s)>, ('"PUBLIC".', ''), ('"', ''), ("'", '') ]
+
+    2. Adjusted expected output: removed single quotes from DB object name(s).
+
+    Checked on 6.0.0.835; 5.0.3.1661; 4.0.6.3207; 3.0.13.33807.
 """
 
 import pytest
 from firebird.qa import *
-
-substitutions = [('line: [0-9]+, col: [0-9]+', '')]
 
 db = db_factory()
 
@@ -49,12 +61,22 @@ test_script = """
     execute procedure test_upd (2);
 """
 
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
+
+substitutions = [ ('line: \\d+, col: \\d++', '') ]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
+
 act = isql_act('db', test_script, substitutions=substitutions)
 
 expected_stderr = """
     Statement failed, SQLSTATE = 22000
     no current record for fetch operation
-    -At procedure 'TEST_UPD'
+    -At procedure TEST_UPD
 """
 
 @pytest.mark.version('>=3')
