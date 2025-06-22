c7d74fa8778d7a7695e66ef4475203306f7a5393
pavel-zotov <p519446@yandex.ru>
2025-06-22 18:22:48 +0300

    Added/Updated tests\bugs\core_0104_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_0104_test.py
+++ tests/bugs/core_0104_test.py
@@ -7,6 +7,13 @@ TITLE:       Dropping and recreating a table in the same txn disables PK
 DESCRIPTION:
 JIRA:        CORE-104
 FBTEST:      bugs.core_0104
+NOTES:
+    [22.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -31,7 +38,17 @@ insert into test values (1);
 commit;
 """
 
-act = isql_act('db', test_script)
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
+
+substitutions = [ ('line: \\d+, col: \\d++', ''), ('INTEG_\\d+', 'INTEG') ]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
+
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stderr = """Statement failed, SQLSTATE = 23000
 violation of PRIMARY or UNIQUE KEY constraint "INTEG_4" on table "TEST"
