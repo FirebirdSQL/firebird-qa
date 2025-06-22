8c5592b7e5074267a20e7e1b870e7d89a0390a9d
pavel-zotov <p519446@yandex.ru>
2025-06-22 18:28:02 +0300

    Added/Updated tests\bugs\core_0166_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_0166_test.py
+++ tests/bugs/core_0166_test.py
@@ -7,6 +7,13 @@ TITLE:       Cannot specify PLAN in UPDATE statement
 DESCRIPTION:
 JIRA:        CORE-166
 FBTEST:      bugs.core_0166
+NOTES:
+    [22.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -58,7 +65,17 @@ test_script = """
     from company c order by c.id;
 """
 
-act = isql_act('db', test_script)
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
+
+substitutions = [ ('line: \\d+, col: \\d++', '') ]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
+
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
     ID                              100
