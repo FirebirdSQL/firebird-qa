f617eb5dc0023437b03ba61ccc51ecf43a20ae65
pavel-zotov <p519446@yandex.ru>
2025-06-22 19:33:50 +0300

    Added/Updated tests\bugs\core_0480_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_0480_test.py
+++ tests/bugs/core_0480_test.py
@@ -7,32 +7,48 @@ TITLE:       Foreign key relation VARCHAR <-> INT
 DESCRIPTION:
 JIRA:        CORE-480
 FBTEST:      bugs.core_0480
+NOTES:
+    [22.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """create table T1 (PK1 INTEGER, COL VARCHAR(10));
-commit;"""
+db = db_factory()
 
-db = db_factory(init=init_script)
-
-test_script = """create table T2 (PK2 INTEGER, FK1 VARCHAR(10), COL VARCHAR(10),
-foreign key (FK1) references T1 (PK1));
-
+test_script = """
+    create table t1 (pk1 integer, col varchar(10));
+    create table t2 (pk2 integer, fk1 varchar(10), col varchar(10),
+    foreign key (fk1) references t1 (pk1));
 """
 
-act = isql_act('db', test_script)
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
 
-expected_stderr = """Statement failed, SQLSTATE = 42000
-unsuccessful metadata update
--CREATE TABLE T2 failed
--could not find UNIQUE or PRIMARY KEY constraint in table T1 with specified columns
+substitutions = [('[ \t]+', ' ')]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
+
+act = isql_act('db', test_script, substitutions = substitutions)
+
+expected_stdout = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -CREATE TABLE T2 failed
+    -could not find UNIQUE or PRIMARY KEY constraint in table T1 with specified columns
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
 
