27ffb3382e143eb210a144295328350a578b26b4
pavel-zotov <p519446@yandex.ru>
2025-06-23 16:12:00 +0300

    Added/Updated tests\bugs\core_0879_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_0879_test.py
+++ tests/bugs/core_0879_test.py
@@ -7,6 +7,13 @@ TITLE:       Dependencies are not cleared when creation of expression index fail
 DESCRIPTION:
 JIRA:        CORE-879
 FBTEST:      bugs.core_0879
+NOTES:
+    [23.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -23,9 +30,19 @@ test_script = """
     show table tab;
 """
 
-act = isql_act('db', test_script)
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
 
-expected_stderr = """
+substitutions = [('[ \t]+', ' '), ('Table: .*', '')]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
+
+act = isql_act('db', test_script, substitutions = substitutions)
+
+expected_stdout = """
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -key size exceeds implementation restriction for index "IX"
@@ -34,7 +51,7 @@ expected_stderr = """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
 
