0991def2aba748411e03be756eee152f7998dd57
pavel-zotov <p519446@yandex.ru>
2025-06-23 15:46:46 +0300

    Added/Updated tests\bugs\core_0851_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_0851_test.py
+++ tests/bugs/core_0851_test.py
@@ -7,31 +7,47 @@ TITLE:       Field can be used multiple times in multi-segment index definition
 DESCRIPTION:
 JIRA:        CORE-851
 FBTEST:      bugs.core_0851
+NOTES:
+    [23.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """create table t (i integer);
-commit;
+db = db_factory()
+
+test_script = """
+    create table t (f01 int);
+    create index ti on t(f01,f01);
 """
 
-db = db_factory(init=init_script)
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
 
-test_script = """create index ti on t(i,i);
-"""
+substitutions = [('[ \t]+', ' ')]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
 
-act = isql_act('db', test_script)
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stderr = """Statement failed, SQLSTATE = 42000
-unsuccessful metadata update
--CREATE INDEX TI failed
--Field I cannot be used twice in index TI
+expected_stdout = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -CREATE INDEX TI failed
+    -Field F01 cannot be used twice in index TI
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
 
