09edc51b7fc388d386547d138b8fd67b3ef573e4
pavel-zotov <p519446@yandex.ru>
2025-06-23 17:56:10 +0300

    Added/Updated tests\bugs\core_0899_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_0899_test.py
+++ tests/bugs/core_0899_test.py
@@ -2,139 +2,147 @@
 
 """
 ID:          issue-1296
-ISSUE:       1296
+ISSUE:       https://github.com/FirebirdSQL/firebird/issues/1296
 TITLE:       Problems with explicit cursors in unwanted states
 DESCRIPTION:
 JIRA:        CORE-899
 FBTEST:      bugs.core_0899
+NOTES:
+    [23.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """create table T (ID integer, TXT varchar(30));
-commit;
+db = db_factory()
 
-insert into T values (1,'Text description');
-commit;
+test_script = """
+    create table T (ID integer, TXT varchar(30));
+    commit;
 
-set term ^;
+    insert into T values (1,'Text description');
+    commit;
 
-create procedure SP_OK returns (ID integer, TXT varchar(30))
-as
-  declare C cursor for ( select ID, TXT from T );
-begin
-  open C;
-  while (1 = 1) do
-  begin
-    fetch C into :ID, :TXT;
-    if (ROW_COUNT = 0) then
-      leave;
-    update T set TXT = 'OK' where current of C;
-    suspend;
-  end
-  close C;
-end ^
+    set term ^;
 
-create procedure SP_CLOSED returns (ID integer, TXT varchar(30))
-as
-  declare C cursor for ( select ID, TXT from T );
-begin
-  open C;
-  while (1 = 1) do
-  begin
-    fetch C into :ID, :TXT;
-    if (ROW_COUNT = 0) then
-      leave;
-    suspend;
-  end
-  close C;
-  update T set TXT = 'SP_CLOSED' where current of C;
-end ^
+    create procedure SP_OK returns (ID integer, TXT varchar(30))
+    as
+      declare C cursor for ( select ID, TXT from T );
+    begin
+      open C;
+      while (1 = 1) do
+      begin
+        fetch C into :ID, :TXT;
+        if (ROW_COUNT = 0) then
+          leave;
+        update T set TXT = 'OK' where current of C;
+        suspend;
+      end
+      close C;
+    end ^
 
-create procedure SP_NOTOPEN returns (ID integer, TXT varchar(30))
-as
-  declare C cursor for ( select ID, TXT from T );
-begin
-  update T set TXT = 'SP_NOTOPEN' where current of C;
-  open C;
-  while (1 = 1) do
-  begin
-    fetch C into :ID, :TXT;
-    if (ROW_COUNT = 0) then
-      leave;
-    suspend;
-  end
-  close C;
-end ^
+    create procedure SP_CLOSED returns (ID integer, TXT varchar(30))
+    as
+      declare C cursor for ( select ID, TXT from T );
+    begin
+      open C;
+      while (1 = 1) do
+      begin
+        fetch C into :ID, :TXT;
+        if (ROW_COUNT = 0) then
+          leave;
+        suspend;
+      end
+      close C;
+      update T set TXT = 'SP_CLOSED' where current of C;
+    end ^
 
-create procedure SP_FETCHED returns (ID integer, TXT varchar(30))
-as
-  declare C cursor for ( select ID, TXT from T );
-begin
-  open C;
-  while (1 = 1) do
-  begin
-    fetch C into :ID, :TXT;
-    if (ROW_COUNT = 0) then
-      leave;
-    suspend;
-  end
-  update T set TXT = 'SP_FETCHED' where current of C;
-  close C;
-end ^
+    create procedure SP_NOTOPEN returns (ID integer, TXT varchar(30))
+    as
+      declare C cursor for ( select ID, TXT from T );
+    begin
+      update T set TXT = 'SP_NOTOPEN' where current of C;
+      open C;
+      while (1 = 1) do
+      begin
+        fetch C into :ID, :TXT;
+        if (ROW_COUNT = 0) then
+          leave;
+        suspend;
+      end
+      close C;
+    end ^
 
-set term ; ^
+    create procedure SP_FETCHED returns (ID integer, TXT varchar(30))
+    as
+      declare C cursor for ( select ID, TXT from T );
+    begin
+      open C;
+      while (1 = 1) do
+      begin
+        fetch C into :ID, :TXT;
+        if (ROW_COUNT = 0) then
+          leave;
+        suspend;
+      end
+      update T set TXT = 'SP_FETCHED' where current of C;
+      close C;
+    end ^
 
-commit;
+    set term ; ^
+
+    commit;
+
+    set list on;
+    select * from SP_OK;
+    select * from SP_CLOSED;
+    select * from SP_NOTOPEN;
+    select * from SP_FETCHED;
 """
 
-db = db_factory(init=init_script)
+substitutions=[('[ \t]+', ' '), ('line:\\s+\\d+,', 'line: x'), ('col:\\s+\\d+', 'col: y')]
 
-test_script = """select * from SP_OK;
-select * from SP_CLOSED;
-select * from SP_NOTOPEN;
-select * from SP_FETCHED;
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
 
-"""
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
 
-act = isql_act('db', test_script,
-               substitutions=[('line:\\s[0-9]+,', 'line: x'), ('col:\\s[0-9]+', 'col: y')])
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-          ID TXT
-============ ==============================
-           1 Text description
+    ID 1
+    TXT Text description
 
+    ID 1
+    TXT OK
 
-          ID TXT
-============ ==============================
-           1 OK
+    Statement failed, SQLSTATE = 22000
+    no current record for fetch operation
+    -At procedure SP_CLOSED line: x col: y
 
-          ID TXT
-============ ==============================
+    Statement failed, SQLSTATE = 22000
+    no current record for fetch operation
+    -At procedure SP_NOTOPEN line: x col: y
 
-          ID TXT
-============ ==============================
-           1 OK
-"""
+    ID 1
+    TXT OK
 
-expected_stderr = """Statement failed, SQLSTATE = 22000
-no current record for fetch operation
--At procedure 'SP_CLOSED' line: 14, col: 3
-Statement failed, SQLSTATE = 22000
-no current record for fetch operation
--At procedure 'SP_NOTOPEN' line: 5, col: 3
-Statement failed, SQLSTATE = 22000
-no current record for fetch operation
--At procedure 'SP_FETCHED' line: 13, col: 3
+    Statement failed, SQLSTATE = 22000
+    no current record for fetch operation
+    -At procedure SP_FETCHED line: x col: y
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
-
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
