e1c1b77bca612e5df3243bd2d7b2b698ed74b088
pavel-zotov <p519446@yandex.ru>
2025-06-23 21:00:10 +0300

    Added/Updated tests\bugs\core_0945_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_0945_test.py
+++ tests/bugs/core_0945_test.py
@@ -7,6 +7,13 @@ TITLE:       Bad error message when tring to create FK to non-existent table
 DESCRIPTION:
 JIRA:        CORE-945
 FBTEST:      bugs.core_0945
+NOTES:
+    [23.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -14,28 +21,38 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """CREATE TABLE TAB_TestA (
-  UID INTEGER NOT NULL PRIMARY KEY
-);
-
-CREATE TABLE TAB_TestB (
-  UID INTEGER NOT NULL PRIMARY KEY,
-  TestA INTEGER CONSTRAINT FK_TestA REFERENCES TABTestA(UID) ON UPDATE CASCADE
-);
-
+test_script = """
+    create table tmain (
+        uid int not null primary key
+    );
+    create table tdetl (
+        uid int not null primary key,
+        pid integer constraint fk_tdetl references non_existing_0945(uid) on update cascade
+    );
 """
 
-act = isql_act('db', test_script)
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
 
-expected_stderr = """Statement failed, SQLSTATE = 42000
-unsuccessful metadata update
--CREATE TABLE TAB_TESTB failed
--Table TABTESTA not found
+substitutions = [('[ \t]+', ' ')]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
+
+act = isql_act('db', test_script, substitutions = substitutions)
+
+expected_stdout = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -CREATE TABLE TDETL failed
+    -Table NON_EXISTING_0945 not found
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
 
