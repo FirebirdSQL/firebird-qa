2c04ed99ffb4ce3dfb5b923420a79f534a4b79ff
pavel-zotov <p519446@yandex.ru>
2025-06-23 15:25:04 +0300

    Added/Updated tests\bugs\core_0847_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_0847_test.py
+++ tests/bugs/core_0847_test.py
@@ -7,76 +7,83 @@ TITLE:       Computed field can't be changed to non-computed using 'alter table 
 DESCRIPTION:
 JIRA:        CORE-847
 FBTEST:      bugs.core_0847
+NOTES:
+    [23.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """
-    recreate table t (
-      f1 varchar(10),
-      f2 varchar(10),
-      cf computed by (f1 || ' - ' || f2)
-    );
-
-    insert into t (f1,f2) values ('0123456789','abcdefghij');
-    commit;
-"""
-
-db = db_factory(init=init_script)
-
 test_script = """
     set blob off;
     set list on;
 
-    select f1,f2,cf as cf_before_altering from t;
+    recreate table t (
+      f1 smallint,
+      f2 smallint,
+      sum_f1_f2 computed by (f1+f2)
+    );
+
+    insert into t (f1,f2) values (1,2);
+    commit;
+
+    select f1,f2,sum_f1_f2 as cf_before_altering from t;
 
     select b.rdb$field_name field_name, cast(a.rdb$computed_source as varchar(80)) computed_source_before_altering
     from rdb$fields a
     join rdb$relation_fields b  on a.rdb$field_name = b.rdb$field_source
-    where b.rdb$field_name = upper('CF');
+    where b.rdb$field_name = upper('SUM_F1_F2');
 
-    alter table t alter cf type varchar(30);
+    alter table t alter sum_f1_f2 type bigint;
     commit;
 
-    select f1,f2,cf as cf_after_altering from t;
+    select f1,f2,sum_f1_f2 as cf_after_altering from t;
 
     select b.rdb$field_name field_name, cast(a.rdb$computed_source as varchar(80)) computed_source_after_altering
     from rdb$fields a
     join rdb$relation_fields b  on a.rdb$field_name = b.rdb$field_source
-    where b.rdb$field_name = upper('CF');
+    where b.rdb$field_name = upper('SUM_F1_F2');
 """
 
-act = isql_act('db', test_script)
+db = db_factory()
+
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
+
+substitutions = [('[ \t]+', ' ')]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
+
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-    F1                              0123456789
-    F2                              abcdefghij
-    CF_BEFORE_ALTERING              0123456789 - abcdefghij
-
-    FIELD_NAME                      CF
-    COMPUTED_SOURCE_BEFORE_ALTERING (f1 || ' - ' || f2)
-
-    F1                              0123456789
-    F2                              abcdefghij
-    CF_AFTER_ALTERING               0123456789 - abcdefghij
-
-    FIELD_NAME                      CF
-    COMPUTED_SOURCE_AFTER_ALTERING  (f1 || ' - ' || f2)
-"""
-
-expected_stderr = """
+    F1 1
+    F2 2
+    CF_BEFORE_ALTERING 3
+    FIELD_NAME SUM_F1_F2
+    COMPUTED_SOURCE_BEFORE_ALTERING (f1+f2)
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -ALTER TABLE T failed
-    -Cannot add or remove COMPUTED from column CF
+    -Cannot add or remove COMPUTED from column SUM_F1_F2
+    F1 1
+    F2 2
+    CF_AFTER_ALTERING 3
+    FIELD_NAME SUM_F1_F2
+    COMPUTED_SOURCE_AFTER_ALTERING (f1+f2)
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
 
