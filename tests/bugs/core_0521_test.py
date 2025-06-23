0e9ee70d049be846067c6da91257db070bed293b
pavel-zotov <p519446@yandex.ru>
2025-06-23 12:34:40 +0300

    Added/Updated tests\bugs\core_0521_test.py: Expected output was separated depending on FB version: we have to show SCHEMA name as prefix for DB object (since 6.0.0.834). Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_0521_test.py
+++ tests/bugs/core_0521_test.py
@@ -7,6 +7,9 @@ TITLE:       Permissions are checked case-insensitively
 DESCRIPTION:
 JIRA:        CORE-521
 FBTEST:      bugs.core_0521
+    [23.06.2025] pzotov
+    Expected output was separated depending on FB version: we have to show SCHEMA name as prefix for DB object (since 6.0.0.834).
+    Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -51,22 +54,25 @@ test_script = """
     commit;
 """
 
-act = isql_act('db', test_script, substitutions=[('execute', 'EXECUTE'), ('-Effective user is.*', '')])
+substitutions = [('[ \t]+', ' '), ('execute', 'EXECUTE'), ('-Effective user is.*', '')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-    WHOAMI                          TMP$C0521
-"""
-
-expected_stderr = """
+expected_stdout_5x = """
+    WHOAMI TMP$C0521
     Statement failed, SQLSTATE = 28000
     no permission for EXECUTE access to PROCEDURE PeRm
 """
 
+expected_stdout_6x = """
+    WHOAMI TMP$C0521
+    Statement failed, SQLSTATE = 28000
+    no permission for EXECUTE access to PROCEDURE "PUBLIC"."PeRm"
+"""
+
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
+
 
