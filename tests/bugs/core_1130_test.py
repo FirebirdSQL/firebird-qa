d6b2ff1711676901552188538e87facccdfbc128
pavel-zotov <p519446@yandex.ru>
2025-06-24 12:42:07 +0300

    Added/Updated tests\bugs\core_1130_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813. For this test 'schema:' in SQLDA output is suppressed because as not relevant to check. Checked on 6.0.0.858; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_1130_test.py
+++ tests/bugs/core_1130_test.py
@@ -12,42 +12,44 @@ FBTEST:      bugs.core_1130
 import pytest
 from firebird.qa import *
 
-init_script = """SET TERM ^;
-create procedure p
-  returns (r int)
-as
-begin
-  r = 1;
-  suspend;
-end
-^
-SET TERM ;^
-COMMIT;
+db = db_factory()
+
+test_script = """
+    set term ^;
+    create procedure sp_test returns (r int) as
+    begin
+        r = 1;
+        suspend;
+    end
+    ^
+    set term ;^
+    commit;
+
+    set planonly;
+    select p.*
+    from sp_test p
+    left join (select rdb$relation_id from rdb$relations ) r on p.r = r.rdb$relation_id;
 """
 
-db = db_factory(init=init_script)
+substitutions = [('RDB\\$INDEX_\\d+', 'RDB\\$INDEX')]
 
-test_script = """SET PLAN ON;
-select *
-from p
-  left join ( select rdb$relation_id from rdb$relations ) r
-    on p.r = r.rdb$relation_id;
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
 
-"""
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
 
-act = isql_act('db', test_script)
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """PLAN JOIN (P NATURAL, R RDB$RELATIONS INDEX (RDB$INDEX_1))
-
-           R RDB$RELATION_ID
-============ ===============
-           1               1
-
+expected_stdout = """
+    PLAN JOIN (P NATURAL, R RDB$RELATIONS INDEX (RDB$INDEX_1))
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
