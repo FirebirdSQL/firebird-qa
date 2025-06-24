ac475fc74a17bf949c72b274b4549a979a9b509d
pavel-zotov <p519446@yandex.ru>
2025-06-24 10:21:50 +0300

    Added/Updated tests\bugs\core_1056_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.853; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_1056_test.py
+++ tests/bugs/core_1056_test.py
@@ -7,50 +7,57 @@ TITLE:       A query could produce different results, depending on the presence 
 DESCRIPTION:
 JIRA:        CORE-1056
 FBTEST:      bugs.core_1056
+    [24.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.858; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """create table t (c varchar(10) character set win1250 collate pxw_csy);
-insert into t values ('ch');
-commit;
+db = db_factory()
+
+test_script = """
+
+    set list on;
+    create table t (c varchar(10) character set win1250 collate pxw_csy);
+    insert into t values ('ch');
+    commit;
+
+    set plan on;
+    select * from t where c starting with 'c';
+    commit;
+
+    create index t_c on t (c);
+    commit;
+
+    select * from t where c starting with 'c';
 """
 
-db = db_factory(init=init_script)
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
 
-test_script = """set plan on;
+substitutions = [('[ \t]+', ' ')]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
 
-select * from t where c starting with 'c';
-commit;
-
-create index t_c on t (c);
-commit;
-
-select * from t where c starting with 'c';
-"""
-
-act = isql_act('db', test_script)
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-PLAN (T NATURAL)
-
-C
-==========
-ch
-
-
-PLAN (T INDEX (T_C))
-
-C
-==========
-ch
-
+    PLAN (T NATURAL)
+    C ch
+    PLAN (T INDEX (T_C))
+    C ch
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
