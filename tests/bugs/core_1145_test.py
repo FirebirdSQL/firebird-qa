ce30152647128bf46358be754e83a6b15bf4339e
pavel-zotov <p519446@yandex.ru>
2025-06-24 16:26:58 +0300

    Added/Updated tests\bugs\core_1145_test.py: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output. Checked on 6.0.0.858; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_1145_test.py
+++ tests/bugs/core_1145_test.py
@@ -7,77 +7,83 @@ TITLE:       Server locks up while attempting to commit a deletion of an express
 DESCRIPTION:
 JIRA:        CORE-1145
 FBTEST:      bugs.core_1145
+NOTES:
+    [24.06.2025] pzotov
+    ::: NB :::
+    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
+
+    Checked on 6.0.0.858; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """create table expt1 (col1 int);
-create table expt2 (col2 int);
-commit;
+db = db_factory()
 
-insert into expt1 values (1);
-insert into expt1 values (2);
+test_script = """
+    set list on;
+    create table expt1 (col1 int);
+    create table expt2 (col2 int);
+    commit;
 
-insert into expt2 values (1);
-insert into expt2 values (2);
-commit;
+    insert into expt1 values (1);
+    insert into expt1 values (2);
 
-create index iexpt1 on expt1 computed (col1 + 1);
-create index iexpt2 on expt2 computed (col2 + 1);
-commit;
+    insert into expt2 values (1);
+    insert into expt2 values (2);
+    commit;
+
+    create index iexpt1 on expt1 computed (col1 + 1);
+    create index iexpt2 on expt2 computed (col2 + 1);
+    commit;
+
+    set plan on;
+    select 'point-1' msg, e.* from expt1 e where col1 + 1 = 2;
+    select 'point-2' msg, e.* from expt2 e where col2 + 1 = 2;
+    commit;
+
+    drop index iexpt2;
+    commit; -- lockup
+
+    select 'point-3' msg, e.* from expt1 e where col1 + 1 = 2;
+    select 'point-4' msg, e.* from expt2 e where col2 + 1 = 2;
+    commit;
 """
 
-db = db_factory(init=init_script)
 
-test_script = """set plan on;
-select * from expt1 where col1 + 1 = 2;
-select * from expt2 where col2 + 1 = 2;
-commit;
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
 
-drop index iexpt2;
-commit; -- lockup
+substitutions = [('[ \t]+', ' ')]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
 
-select * from expt1 where col1 + 1 = 2;
-select * from expt2 where col2 + 1 = 2;
-commit;
-"""
-
-act = isql_act('db', test_script)
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-PLAN (EXPT1 INDEX (IEXPT1))
+    PLAN (E INDEX (IEXPT1))
+    MSG point-1
+    COL1 1
 
-        COL1
-============
-           1
+    PLAN (E INDEX (IEXPT2))
+    MSG point-2
+    COL2 1
 
+    PLAN (E INDEX (IEXPT1))
+    MSG point-3
+    COL1 1
 
-PLAN (EXPT2 INDEX (IEXPT2))
-
-        COL2
-============
-           1
-
-
-PLAN (EXPT1 INDEX (IEXPT1))
-
-        COL1
-============
-           1
-
-
-PLAN (EXPT2 NATURAL)
-
-        COL2
-============
-           1
-
+    PLAN (E NATURAL)
+    MSG point-4
+    COL2 1
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
