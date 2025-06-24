4a87033edaa89d10fa19db80f63635363528398c
pavel-zotov <p519446@yandex.ru>
2025-06-24 23:39:59 +0300

    Added/Updated tests\bugs\core_1306_test.py: Separated execution plans for FB major versions prior/since 6.x. No substitutions are used to suppress schema name and quotes to enclosing object names. Checked on 6.0.0.858; 3.0.13.33813.

--- tests/bugs/core_1306_test.py
+++ tests/bugs/core_1306_test.py
@@ -12,44 +12,42 @@ FBTEST:      bugs.core_1306
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE "TABLE" (ID INTEGER NOT NULL PRIMARY KEY);
+db = db_factory()
 
-COMMIT;
-
-INSERT INTO "TABLE" (ID) VALUES (1);
-INSERT INTO "TABLE" (ID) VALUES (2);
-INSERT INTO "TABLE" (ID) VALUES (3);
-
-COMMIT;
-
-CREATE VIEW "VIEW" AS SELECT * FROM "TABLE";
-
-commit;"""
-
-db = db_factory(init=init_script)
-
-test_script = """set plan on;
-
-SELECT * FROM "TABLE" WHERE ID = 1
-UNION ALL
-SELECT * FROM "VIEW" WHERE ID = 1 ;
+test_script = """
+    set list on;
+    create table "TABLE" (id integer not null primary key);
+    commit;
+    insert into "TABLE" (id) values (1);
+    insert into "TABLE" (id) values (2);
+    insert into "TABLE" (id) values (3);
+    commit;
+    create view "VIEW" as select * from "TABLE";
+    commit;
+    set plan on;
+    select * from "TABLE" where id = 1
+    union all
+    select * from "VIEW" where id = 1 ;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-PLAN (TABLE INDEX (RDB$PRIMARY1), VIEW TABLE INDEX (RDB$PRIMARY1))
+expected_stdout_5x = """
+    PLAN (TABLE INDEX (RDB$PRIMARY1), VIEW TABLE INDEX (RDB$PRIMARY1))
+    ID 1
+    ID 1
+"""
 
-          ID
-============
-           1
-           1
-
+expected_stdout_6x = """
+    PLAN ("PUBLIC"."TABLE" INDEX ("PUBLIC"."RDB$PRIMARY1"), "PUBLIC"."VIEW" "PUBLIC"."TABLE" INDEX ("PUBLIC"."RDB$PRIMARY1"))
+    ID 1
+    ID 1
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
