53d921c3858424448ab992906c1ed460ad891126
pavel-zotov <p519446@yandex.ru>
2025-06-24 23:01:35 +0300

    Added/Updated tests\bugs\core_1173_test.py: Separated execution plans for FB major versions prior/since 6.x. No substitutions are used to suppress schema name and quotes to enclosing object names. Checked on 6.0.0.858; 3.0.13.33813.

--- tests/bugs/core_1173_test.py
+++ tests/bugs/core_1173_test.py
@@ -7,6 +7,13 @@ TITLE:       Expression index based on computed fields
 DESCRIPTION: Index based on COMPUTED-BY column must be taken in account by optimizer.
 JIRA:        CORE-1173
 FBTEST:      bugs.core_1173
+NOTES:
+    [24.06.2025] pzotov
+    Separated execution plans for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema name and quotes to enclosing object names.
+    Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.858; 3.0.13.33813.
 """
 
 import pytest
@@ -77,16 +84,23 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
+expected_stdout_5x = """
     PLAN (TEST INDEX (TEST_ON_COMPUTED_FIELD_ASC))
     PLAN (TEST INDEX (TEST_FDATE_FTIME_ASC))
     PLAN (TEST INDEX (TEST_ON_COMPUTED_FIELD_DEC))
     PLAN (TEST INDEX (TEST_FDATE_FTIME_DEC))
 """
 
+expected_stdout_6x = """
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_ON_COMPUTED_FIELD_ASC"))
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_FDATE_FTIME_ASC"))
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_ON_COMPUTED_FIELD_DEC"))
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_FDATE_FTIME_DEC"))
+"""
+
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
