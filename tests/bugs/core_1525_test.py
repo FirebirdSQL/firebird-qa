3a60c7dcf42b1460a90f5afce28a0316c13925c1
pavel-zotov <p519446@yandex.ru>
2025-06-25 18:58:24 +0300

    Added/Updated tests\bugs\core_1525_test.py: Separated expected PLAN for FB major versions prior/since 6.x. Checked on 6.0.0.863; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_1525_test.py
+++ tests/bugs/core_1525_test.py
@@ -7,6 +7,12 @@ TITLE:       Computed field + index not working in WHERE
 DESCRIPTION:
 JIRA:        CORE-1525
 FBTEST:      bugs.core_1525
+NOTES:
+    [25.06.2025] pzotov
+    Separated expected PLAN for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.863; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -41,29 +47,34 @@ test_script = """
     where cast ('2007-09-09' as date) < comp_last_day;
 """
 
-act = isql_act('db', test_script)
-
-expected_stdout = """
-    ID                              1
-    LAST_DAY                        2007-10-10
-    COMP_LAST_DAY                   2007-10-10
-    ID                              2
-    LAST_DAY                        <null>
-    COMP_LAST_DAY                   2999-12-31
-
-    PLAN (TEST_1 INDEX (IDX_1))
-
-    ID                              1
-    LAST_DAY                        2007-10-10
-    COMP_LAST_DAY                   2007-10-10
-    ID                              2
-    LAST_DAY                        <null>
-    COMP_LAST_DAY                   2999-12-31
-"""
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 @pytest.mark.version('>=2.0.7')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
 
+    # 25.06.2025 Separate PLAN depending on major FB version:
+    ##########################
+    expected_plan = 'PLAN (TEST_1 INDEX (IDX_1))' if act.is_version('<6') else 'PLAN ("PUBLIC"."TEST_1" INDEX ("PUBLIC"."IDX_1"))'
+
+    expected_stdout = f"""
+        ID                              1
+        LAST_DAY                        2007-10-10
+        COMP_LAST_DAY                   2007-10-10
+        ID                              2
+        LAST_DAY                        <null>
+        COMP_LAST_DAY                   2999-12-31
+
+        {expected_plan}
+
+        ID                              1
+        LAST_DAY                        2007-10-10
+        COMP_LAST_DAY                   2007-10-10
+        ID                              2
+        LAST_DAY                        <null>
+        COMP_LAST_DAY                   2999-12-31
+    """
+
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
