ec9ee39c28b659122b5986f20f9ec52784f2fc1e
pavel-zotov <p519446@yandex.ru>
2025-06-25 11:44:38 +0300

    Added/Updated tests\bugs\core_1271_test.py: Minimal snapshot number for 6.x: 6.0.0.863. See notes.

--- tests/bugs/core_1271_test.py
+++ tests/bugs/core_1271_test.py
@@ -7,6 +7,15 @@ TITLE:       Ceation of invalid procedures/triggers allowed
 DESCRIPTION:
 JIRA:        CORE-1271
 FBTEST:      bugs.core_1271
+NOTES:
+    [25.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Minimal snapshot number for 6.x: 6.0.0.863, see letter from Adriano, 24.06.2025 23:24, commit:
+    https://github.com/FirebirdSQL/firebird/commit/79ff650e5af7a0d6141e166b0cb8208ef211f0a7
+
+    Checked on 6.0.0.863; 6.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -15,33 +24,43 @@ from firebird.qa import *
 db = db_factory()
 
 test_script = """
+    create table test(id int primary key using index test_id, f01 timestamp);
+    create index test_f01 on test(f01);
+    commit;
     set term ^;
-    create procedure p returns (out int) as
+    create procedure sp_test (a_id int) returns (o_f01 type of column test.f01) as
     begin
-        for
-            select rdb$relation_id
-            from rdb$relations
-            plan (rdb$relations order rdb$index_1)
-            order by rdb$description
-            into :out
+        for 
+            select f01 from test where id = :a_id plan (test order test_f01) 
+            into o_f01
         do
             suspend;
     end
     ^
-    commit^
+    commit
+    ^
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 2F000
-    Error while parsing procedure P's BLR
-    -index RDB$INDEX_1 cannot be used in the specified plan
+    Error while parsing procedure SP_TEST's BLR
+    -index TEST_F01 cannot be used in the specified plan
 """
+
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 2F000
+    Error while parsing procedure "PUBLIC"."SP_TEST"'s BLR
+    -index "PUBLIC"."TEST_F01" cannot be used in the specified plan
+"""
+
+expected_stdout = ''
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
 
