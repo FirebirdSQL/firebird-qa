7d4db957864bb9ae1e1f4f461649b5cdf493aec9
pavel-zotov <p519446@yandex.ru>
2025-06-25 20:16:49 +0300

    Added/Updated tests\bugs\core_1559_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.863; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_1559_test.py
+++ tests/bugs/core_1559_test.py
@@ -7,6 +7,12 @@ TITLE:       Dropping NOT NULL contranint doesn'have the desired effect
 DESCRIPTION:
 JIRA:        CORE-1559
 FBTEST:      bugs.core_1559
+NOTES:
+    [25.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.863; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -14,35 +20,33 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """create table t (n integer constraint c not null);
-COMMIT;
-insert into t values (null);
-COMMIT;
-alter table t drop constraint c;
-COMMIT;
-insert into t values (null);
-COMMIT;
-SELECT * FROM t;
+test_script = """
+    set list on;
+    create table test (n integer constraint explicit_check_for_nn not null);
+    insert into test values (null);
+    commit;
+    alter table test drop constraint explicit_check_for_nn;
+    insert into test values (null);
+    select * from test;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-           N
-============
-      <null>
-
+expected_stdout_5x = """
+    Statement failed, SQLSTATE = 23000
+    validation error for column "TEST"."N", value "*** null ***"
+    N <null>
 """
 
-expected_stderr = """Statement failed, SQLSTATE = 23000
-validation error for column "T"."N", value "*** null ***"
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 23000
+    validation error for column "PUBLIC"."TEST"."N", value "*** null ***"
+    N <null>
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
