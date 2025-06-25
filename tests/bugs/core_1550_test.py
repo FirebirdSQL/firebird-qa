642a8ae7f3bc0e09979f68293e59d5b930b6d17f
pavel-zotov <p519446@yandex.ru>
2025-06-25 20:11:04 +0300

    Added/Updated tests\bugs\core_1550_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.863; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_1550_test.py
+++ tests/bugs/core_1550_test.py
@@ -7,6 +7,12 @@ TITLE:       Unnecessary index scan happens when the same index is mapped to bot
 DESCRIPTION:
 JIRA:        CORE-1550
 FBTEST:      bugs.core_1550
+NOTES:
+    [25.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.863; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -31,13 +37,16 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
+expected_stdout_5x = """
     PLAN (TEST ORDER TEST_ID)
+"""
+expected_stdout_6x = """
+    PLAN ("PUBLIC"."TEST" ORDER "PUBLIC"."TEST_ID")
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
