0820241445d758a313d39b98a2b37ae917df968e
pavel-zotov <p519446@yandex.ru>
2025-06-26 15:43:02 +0300

    Added/Updated tests\bugs\core_2073_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2073_test.py
+++ tests/bugs/core_2073_test.py
@@ -7,64 +7,69 @@ TITLE:       Expression indexes bug: incorrect result for the inverted boolean
 DESCRIPTION:
 JIRA:        CORE-1000
 FBTEST:      bugs.core_2073
+NOTES:
+    [26.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE TMP_DATE1
-(
-  DATE1 DATE,
-  DATE2 DATE
-);
-COMMIT;
-SET TERM !!;
-EXECUTE BLOCK
-AS
-  DECLARE VARIABLE D DATE;
-BEGIN
-  D = '01.01.2008';
-  WHILE (D < '01.08.2008') DO BEGIN
-    INSERT INTO TMP_DATE1(DATE1, DATE2)
-      VALUES(:D, :D + 100);
-      D = D + 1;
-  END
-END!!
-SET TERM ;!!
-COMMIT;
-CREATE INDEX TMP_DATE1_IDX1 ON TMP_DATE1 COMPUTED BY (DATE1+0);
-CREATE INDEX TMP_DATE1_IDX2 ON TMP_DATE1 (DATE1);
-COMMIT;
+db = db_factory()
+
+test_script = """
+    set list on;
+    create table tmp_date1 (
+        date1 date,
+        date2 date
+    );
+    commit;
+    set term ^;
+    execute block as
+        declare variable d date;
+    begin
+        d = '01.01.2008';
+        while (d < '01.08.2008') do begin
+            insert into tmp_date1(date1, date2) values(:d, :d + 100);
+            d = d + 1;
+        end
+    end^
+    set term ;^
+    commit;
+
+    create index tmp_date1_idx1 on tmp_date1 computed by (date1+0);
+    create index tmp_date1_idx2 on tmp_date1 (date1);
+    commit;
+
+    set plan on;
+    select count(*) from tmp_date1 t where '01.03.2008' between t.date1+0 and t.date2;
+    select count(*) from tmp_date1 t  where '01.03.2008' >= t.date1;
 """
 
-db = db_factory(init=init_script)
 
-test_script = """SET PLAN ON;
-SELECT count(*) FROM TMP_DATE1 T WHERE '01.03.2008' BETWEEN T.DATE1+0 AND T.DATE2;
-SELECT count(*) FROM TMP_DATE1 T  WHERE '01.03.2008' >= T.DATE1;
-"""
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-act = isql_act('db', test_script)
-
-expected_stdout = """
-PLAN (T INDEX (TMP_DATE1_IDX1))
-
-                COUNT
-=====================
-                   61
-
-
-PLAN (T INDEX (TMP_DATE1_IDX2))
-
-                COUNT
-=====================
-                   61
+expected_stdout_5x = """
+    PLAN (T INDEX (TMP_DATE1_IDX1))
+    COUNT 61
+    PLAN (T INDEX (TMP_DATE1_IDX2))
+    COUNT 61
 
 """
 
-@pytest.mark.version('>=3.0')
+expected_stdout_6x = """
+    PLAN ("T" INDEX ("PUBLIC"."TMP_DATE1_IDX1"))
+    COUNT 61
+    PLAN ("T" INDEX ("PUBLIC"."TMP_DATE1_IDX2"))
+    COUNT 61
+"""
+
+@pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
