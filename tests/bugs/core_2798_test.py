94f7475f9c0bb314b213ae748016416a2335d0d7
pavel-zotov <p519446@yandex.ru>
2025-06-26 23:22:14 +0300

    Added/Updated tests\bugs\core_2798_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2798_test.py
+++ tests/bugs/core_2798_test.py
@@ -7,6 +7,12 @@ TITLE:       Incomplete plan output (lack of view names) when selecting from vie
 DESCRIPTION:
 JIRA:        CORE-2798
 FBTEST:      bugs.core_2798
+NOTES:
+    [26.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -31,13 +37,16 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-    PLAN JOIN (V P1 NATURAL, V P2 NATURAL, V T1 NATURAL, V T2 NATURAL)
+expected_stdout_5x = """
+    PLAN JOIN (V P1 NATURAL, V P2 NATURAL, V T1 NATURAL, V T2 NATURAL) 
+"""
+
+expected_stdout_6x = """
+    PLAN JOIN ("PUBLIC"."V" "PUBLIC"."P1" NATURAL, "PUBLIC"."V" "P2" NATURAL, "PUBLIC"."V" "PUBLIC"."T1" NATURAL, "PUBLIC"."V" "T2" NATURAL)
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
