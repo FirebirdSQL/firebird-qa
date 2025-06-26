ecd7b6261bd82320212f2649be71f24ecebab42e
pavel-zotov <p519446@yandex.ru>
2025-06-26 21:01:42 +0300

    Added/Updated tests\bugs\core_2508_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2508_test.py
+++ tests/bugs/core_2508_test.py
@@ -16,6 +16,12 @@ NOTES:
     Old query that did use IN predicate no more applicable here: all occurences of the same index
     that works for mining data are now "collapsed" to the single one, i.e.:
     PLAN (<tabname> INDEX (<idxname>, <idxname>, <idxname>)) ==> PLAN (<tabname> INDEX (<idxname>)).
+
+    [26.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -42,14 +48,19 @@ test_script = """
     -- PLAN (T INDEX (abc(, mod())
 """
 
-act = isql_act('db', test_script)
+substitutions = [ ('[ \t]+', ' '), ('-At block line: [\\d]+, col: [\\d]+', '-At block line')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
+expected_stdout_5x = """
     PLAN (T INDEX (abc(, mod())
 """
 
-@pytest.mark.version('>=3.0')
+expected_stdout_6x = """
+    PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."abc(", "PUBLIC"."mod("))
+"""
+
+@pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
