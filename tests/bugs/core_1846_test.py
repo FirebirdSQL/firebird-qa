358b4059f894269c1ad97edf6b8fc43fb9bc88c1
pavel-zotov <p519446@yandex.ru>
2025-06-26 13:22:37 +0300

    Added/Updated tests\bugs\core_1846_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_1846_test.py
+++ tests/bugs/core_1846_test.py
@@ -7,6 +7,12 @@ TITLE:       Allow index walk (ORDER plan) when there is a composite index {A, B
 DESCRIPTION:
 JIRA:        CORE-1846
 FBTEST:      bugs.core_1846
+NOTES:
+    [26.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -36,14 +42,18 @@ test_script = """
 
 act = isql_act('db', test_script, substitutions=[('=.*', '')])
 
-expected_stdout = """
+expected_stdout_5x = """
     PLAN (TEST ORDER TEST_N1_N2_ASC)
     PLAN (TEST ORDER TEST_N2_N1_DESC)
 """
 
+expected_stdout_6x = """
+    PLAN ("PUBLIC"."TEST" ORDER "PUBLIC"."TEST_N1_N2_ASC")
+    PLAN ("PUBLIC"."TEST" ORDER "PUBLIC"."TEST_N2_N1_DESC")
+"""
+
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
