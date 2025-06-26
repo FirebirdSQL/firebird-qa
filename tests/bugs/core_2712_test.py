618c84e962b12bc935d78236d633751b92129111
pavel-zotov <p519446@yandex.ru>
2025-06-26 23:03:46 +0300

    Added/Updated tests\bugs\core_2712_test.py: Re-implemented via try/except and check show exception data. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2712_test.py
+++ tests/bugs/core_2712_test.py
@@ -7,6 +7,12 @@ TITLE:       Do not print "invalid request BLR" for par.cpp errors with valid BL
 DESCRIPTION:
 JIRA:        CORE-2712
 FBTEST:      bugs.core_2712
+NOTES:
+    [26.06.2025] pzotov
+    Re-implemented via try/except and check show exception data.
+    Suppressing quotes around `id <...>` as irrelevant to this test.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -15,21 +21,32 @@ from firebird.driver import DatabaseError
 
 db = db_factory()
 
-act = python_act('db', substitutions=[('table id [0-9]+ is not defined', 'table is not defined')])
+act = python_act('db', substitutions=[('table (")?id \\d+(")? is not defined', 'table is not defined')])
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action, capsys):
-    with act.db.connect() as att1:
-        cur1 = att1.cursor()
+    with act.db.connect() as con:
+        cur1 = con.cursor()
         cur1.execute("recreate table test(x int)")
-        att1.commit()
+        con.commit()
         cur1.execute("insert into test values(1)")
-        att1.commit()
-        with act.db.connect() as att2:
-            cur2 = att2.cursor()
-            cur2.execute("select 1 from rdb$database")
-
-            cur1.execute("drop table test")
-            with pytest.raises(DatabaseError, match='.*table id [0-9]+ is not defined.*'):
+        con.commit()
+        try:
+            with act.db.connect() as con2:
+                cur2 = con2.cursor()
+                cur2.execute("select 1 from rdb$database")
+                cur1.execute("drop table test")
                 cur2.prepare("update test set x=-x")
-                att2.commit()
+                con2.commit()
+        except Exception as e:
+            print(e.__str__())
+            for x in e.gds_codes:
+                print(x)
+
+        act.expected_stdout = """
+            table is not defined
+            335544395
+        """
+        act.stdout = capsys.readouterr().out
+        assert act.clean_stdout == act.clean_expected_stdout
+        act.reset()
