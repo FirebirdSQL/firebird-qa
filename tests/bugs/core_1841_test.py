9208092c70ae1e9b10b70a66fedd380bec663de7
pavel-zotov <p519446@yandex.ru>
2025-06-26 13:20:11 +0300

    Added/Updated tests\bugs\core_1841_test.py: Re-implemented: use f-notation in order to remove hard-coded DDL string from test_script and expected_out. Removed 'SHOW VIEW'command because its output can change in any intensive developing FB version. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_1841_test.py
+++ tests/bugs/core_1841_test.py
@@ -5,10 +5,14 @@ ID:          issue-2270
 ISSUE:       2270
 TITLE:       Possible overflow in RDB$VIEW_RELATIONS.RDB$CONTEXT_NAME
 DESCRIPTION:
-  Originale tite is: If some VIEW used derived tables and long table names/aliases,
-    It is possible to overflow RDB$VIEW_RELATIONS.RDB$CONTEXT_NAME
+    Original titte is: If some VIEW used derived tables and long table names/aliases, it is possible to overflow RDB$VIEW_RELATIONS.RDB$CONTEXT_NAME
 JIRA:        CORE-1841
 FBTEST:      bugs.core_1841
+NOTES:
+    [26.06.2025] pzotov
+    Re-implemented: use f-notation in order to remove hard-coded DDL string from test_script and expected_out.
+    Removed 'SHOW VIEW'command because its output can change in any intensive developing FB version.
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -16,26 +20,28 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """create view x (id) as
-select RDB$RELATION_ID
- from (select * from RDB$DATABASE long_alias_long_alias_1) long_alias_long_alias_2;
-COMMIT;
-SHOW VIEW x;
+VIEW_DDL = 'select rdb$relation_id from (select * from rdb$database long_alias_long_alias_1) long_alias_long_alias_2'
+test_script = f"""
+    set list on;
+    set blob all;
+    set count on;
+    create view v_test (id) as
+    {VIEW_DDL};
+    commit;
+    select r.rdb$view_source as blob_id from rdb$relations r where r.rdb$relation_name = upper('v_test');
 """
 
-act = isql_act('db', test_script)
+substitutions = [('BLOB_ID .*', '')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """ID                              SMALLINT Expression
-View Source:
-==== ======
-
-select RDB$RELATION_ID
- from (select * from RDB$DATABASE long_alias_long_alias_1) long_alias_long_alias_2
+expected_stdout = f"""
+    {VIEW_DDL}
+    Records affected: 1
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
