47e31678c24bbab75c2c7d0a53241d16aac6a76a
pavel-zotov <p519446@yandex.ru>
2025-06-26 21:46:09 +0300

    Added/Updated tests\bugs\core_2678_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2678_test.py
+++ tests/bugs/core_2678_test.py
@@ -7,6 +7,12 @@ TITLE:       Full outer join cannot use available indices (very slow execution)
 DESCRIPTION:
 JIRA:        CORE-2678
 FBTEST:      bugs.core_2678
+NOTES:
+    [26.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -52,15 +58,19 @@ test_script = """
         d1.c3, d2.c3;
 """
 
+
 act = isql_act('db', test_script)
 
-expected_stdout = """
+expected_stdout_5x = """
     PLAN SORT (JOIN (JOIN (D2 NATURAL, D1 INDEX (IDX_TD_DATA1)), JOIN (D1 NATURAL, D2 INDEX (IDX_TD_DATA2))))
 """
 
-@pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
+expected_stdout_6x = """
+    PLAN SORT (JOIN (JOIN ("D2" NATURAL, "D1" INDEX ("PUBLIC"."IDX_TD_DATA1")), JOIN ("D1" NATURAL, "D2" INDEX ("PUBLIC"."IDX_TD_DATA2"))))
+"""
 
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
