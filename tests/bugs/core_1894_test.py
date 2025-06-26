8a496c3b2f315e4e0b9c92b77194e2e3b79b3ebd
pavel-zotov <p519446@yandex.ru>
2025-06-26 13:30:31 +0300

    Added/Updated tests\bugs\core_1894_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_1894_test.py
+++ tests/bugs/core_1894_test.py
@@ -3,10 +3,16 @@
 """
 ID:          issue-2325
 ISSUE:       2325
-TITLE:       Circular dependencies between computed fields crashs the engine
+TITLE:       Circular dependencies between computed fields crash the engine
 DESCRIPTION:
 JIRA:        CORE-1894
 FBTEST:      bugs.core_1894
+NOTES:
+    [26.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -37,12 +43,11 @@ test_script = """
 
     select * from t;
     select * from t2; -- THIS LEAD SERVER CRASH (checked on WI-T4.0.0.399)
-
 """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -Cannot have circular dependencies with computed fields
@@ -63,9 +68,28 @@ expected_stderr = """
     -there are 1 dependencies
 """
 
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -Cannot have circular dependencies with computed fields
+
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -cannot delete
+    -COLUMN "PUBLIC"."T2"."C1"
+    -there are 1 dependencies
+
+    Statement failed, SQLSTATE = 42000
+    Cannot have circular dependencies with computed fields
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -cannot delete
+    -COLUMN "PUBLIC"."T2"."C1"
+    -there are 1 dependencies
+"""
+
 @pytest.mark.version('>=3.0.2')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
