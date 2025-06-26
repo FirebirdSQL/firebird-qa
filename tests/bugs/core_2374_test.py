6ee68605d72f525bb08bdb97fdaed4ce332097b2
pavel-zotov <p519446@yandex.ru>
2025-06-26 20:29:41 +0300

    Added/Updated tests\bugs\core_2374_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2374_test.py
+++ tests/bugs/core_2374_test.py
@@ -7,6 +7,12 @@ TITLE:       ALTER TRIGGER / PROCEDURE wrong error message
 DESCRIPTION:
 JIRA:        CORE-2374
 FBTEST:      bugs.core_2374
+NOTES:
+    [26.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -33,20 +39,32 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -ALTER PROCEDURE TEST1 failed
     -Procedure TEST1 not found
+
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -ALTER TRIGGER TRG1 failed
     -Trigger TRG1 not found
 """
 
-@pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -ALTER PROCEDURE "PUBLIC"."TEST1" failed
+    -Procedure "PUBLIC"."TEST1" not found
 
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -ALTER TRIGGER "PUBLIC"."TRG1" failed
+    -Trigger "PUBLIC"."TRG1" not found
+"""
+
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
