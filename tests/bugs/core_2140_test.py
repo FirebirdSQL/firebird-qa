0e721f00cf74be50e06c846fabb4835bf4c34274
pavel-zotov <p519446@yandex.ru>
2025-06-26 16:53:28 +0300

    Added/Updated tests\bugs\core_2140_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2140_test.py
+++ tests/bugs/core_2140_test.py
@@ -7,6 +7,12 @@ TITLE:       Error messages after parameters substitution contains '\\n' charact
 DESCRIPTION:
 JIRA:        CORE-2140
 FBTEST:      bugs.core_2140
+NOTES:
+    [26.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -14,36 +20,65 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """
-    set list on;
-    set term ^ ;
-    execute block returns (y int) as
-    begin
-      for execute statement
-          ('select rdb$relation_id from rdb$database where rdb$relation_id = :x') (1)
-        with autonomous transaction
-        into y
-      do suspend;
-    end ^
-"""
+substitutions = [
+    ('Data source : Firebird::.*', 'Data source : Firebird::'),
+    ('col(umn)?(:)?.*', 'col x'),
+    ('-At block line: [\\d]+, col: [\\d]+', '')
+]
 
-act = isql_act('db', test_script,
-                 substitutions=[('column.*', 'column x'),
-                                ('-At block line: [\\d]+, col: [\\d]+', '')])
-
-expected_stderr = """
-    Statement failed, SQLSTATE = 42S22
-    Dynamic SQL Error
-    -SQL error code = -206
-    -Column unknown
-    -X
-    -At line 1, column 67
-    -At block line: 5, col: 3
-"""
+act = isql_act('db', substitutions = substitutions)
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
 
+    # Statement that will be passed in ES/EDS (we have to check its presence in error message):
+    EDS_STATEMENT = 'select rdb$relation_id from rdb$database where rdb$relation_id = :x'
+
+    test_script = f"""
+        set list on;
+        set term ^ ;
+        execute block returns (y int) as
+        begin
+          for
+              execute statement  ('{EDS_STATEMENT}') (1)
+              on external 'localhost:' || rdb$get_context('SYSTEM', 'DB_NAME')
+              as user '{act.db.user}' password '{act.db.password}'
+              with autonomous transaction
+          into y
+              do suspend;
+        end
+        ^
+    """
+    
+    # BEFOREE fix output was:
+    # Statement : select rdb$relation_id from rdb$database where rdb$relation_id = :x\nData source : Internal::.
+
+    expected_stdout_5x = f"""
+        Statement failed, SQLSTATE = 42000
+        Execute statement error at isc_dsql_prepare :
+        335544569 : Dynamic SQL Error
+        335544436 : SQL error code = -206
+        335544578 : Column unknown
+        335544382 : X
+        336397208 : At line 1, column 66
+        Statement : {EDS_STATEMENT}
+        Data source : Firebird::
+        -At block line: 3, col: 11
+    """
+
+    expected_stdout_6x = f"""
+        Statement failed, SQLSTATE = 42000
+        Execute statement error at isc_dsql_prepare :
+        335544569 : Dynamic SQL Error
+        335544436 : SQL error code = -206
+        335544578 : Column unknown
+        335544382 : "X"
+        336397208 : At line 1, column 66
+        Statement : {EDS_STATEMENT}
+        Data source : Firebird::
+        -At block line: 3, col: 11
+    """
+    
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.isql(switches = ['-q'], input = test_script, combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
