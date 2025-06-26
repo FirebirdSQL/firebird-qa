cffb5b6569f5b521f884c0263c0cfba19ad56da8
pavel-zotov <p519446@yandex.ru>
2025-06-26 13:56:28 +0300

    Added/Updated tests\bugs\core_1986_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214.

--- tests/bugs/core_1986_test.py
+++ tests/bugs/core_1986_test.py
@@ -7,42 +7,55 @@ TITLE:       Altering domains name drops dependencies using the domain
 DESCRIPTION:
 JIRA:        CORE-1986
 FBTEST:      bugs.core_1986
+NOTES:
+    [26.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE DOMAIN D_SOME AS INTEGER;
+db = db_factory()
 
-CREATE OR ALTER PROCEDURE SP_SOME(
-    SOME_PARAM D_SOME)
-AS
-BEGIN
-END;
-"""
-
-db = db_factory(init=init_script)
-
-test_script = """ALTER DOMAIN D_SOME TO D_OTHER;
-
-execute procedure SP_SOME (1);
-commit;
-execute procedure SP_SOME (1);
-commit;
+test_script = """
+    create domain dm_int as int;
+    set term ^;
+    create or alter procedure sp_some(a_x dm_int)
+    as
+    begin
+    end
+    ^
+    set term ;^
+    alter domain dm_int to d_other;
+    execute procedure sp_some (1);
+    commit;
+    execute procedure sp_some (1);
+    commit;
 """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """Statement failed, SQLSTATE = 42000
-unsuccessful metadata update
--cannot delete
--DOMAIN D_SOME
--there are 1 dependencies
+expected_stdout_5x = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -cannot delete
+    -DOMAIN DM_INT
+    -there are 1 dependencies
+"""
+
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -cannot delete
+    -DOMAIN "PUBLIC"."DM_INT"
+    -there are 1 dependencies
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
