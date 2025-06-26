052c4ae9ac0116f73bf239f82bdd2efcfc5b818a
pavel-zotov <p519446@yandex.ru>
2025-06-26 20:49:33 +0300

    Added/Updated tests\bugs\core_2424_test.py: Reimplemented - see notes. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2424_test.py
+++ tests/bugs/core_2424_test.py
@@ -7,6 +7,13 @@ TITLE:       Make CREATE VIEW infer column names for views involving a GROUP BY 
 DESCRIPTION:
 JIRA:        CORE-2424
 FBTEST:      bugs.core_2424
+NOTES:
+    [26.06.2025] pzotov
+    Reimplemented. It is enough just to try to run command CREATE VIEW with check presence 
+    of appropriate record in RDB$RELATIONS.
+    No need in 'SHOW VIEW'.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -14,27 +21,25 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """create view V as select d.rdb$relation_id from rdb$database d group by d.rdb$relation_id;
-show view v;
-recreate view V as select a from (select 1 a from rdb$database);
-show view v;
+test_script = """
+    set list on;
+    create view v_test as
+    select d.rdb$relation_id
+    from rdb$database d
+    group by d.rdb$relation_id;
+    commit;
+    select count(*) as view_created from rdb$relations where rdb$relation_name = upper('v_test');
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """RDB$RELATION_ID                 SMALLINT Expression
-View Source:
-==== ======
- select d.rdb$relation_id from rdb$database d group by d.rdb$relation_id
-A                               INTEGER Expression
-View Source:
-==== ======
- select a from (select 1 a from rdb$database)
+expected_stdout = """
+    VIEW_CREATED 1
 """
 
-@pytest.mark.version('>=3.0')
+@pytest.mark.version('>=3')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
