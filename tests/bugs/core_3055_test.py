f748104b4258e2e1d6a7dec35dbe786c8364925b
pavel-zotov <p519446@yandex.ru>
2025-06-27 13:35:14 +0300

    Added/Updated tests\bugs\core_3055_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3055_test.py
+++ tests/bugs/core_3055_test.py
@@ -3,11 +3,16 @@
 """
 ID:          issue-3435
 ISSUE:       3435
-TITLE:       Variable/argument name could be absent or be wrong in error messages when more
- than 256 variables are used
+TITLE:       Variable/argument name could be absent or be wrong in error messages when more than 256 variables are used
 DESCRIPTION:
 JIRA:        CORE-3055
 FBTEST:      bugs.core_3055
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -2033,15 +2038,20 @@ test_script = """
 
 act = isql_act('db', test_script, substitutions=[('-At block line: [\\d]+, col: [\\d]+', '-At block line')])
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 42000
     validation error for variable V_NN, value "*** null ***"
-    -At block line: 2004, col: 9
+    -At block line
 """
 
-@pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 42000
+    validation error for variable "V_NN", value "*** null ***"
+    -At block line
+"""
 
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
