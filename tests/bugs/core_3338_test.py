51e5856450eec7f12e8800e71474b4e312327265
pavel-zotov <p519446@yandex.ru>
2025-06-27 18:18:33 +0300

    Added/Updated tests\bugs\core_3338_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3338_test.py
+++ tests/bugs/core_3338_test.py
@@ -7,6 +7,12 @@ TITLE:       Regression: Code changes disabled support for expression indexes wi
 DESCRIPTION:
 JIRA:        CORE-3338
 FBTEST:      bugs.core_3338
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -29,14 +35,18 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
+expected_out_5x = """
     PLAN (T INDEX (T_N2_COALESCE))
     PLAN (T INDEX (T_N2_DECODE))
 """
 
-@pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
+expected_out_6x = """
+    PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_N2_COALESCE"))
+    PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_N2_DECODE"))
+"""
 
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+    act.expected_stdout = expected_out_5x if act.is_version('<6') else expected_out_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
