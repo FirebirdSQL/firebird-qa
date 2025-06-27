9d1bfa78a1b84e1dca1c9540e1d282322f040bea
pavel-zotov <p519446@yandex.ru>
2025-06-27 20:53:36 +0300

    Added/Updated tests\bugs\core_3553_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3553_test.py
+++ tests/bugs/core_3553_test.py
@@ -13,6 +13,12 @@ NOTES:
     FB 5.0.0.455 and later: data sources with equal cardinality now present in the HASH plan in order they are specified in the query.
     Reversed order was used before this build. Because of this, two cases of expected stdout must be taken in account, see variables
     'fb3x_checked_stdout' and 'fb5x_checked_stdout'.
+
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -29,17 +35,21 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-fb3x_checked_stdout = """
+expected_stdout_4x = """
     PLAN HASH (D2 NATURAL, D1 NATURAL)
 """
 
-fb5x_checked_stdout = """
+expected_stdout_5x = """
     PLAN HASH (D1 NATURAL, D2 NATURAL)
+"""
+
+expected_stdout_6x = """
+    PLAN HASH ("D1" NATURAL, "D2" NATURAL)
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = fb3x_checked_stdout if act.is_version('<5') else fb5x_checked_stdout
+    act.expected_stdout = expected_stdout_4x if act.is_version('<5') else expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute()
     assert act.clean_stdout == act.clean_expected_stdout
 
