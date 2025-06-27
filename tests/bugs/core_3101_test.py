a92050c99020df318176b8410f80192248163ccd
pavel-zotov <p519446@yandex.ru>
2025-06-27 14:26:00 +0300

    Added/Updated tests\bugs\core_3101_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3101_test.py
+++ tests/bugs/core_3101_test.py
@@ -7,6 +7,12 @@ TITLE:       Cannot alter the domain after migrating from older versions
 DESCRIPTION:
 JIRA:        CORE-3101
 FBTEST:      bugs.core_3101
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -21,17 +27,23 @@ test_script = """
     show domain state;
 """
 
-act = isql_act('db', test_script)
+substitutions = [ ('[ \t]+', ' ') ] 
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-    STATE                           SMALLINT Nullable
-    STATE                           SMALLINT Nullable
-                                    default 0
+expected_out_5x = """
+    STATE SMALLINT Nullable
+    STATE SMALLINT Nullable
+    default 0
+"""
+
+expected_out_6x = """
+    PUBLIC.STATE SMALLINT Nullable
+    PUBLIC.STATE SMALLINT Nullable
+    default 0
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_out_5x if act.is_version('<6') else expected_out_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
