9ff252dea259c46bbd603c4a4dde6de32fc4254c
pavel-zotov <p519446@yandex.ru>
2025-06-27 11:13:56 +0300

    Added/Updated tests\bugs\core_2916_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2916_test.py
+++ tests/bugs/core_2916_test.py
@@ -7,6 +7,12 @@ TITLE:       Broken error handling in the case of a conversion error happened du
 DESCRIPTION:
 JIRA:        CORE-2916
 FBTEST:      bugs.core_2916
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -14,70 +20,38 @@ from firebird.qa import *
 
 # version: 3.0
 
-init_script_1 = """create table tab (col date);
-insert into tab (col) values (current_date);
-commit;
-"""
+db = db_factory()
 
-db_1 = db_factory(init=init_script_1)
-
-test_script_1 = """create index itab on tab computed (cast(col as int));
-commit;
-select * from tab where cast(col as int) is null;"""
-
-act_1 = isql_act('db_1', test_script_1,
-                 substitutions=[('[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]', '2011-05-03')])
-
-expected_stdout_1 = """
-        COL
-===========
-"""
-
-expected_stderr_1 = """Statement failed, SQLSTATE = 22018
-
-conversion error from string "2011-05-03"
-
-Statement failed, SQLSTATE = 22018
-
-conversion error from string "2011-05-03"
-"""
-
-@pytest.mark.version('>=3.0,<4.0')
-def test_1(act_1: Action):
-    act_1.expected_stdout = expected_stdout_1
-    act_1.expected_stderr = expected_stderr_1
-    act_1.execute()
-    assert (act_1.clean_stderr == act_1.clean_expected_stderr and
-            act_1.clean_stdout == act_1.clean_expected_stdout)
-
-# version: 4.0
-
-db_2 = db_factory()
-
-test_script_2 = """
+test_script = """
     recreate table tab (col date);
-    insert into tab (col) values ( date '29.02.2004' );
+    insert into tab (col) values ( current_date );
     commit;
-
     create index itab on tab computed (cast(col as int));
     commit;
-    set list on;
-    select * from tab where cast(col as int) is null;
 """
 
-act_2 = isql_act('db_2', test_script_2)
+substitutions = [('(-)?conversion error from string.*', 'conversion error from string')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stderr_2 = """
+expected_stdout_3x = """
+    Statement failed, SQLSTATE = 22018
+    conversion error from string "2021-09-27"
+"""
+
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 22018
     Expression evaluation error for index "***unknown***" on table "TAB"
-    -conversion error from string "2004-02-29"
-    Statement failed, SQLSTATE = 22018
-    conversion error from string "2004-02-29"
+    conversion error from string "2021-09-27"
 """
 
-@pytest.mark.version('>=4.0')
-def test_2(act_2: Action):
-    act_2.expected_stderr = expected_stderr_2
-    act_2.execute()
-    assert act_2.clean_stderr == act_2.clean_expected_stderr
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 22018
+    Expression evaluation error for index ""***unknown***"" on table ""PUBLIC"."TAB""
+    conversion error from string "2021-09-27"
+"""
 
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+    act.expected_stdout = expected_stdout_3x if act.is_version('<4') else expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
