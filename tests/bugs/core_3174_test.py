2312aba532b93151ea706c7b26f11978f4b8bce5
pavel-zotov <p519446@yandex.ru>
2025-06-27 14:30:55 +0300

    Added/Updated tests\bugs\core_3174_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3174_test.py
+++ tests/bugs/core_3174_test.py
@@ -7,6 +7,12 @@ TITLE:       Expression index with TRIM may lead to incorrect indexed lookup
 DESCRIPTION:
 JIRA:        CORE-3174
 FBTEST:      bugs.core_3174
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -35,6 +41,7 @@ db = db_factory(init=init_script)
 
 test_script = """
     set plan on;
+    set list on;
     --set echo on;
 
     select '1.a' as test_no, id,'.' || c_pad || '.' as c_pad from t where trim(c_pad) = '123';
@@ -54,117 +61,240 @@ test_script = """
     select '2.g' as test_no, id,'.' || c_nopad || '.' as c_nopad from t where trim(leading from c_nopad) starting with '123';
 """
 
-act = isql_act('db', test_script, substitutions=[('=.*', '')])
-
-expected_stdout = """
-    PLAN (T NATURAL)
-
-    TEST_NO           ID C_PAD
-    ======= ============ ============
-    1.a                1 .123.
-    1.a                2 . 123.
-    1.a                3 .123  .
-    1.a                4 . 123  .
+substitutions = [ ('[ \t]+', ' ') ] 
+act = isql_act('db', test_script, substitutions = substitutions)
 
 
-    PLAN (T NATURAL)
-
-    TEST_NO           ID C_PAD
-    ======= ============ ============
-    1.b                1 .123.
-    1.b                3 .123  .
-
-
-    PLAN (T INDEX (T_C_PAD_TRIM_RIGHT))
-
-    TEST_NO           ID C_PAD
-    ======= ============ ============
-    1.c                1 .123.
-    1.c                3 .123  .
-
-
-    PLAN (T INDEX (T_C_PAD_TRIM_LEFT))
-
-    TEST_NO           ID C_PAD
-    ======= ============ ============
-    1.d                1 .123.
-    1.d                2 . 123.
-    1.d                3 .123  .
-    1.d                4 . 123  .
-
-
-    PLAN (T INDEX (T_C_PAD_TRIM_RIGHT))
-
-    TEST_NO           ID C_PAD
-    ======= ============ ============
-    1.e                1 .123.
-    1.e                3 .123  .
-
-
-    PLAN (T INDEX (T_C_PAD_TRIM_LEFT))
-
-    TEST_NO           ID C_PAD
-    ======= ============ ============
-    1.f                1 .123.
-    1.f                2 . 123.
-    1.f                3 .123  .
-    1.f                4 . 123  .
-
-
-    PLAN (T NATURAL)
-
-    TEST_NO           ID C_NOPAD
-    ======= ============ ================================================
-    2.a                1 .123.
-    2.a                2 . 123.
-    2.a                3 .123  .
-    2.a                4 . 123  .
-
-
-    PLAN (T NATURAL)
-
-    TEST_NO           ID C_NOPAD
-    ======= ============ ================================================
-    2.b                1 .123.
-
-
-    PLAN (T INDEX (T_C_NOPAD_TRIM_RIGHT))
-
-    TEST_NO           ID C_NOPAD
-    ======= ============ ================================================
-    2.c                1 .123.
-    2.c                3 .123  .
-
-
-    PLAN (T INDEX (T_C_NOPAD_TRIM_LEFT))
-
-    TEST_NO           ID C_NOPAD
-    ======= ============ ================================================
-    2.d                1 .123.
-    2.d                2 . 123.
-
-
-    PLAN (T INDEX (T_C_NOPAD_TRIM_RIGHT))
-
-    TEST_NO           ID C_NOPAD
-    ======= ============ ================================================
-    2.f                1 .123.
-    2.f                3 .123  .
-
-
-    PLAN (T INDEX (T_C_NOPAD_TRIM_LEFT))
-
-    TEST_NO           ID C_NOPAD
-    ======= ============ ================================================
-    2.g                1 .123.
-    2.g                2 . 123.
-    2.g                3 .123  .
-    2.g                4 . 123  .
+expected_out_5x = """
+        PLAN (T NATURAL)
+        TEST_NO 1.a
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.a
+        ID 2
+        C_PAD . 123.
+        TEST_NO 1.a
+        ID 3
+        C_PAD .123 .
+        TEST_NO 1.a
+        ID 4
+        C_PAD . 123 .
+        PLAN (T NATURAL)
+        TEST_NO 1.b
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.b
+        ID 3
+        C_PAD .123 .
+        PLAN (T INDEX (T_C_PAD_TRIM_RIGHT))
+        TEST_NO 1.c
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.c
+        ID 3
+        C_PAD .123 .
+        PLAN (T INDEX (T_C_PAD_TRIM_LEFT))
+        TEST_NO 1.d
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.d
+        ID 2
+        C_PAD . 123.
+        TEST_NO 1.d
+        ID 3
+        C_PAD .123 .
+        TEST_NO 1.d
+        ID 4
+        C_PAD . 123 .
+        PLAN (T INDEX (T_C_PAD_TRIM_RIGHT))
+        TEST_NO 1.e
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.e
+        ID 3
+        C_PAD .123 .
+        PLAN (T INDEX (T_C_PAD_TRIM_LEFT))
+        TEST_NO 1.f
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.f
+        ID 2
+        C_PAD . 123.
+        TEST_NO 1.f
+        ID 3
+        C_PAD .123 .
+        TEST_NO 1.f
+        ID 4
+        C_PAD . 123 .
+        PLAN (T NATURAL)
+        TEST_NO 2.a
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.a
+        ID 2
+        C_NOPAD . 123.
+        TEST_NO 2.a
+        ID 3
+        C_NOPAD .123 .
+        TEST_NO 2.a
+        ID 4
+        C_NOPAD . 123 .
+        PLAN (T NATURAL)
+        TEST_NO 2.b
+        ID 1
+        C_NOPAD .123.
+        PLAN (T INDEX (T_C_NOPAD_TRIM_RIGHT))
+        TEST_NO 2.c
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.c
+        ID 3
+        C_NOPAD .123 .
+        PLAN (T INDEX (T_C_NOPAD_TRIM_LEFT))
+        TEST_NO 2.d
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.d
+        ID 2
+        C_NOPAD . 123.
+        PLAN (T INDEX (T_C_NOPAD_TRIM_RIGHT))
+        TEST_NO 2.f
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.f
+        ID 3
+        C_NOPAD .123 .
+        PLAN (T INDEX (T_C_NOPAD_TRIM_LEFT))
+        TEST_NO 2.g
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.g
+        ID 2
+        C_NOPAD . 123.
+        TEST_NO 2.g
+        ID 3
+        C_NOPAD .123 .
+        TEST_NO 2.g
+        ID 4
+        C_NOPAD . 123 .
 """
 
-@pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
+expected_out_6x = """
+        PLAN ("PUBLIC"."T" NATURAL)
+        TEST_NO 1.a
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.a
+        ID 2
+        C_PAD . 123.
+        TEST_NO 1.a
+        ID 3
+        C_PAD .123 .
+        TEST_NO 1.a
+        ID 4
+        C_PAD . 123 .
+        PLAN ("PUBLIC"."T" NATURAL)
+        TEST_NO 1.b
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.b
+        ID 3
+        C_PAD .123 .
+        PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_C_PAD_TRIM_RIGHT"))
+        TEST_NO 1.c
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.c
+        ID 3
+        C_PAD .123 .
+        PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_C_PAD_TRIM_LEFT"))
+        TEST_NO 1.d
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.d
+        ID 2
+        C_PAD . 123.
+        TEST_NO 1.d
+        ID 3
+        C_PAD .123 .
+        TEST_NO 1.d
+        ID 4
+        C_PAD . 123 .
+        PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_C_PAD_TRIM_RIGHT"))
+        TEST_NO 1.e
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.e
+        ID 3
+        C_PAD .123 .
+        PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_C_PAD_TRIM_LEFT"))
+        TEST_NO 1.f
+        ID 1
+        C_PAD .123.
+        TEST_NO 1.f
+        ID 2
+        C_PAD . 123.
+        TEST_NO 1.f
+        ID 3
+        C_PAD .123 .
+        TEST_NO 1.f
+        ID 4
+        C_PAD . 123 .
+        PLAN ("PUBLIC"."T" NATURAL)
+        TEST_NO 2.a
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.a
+        ID 2
+        C_NOPAD . 123.
+        TEST_NO 2.a
+        ID 3
+        C_NOPAD .123 .
+        TEST_NO 2.a
+        ID 4
+        C_NOPAD . 123 .
+        PLAN ("PUBLIC"."T" NATURAL)
+        TEST_NO 2.b
+        ID 1
+        C_NOPAD .123.
+        PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_C_NOPAD_TRIM_RIGHT"))
+        TEST_NO 2.c
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.c
+        ID 3
+        C_NOPAD .123 .
+        PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_C_NOPAD_TRIM_LEFT"))
+        TEST_NO 2.d
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.d
+        ID 2
+        C_NOPAD . 123.
+        PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_C_NOPAD_TRIM_RIGHT"))
+        TEST_NO 2.f
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.f
+        ID 3
+        C_NOPAD .123 .
+        PLAN ("PUBLIC"."T" INDEX ("PUBLIC"."T_C_NOPAD_TRIM_LEFT"))
+        TEST_NO 2.g
+        ID 1
+        C_NOPAD .123.
+        TEST_NO 2.g
+        ID 2
+        C_NOPAD . 123.
+        TEST_NO 2.g
+        ID 3
+        C_NOPAD .123 .
+        TEST_NO 2.g
+        ID 4
+        C_NOPAD . 123 .
+"""
 
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+    act.expected_stdout = expected_out_5x if act.is_version('<6') else expected_out_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
