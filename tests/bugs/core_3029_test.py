7f7173841d89b1256dd578559e09110121b2f98e
pavel-zotov <p519446@yandex.ru>
2025-06-27 13:32:01 +0300

    Added/Updated tests\bugs\core_3029_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3029_test.py
+++ tests/bugs/core_3029_test.py
@@ -7,110 +7,142 @@ TITLE:       Bugcheck "Too many savepoints (287)" at rollback after exception at
 DESCRIPTION:
 JIRA:        CORE-3029
 FBTEST:      bugs.core_3029
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """create sequence test_gen;
+db = db_factory()
 
-recreate table test_row
-(id int not null,
- did int not null,
- pid int not null,
- dep int not null
-);
-alter table test_row add constraint pk_test_row primary key(id);
-create unique index ix_test_row1 on test_row(did, pid, dep);
-commit;
+test_script = """
+    set list on;
+    create sequence test_gen;
+    recreate table test_row
+    (id int not null,
+     did int not null,
+     pid int not null,
+     dep int not null
+    );
+    alter table test_row add constraint pk_test_row primary key(id);
+    create unique index ix_test_row1 on test_row(did, pid, dep);
+    commit;
 
-insert into test_row(id, did, pid,dep) values(1, 2, 3, 4);
-commit;
+    insert into test_row(id, did, pid,dep) values(1, 2, 3, 4);
+    commit;
+
+    set term ^;
+    execute block returns(id int, did int, dep int, pid int)
+    as
+    declare variable xid int;
+    begin
+      select id,did, pid,dep
+        from test_row
+       where id=(select min(id) from test_row)
+        into :xid, :did, pid, :dep;
+
+      while (1=1) do
+      begin
+        delete from test_row r where r.id = :xid;
+
+        insert into test_row(id, did, dep, pid)
+        values (gen_id(test_gen, 1), :did, :dep, :pid);
+
+        suspend;
+
+      when any do
+        exception;
+      end
+    end ^
+    rollback ^
+    create or alter procedure sp_test
+      returns(id int, did int, dep int, pid int)
+    as
+    declare variable xid int;
+    begin
+      select id,did, pid,dep
+        from test_row
+       where id=(select min(id) from test_row)
+        into :xid, :did, pid, :dep;
+
+      while (1=1) do
+      begin
+        delete from test_row r where r.id = :xid;
+
+        insert into test_row(id, did, dep, pid)
+        values (gen_id(test_gen, 1), :did, :dep, :pid);
+
+        suspend;
+
+      when any do
+        exception;
+      end
+    end ^
+    select * from sp_test ^
+    rollback ^
 """
 
-db = db_factory(init=init_script)
+substitutions = [ ('[ \t]+', ' '), ('line.*', ''), ('col.*', '')] 
 
-test_script = """set term !!;
-execute block returns(id int, did int, dep int, pid int)
-as
-declare variable xid int;
-begin
-  select id,did, pid,dep
-    from test_row
-   where id=(select min(id) from test_row)
-    into :xid, :did, pid, :dep;
+act = isql_act('db', test_script, substitutions = substitutions)
 
-  while (1=1) do
-  begin
-    delete from test_row r where r.id = :xid;
-
-    insert into test_row(id, did, dep, pid)
-    values (gen_id(test_gen, 1), :did, :dep, :pid);
-
-    suspend;
-
-  when any do
-    exception;
-  end
-end !!
-rollback !!
-create or alter procedure sp_test
-  returns(id int, did int, dep int, pid int)
-as
-declare variable xid int;
-begin
-  select id,did, pid,dep
-    from test_row
-   where id=(select min(id) from test_row)
-    into :xid, :did, pid, :dep;
-
-  while (1=1) do
-  begin
-    delete from test_row r where r.id = :xid;
-
-    insert into test_row(id, did, dep, pid)
-    values (gen_id(test_gen, 1), :did, :dep, :pid);
-
-    suspend;
-
-  when any do
-    exception;
-  end
-end !!
-select * from sp_test !!
-rollback !!
+expected_stdout_5x = """
+    ID <null>
+    DID 2
+    DEP 4
+    PID 3
+    ID <null>
+    DID 2
+    DEP 4
+    PID 3
+    Statement failed, SQLSTATE = 23000
+    attempt to store duplicate value (visible to active transactions) in unique index "IX_TEST_ROW1"
+    -Problematic key value is ("DID" = 2, "PID" = 3, "DEP" = 4)
+    -At block
+    -At block
+    ID <null>
+    DID 2
+    DEP 4
+    PID 3
+    Statement failed, SQLSTATE = 23000
+    attempt to store duplicate value (visible to active transactions) in unique index "IX_TEST_ROW1"
+    -Problematic key value is ("DID" = 2, "PID" = 3, "DEP" = 4)
+    -At procedure 'SP_TEST'
+    -At procedure 'SP_TEST'
 """
 
-act = isql_act('db', test_script, substitutions=[('line.*', ''), ('col.*', '')])
-
-expected_stdout = """
-          ID          DID          DEP          PID
-============ ============ ============ ============
-      <null>            2            4            3
-      <null>            2            4            3
-
-          ID          DID          DEP          PID
-============ ============ ============ ============
-      <null>            2            4            3
-"""
-
-expected_stderr = """Statement failed, SQLSTATE = 23000
-attempt to store duplicate value (visible to active transactions) in unique index "IX_TEST_ROW1"
--Problematic key value is ("DID" = 2, "PID" = 3, "DEP" = 4)
--At block
--At block
-Statement failed, SQLSTATE = 23000
-attempt to store duplicate value (visible to active transactions) in unique index "IX_TEST_ROW1"
--Problematic key value is ("DID" = 2, "PID" = 3, "DEP" = 4)
--At procedure 'SP_TEST' line: 15, col: 5
--At procedure 'SP_TEST' line: 20, col: 12
+expected_stdout_6x = """
+    ID <null>
+    DID 2
+    DEP 4
+    PID 3
+    ID <null>
+    DID 2
+    DEP 4
+    PID 3
+    Statement failed, SQLSTATE = 23000
+    attempt to store duplicate value (visible to active transactions) in unique index "PUBLIC"."IX_TEST_ROW1"
+    -Problematic key value is ("DID" = 2, "PID" = 3, "DEP" = 4)
+    -At block
+    -At block
+    ID <null>
+    DID 2
+    DEP 4
+    PID 3
+    Statement failed, SQLSTATE = 23000
+    attempt to store duplicate value (visible to active transactions) in unique index "PUBLIC"."IX_TEST_ROW1"
+    -Problematic key value is ("DID" = 2, "PID" = 3, "DEP" = 4)
+    -At procedure "PUBLIC"."SP_TEST"
+    -At procedure "PUBLIC"."SP_TEST"
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
