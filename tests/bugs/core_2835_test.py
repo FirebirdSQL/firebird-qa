223b2926666852478b28c3d9f13cecb1c26e3a90
pavel-zotov <p519446@yandex.ru>
2025-06-27 10:04:01 +0300

    Added/Updated tests\bugs\core_2835_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2835_test.py
+++ tests/bugs/core_2835_test.py
@@ -7,12 +7,18 @@ TITLE:       Natural is used to select instead of primary key index
 DESCRIPTION:
 JIRA:        CORE-2835
 FBTEST:      bugs.core_2835
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-db_1 = db_factory()
+db = db_factory()
 
 test_script = """
     create table net_net_device(
@@ -75,15 +81,19 @@ test_script = """
       and t0_dep.id=t3_nd_dependantdevices_relation.secondary_devid;
 """
 
-act = isql_act('db_1', test_script)
 
-expected_stdout = """
+act = isql_act('db', test_script)
+
+expected_stdout_5x = """
     PLAN SORT (JOIN (T1_ND INDEX (PK_NET_NET_DEVICE), T3_ND_DEPENDANTDEVICES_RELATION INDEX (FK_NET_DEV_INTERCONNECTION_001), T0_DEP INDEX (PK_NET_NET_DEVICE)))
+"""
+
+expected_stdout_6x = """
+    PLAN SORT (JOIN ("T1_ND" INDEX ("PUBLIC"."PK_NET_NET_DEVICE"), "T3_ND_DEPENDANTDEVICES_RELATION" INDEX ("PUBLIC"."FK_NET_DEV_INTERCONNECTION_001"), "T0_DEP" INDEX ("PUBLIC"."PK_NET_NET_DEVICE")))
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
