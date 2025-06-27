4b7d0e69fe794443b0805d674adf487fa42ecb13
pavel-zotov <p519446@yandex.ru>
2025-06-27 23:31:01 +0300

    Added/Updated tests\bugs\core_3627_test.py: Output of constraint name is suppressed (not needed for this test). Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3627_test.py
+++ tests/bugs/core_3627_test.py
@@ -7,6 +7,12 @@ TITLE:       Server crashes with access violation when inserting row into table 
 DESCRIPTION:
 JIRA:        CORE-3627
 FBTEST:      bugs.core_3627
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -38,15 +44,20 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 23000
     attempt to store duplicate value (visible to active transactions) in unique index "CLASSIDKSGIDX"
     -Problematic key value is ("CLASSID" = 1, "KSGFK" = NULL)
 """
 
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 23000
+    attempt to store duplicate value (visible to active transactions) in unique index "PUBLIC"."CLASSIDKSGIDX"
+    -Problematic key value is ("CLASSID" = 1, "KSGFK" = NULL)
+"""
+
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
