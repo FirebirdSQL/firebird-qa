6fd7a2f7c66fd9db64ddaa1cc6b918928fb47be8
pavel-zotov <p519446@yandex.ru>
2025-06-27 15:34:58 +0300

    Added/Updated tests\bugs\core_3283_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3283_test.py
+++ tests/bugs/core_3283_test.py
@@ -7,6 +7,12 @@ TITLE:       BAD PLAN with using LEFT OUTER JOIN in SUBSELECT
 DESCRIPTION:
 JIRA:        CORE-3283
 FBTEST:      bugs.core_3283
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -43,14 +49,18 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
+expected_out_5x = """
     PLAN SORT (JOIN (T2 INDEX (TTT_ID), T3 INDEX (TTT_ID)))
     PLAN (T1 INDEX (TTT_ID))
 """
 
+expected_out_6x = """
+    PLAN SORT (JOIN ("T2" INDEX ("PUBLIC"."TTT_ID"), "T3" INDEX ("PUBLIC"."TTT_ID")))
+    PLAN ("T1" INDEX ("PUBLIC"."TTT_ID"))
+"""
+
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_out_5x if act.is_version('<6') else expected_out_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
