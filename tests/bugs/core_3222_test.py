eb90953b3480a8c8525c4d4e88d89687b94688f6
pavel-zotov <p519446@yandex.ru>
2025-06-27 15:25:29 +0300

    Added/Updated tests\bugs\core_3222_test.py: Re-implemented: use f-notation in order to remove hard-coded DDL string from test_script and expected_out.. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3222_test.py
+++ tests/bugs/core_3222_test.py
@@ -7,47 +7,53 @@ TITLE:       View with "WITH CHECK OPTION" doesn't like TRIM function in WHERE
 DESCRIPTION:
 JIRA:        CORE-3222
 FBTEST:      bugs.core_3222
+NOTES:
+    [27.06.2025] pzotov
+    Re-implemented: use f-notation in order to remove hard-coded DDL string from test_script and expected_out.
+    Removed 'SHOW VIEW'command because its output can change in any intensive developing FB version.
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE Foo (
-  Bar INTEGER,
-  Str CHAR(31)
-);
-COMMIT;
+db = db_factory()
+
+VIEW_DDL = """
+    select bar
+    from test
+    where(trim(str) = 'test')
+    with check option
 """
 
-db = db_factory(init=init_script)
+test_script = f"""
+    set list on;
+    set blob all;
+    set count on;
+    create table test (
+      bar integer,
+      str char(31)
+    );
+    commit;
 
-test_script = """CREATE VIEW VIEW_Foo (
-     Bar
-) AS SELECT
-     Bar
-  FROM Foo
-  WHERE(Trim(Str) = 'test')
-WITH CHECK OPTION
-;
-COMMIT;
-SHOW VIEW VIEW_Foo;
+    create view v_test (bar) as
+    {VIEW_DDL}
+    ;
+    commit;
+    select r.rdb$view_source as blob_id from rdb$relations r where r.rdb$relation_name = upper('v_test');
 """
 
-act = isql_act('db', test_script)
+substitutions = [('BLOB_ID .*', '')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """BAR                             INTEGER Nullable
-View Source:
-==== ======
- SELECT
-     Bar
-  FROM Foo
-  WHERE(Trim(Str) = 'test')
-WITH CHECK OPTION
+expected_stdout = f"""
+    {VIEW_DDL}
+    Records affected: 1
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
