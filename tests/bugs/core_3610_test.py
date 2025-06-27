f521a473fd0b56dcf067c983b5f70dab41dd470a
pavel-zotov <p519446@yandex.ru>
2025-06-27 23:28:14 +0300

    Added/Updated tests\bugs\core_3610_test.py: Output of constraint name is suppressed (not needed for this test). Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3610_test.py
+++ tests/bugs/core_3610_test.py
@@ -7,14 +7,16 @@ TITLE:       Can insert DUPLICATE keys in UNIQUE index
 DESCRIPTION:
 JIRA:        CORE-3610
 FBTEST:      bugs.core_3610
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
-
-substitutions = [('Data source : Firebird::localhost:.*', 'Data source : Firebird::localhost:'),
-                 ('335544382 : Problematic key', '335545072 : Problematic key'),
-                 ('-At block line: [\\d]+, col: [\\d]+', '-At block line')]
 
 db = db_factory()
 
@@ -40,28 +42,24 @@ test_script = """
     set term ;^
     rollback;
 
-    --                                    ||||||||||||||||||||||||||||
-    -- ###################################|||  FB 4.0+, SS and SC  |||##############################
-    --                                    ||||||||||||||||||||||||||||
-    -- If we check SS or SC and ExtConnPoolLifeTime > 0 (config parameter FB 4.0+) then current
-    -- DB (bugs.core_NNNN.fdb) will be 'captured' by firebird.exe process and fbt_run utility
-    -- will not able to drop this database at the final point of test.
-    -- Moreover, DB file will be hold until all activity in firebird.exe completed and AFTER this
-    -- we have to wait for <ExtConnPoolLifeTime> seconds after it (discussion and small test see
-    -- in the letter to hvlad and dimitr 13.10.2019 11:10).
-    -- This means that one need to kill all connections to prevent from exception on cleanup phase:
-    -- SQLCODE: -901 / lock time-out on wait transaction / object <this_test_DB> is in use
-    -- #############################################################################################
-    delete from mon$attachments where mon$attachment_id != current_connection;
-    commit;
-
     set list on;
     select * from test;
 """
 
+substitutions = [('[ \t]+', ' '),
+                 ('Data source : Firebird::localhost:.*', 'Data source : Firebird::localhost:'),
+                 ('335544382 : Problematic key', '335545072 : Problematic key'),
+                 ('-At block line: [\\d]+, col: [\\d]+', '-At block line')]
 act = isql_act('db', test_script, substitutions=substitutions)
 
-expected_stdout = """
+expected_stdout_5x = """
+    Statement failed, SQLSTATE = 42000
+    Execute statement error at isc_dsql_execute2 :
+    335544665 : violation of PRIMARY or UNIQUE KEY constraint "TEST_UNQ" on table "TEST"
+    335545072 : Problematic key value is ("F01" = 1)
+    Statement : update test set f01 = ? where id = ?
+    Data source : Firebird::localhost:
+    -At block line
     ID                              1
     F01                             1
     ID                              2
@@ -70,22 +68,25 @@ expected_stdout = """
     F01                             <null>
 """
 
-expected_stderr = """
+expected_stdout_6x = """
     Statement failed, SQLSTATE = 42000
     Execute statement error at isc_dsql_execute2 :
-    335544665 : violation of PRIMARY or UNIQUE KEY constraint "TEST_UNQ" on table "TEST"
+    335544665 : violation of PRIMARY or UNIQUE KEY constraint "TEST_UNQ" on table "PUBLIC"."TEST"
     335545072 : Problematic key value is ("F01" = 1)
     Statement : update test set f01 = ? where id = ?
     Data source : Firebird::localhost:
-    -At block line: 3, col: 9
+    -At block line
+    ID                              1
+    F01                             1
+    ID                              2
+    F01                             <null>
+    ID                              3
+    F01                             <null>
 """
 
 @pytest.mark.es_eds
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
