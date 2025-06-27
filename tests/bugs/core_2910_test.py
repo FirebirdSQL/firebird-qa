408aea8aed977646eb75b52200ed929d34c1c9f1
pavel-zotov <p519446@yandex.ru>
2025-06-27 10:42:13 +0300

    Added/Updated tests\bugs\core_2910_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_2910_test.py
+++ tests/bugs/core_2910_test.py
@@ -7,46 +7,55 @@ TITLE:       PK index is not used for derived tables
 DESCRIPTION:
 JIRA:        CORE-2910
 FBTEST:      bugs.core_2910
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE R$TMP (
-    POSTING_ID INTEGER
-);
+db = db_factory()
 
-CREATE TABLE TMP (
-    POSTING_ID INTEGER NOT NULL
-);
+test_script = """
+    create table r$tmp (
+        posting_id integer
+    );
 
-ALTER TABLE TMP ADD CONSTRAINT PK_TMP PRIMARY KEY (POSTING_ID);
-commit;
-"""
+    create table tmp (
+        posting_id integer not null
+    );
 
-db = db_factory(charset='UTF8', init=init_script)
+    alter table tmp add constraint pk_tmp primary key (posting_id);
+    commit;
 
-test_script = """SET PLAN ON;
-select r.POSTING_ID as r$POSTING_ID, t.POSTING_ID from (
-      SELECT POSTING_ID
-      FROM r$tmp
-    ) r left join (
-        select POSTING_ID from
-          (select
-            posting_id
-          from tmp)
-    ) t on r.POSTING_ID = t.POSTING_ID;
+    set plan on;
+    select r.posting_id as r$posting_id, t.posting_id from (
+          select posting_id
+          from r$tmp
+        ) r left join (
+            select posting_id from
+              (select
+                posting_id
+              from tmp)
+        ) t on r.posting_id = t.posting_id;
 """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-PLAN JOIN (R R$TMP NATURAL, T TMP INDEX (PK_TMP))
+expected_stdout_5x = """
+    PLAN JOIN (R R$TMP NATURAL, T TMP INDEX (PK_TMP))
 """
 
-@pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
+expected_stdout_6x = """
+    PLAN JOIN ("R" "PUBLIC"."R$TMP" NATURAL, "T" "PUBLIC"."TMP" INDEX ("PUBLIC"."PK_TMP"))
+"""
 
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
