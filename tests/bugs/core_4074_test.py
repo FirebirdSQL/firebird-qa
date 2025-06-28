3b9fa5fbf644daf8444156d067f31b61e6181976
pavel-zotov <p519446@yandex.ru>
2025-06-28 21:47:57 +0300

    Added/Updated tests\bugs\core_4074_test.py: Replaced 'SHOW' command with query to RDB tables. Bug still can be seen in 2.5.9.27156. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4074_test.py
+++ tests/bugs/core_4074_test.py
@@ -7,40 +7,65 @@ TITLE:       Computed by columns and position function
 DESCRIPTION:
 JIRA:        CORE-4074
 FBTEST:      bugs.core_4074
+NOTES:
+    [28.06.2025] pzotov
+    Replaced 'SHOW' command with query to RDB tables.
+    See also test for https://github.com/FirebirdSQL/firebird/issues/4357
+    Bug still can be seen in 2.5.9.27156 - rdb$computed_source has weird content:
+    =======
+    ( 'fabio ' || position('x','schunig') ),
+      f02 numeric(8,2) default 0
+    )
+    =======
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """
-    recreate table test01 (
-      f01 computed by ( 'fabio ' || position('x','schunig') ),
+db = db_factory()
+
+COMPUTED_EXPR = "'fabio ' || position('x','schunig')"
+test_script = f"""
+    set list on;
+    set blob all;
+    recreate table test (
+      f01 computed by ( {COMPUTED_EXPR} ),
       f02 numeric(8,2) default 0
     );
+    commit;
+    select
+        rf.rdb$field_name fld_name
+        ,f.rdb$field_type fld_type
+        ,f.rdb$field_length fld_length
+        ,f.rdb$field_scale fld_scale
+        ,f.rdb$computed_source as rdb_blob_id
+    from rdb$relation_fields rf
+    left join rdb$fields f on rf.rdb$field_source = f.rdb$field_name
+    where rf.rdb$relation_name = upper('TEST');
 """
 
-db = db_factory(init=init_script)
-
-test_script = """
-    show table test01;
-    -- ::: NB ::: On WI-V2.5.4.26856, 26-mar-2015, output is:
-    -- F01                             Computed by: ( 'fabio ' || position('x','schunig') ),
-    --   f02 numeric(8,2) default 0
-    -- )
-    -- F02                             NUMERIC(8, 2) Nullable )
-    -- (i.e. it DOES contain "strange" last line)
-"""
-
-act = isql_act('db', test_script)
+substitutions = [ ('[ \t]+', ' '), ('RDB_BLOB_ID.*', '') ]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-    F01                             Computed by: ( 'fabio ' || position('x','schunig') )
-    F02                             NUMERIC(8, 2) Nullable default 0
+    FLD_NAME                        F01
+    FLD_TYPE                        37
+    FLD_LENGTH                      17
+    FLD_SCALE                       0
+    RDB_BLOB_ID                     2:1e4
+    ( 'fabio ' || position('x','schunig') )
+
+    FLD_NAME                        F02
+    FLD_TYPE                        8
+    FLD_LENGTH                      4
+    FLD_SCALE                       -2
+    RDB_BLOB_ID                     <null>
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
