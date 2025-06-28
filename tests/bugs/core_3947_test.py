dfaa7dbe9bb8d68ad38cf5273ac89a17cc6152e1
pavel-zotov <p519446@yandex.ru>
2025-06-28 15:31:46 +0300

    Added/Updated tests\bugs\core_3947_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3947_test.py
+++ tests/bugs/core_3947_test.py
@@ -9,6 +9,12 @@ DESCRIPTION:
   not be usable for ORDER, only for lookups.
 JIRA:        CORE-3947
 FBTEST:      bugs.core_3947
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -45,16 +51,22 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
+expected_stdout_5x = """
     PLAN (T ORDER T_S1_NON_UNQ)
     PLAN SORT (T NATURAL)
     PLAN SORT (T NATURAL)
 """
 
+expected_stdout_6x = """
+    PLAN ("PUBLIC"."T" ORDER "PUBLIC"."T_S1_NON_UNQ")
+    PLAN SORT ("PUBLIC"."T" NATURAL)
+    PLAN SORT ("PUBLIC"."T" NATURAL)
+"""
+
 @pytest.mark.intl
 @pytest.mark.version('>=3.0')
+@pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
