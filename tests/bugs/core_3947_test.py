492c70f83cfa863eabca2adc9eef7f2bce98d680
pavel-zotov <p519446@yandex.ru>
2025-06-28 15:33:21 +0300

    Added/Updated tests\bugs\core_3947_test.py: removed duplicated mark

--- tests/bugs/core_3947_test.py
+++ tests/bugs/core_3947_test.py
@@ -65,7 +65,6 @@ expected_stdout_6x = """
 
 @pytest.mark.intl
 @pytest.mark.version('>=3.0')
-@pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
