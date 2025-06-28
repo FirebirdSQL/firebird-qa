962502b5c05b07679b350de4014a9a1b917243a6
pavel-zotov <p519446@yandex.ru>
2025-06-28 23:00:54 +0300

    Added/Updated tests\bugs\core_4122_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4122_test.py
+++ tests/bugs/core_4122_test.py
@@ -7,6 +7,12 @@ TITLE:       Metadata export with isql (option -ex) does not export functions pr
 DESCRIPTION:
 JIRA:        CORE-4122
 FBTEST:      bugs.core_4122
+NOTES:
+    [28.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -51,7 +57,7 @@ db = db_factory(init=init_script)
 
 act = python_act('db', substitutions=[('/* CREATE DATABASE .*', '')])
 
-expected_stdout = """
+expected_stdout_5x = """
     SET SQL DIALECT 3;
 
     /* CREATE DATABASE 'localhost/3330:test.fdb' PAGE_SIZE 4096 DEFAULT CHARACTER SET NONE; */
@@ -112,6 +118,7 @@ expected_stdout = """
     /* Package bodies */
 
     /* Package body: PKG_TEST, Owner: SYSDBA */
+
     CREATE PACKAGE BODY PKG_TEST AS
     begin
       function F_TEST_INSIDE_PKG
@@ -127,10 +134,75 @@ expected_stdout = """
     SET AUTODDL ON;
 """
 
+expected_stdout_6x = """
+    SET SQL DIALECT 3;
+    /*
+    /* Schema definitions */
+    /* Schema: PUBLIC, Owner: SYSDBA */
+    CREATE OR ALTER SCHEMA PUBLIC;
+    COMMIT WORK;
+    COMMIT WORK;
+    COMMIT WORK;
+    SET AUTODDL OFF;
+    SET TERM ^ ;
+    /* Stored functions headers */
+    CREATE OR ALTER FUNCTION PUBLIC.F_TEST_OUTSIDE_PKG RETURNS SMALLINT
+    AS
+    BEGIN END ^
+    SET TERM ; ^
+    COMMIT WORK;
+    SET AUTODDL ON;
+    COMMIT WORK;
+    SET AUTODDL OFF;
+    SET TERM ^ ;
+    /* Package headers */
+    /* Package header: PUBLIC.PKG_TEST, Owner: SYSDBA */
+    CREATE PACKAGE PUBLIC.PKG_TEST AS
+    begin
+    function F_TEST_INSIDE_PKG
+    returns smallint;
+    end^
+    SET TERM ; ^
+    COMMIT WORK;
+    SET AUTODDL ON;
+    COMMIT WORK;
+    SET AUTODDL OFF;
+    SET TERM ^ ;
+    /* Stored functions bodies */
+    ALTER FUNCTION PUBLIC.F_TEST_OUTSIDE_PKG RETURNS SMALLINT
+    AS
+    begin
+    return -1;
+    end ^
+    SET TERM ; ^
+    COMMIT WORK;
+    SET AUTODDL ON;
+    COMMIT WORK;
+    SET AUTODDL OFF;
+    SET TERM ^ ;
+    /* Package bodies */
+    /* Package body: PUBLIC.PKG_TEST, Owner: SYSDBA */
+    CREATE PACKAGE BODY PUBLIC.PKG_TEST AS
+    begin
+    function F_TEST_INSIDE_PKG
+    returns smallint
+    as
+    begin
+    return 1;
+    end
+    end^
+    SET TERM ; ^
+    COMMIT WORK;
+    SET AUTODDL ON;
+    /* Grant permissions for this database */
+    GRANT USAGE ON SCHEMA PUBLIC TO USER PUBLIC;
+"""
+
+
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.isql(switches=['-x'])
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.isql(switches = ['-x'], combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
 
