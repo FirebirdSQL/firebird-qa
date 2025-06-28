0b0db13b913659bce856c31dc5d118acbe3749c9
pavel-zotov <p519446@yandex.ru>
2025-06-28 14:08:05 +0300

    Added/Updated tests\bugs\core_3736_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3736_test.py
+++ tests/bugs/core_3736_test.py
@@ -8,6 +8,15 @@ TITLE:       WITH LOCK clause is allowed for users with read-only rights on some
 DESCRIPTION:
 JIRA:        CORE-3736
 FBTEST:      bugs.core_3736
+NOTES:
+    [28.06.2025] pzotov
+    Reimplemented: use variables to be used (via f-notations) in expected_out_* instead of hard-coding.
+    Only error messages are checked in this test (rather than both stdout and stderr).
+
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -18,8 +27,12 @@ db = db_factory()
 user_1_ro = user_factory('db', name='tmp$c3736_ro', password='tmp$c3736_ro')
 user_1_ud = user_factory('db', name='tmp$c3736_ud', password='tmp$c3736_ud')
 
-test_script = """
+READ_ONLY_STTM = "select current_user, 'select_read_only', id from t_read_only_for_non_sys"
+WITH_LOCK_STTM = "select current_user, 'select_with_lock', id from t_read_only_for_non_sys for update with lock"
+
+test_script = f"""
     set wng off;
+    set list on;
 
     recreate table t_read_only_for_non_sys(id int);
     commit;
@@ -52,7 +65,7 @@ test_script = """
       into v_dbname;
 
       for
-        execute statement 'select current_user, ''select_read_only'', id from t_read_only_for_non_sys' --  for update with lock'
+        execute statement q'#{READ_ONLY_STTM}#'
         on external (v_dbname)
         as user (v_usr_ro) password (v_pwd_ro)
         into who_am_i, my_action, what_i_see
@@ -60,7 +73,7 @@ test_script = """
         suspend;
 
       for
-        execute statement 'select current_user, ''select_with_lock'', id from t_read_only_for_non_sys for update with lock'
+        execute statement q'#{WITH_LOCK_STTM}#'
         on external (v_dbname)
         as user (v_usr_ro) password (v_pwd_ro)
         into who_am_i, my_action, what_i_see
@@ -90,7 +103,7 @@ test_script = """
       into v_dbname;
 
       for
-        execute statement 'select current_user, ''select_with_lock'', id from t_read_only_for_non_sys for update with lock'
+        execute statement q'#{WITH_LOCK_STTM}#'
         on external (v_dbname)
         as user (v_usr_ud) password (v_pwd_ud)
         into who_am_i, my_action, what_i_see
@@ -101,54 +114,70 @@ test_script = """
     ^
     set term ;^
     rollback;
-
-    --                                    ||||||||||||||||||||||||||||
-    -- ###################################|||  FB 4.0+, SS and SC  |||##############################
-    --                                    ||||||||||||||||||||||||||||
-    -- If we check SS or SC and ExtConnPoolLifeTime > 0 (config parameter FB 4.0+) then current
-    -- DB (bugs.core_NNNN.fdb) will be 'captured' by firebird.exe process and fbt_run utility
-    -- will not able to drop this database at the final point of test.
-    -- Moreover, DB file will be hold until all activity in firebird.exe completed and AFTER this
-    -- we have to wait for <ExtConnPoolLifeTime> seconds after it (discussion and small test see
-    -- in the letter to hvlad and dimitr 13.10.2019 11:10).
-    -- This means that one need to kill all connections to prevent from exception on cleanup phase:
-    -- SQLCODE: -901 / lock time-out on wait transaction / object <this_test_DB> is in use
-    -- #############################################################################################
-    delete from mon$attachments where mon$attachment_id != current_connection;
-    commit;
 """
 
-act = isql_act('db', test_script,
-                 substitutions=[('^((?!335544352|335544878).)*$', ''), ('number is.*', '')])
+# Only error messages are checked in this test:
+substitutions = [ ('(-)?At block line(:)?\\s+\\d+.*', ''),
+                  ('Data source : Firebird::.*', 'Data source : Firebird::'),
+                  ('335544878 : concurrent transaction number.*', '335544878 : concurrent transaction number'),
+                ]
 
-expected_stdout = """
-    WHO_AM_I                        MY_ACTION              WHAT_I_SEE
-    =============================== ==================== ============
-    TMP$C3736_RO                    select_read_only                1
-    TMP$C3736_RO                    select_read_only                2
-    TMP$C3736_RO                    select_read_only                3
-    TMP$C3736_RO                    select_read_only                4
-    TMP$C3736_RO                    select_read_only                5
-
-
-    WHO_AM_I                        MY_ACTION              WHAT_I_SEE
-    =============================== ==================== ============
-    TMP$C3736_UD                    select_with_lock                1
-    TMP$C3736_UD                    select_with_lock                2
-    TMP$C3736_UD                    select_with_lock                3
-"""
-
-expected_stderr = """
-    335544352 : no permission for UPDATE access to TABLE T_READ_ONLY_FOR_NON_SYS
-    335544878 : concurrent transaction number is 806
-"""
+act = isql_act('db', test_script, substitutions = substitutions)
 
 @pytest.mark.es_eds
 @pytest.mark.version('>=3.0')
 def test_1(act: Action, user_1_ro: User, user_1_ud: User):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
 
+    expected_stderr_3x = f"""
+        Statement failed, SQLSTATE = 42000
+        Execute statement error at isc_dsql_prepare :
+        335544352 : no permission for UPDATE access to TABLE T_READ_ONLY_FOR_NON_SYS
+        Statement : {WITH_LOCK_STTM}
+        Data source : Firebird::
+
+        Statement failed, SQLSTATE = 42000
+        Execute statement error at isc_dsql_fetch :
+        335544336 : deadlock
+        335544451 : update conflicts with concurrent update
+        335544878 : concurrent transaction number
+        Statement : {WITH_LOCK_STTM}
+        Data source : Firebird::
+    """
+
+    expected_stderr_5x = f"""
+        Statement failed, SQLSTATE = 42000
+        Execute statement error at isc_dsql_prepare :
+        335544352 : no permission for UPDATE access to TABLE T_READ_ONLY_FOR_NON_SYS
+        335545254 : Effective user is {user_1_ro.name.upper()}
+        Statement : {WITH_LOCK_STTM}
+        Data source : Firebird::
+        
+        Statement failed, SQLSTATE = 42000
+        Execute statement error at isc_dsql_fetch :
+        335544336 : deadlock
+        335544451 : update conflicts with concurrent update
+        335544878 : concurrent transaction number
+        Statement : {WITH_LOCK_STTM}
+        Data source : Firebird::
+    """
+
+    expected_stderr_6x = f"""
+        Statement failed, SQLSTATE = 42000
+        Execute statement error at isc_dsql_prepare :
+        335544352 : no permission for UPDATE access to TABLE "PUBLIC"."T_READ_ONLY_FOR_NON_SYS"
+        335545254 : Effective user is {user_1_ro.name.upper()}
+        Statement : {WITH_LOCK_STTM}
+        Data source : Firebird::
+
+        Statement failed, SQLSTATE = 42000
+        Execute statement error at isc_dsql_fetch :
+        335544336 : deadlock
+        335544451 : update conflicts with concurrent update
+        335544878 : concurrent transaction number
+        Statement : {WITH_LOCK_STTM}
+        Data source : Firebird::
+    """
+
+    act.expected_stderr = expected_stderr_3x if act.is_version('<4') else expected_stderr_5x if act.is_version('<6') else expected_stderr_6x
+    act.execute(combine_output = False) # ::: NB ::: we need to parse only error messages.
+    assert act.clean_stderr == act.clean_expected_stderr
