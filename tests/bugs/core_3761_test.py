8ac9f1523f006be9979dae60636c7c12c1f76d0a
pavel-zotov <p519446@yandex.ru>
2025-06-28 14:12:25 +0300

    Added/Updated tests\bugs\core_3761_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3761_test.py
+++ tests/bugs/core_3761_test.py
@@ -9,20 +9,24 @@ JIRA:        CORE-3761
 FBTEST:      bugs.core_3761
 NOTES:
     [25.11.2023] pzotov
-    Writing code requires more care since 6.0.0.150: ISQL does not allow specifying duplicate delimiters without any statements between them (two semicolon, two carets etc).
+    Writing code requires more care since 6.0.0.150: ISQL does not allow specifying duplicate
+    delimiters without any statements between them (two semicolon, two carets etc).
+
+    [28.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """
-    create exception check_exception 'check exception';
-    commit;
-"""
-
-db = db_factory(init=init_script)
+db = db_factory()
 
 test_script = """
+    create exception check_exception 'check exception';
+    commit;
     set term ^;
     execute block as
     begin
@@ -31,19 +35,24 @@ test_script = """
     set term ;^
 """
 
-act = isql_act('db', test_script, substitutions=[('-At block line: [\\d]+, col: [\\d]+', '-At block line')])
+act = isql_act('db', test_script, substitutions=[('(-)?At block line(:)?\\s+\\d+.*', '')])
 
-expected_stdout = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = HY000
     exception 1
     -CHECK_EXCEPTION
     -word
-    -At block line: 4, col: 2
 """
 
-@pytest.mark.version('>=3')
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = HY000
+    exception 1
+    -"PUBLIC"."CHECK_EXCEPTION"
+    -word
+"""
+
+@pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
