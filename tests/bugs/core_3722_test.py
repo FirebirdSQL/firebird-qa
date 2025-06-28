6e6c3460c583f1d1e68fc5bc3acf5646629bc1f2
pavel-zotov <p519446@yandex.ru>
2025-06-28 12:59:07 +0300

    Added/Updated tests\bugs\core_3722_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_3722_test.py
+++ tests/bugs/core_3722_test.py
@@ -7,36 +7,49 @@ TITLE:       IS NOT DISTINCT FROM NULL doesn't use index
 DESCRIPTION:
 JIRA:        CORE-3722
 FBTEST:      bugs.core_3722
+NOTES:
+    [27.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """create table t (a varchar(5));
-create index t_a on t (a);
-"""
+db = db_factory()
 
-db = db_factory(init=init_script)
-
-test_script = """SET PLAN ON;
-select * from t where a is null;
-select * from t where a is not distinct from null;
-select * from t where a is not distinct from null PLAN (T INDEX (T_A));
-select * from t where a is not distinct from nullif('', '');
+test_script = """
+    create table test (f01 int);
+    insert into test select iif(rand() < 0.5, null, rand()*1000) from rdb$types;
+    commit;
+    create index test_f01 on test (f01);
+    set planonly;
+    select * from test where f01 is null;
+    select * from test where f01 is not distinct from null;
+    select * from test where f01 is not distinct from null PLAN (test INDEX (test_f01));
+    select * from test where f01 is not distinct from nullif('', '');
 """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-PLAN (T INDEX (T_A))
-PLAN (T INDEX (T_A))
-PLAN (T INDEX (T_A))
-PLAN (T INDEX (T_A))
+expected_stdout_5x = """
+    PLAN (TEST INDEX (TEST_F01))
+    PLAN (TEST INDEX (TEST_F01))
+    PLAN (TEST INDEX (TEST_F01))
+    PLAN (TEST INDEX (TEST_F01))
+"""
+
+expected_stdout_6x = """
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_F01"))
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_F01"))
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_F01"))
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_F01"))
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
