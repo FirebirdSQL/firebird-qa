bdac4ba6bc5e2e5f6a517b4f7c2308cf12a89e79
pavel-zotov <p519446@yandex.ru>
2025-06-28 22:46:55 +0300

    Added/Updated tests\bugs\core_4119_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4119_test.py
+++ tests/bugs/core_4119_test.py
@@ -7,6 +7,12 @@ TITLE:       Metadata source becomes wrong after twice transliteration to the me
 DESCRIPTION:
 JIRA:        CORE-4119
 FBTEST:      bugs.core_4119
+NOTES:
+    [28.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -18,28 +24,36 @@ db = db_factory(charset='WIN1251')
 act = python_act('db', substitutions=[('=.*', '')])
 script_file = temp_file('test_script.sql')
 
-expected_stdout = """
+expected_stdout_5x = """
     Procedure text:
-    =============================================================================
     begin
     -- Моя процедура
     end
-    =============================================================================
+"""
+
+expected_stdout_6x = """
+    Procedure: PUBLIC.MYPROC
+    Procedure text:
+    begin
+    -- Моя процедура
+    end
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action, script_file: Path):
-    script_file.write_text("""
-    set term ^;
-    create procedure myproc as
-    begin
-        -- Моя процедура
-    end^
-    set term ;^
-    show procedure myproc;
-    """, encoding='cp1251')
-    act.expected_stdout = expected_stdout
-    act.isql(switches=['-q'], input_file=script_file, charset='WIN1251')
-    assert act.clean_stdout == act.clean_expected_stdout
+    sp_ddl = """
+        set term ^;
+        create procedure myproc as
+        begin
+            -- Моя процедура
+        end^
+        set term ;^
+        show procedure myproc;
+    """
 
+    script_file.write_text(sp_ddl, encoding='cp1251')
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.isql(switches=['-q'], input_file = script_file, charset='win1251')
+    assert act.clean_stdout == act.clean_expected_stdout
 
