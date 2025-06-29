9abef8b78b3e03bfde246c665f63477c4c23f63f
pavel-zotov <p519446@yandex.ru>
2025-06-29 19:09:48 +0300

    Added/Updated tests\bugs\core_4539_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4539_test.py
+++ tests/bugs/core_4539_test.py
@@ -7,6 +7,12 @@ TITLE:       Server does not accept the right plan
 DESCRIPTION:
 JIRA:        CORE-4539
 FBTEST:      bugs.core_4539
+NOTES:
+    [29.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -41,13 +47,16 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
+expected_stdout_5x = """
     PLAN JOIN (H NATURAL, C INDEX (COLOR_NAME))
+"""
+
+expected_stdout_6x = """
+    PLAN JOIN ("H" NATURAL, "C" INDEX ("PUBLIC"."COLOR_NAME"))
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
