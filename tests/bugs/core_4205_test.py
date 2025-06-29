2cd912bbe297b2c187125b1d3b8d0db778971054
pavel-zotov <p519446@yandex.ru>
2025-06-29 13:31:18 +0300

    Added/Updated tests\bugs\core_4205_test.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4205_test.py
+++ tests/bugs/core_4205_test.py
@@ -7,6 +7,10 @@ TITLE:       ISQL -x does not output the START WITH clause of generators/sequenc
 DESCRIPTION:
 JIRA:        CORE-4205
 FBTEST:      bugs.core_4205
+NOTES:
+    [29.06.2025] pzotov
+    Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -23,15 +27,17 @@ db = db_factory(sql_dialect=3, init=init_script)
 
 act = python_act('db', substitutions=[('^((?!(SQLSTATE|CREATE GENERATOR)).)*$', '')])
 
-expected_stdout = """
-    CREATE GENERATOR TMP_GEN_42051 START WITH 9223372036854775807 INCREMENT -2147483647;
-    CREATE GENERATOR TMP_GEN_42052 START WITH -9223372036854775808 INCREMENT 2147483647;
-    CREATE GENERATOR TMP_GEN_42053 START WITH 9223372036854775807 INCREMENT 2147483647;
-    CREATE GENERATOR TMP_GEN_42054 START WITH -9223372036854775808 INCREMENT -2147483647;
-"""
-
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else  'PUBLIC.'
+    expected_stdout = f"""
+        CREATE GENERATOR {SQL_SCHEMA_PREFIX}TMP_GEN_42051 START WITH 9223372036854775807 INCREMENT -2147483647;
+        CREATE GENERATOR {SQL_SCHEMA_PREFIX}TMP_GEN_42052 START WITH -9223372036854775808 INCREMENT 2147483647;
+        CREATE GENERATOR {SQL_SCHEMA_PREFIX}TMP_GEN_42053 START WITH 9223372036854775807 INCREMENT 2147483647;
+        CREATE GENERATOR {SQL_SCHEMA_PREFIX}TMP_GEN_42054 START WITH -9223372036854775808 INCREMENT -2147483647;
+    """
+
     act.expected_stdout = expected_stdout
     act.isql(switches=['-x'], combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
