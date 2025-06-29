bc1f3ca6d98a40c592679e6f907c3c59fd02a102
pavel-zotov <p519446@yandex.ru>
2025-06-29 16:47:45 +0300

    Added/Updated tests\bugs\core_4373_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4373_test.py
+++ tests/bugs/core_4373_test.py
@@ -7,6 +7,12 @@ TITLE:       Duplicate names in package are not checked
 DESCRIPTION:
 JIRA:        CORE-4373
 FBTEST:      bugs.core_4373
+NOTES:
+    [29.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -49,7 +55,7 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 42000
     CREATE PACKAGE NEW_PACKAGE failed
     -Duplicate PROCEDURE EXTERNAL_PROC
@@ -59,9 +65,18 @@ expected_stderr = """
     -Duplicate PROCEDURE INTERNAL_PROC
 """
 
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 42000
+    CREATE PACKAGE "PUBLIC"."NEW_PACKAGE" failed
+    -Duplicate PROCEDURE "EXTERNAL_PROC"
+
+    Statement failed, SQLSTATE = 42000
+    CREATE PACKAGE BODY "PUBLIC"."NEW_PACKAGE" failed
+    -Duplicate PROCEDURE "INTERNAL_PROC"
+"""
+
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
