568606e38a5ecc6fd69aae9cd13630a82b5910e9
pavel-zotov <p519446@yandex.ru>
2025-06-29 17:48:17 +0300

    Added/Updated tests\bugs\core_4453_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4453_test.py
+++ tests/bugs/core_4453_test.py
@@ -5,19 +5,24 @@ ID:          issue-4773
 ISSUE:       4773
 TITLE:       Regression: NOT NULL constraint, declared in domain, does not work
 DESCRIPTION:
+    Tests that manipulates with NULL fields/domains and check results:
+    CORE-1518 Adding a non-null restricted column to a populated table renders the table inconsistent
+    CORE-4453 (Regression: NOT NULL constraint, declared in domain, does not work)
+    CORE-4725 (Inconsistencies with ALTER DOMAIN and ALTER TABLE with DROP NOT NULL and PRIMARY KEYs)
+    CORE-4733 (Command "Alter table <T> alter TYPE <C> <DOMAIN_WITH_NOT_NULL" does not verifies data in column <C> and makes incorrect assignments in <C> to ZERO / JULIAN_DATE / ASCII(0) for types INT, TIMESTAMP and VARCHAR)
 JIRA:        CORE-4453
 FBTEST:      bugs.core_4453
+    [29.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
 init_script = """
-    -- Tests that manipulates with NULL fields/domains and check results:
-    -- CORE-1518 Adding a non-null restricted column to a populated table renders the table inconsistent
-    -- CORE-4453 (Regression: NOT NULL constraint, declared in domain, does not work)
-    -- CORE-4725 (Inconsistencies with ALTER DOMAIN and ALTER TABLE with DROP NOT NULL and PRIMARY KEYs)
-    -- CORE-4733 (Command "Alter table <T> alter TYPE <C> <DOMAIN_WITH_NOT_NULL" does not verifies data in column <C> and makes incorrect assignments in <C> to ZERO / JULIAN_DATE / ASCII(0) for types INT, TIMESTAMP and VARCHAR)
     create domain dm_01 varchar(20) not null;
     commit;
     create table t_01(s dm_01, x int);
@@ -33,14 +38,18 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 23000
     validation error for column "T_01"."S", value "*** null ***"
 """
 
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 23000
+    validation error for column "PUBLIC"."T_01"."S", value "*** null ***"
+"""
+
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
