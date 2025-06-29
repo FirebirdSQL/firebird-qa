c291756ee70984e638a40424785c63231de07267
pavel-zotov <p519446@yandex.ru>
2025-06-29 10:30:24 +0300

    Added/Updated tests\bugs\core_4149_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4149_test.py
+++ tests/bugs/core_4149_test.py
@@ -7,6 +7,12 @@ TITLE:       New permission types are not displayed by ISQL
 DESCRIPTION:
 JIRA:        CORE-4149
 FBTEST:      bugs.core_4149
+NOTES:
+    [29.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -31,18 +37,29 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-/* Grant permissions for this database */
-GRANT SELECT ON TEST TO PUBLIC
+expected_stdout_5x = """
+    /* Grant permissions for this database */
+    GRANT SELECT ON TEST TO PUBLIC
 
-/* Grant permissions for this database */
-GRANT SELECT ON TEST TO PUBLIC
-GRANT USAGE ON SEQUENCE G_TEST TO PUBLIC
+    /* Grant permissions for this database */
+    GRANT SELECT ON TEST TO PUBLIC
+    GRANT USAGE ON SEQUENCE G_TEST TO PUBLIC
+"""
+
+expected_stdout_6x = """
+    /* Grant permissions for this database */
+    GRANT SELECT ON PUBLIC.TEST TO USER PUBLIC
+    GRANT USAGE ON SCHEMA PUBLIC TO USER PUBLIC
+
+    /* Grant permissions for this database */
+    GRANT SELECT ON PUBLIC.TEST TO USER PUBLIC
+    GRANT USAGE ON SEQUENCE PUBLIC.G_TEST TO USER PUBLIC
+    GRANT USAGE ON SCHEMA PUBLIC TO USER PUBLIC
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
