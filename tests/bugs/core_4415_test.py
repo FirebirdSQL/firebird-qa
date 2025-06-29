f38705d26e9b681d12faa9dce9a4abb01d9ac172
pavel-zotov <p519446@yandex.ru>
2025-06-29 17:32:49 +0300

    Added/Updated tests\bugs\core_4415_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4415_test.py
+++ tests/bugs/core_4415_test.py
@@ -7,6 +7,12 @@ TITLE:       Useless extraction of generic DDL trigger
 DESCRIPTION:
 JIRA:        CORE-4415
 FBTEST:      bugs.core_4415
+NOTES:
+    [29.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -21,17 +27,21 @@ test_script = """
     -- TR, Sequence: 0, Type: BEFORE CREATE TABLE OR ALTER TABLE OR DROP TABLE OR ... OR <unknown>, Active // length = 967 characters.
 """
 
-act = isql_act('db', test_script, substitutions=[('\\+.*', ''), ('\\=.*', ''),
-                                                 ('Trigger text.*', '')])
+substitutions = [('\\+.*', ''), ('\\=.*', ''), ('Trigger text.*', '')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
+expected_stdout_5x = """
     TR, Sequence: 0, Type: BEFORE ANY DDL STATEMENT, Active
+    as begin end
+"""
+
+expected_stdout_6x = """
+    PUBLIC.TR, Sequence: 0, Type: BEFORE ANY DDL STATEMENT, Active
     as begin end
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
