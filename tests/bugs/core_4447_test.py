c21718b1c0cc6e9cf7ad8b82a1ab43cdc0119d4f
pavel-zotov <p519446@yandex.ru>
2025-06-29 17:35:00 +0300

    Added/Updated tests\bugs\core_4447_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4447_test.py
+++ tests/bugs/core_4447_test.py
@@ -7,6 +7,12 @@ TITLE:       Positioned UPDATE statement prohibits index usage for the subsequen
 DESCRIPTION:
 JIRA:        CORE-4447
 FBTEST:      bugs.core_4447
+NOTES:
+    [29.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -45,14 +51,18 @@ test_script = """
 
 act = isql_act('db', test_script, substitutions = [('(--\\s+)?line \\d+, col(umn)? \\d+', '')])
 
-expected_stdout = """
-  PLAN (T INDEX (TT_PK_XY))
-  PLAN (C TS NATURAL)
+expected_stdout_5x = """
+    PLAN (T INDEX (TT_PK_XY))
+    PLAN (C TS NATURAL)
+"""
+
+expected_stdout_6x = """
+    PLAN ("T" INDEX ("PUBLIC"."TT_PK_XY"))
+    PLAN ("C" "PUBLIC"."TS" NATURAL)
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
