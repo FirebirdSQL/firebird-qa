9e9e90b4c20eeaf0f340695d491de251614cf0a8
pavel-zotov <p519446@yandex.ru>
2025-06-29 17:02:18 +0300

    Added/Updated tests\bugs\core_4381_test.py: Added subst to suppress displaying name of stored procedure: on 6.x it is prefixed by SQL schema and enclosed in quotes. For this test it is enough just to show proper numbers of line and column. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4381_test.py
+++ tests/bugs/core_4381_test.py
@@ -7,6 +7,12 @@ TITLE:       Incorrect line/column information in runtime errors
 DESCRIPTION:
 JIRA:        CORE-4381
 FBTEST:      bugs.core_4381
+NOTES:
+    [29.06.2025] pzotov
+    Added subst to suppress displaying name of stored procedure: on 6.x it is prefixed by SQL schema and enclosed in quotes.
+    For this test it is enough just to show proper numbers of line and column.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -32,17 +38,17 @@ execute procedure p1
 ^
 """
 
-act = isql_act('db', test_script)
+substitutions = [('-At procedure \\S+', 'At procedure')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stderr = """
+expected_stdout = """
     Statement failed, SQLSTATE = 22018
     conversion error from string "a"
-    -At procedure 'P1' line: 3, col: 28
+    At procedure line: 3, col: 28
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
-
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
