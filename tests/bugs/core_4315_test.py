3eff925852f557f56ab690287babb6a94280fd73
pavel-zotov <p519446@yandex.ru>
2025-06-29 15:04:48 +0300

    Added/Updated tests\bugs\core_4315_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4315_test.py
+++ tests/bugs/core_4315_test.py
@@ -3,25 +3,27 @@
 """
 ID:          issue-4638
 ISSUE:       4638
-TITLE:       Usage of field(s) alias in view WITH CHECK OPTION leads to incorrect compile
-  error or incorrect internal triggers
+TITLE:       Usage of field(s) alias in view WITH CHECK OPTION leads to incorrect compile error or incorrect internal triggers
 DESCRIPTION:
 JIRA:        CORE-4315
 FBTEST:      bugs.core_4315
+NOTES:
+    [29.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """
+db = db_factory()
+
+test_script = """
     recreate view v1 as select 1 id from rdb$database;
     commit;
     recreate table t1 (n1 integer, n2 integer);
-"""
-
-db = db_factory(init=init_script)
-
-test_script = """
     -- Compilation error in LI-T3.0.0.30830 (13-jan-2014): "Column unknown":
     recreate view v1 as select t1.n1 from t1 t1 where t1.n1 < t1.n2 with check option;
     recreate view v1 as select t1.n1 from t1 where t1.n1 < t1.n2 with check option;
@@ -39,9 +41,10 @@ test_script = """
     order by rdb$trigger_name;
 """
 
-act = isql_act('db', test_script, substitutions=[('RDB\\$TRIGGER_BLR.*', '')])
+substitutions = [('[\t ]+', ' '), ('RDB\\$TRIGGER_BLR.*', '')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
+expected_stdout_5x = """
 RDB$TRIGGER_BLR                 c:2d2
         	blr_version5,
         	blr_begin,
@@ -82,9 +85,171 @@ RDB$TRIGGER_BLR                 c:2d3
         	blr_eoc
 """
 
+expected_stdout_6x = """
+RDB$TRIGGER_BLR                 c:1e0
+        	blr_version5,
+        	blr_begin,
+        	   blr_for,
+        	      blr_rse, 1,
+        	         blr_relation2, 2, 'T','1',
+        	            4, 34,'T','1',34, 2,
+        	         blr_boolean,
+        	            blr_and,
+        	               blr_lss,
+        	                  blr_field, 2, 2, 'N','1',
+        	                  blr_field, 2, 2, 'N','2',
+        	               blr_equiv,
+        	                  blr_field, 0, 2, 'N','1',
+        	                  blr_field, 2, 2, 'N','1',
+        	         blr_end,
+        	      blr_if,
+        	         blr_lss,
+        	            blr_field, 1, 2, 'N','1',
+        	            blr_field, 2, 2, 'N','2',
+        	         blr_begin,
+        	            blr_end,
+        	         blr_abort, blr_gds_code, 16, 'c','h','e','c','k','_','c','o','n','s','t','r','a','i','n','t',
+        	   blr_end,
+        	blr_eoc
+
+
+RDB$TRIGGER_BLR                 c:1e1
+        	blr_version5,
+        	blr_begin,
+        	   blr_if,
+        	      blr_lss,
+        	         blr_field, 1, 2, 'N','1',
+        	         blr_null,
+        	      blr_begin,
+        	         blr_end,
+        	      blr_abort, blr_gds_code, 16, 'c','h','e','c','k','_','c','o','n','s','t','r','a','i','n','t',
+        	   blr_end,
+        	blr_eoc
+
+
+RDB$TRIGGER_BLR                 c:1e2
+        	blr_version5,
+        	blr_begin,
+        	   blr_for,
+        	      blr_rse, 1,
+        	         blr_relation, 2, 'T','1', 2,
+        	         blr_boolean,
+        	            blr_and,
+        	               blr_lss,
+        	                  blr_field, 2, 2, 'N','1',
+        	                  blr_field, 2, 2, 'N','2',
+        	               blr_equiv,
+        	                  blr_field, 0, 2, 'N','1',
+        	                  blr_field, 2, 2, 'N','1',
+        	         blr_end,
+        	      blr_if,
+        	         blr_lss,
+        	            blr_field, 1, 2, 'N','1',
+        	            blr_field, 2, 2, 'N','2',
+        	         blr_begin,
+        	            blr_end,
+        	         blr_abort, blr_gds_code, 16, 'c','h','e','c','k','_','c','o','n','s','t','r','a','i','n','t',
+        	   blr_end,
+        	blr_eoc
+
+
+RDB$TRIGGER_BLR                 c:1e3
+        	blr_version5,
+        	blr_begin,
+        	   blr_if,
+        	      blr_lss,
+        	         blr_field, 1, 2, 'N','1',
+        	         blr_null,
+        	      blr_begin,
+        	         blr_end,
+        	      blr_abort, blr_gds_code, 16, 'c','h','e','c','k','_','c','o','n','s','t','r','a','i','n','t',
+        	   blr_end,
+        	blr_eoc
+
+
+RDB$TRIGGER_BLR                 c:1e4
+        	blr_version5,
+        	blr_begin,
+        	   blr_for,
+        	      blr_rse, 1,
+        	         blr_relation2, 2, 'T','1',
+        	            3, 34,'X',34, 2,
+        	         blr_boolean,
+        	            blr_and,
+        	               blr_lss,
+        	                  blr_field, 2, 2, 'N','1',
+        	                  blr_field, 2, 2, 'N','2',
+        	               blr_equiv,
+        	                  blr_field, 0, 2, 'N','1',
+        	                  blr_field, 2, 2, 'N','1',
+        	         blr_end,
+        	      blr_if,
+        	         blr_lss,
+        	            blr_field, 1, 2, 'N','1',
+        	            blr_field, 2, 2, 'N','2',
+        	         blr_begin,
+        	            blr_end,
+        	         blr_abort, blr_gds_code, 16, 'c','h','e','c','k','_','c','o','n','s','t','r','a','i','n','t',
+        	   blr_end,
+        	blr_eoc
+
+
+RDB$TRIGGER_BLR                 c:1e5
+        	blr_version5,
+        	blr_begin,
+        	   blr_if,
+        	      blr_lss,
+        	         blr_field, 1, 2, 'N','1',
+        	         blr_null,
+        	      blr_begin,
+        	         blr_end,
+        	      blr_abort, blr_gds_code, 16, 'c','h','e','c','k','_','c','o','n','s','t','r','a','i','n','t',
+        	   blr_end,
+        	blr_eoc
+
+
+RDB$TRIGGER_BLR                 c:1e6
+        	blr_version5,
+        	blr_begin,
+        	   blr_for,
+        	      blr_rse, 1,
+        	         blr_relation, 2, 'T','1', 2,
+        	         blr_boolean,
+        	            blr_and,
+        	               blr_lss,
+        	                  blr_field, 2, 2, 'N','1',
+        	                  blr_field, 2, 2, 'N','2',
+        	               blr_equiv,
+        	                  blr_field, 0, 1, 'A',
+        	                  blr_field, 2, 2, 'N','1',
+        	         blr_end,
+        	      blr_if,
+        	         blr_lss,
+        	            blr_field, 1, 1, 'A',
+        	            blr_field, 2, 2, 'N','2',
+        	         blr_begin,
+        	            blr_end,
+        	         blr_abort, blr_gds_code, 16, 'c','h','e','c','k','_','c','o','n','s','t','r','a','i','n','t',
+        	   blr_end,
+        	blr_eoc
+
+
+RDB$TRIGGER_BLR                 c:1e7
+        	blr_version5,
+        	blr_begin,
+        	   blr_if,
+        	      blr_lss,
+        	         blr_field, 1, 1, 'A',
+        	         blr_null,
+        	      blr_begin,
+        	         blr_end,
+        	      blr_abort, blr_gds_code, 16, 'c','h','e','c','k','_','c','o','n','s','t','r','a','i','n','t',
+        	   blr_end,
+        	blr_eoc
+"""
+
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
