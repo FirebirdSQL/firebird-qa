f09b4101ec2fad9afc65ae2304eea863ccf15b2c
pavel-zotov <p519446@yandex.ru>
2025-06-29 13:48:34 +0300

    Added/Updated tests\bugs\core_4244_test.py: Replaced 'SHOW' command with query to RDB tables. Bug still can be seen in 2.5.9.27156. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4244_test.py
+++ tests/bugs/core_4244_test.py
@@ -7,6 +7,11 @@ TITLE:       Problem with creation procedure which contain adding text in DOS864
 DESCRIPTION:
 JIRA:        CORE-4244
 FBTEST:      bugs.core_4244
+NOTES:
+    [29.06.2025] pzotov
+    Confirmed bug on 2.1.7.18553.
+    Replaced 'SHOW' command with query to RDB tables.
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -14,9 +19,7 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """
-    set term ^;
-    create or alter procedure sp_test as
+PROC_DDL = """
         declare char_one_byte char(1) character set dos864;
         declare str varchar(1000) character set dos864;
     begin
@@ -24,31 +27,29 @@ test_script = """
         str='B';
         str=str||char_one_byte;
     end
+"""
+test_script = f"""
+    set list on;
+    set count on;
+    set term ^;
+    create or alter procedure sp_test as
+    {PROC_DDL}
     ^
     set term ;^
     commit;
-    -- Confirmed for 2.1.7:
-    -- Statement failed, SQLCODE = -802
-    -- arithmetic exception, numeric overflow, or string truncation
-    -- -Cannot transliterate character between character sets
-    show proc sp_test;
+    select p.rdb$procedure_source as blob_id from rdb$procedures p where p.rdb$procedure_name = upper('sp_test');
 """
 
-act = isql_act('db', test_script, substitutions=[('=.*', '')])
+substitutions = [('BLOB_ID.*', '')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-    Procedure text:
-        declare char_one_byte char(1) character set dos864;
-        declare str varchar(1000) character set dos864;
-    begin
-        char_one_byte='A';
-        str='B';
-        str=str||char_one_byte;
-    end
+expected_stdout = f"""
+    {PROC_DDL}
+    Records affected: 1
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
