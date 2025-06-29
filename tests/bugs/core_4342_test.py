00531426466eb6d82b2b1cd8ff476cd4b192a2d2
pavel-zotov <p519446@yandex.ru>
2025-06-29 15:51:37 +0300

    Added/Updated tests\bugs\core_4342_test.py: Re-implemented: use f-notation to substitute fixture values in the expected output. Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4342_test.py
+++ tests/bugs/core_4342_test.py
@@ -7,101 +7,135 @@ TITLE:       Non-privileged user can delete records from RDB$SECURITY_CLASSES ta
 DESCRIPTION:
 JIRA:        CORE-4342
 FBTEST:      bugs.core_4342
+NOTES:
+    [29.06.2025] pzotov
+    Re-implemented: use f-notation to substitute fixture values in the expected output.
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
 db = db_factory()
-user_boss = user_factory('db', name='boss', password='123')
-user_mngr = user_factory('db', name='mngr', password='456')
+tmp_boss = user_factory('db', name='boss', password='123')
+tmp_mngr = user_factory('db', name='mngr', password='456')
 
-test_script = """
-    -- Add these DDL privileges in order to have some rows in
-    -- rdb$security_classes table for user BOSS:
-    grant create table to boss;
-    grant alter any table to boss;
-    grant drop any table to boss;
-    commit;
+substitutions = [('[\t ]+', ' ')]
+act = isql_act('db', substitutions = substitutions)
 
-    set list on;
-    select current_user,count(*) acl_count from rdb$security_classes where rdb$acl containing 'boss';
+@pytest.mark.version('>=3.0')
+def test_1(act: Action, tmp_boss: User, tmp_mngr: User):
 
-    select 1 from rdb$security_classes where rdb$acl containing 'boss' with lock;
-    update rdb$security_classes set rdb$security_class = rdb$security_class where rdb$acl containing 'boss';
-    delete from rdb$security_classes where rdb$acl containing 'boss';
-    commit;
+    test_script = f"""
+        -- Add these DDL privileges in order to have some rows in
+        -- rdb$security_classes table for user {tmp_boss.name}:
+        grant create table to {tmp_boss.name};
+        grant alter any table to {tmp_boss.name};
+        grant drop any table to {tmp_boss.name};
+        commit;
 
-    connect '$(DSN)' user 'MNGR' password '456';
-    select current_user,count(*) acl_count from rdb$security_classes where rdb$acl containing 'boss';
+        set list on;
+        select current_user,count(*) acl_count from rdb$security_classes where rdb$acl containing '{tmp_boss.name}';
 
-    select 1 from rdb$security_classes where rdb$acl containing 'boss' with lock;
-    update rdb$security_classes set rdb$security_class = rdb$security_class where rdb$acl containing 'boss';
-    delete from rdb$security_classes where rdb$acl containing 'boss';
-    commit;
-"""
+        select 1 from rdb$security_classes where rdb$acl containing '{tmp_boss.name}' with lock;
+        update rdb$security_classes set rdb$security_class = rdb$security_class where rdb$acl containing '{tmp_boss.name}';
+        delete from rdb$security_classes where rdb$acl containing '{tmp_boss.name}';
+        commit;
 
-expected_stdout = """
-    USER                            SYSDBA
-    ACL_COUNT                       1
+        connect '{act.db.dsn}' user '{tmp_mngr.name}' password '{tmp_mngr.password}';
+        select current_user,count(*) acl_count from rdb$security_classes where rdb$acl containing '{tmp_boss.name}';
 
-    USER                            MNGR
-    ACL_COUNT                       1
-"""
+        select 1 from rdb$security_classes where rdb$acl containing '{tmp_boss.name}' with lock;
+        update rdb$security_classes set rdb$security_class = rdb$security_class where rdb$acl containing '{tmp_boss.name}';
+        delete from rdb$security_classes where rdb$acl containing '{tmp_boss.name}';
+        commit;
+    """
 
-# version: 3.0
+    expected_stdout_3x = f"""
+        USER                            {act.db.user.upper()}
+        ACL_COUNT                       1
 
-act = isql_act('db', test_script)
+        Statement failed, SQLSTATE = HY000
+        Cannot select system table RDB$SECURITY_CLASSES for update WITH LOCK
 
-expected_stderr_1 = """
-    Statement failed, SQLSTATE = HY000
-    Cannot select system table RDB$SECURITY_CLASSES for update WITH LOCK
-    Statement failed, SQLSTATE = 42000
-    UPDATE operation is not allowed for system table RDB$SECURITY_CLASSES
-    Statement failed, SQLSTATE = 42000
-    DELETE operation is not allowed for system table RDB$SECURITY_CLASSES
-    Statement failed, SQLSTATE = HY000
-    Cannot select system table RDB$SECURITY_CLASSES for update WITH LOCK
-    Statement failed, SQLSTATE = 28000
-    no permission for UPDATE access to TABLE RDB$SECURITY_CLASSES
-    Statement failed, SQLSTATE = 28000
-    no permission for DELETE access to TABLE RDB$SECURITY_CLASSES
-"""
+        Statement failed, SQLSTATE = 42000
+        UPDATE operation is not allowed for system table RDB$SECURITY_CLASSES
+
+        Statement failed, SQLSTATE = 42000
+        DELETE operation is not allowed for system table RDB$SECURITY_CLASSES
+
+        USER                            {tmp_mngr.name.upper()}
+        ACL_COUNT                       1
+
+        Statement failed, SQLSTATE = HY000
+        Cannot select system table RDB$SECURITY_CLASSES for update WITH LOCK
+
+        Statement failed, SQLSTATE = 28000
+        no permission for UPDATE access to TABLE RDB$SECURITY_CLASSES
+
+        Statement failed, SQLSTATE = 28000
+        no permission for DELETE access to TABLE RDB$SECURITY_CLASSES
+    """
+
+    expected_stdout_5x = f"""
+        USER                            {act.db.user.upper()}
+        ACL_COUNT                       1
+
+        Statement failed, SQLSTATE = HY000
+        Cannot select system table RDB$SECURITY_CLASSES for update WITH LOCK
+
+        Statement failed, SQLSTATE = 42000
+        UPDATE operation is not allowed for system table RDB$SECURITY_CLASSES
+
+        Statement failed, SQLSTATE = 42000
+        DELETE operation is not allowed for system table RDB$SECURITY_CLASSES
+
+        USER                            {tmp_mngr.name.upper()}
+        ACL_COUNT                       1
+
+        Statement failed, SQLSTATE = HY000
+        Cannot select system table RDB$SECURITY_CLASSES for update WITH LOCK
+
+        Statement failed, SQLSTATE = 28000
+        no permission for UPDATE access to TABLE RDB$SECURITY_CLASSES
+        -Effective user is {tmp_mngr.name.upper()}
+
+        Statement failed, SQLSTATE = 28000
+        no permission for DELETE access to TABLE RDB$SECURITY_CLASSES
+        -Effective user is {tmp_mngr.name.upper()}
+    """
 
 
-@pytest.mark.version('>=3.0,<4.0')
-def test_1(act: Action, user_boss: User, user_mngr: User):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr_1
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+    expected_stdout_6x = f"""
+        USER                            {act.db.user.upper()}
+        ACL_COUNT                       1
 
-# version: 4.0
+        Statement failed, SQLSTATE = HY000
+        Cannot select system table "SYSTEM"."RDB$SECURITY_CLASSES" for update WITH LOCK
 
-expected_stderr_2 = """
-    Statement failed, SQLSTATE = HY000
-    Cannot select system table RDB$SECURITY_CLASSES for update WITH LOCK
-    Statement failed, SQLSTATE = 42000
-    UPDATE operation is not allowed for system table RDB$SECURITY_CLASSES
-    Statement failed, SQLSTATE = 42000
-    DELETE operation is not allowed for system table RDB$SECURITY_CLASSES
-    Statement failed, SQLSTATE = HY000
-    Cannot select system table RDB$SECURITY_CLASSES for update WITH LOCK
-    Statement failed, SQLSTATE = 28000
-    no permission for UPDATE access to TABLE RDB$SECURITY_CLASSES
-    -Effective user is MNGR
-    Statement failed, SQLSTATE = 28000
-    no permission for DELETE access to TABLE RDB$SECURITY_CLASSES
-    -Effective user is MNGR
-"""
+        Statement failed, SQLSTATE = 42000
+        UPDATE operation is not allowed for system table "SYSTEM"."RDB$SECURITY_CLASSES"
+        Statement failed, SQLSTATE = 42000
 
+        DELETE operation is not allowed for system table "SYSTEM"."RDB$SECURITY_CLASSES"
+        USER                            {tmp_mngr.name.upper()}
+        ACL_COUNT                       1
 
-@pytest.mark.version('>=4.0')
-def test_2(act: Action, user_boss: User, user_mngr: User):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr_2
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+        Statement failed, SQLSTATE = HY000
+        Cannot select system table "SYSTEM"."RDB$SECURITY_CLASSES" for update WITH LOCK
 
+        Statement failed, SQLSTATE = 28000
+        no permission for UPDATE access to TABLE "SYSTEM"."RDB$SECURITY_CLASSES"
+        -Effective user is {tmp_mngr.name.upper()}
+
+        Statement failed, SQLSTATE = 28000
+        no permission for DELETE access to TABLE "SYSTEM"."RDB$SECURITY_CLASSES"
+        -Effective user is {tmp_mngr.name.upper()}
+    """
+
+    act.expected_stdout = expected_stdout_3x if act.is_version('<4') else expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.isql(switches = ['-q'], input = test_script, combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
