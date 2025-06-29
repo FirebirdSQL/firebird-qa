4b230e94f5a0442dbb6659d73c1722f17fc57536
pavel-zotov <p519446@yandex.ru>
2025-06-29 18:06:00 +0300

    Added/Updated tests\bugs\core_4470_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4470_test.py
+++ tests/bugs/core_4470_test.py
@@ -10,6 +10,12 @@ NOTES:
   Replaced PSQL function name 'localtime()' with 'fn_local_time()': first of them became keyword in FB 4.0
 JIRA:        CORE-4470
 FBTEST:      bugs.core_4470
+NOTES:
+    [29.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -254,9 +260,14 @@ db = db_factory(init=init_script)
 
 act = python_act('db')
 
-expected_stdout = """
+expected_stdout_5x = """
     METEO
     WF
+"""
+
+expected_stdout_6x = """
+    PUBLIC.METEO
+    PUBLIC.WF
 """
 
 fbk_file = temp_file('test.fbk')
@@ -269,6 +280,13 @@ def test_1(act: Action, fbk_file: Path):
         srv.database.restore(backup=fbk_file, database=act.db.db_path,
                              flags=SrvRestoreFlag.REPLACE)
         srv.wait()
-    act.expected_stdout = expected_stdout
-    act.isql(switches=['-q'], input='show view; show package;')
+
+    test_script = """
+        show view;
+        show package;
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.isql(switches=['-q'], input = test_script, combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
+
