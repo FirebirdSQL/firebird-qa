4e65dc1e5dc20843ea720c9be0339a3e441849fd
pavel-zotov <p519446@yandex.ru>
2025-06-29 14:32:54 +0300

    Added/Updated tests\bugs\core_4281_test.py: Removed 'SHOW' command. It is enought to check that function returns proper result after 'ALTE DOMAIN' statement. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4281_test.py
+++ tests/bugs/core_4281_test.py
@@ -3,11 +3,14 @@
 """
 ID:          issue-4604
 ISSUE:       4604
-TITLE:       FB 3: TYPE OF arguments of stored functions will hang firebird engine
-  if depending domain or column is changed
+TITLE:       FB 3: TYPE OF arguments of stored functions will hang firebird engine if depending domain or column is changed
 DESCRIPTION:
 JIRA:        CORE-4281
 FBTEST:      bugs.core_4281
+NOTES:
+    [29.06.2025] pzotov
+    Removed 'SHOW' command. It is enought to check that function returns proper result after 'ALTE DOMAIN' statement.
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -16,28 +19,40 @@ from firebird.qa import *
 db = db_factory()
 
 test_script = """
-    create domain testdomain as integer;
-    commit;
+    set list on;
 
-    create function testfunction (arg1 type of testdomain) returns integer as
+    create domain dm_test as integer;
+    commit;
+    set term ^;
+    create function fn_test (a_x type of dm_test) returns integer as
     begin
-    end;
-
+        return sign(a_x);
+    end ^
+    set term ;^
     commit;
-    alter domain testdomain type bigint;
+    select fn_test(-2147483648) as fn_neg from rdb$database;
+    select fn_test( 2147483648) as fb_pos from rdb$database;
+    
+    alter domain dm_test type bigint;
     commit;
 
-    show domain testdomain;
+    select fn_test( 2147483648) as fb_pos from rdb$database;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[\t ]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-    TESTDOMAIN                      BIGINT Nullable
+    FN_NEG -1
+    Statement failed, SQLSTATE = 22003
+    arithmetic exception, numeric overflow, or string truncation
+    -numeric value is out of range
+    FB_POS 1
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
+
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
