dd017cae5ae8cd5ff851df236d5b579ef0fbf74c
pavel-zotov <p519446@yandex.ru>
2025-06-29 19:17:10 +0300

    Added/Updated tests\bugs\core_4557_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4557_test.py
+++ tests/bugs/core_4557_test.py
@@ -3,56 +3,53 @@
 """
 ID:          issue-1578
 ISSUE:       1578
-TITLE:       FB 3.0 crashes on EXIT (or QUIT) command if use UTF8-collation + create domain
-  based on it + issue SHOW DOMAIN
+TITLE:       FB 3.0 crashes on EXIT (or QUIT) command if use UTF8-collation + create domain based on it + issue SHOW DOMAIN
 DESCRIPTION:
 JIRA:        CORE-4557
 FBTEST:      bugs.core_4557
+NOTES:
+    [29.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """
+db = db_factory()
+
+test_script = """
   create collation name_coll for utf8 from unicode CASE INSENSITIVE;
   create collation nums_coll for utf8 from unicode CASE INSENSITIVE 'NUMERIC-SORT=1';
   commit;
   create domain dm_name as varchar(80) character set utf8 collate name_coll;
   create domain dm_nums as varchar(20) character set utf8 collate nums_coll;
   commit;
-"""
-
-db = db_factory(init=init_script)
-
-test_script = """
   show domain; -- FB crashes if this will be uncommented
   exit;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-# version: 3.0
-
-expected_stdout_1 = """
-  DM_NAME                                DM_NUMS
+expected_stdout_3x = """
+    DM_NAME DM_NUMS
 """
 
-@pytest.mark.version('>=3.0,<4.0')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout_1
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
-
-# version: 4.0
-
-expected_stdout_2 = """
+expected_stdout_5x = """
     DM_NAME
     DM_NUMS
 """
 
-@pytest.mark.version('>=4.0')
-def test_2(act: Action):
-    act.expected_stdout = expected_stdout_2
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
+expected_stdout_6x = """
+    PUBLIC.DM_NAME
+    PUBLIC.DM_NUMS
+"""
 
+@pytest.mark.version('>=3.0')
+def test_2(act: Action):
+    act.expected_stdout = expected_stdout_3x if act.is_version('<4') else expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
