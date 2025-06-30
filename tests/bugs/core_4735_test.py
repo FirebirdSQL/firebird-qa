1578b02fb37a84b0d6ceca05aa763cdcd45ff62e
pavel-zotov <p519446@yandex.ru>
2025-06-30 14:54:29 +0300

    Added/Updated tests\bugs\core_4735_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4735_test.py
+++ tests/bugs/core_4735_test.py
@@ -3,15 +3,20 @@
 """
 ID:          issue-5041
 ISSUE:       5041
-TITLE:       Expression 'where bool_field IS true | false' should also use index as
-  'where bool_field = true | false' (if such index exists)
+TITLE:       Expression 'where bool_field IS true | false' should also use index as 'where bool_field = true | false' (if such index exists)
 DESCRIPTION:
-NOTES:
-[28.01.2019]
-  Changed expected PLAN of execution after dimitr's letter 28.01.2019 17:28:
-  'is NOT <bool>' and 'is distinct from <bool>' should use  PLAN NATURAL.
 JIRA:        CORE-4735
 FBTEST:      bugs.core_4735
+NOTES:
+    [28.01.2019] pzotov
+    Changed expected PLAN of execution after dimitr's letter 28.01.2019 17:28:
+    'is NOT <bool>' and 'is distinct from <bool>' should use  PLAN NATURAL.
+
+    [30.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -36,7 +41,7 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
+expected_stdout_5x = """
     PLAN (TEST INDEX (TEST_X))
     PLAN (TEST INDEX (TEST_X))
     PLAN (TEST INDEX (TEST_X))
@@ -47,9 +52,19 @@ expected_stdout = """
     PLAN (TEST NATURAL)
 """
 
+expected_stdout_6x = """
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_X"))
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_X"))
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_X"))
+    PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_X"))
+    PLAN ("PUBLIC"."TEST" NATURAL)
+    PLAN ("PUBLIC"."TEST" NATURAL)
+    PLAN ("PUBLIC"."TEST" NATURAL)
+    PLAN ("PUBLIC"."TEST" NATURAL)
+"""
+
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
