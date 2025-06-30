edd3c28099662263545300c9fd5f6d1fa2a25f12
pavel-zotov <p519446@yandex.ru>
2025-06-30 18:52:51 +0300

    Added/Updated tests\bugs\core_4807_test.py: Removed check of STDOUT: no sense in this test. Only STDERR must be checked. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4807_test.py
+++ tests/bugs/core_4807_test.py
@@ -5,10 +5,14 @@ ID:          issue-5105
 ISSUE:       5105
 TITLE:       Regression: List of aggregation is not checked properly
 DESCRIPTION:
-  Field inside subquery not present in GROUP BY clause and therefore can't be used in
-  SELECT list as is (only as argument of some aggregation function).
+    Field inside subquery not present in GROUP BY clause and therefore can't be used in
+    SELECT list as is (only as argument of some aggregation function).
 JIRA:        CORE-4807
 FBTEST:      bugs.core_4807
+NOTES:
+    [30.06.2025] pzotov
+    Removed check of STDOUT: no sense in this test. Only STDERR must be checked.
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -29,11 +33,6 @@ test_script = """
 
 act = isql_act('db', test_script, substitutions=[('SORT \\(\\(T NATURAL\\)\\)', 'SORT (T NATURAL)')])
 
-expected_stdout = """
-    PLAN (RDB$DATABASE NATURAL)
-    PLAN SORT ((T NATURAL))
-"""
-
 expected_stderr = """
     Statement failed, SQLSTATE = 42000
     Dynamic SQL Error
@@ -41,11 +40,8 @@ expected_stderr = """
     -Invalid expression in the select list (not contained in either an aggregate function or the GROUP BY clause)
 """
 
-@pytest.mark.version('>=3')
+@pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
     act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
-
+    act.execute(combine_output = False)
+    assert act.clean_stderr == act.clean_expected_stderr
