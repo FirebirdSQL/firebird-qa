671bc9b8c15fc686135ca67e388c893aa5038bd1
pavel-zotov <p519446@yandex.ru>
2025-06-30 14:51:46 +0300

    Added/Updated tests\bugs\core_4733_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4733_test.py
+++ tests/bugs/core_4733_test.py
@@ -9,6 +9,12 @@ TITLE:       Command "Alter table <T> alter TYPE <C> <DOMAIN_WITH_NOT_NULL" does
 DESCRIPTION:
 JIRA:        CORE-4733
 FBTEST:      bugs.core_4733
+NOTES:
+    [30.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -64,40 +70,56 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-    NUM                             INTEGER Nullable
-    DTS                             TIMESTAMP Nullable
-    STR                             VARCHAR(10) CHARACTER SET UTF8 Nullable
-    BOO                             BOOLEAN Nullable
-
-    NUM                             <null>
-    DTS                             <null>
-    STR                             <null>
-    BOO                             <null>
-
-
-    NUM                             (DM_NN_INT) INTEGER Not Null
-    DTS                             (DM_NN_DTS) TIMESTAMP Not Null
-    STR                             (DM_NN_UTF) VARCHAR(10) CHARACTER SET UTF8 Not Null
-    BOO                             (DM_NN_BOO) BOOLEAN Not Null
-"""
-
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 22006
     unsuccessful metadata update
     -Cannot make field NUM of table TEST NOT NULL because there are NULLs present
     -Cannot make field DTS of table TEST NOT NULL because there are NULLs present
     -Cannot make field STR of table TEST NOT NULL because there are NULLs present
     -Cannot make field BOO of table TEST NOT NULL because there are NULLs present
+    NUM                             INTEGER Nullable
+    DTS                             TIMESTAMP Nullable
+    STR                             VARCHAR(10) CHARACTER SET UTF8 Nullable
+    BOO                             BOOLEAN Nullable
+    NUM                             <null>
+    DTS                             <null>
+    STR                             <null>
+    BOO                             <null>
     Statement failed, SQLSTATE = 23000
     validation error for column "TEST"."NUM", value "*** null ***"
+    NUM                             (DM_NN_INT) INTEGER Not Null
+    DTS                             (DM_NN_DTS) TIMESTAMP Not Null
+    STR                             (DM_NN_UTF) VARCHAR(10) CHARACTER SET UTF8 Not Null
+    BOO                             (DM_NN_BOO) BOOLEAN Not Null
+"""
+
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 22006
+    unsuccessful metadata update
+    -Cannot make field "NUM" of table "PUBLIC"."TEST" NOT NULL because there are NULLs present
+    -Cannot make field "DTS" of table "PUBLIC"."TEST" NOT NULL because there are NULLs present
+    -Cannot make field "STR" of table "PUBLIC"."TEST" NOT NULL because there are NULLs present
+    -Cannot make field "BOO" of table "PUBLIC"."TEST" NOT NULL because there are NULLs present
+    Table: PUBLIC.TEST
+    NUM                             INTEGER Nullable
+    DTS                             TIMESTAMP Nullable
+    STR                             VARCHAR(10) CHARACTER SET SYSTEM.UTF8 Nullable
+    BOO                             BOOLEAN Nullable
+    NUM                             <null>
+    DTS                             <null>
+    STR                             <null>
+    BOO                             <null>
+    Statement failed, SQLSTATE = 23000
+    validation error for column "PUBLIC"."TEST"."NUM", value "*** null ***"
+    Table: PUBLIC.TEST
+    NUM                             (PUBLIC.DM_NN_INT) INTEGER Not Null
+    DTS                             (PUBLIC.DM_NN_DTS) TIMESTAMP Not Null
+    STR                             (PUBLIC.DM_NN_UTF) VARCHAR(10) CHARACTER SET SYSTEM.UTF8 Not Null
+    BOO                             (PUBLIC.DM_NN_BOO) BOOLEAN Not Null
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
