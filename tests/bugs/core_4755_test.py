3dcc42cc1ff5a187c14e370eb6ded625077ddde2
pavel-zotov <p519446@yandex.ru>
2025-06-30 16:07:48 +0300

    Added/Updated tests\bugs\core_4755_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4755_test.py
+++ tests/bugs/core_4755_test.py
@@ -7,6 +7,12 @@ TITLE:       Parameterized exception: wrong output when number of arguments grea
 DESCRIPTION:
 JIRA:        CORE-4755
 FBTEST:      bugs.core_4755
+NOTES:
+    [30.06.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -80,19 +86,28 @@ test_script = """
 act = isql_act('db', test_script, substitutions=[('-At block line: [\\d]+, col: [\\d]+',
                                                   '-At block line')])
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = HY000
     exception 1
     -EX_SOMETHING_WRONG
     -Arguments for raising exeption:  10001  10002  10003  10004  10005  10006  10007  10008  10009
-    -At block line: 12, col: 5
+    -At block line
+    Statement failed, SQLSTATE = 07002
+    Number of arguments (10) exceeds the maximum (9) number of EXCEPTION USING arguments
+"""
+
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = HY000
+    exception 1
+    -"PUBLIC"."EX_SOMETHING_WRONG"
+    -Arguments for raising exeption:  10001  10002  10003  10004  10005  10006  10007  10008  10009
+    -At block line
     Statement failed, SQLSTATE = 07002
     Number of arguments (10) exceeds the maximum (9) number of EXCEPTION USING arguments
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
-
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
