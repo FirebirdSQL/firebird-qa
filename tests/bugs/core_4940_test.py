2b8f9a4296a5302db39b1520fd7f4f3f52742aea
pavel-zotov <p519446@yandex.ru>
2025-06-30 23:43:23 +0300

    Added/Updated tests\bugs\core_4940_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_4940_test.py
+++ tests/bugs/core_4940_test.py
@@ -19,44 +19,78 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """
-    -- 1. Specify 'deterministic' flag - it should be reflected in SHOW command:
-    set term ^;
-    create or alter function fn_infinity returns bigint deterministic as
+FUNC_DDL_1 = """
     begin
         return 9223372036854775807;
     end
+"""
+
+FUNC_DDL_2 = """
+    begin
+        return rand() * 9223372036854775807;
+    end
+"""
+
+test_script = f"""
+    -- 1. Specify 'deterministic' flag - it should be reflected in SHOW command:
+    set term ^;
+    create or alter function fn_test returns bigint deterministic as
+    {FUNC_DDL_1}
     ^
     set term ;^
     commit;
 
-    show function fn_infinity;
+    show function fn_test;
 
     -- 2. Remove 'deterministic' flag - it also should be reflected in SHOW command:
     set term ^;
-    alter function fn_infinity returns bigint as
-    begin
-        return 9223372036854775807;
-    end
+    alter function fn_test returns bigint as
+    {FUNC_DDL_2}
     ^
     set term ;^
     commit;
 
-    show function fn_infinity;
+    show function fn_test;
 """
 
-act = isql_act('db', test_script, substitutions=[('^((?!SQLSTATE|[Ff]unction|[Dd]eterministic).)*$', '')])
-
-expected_stdout = """
-    Deterministic function
-    Function text:
-
-    Function text:
-"""
+substitutions = [('===.*', '')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
+
+    expected_stdout_5x = f"""
+        Deterministic function
+        Function text:
+        {FUNC_DDL_1}
+
+        Parameters:
+        OUTPUT BIGINT
+
+        Function text:
+        {FUNC_DDL_2}
+
+        Parameters:
+        OUTPUT BIGINT
+    """
+
+    expected_stdout_6x = f"""
+        Function: PUBLIC.FN_TEST 
+        Deterministic function
+        Function text:
+        {FUNC_DDL_1}
+
+        Parameters:
+        OUTPUT BIGINT
+
+        Function: PUBLIC.FN_TEST 
+        Function text:
+        {FUNC_DDL_2}
+
+        Parameters:
+        OUTPUT BIGINT
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
