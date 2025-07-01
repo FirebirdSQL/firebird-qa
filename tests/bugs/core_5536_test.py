1a3af793122cd9d9e2293004f04d3c3b47542bb2
pavel-zotov <p519446@yandex.ru>
2025-07-01 22:46:49 +0300

    Added/Updated tests\bugs\core_5536_test.py: Refactored: we have to check only rows which contain either 'sqltype' or 'SQLSTATE'. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214.

--- tests/bugs/core_5536_test.py
+++ tests/bugs/core_5536_test.py
@@ -7,6 +7,11 @@ TITLE:       Connections compressed and encrypted in MON$ATTACHMENTS table
 DESCRIPTION:
 JIRA:        CORE-5536
 FBTEST:      bugs.core_5536
+NOTES:
+    [01.07.2025] pzotov
+    Refactored: we have to check only rows which contain either 'sqltype' or 'SQLSTATE'.
+    Added appropriate substitutions.
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
@@ -15,36 +20,24 @@ from firebird.qa import *
 db = db_factory()
 
 test_script = """
-    set planonly;
     set sqlda_display on;
     select mon$wire_compressed, mon$wire_encrypted
     from mon$attachments
     where mon$attachment_id = current_connection
     ;
-    -- Fields that were used before:
-    -- MON$CONNECTION_COMPRESSED in (false, true)
-    -- and MON$CONNECTION_ENCRYPTED in (false, true)
 """
 
-act = isql_act('db', test_script)
+substitutions=[('^((?!(SQLSTATE|sqltype)).)*$', ''), ('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-    INPUT message field count: 0
-
-    PLAN (MON$ATTACHMENTS NATURAL)
-
-    OUTPUT message field count: 2
     01: sqltype: 32764 BOOLEAN Nullable scale: 0 subtype: 0 len: 1
-      :  name: MON$WIRE_COMPRESSED  alias: MON$WIRE_COMPRESSED
-      : table: MON$ATTACHMENTS  owner: SYSDBA
     02: sqltype: 32764 BOOLEAN Nullable scale: 0 subtype: 0 len: 1
-      :  name: MON$WIRE_ENCRYPTED  alias: MON$WIRE_ENCRYPTED
-      : table: MON$ATTACHMENTS  owner: SYSDBA
 """
 
 @pytest.mark.version('>=4.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
