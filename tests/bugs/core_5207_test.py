e356356d79188c2e69468263f2f54ad0347a0cce
pavel-zotov <p519446@yandex.ru>
2025-07-01 15:47:25 +0300

    Added/Updated tests\bugs\core_5207_test.py: Added check to return_code for proper check outcome of extracting and applying metadata. Checked on 6.0.0.884; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_5207_test.py
+++ tests/bugs/core_5207_test.py
@@ -11,6 +11,10 @@ DESCRIPTION:
   Afterall, we try to apply extracted metadata to temp database (that was created auto by fbtest).
 JIRA:        CORE-5207
 FBTEST:      bugs.core_5207
+NOTES:
+    [01.07.2025] pzotov
+    Added check to return_code for proper check outcome of extracting and applying metadata.
+    Checked on 6.0.0.884 (SS/CS); 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -32,13 +36,18 @@ def test_1(act: Action, fbk_file: Path, fdb_file: Path, capsys):
     with act.connect_server() as srv:
         srv.database.restore(database=fdb_file, backup=fbk_file)
         srv.wait()
-    act.isql(switches=['-x', str(fdb_file)], connect_db=False)
+    # -------------------------------------------
+    act.isql(switches=['-x', str(fdb_file)], connect_db = False)
+    assert act.return_code == 0, f'Attempt to extract metadata failed:\n{act.clean_stdout}'
     metadata = act.stdout
+    act.reset()
+    # -------------------------------------------
     # Check metadata
     for line in metadata.splitlines():
         if 'GRANT USAGE ON DOMAIN' in line:
             pytest.fail(f'WRONG GRANT: {line}')
+    # -------------------------------------------
     # Apply metadata to main test database
-    act.reset()
-    act.isql(switches=[], input=metadata)
-    assert act.clean_stdout == act.clean_expected_stdout
+    act.xpected_stdout = ''
+    act.isql(switches = ['-q'], input = metadata, combine_output = True)
+    assert act.return_code == 0, f'Attempt to apply metadata failed:\n{act.clean_stdout}'
