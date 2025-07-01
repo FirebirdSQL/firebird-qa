3f1ec3b1a9271f3f5baf429d4f1005955006084d
pavel-zotov <p519446@yandex.ru>
2025-07-01 21:06:25 +0300

    Added/Updated tests\bugs\core_5313_test.py: Added forgotten branch for 3.x. Refactored: we have to check only rows which contain either 'sqltype' or 'SQLSTATE'. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33794.

--- tests/bugs/core_5313_test.py
+++ tests/bugs/core_5313_test.py
@@ -9,6 +9,8 @@ JIRA:        CORE-5313
 FBTEST:      bugs.core_5313
 NOTES:
     [01.07.2025] pzotov
+    Refactored: we have to check only rows which contain either 'sqltype' or 'SQLSTATE'.
+    Added appropriate substitutions.
     Separated expected output for FB major versions prior/since 6.x.
     No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
     
@@ -26,34 +28,28 @@ test_script = """
     select list(trim(rdb$relation_name), ?) from rdb$relations rows 0;
 """
 
-act = isql_act('db', test_script)
+substitutions=[('^((?!(SQLSTATE|sqltype)).)*$', ''), ('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
+
+expected_stdout_3x = """
+    01: sqltype: 452 TEXT scale: 0 subtype: 0 len: 3 charset: 3 UNICODE_FSS
+    01: sqltype: 520 BLOB Nullable scale: 0 subtype: 1 len: 8 charset: 3 UNICODE_FSS
+"""
 
 expected_stdout_5x = """
-    INPUT message field count: 1
     01: sqltype: 452 TEXT scale: 0 subtype: 0 len: 4 charset: 4 UTF8
-    :  name:   alias:
-    : table:   owner:
-    PLAN (RDB$RELATIONS NATURAL)
-    OUTPUT message field count: 1
     01: sqltype: 520 BLOB Nullable scale: 0 subtype: 1 len: 8 charset: 4 UTF8
-    :  name: LIST  alias: LIST
-    : table:   owner:
 """
 
 expected_stdout_6x = """
-    INPUT message field count: 1
     01: sqltype: 452 TEXT scale: 0 subtype: 0 len: 4 charset: 4 SYSTEM.UTF8
-    :  name:   alias:
-    : table:   schema:   owner:
-    PLAN ("SYSTEM"."RDB$RELATIONS" NATURAL)
-    OUTPUT message field count: 1
     01: sqltype: 520 BLOB Nullable scale: 0 subtype: 1 len: 8 charset: 4 SYSTEM.UTF8
-    :  name: LIST  alias: LIST
-    : table:   schema:   owner:
 """
+
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+
+    act.expected_stdout = expected_stdout_3x if act.is_version('<4') else expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
