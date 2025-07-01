ddedf3a3b7bbdb2b0ca1ea45592ae857fb501bbf
pavel-zotov <p519446@yandex.ru>
2025-07-01 15:00:02 +0300

    Added/Updated tests\bugs\core_5122_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.884; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_5122_test.py
+++ tests/bugs/core_5122_test.py
@@ -7,6 +7,12 @@ TITLE:       Expression index may not be used by the optimizer if created and us
 DESCRIPTION:
 JIRA:        CORE-5122
 FBTEST:      bugs.core_5122
+NOTES:
+    [01.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    
+    Checked on 6.0.0.884; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -16,8 +22,9 @@ db = db_factory()
 
 act = python_act('db')
 
-expected_stdout = """
-    PLAN (TEST INDEX (TEST_CALC_S))
+test_sql=  """
+    set planonly;
+    select * from test where 'zxc' || s starting with 'qwe';
 """
 
 @pytest.mark.version('>=3')
@@ -27,8 +34,15 @@ def test_1(act: Action):
         cur1.execute("recreate table test(s varchar(10))")
         cur1.execute("create index test_calc_s on test computed by ('zxc' || s)")
         con1.commit()
-    #
-    act.expected_stdout = expected_stdout
-    act.isql(switches=['-n'],
-               input="set planonly; select * from test where 'zxc' || s starting with 'qwe';")
+
+    expected_stdout_5x = """
+        PLAN (TEST INDEX (TEST_CALC_S))
+    """
+    expected_stdout_6x = """
+        PLAN ("PUBLIC"."TEST" INDEX ("PUBLIC"."TEST_CALC_S")) 
+    """
+    
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+
+    act.isql(switches=['-q', '-n'], input = test_sql)
     assert act.clean_stdout == act.clean_expected_stdout
