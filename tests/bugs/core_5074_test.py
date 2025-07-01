6500245daf165e9c4fecfd047fb1ecb9b034760b
pavel-zotov <p519446@yandex.ru>
2025-07-01 14:01:09 +0300

    Added/Updated tests\bugs\core_5074_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_5074_test.py
+++ tests/bugs/core_5074_test.py
@@ -7,6 +7,12 @@ TITLE:       Lost the charset ID in selection of array element
 DESCRIPTION:
 JIRA:        CORE-5074
 FBTEST:      bugs.core_5074
+NOTES:
+    [01.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -16,26 +22,33 @@ db = db_factory()
 
 test_script = """
     recreate table test(
-        a char(10)[0:3] character set octets
+        array_col char(10)[0:3] character set octets
     );
     set sqlda_display on;
-    select a[0] from test;
+    select array_col[0] from test;
 """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-    INPUT message field count: 0
-
-    OUTPUT message field count: 1
-    01: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 10 charset: 1 OCTETS
-      :  name: A  alias: A
-      : table: TEST  owner: SYSDBA
-"""
-
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    expected_stdout_5x = """
+        INPUT message field count: 0
+        OUTPUT message field count: 1
+        01: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 10 charset: 1 OCTETS
+        :  name: ARRAY_COL  alias: ARRAY_COL
+        : table: TEST  owner: SYSDBA
+    """
+
+    expected_stdout_6x = """
+        INPUT message field count: 0
+        OUTPUT message field count: 1
+        01: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 10 charset: 1 SYSTEM.OCTETS
+        :  name: ARRAY_COL  alias: ARRAY_COL
+        : table: TEST  schema: PUBLIC  owner: SYSDBA
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output =True)
     assert act.clean_stdout == act.clean_expected_stdout
 
