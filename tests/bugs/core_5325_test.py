e453a272b4f733a968fd4dfb3619d3d08789c526
pavel-zotov <p519446@yandex.ru>
2025-07-01 20:47:45 +0300

    Added/Updated tests\bugs\core_5325_test.py: Added 'SQL_SCHEMA_PREFIX' and variables to be substituted in expected_* on FB 6.x. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33794.

--- tests/bugs/core_5325_test.py
+++ tests/bugs/core_5325_test.py
@@ -7,6 +7,11 @@ TITLE:       Malformed string error when using cyrilic symbols and x'0d0a' in ex
 DESCRIPTION:
 JIRA:        CORE-5325
 FBTEST:      bugs.core_5325
+    [01.07.2025] pzotov
+    Added 'SQL_SCHEMA_PREFIX' and variables - to be substituted in expected_* on FB 6.x
+    Separated expected output for FB major versions prior/since 6.x.
+    
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -22,32 +27,34 @@ db = db_factory()
 act = python_act('db', substitutions=substitutions)
 
 test_script = """
-    create exception error_test 'йцукенг';
+    create exception exc_qwerty 'йцукенг';
     commit;
     set term ^;
     execute block as
     begin
-        exception error_test 'йцу' || _win1251 x'0d0a' || 'кенг';
+        exception exc_qwerty 'йцу' || _win1251 x'0d0a' || 'кенг';
     end^
     set term ;^
 """
 
 script_file = temp_file('test-script.sql')
 
-expected_stderr_1 = """
-    Statement failed, SQLSTATE = HY000
-    exception 1
-    -ERROR_TEST
-    -йцу
-
-    кенг
-"""
-
 @pytest.mark.version('>=3.0')
 def test_1(act: Action, script_file: Path):
     script_file.write_text(test_script, encoding='cp1251')
-    act.expected_stderr = expected_stderr_1
-    act.isql(switches=['-q'], input_file=script_file, charset='WIN1251')
-    assert act.clean_stderr == act.clean_expected_stderr
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    EXCEPTION_NAME = ('exc_qwerty' if act.is_version('<6') else '"exc_qwerty"').upper()
+    expected_out = f"""
+        Statement failed, SQLSTATE = HY000
+        exception 1
+        -{SQL_SCHEMA_PREFIX}{EXCEPTION_NAME}
+        -йцу
+
+        кенг
+    """
+    act.expected_stdout = expected_out
+    act.isql(switches=['-q'], input_file=script_file, charset='WIN1251', combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
 
 
