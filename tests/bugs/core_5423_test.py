3466c013cf7c70e96a6e3611dcbf8ecc3d5c1a58
pavel-zotov <p519446@yandex.ru>
2025-07-01 21:58:10 +0300

    Added/Updated tests\bugs\core_5423_test.py: Refactored: suppress output of column name that is unknown - it has no matter for this test. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_5423_test.py
+++ tests/bugs/core_5423_test.py
@@ -7,6 +7,10 @@ TITLE:       Regression: "Invalid usage of boolean expression" when use "BETWEEN
 DESCRIPTION:
 JIRA:        CORE-5423
 FBTEST:      bugs.core_5423
+NOTES:
+    [01.07.2025] pzotov
+    Refactored: suppress output of column name ('foo') that is unknown - it has no matter for this test.
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -21,30 +25,21 @@ test_script = """
     select 2 k from rdb$database where 1 between 0 and 2 and foo is not null;
 """
 
-act = isql_act('db', test_script,
-               substitutions=[('-At line[:]{0,1}[\\s]+[\\d]+,[\\s]+column[:]{0,1}[\\s]+[\\d]+',
-                               '-At line: column:')])
+substitutions = [ ('[ \t]+', ' '), ('(-)?(")?FOO(")?', ''), (r'(-)?At line(:)?\s+\d+.*', '') ]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-    K                               1
-
+    K 1
     Records affected: 1
-"""
-
-expected_stderr = """
     Statement failed, SQLSTATE = 42S22
     Dynamic SQL Error
     -SQL error code = -206
     -Column unknown
-    -FOO
-    -At line: column:
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
 
