be632ad000abf4d2c36eff66d55cecab7c9856e0
pavel-zotov <p519446@yandex.ru>
2025-07-02 15:37:03 +0300

    Added/Updated tests\bugs\core_5874_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214.

--- tests/bugs/core_5874_test.py
+++ tests/bugs/core_5874_test.py
@@ -10,6 +10,11 @@ DESCRIPTION:
   All cases should produce STDERR with specifying table name and R/O column after dot.
 JIRA:        CORE-5874
 FBTEST:      bugs.core_5874
+NOTES:
+    [02.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
@@ -38,7 +43,7 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 42000
     attempted update of read-only column TEST.hozzáadása
 
@@ -49,8 +54,20 @@ expected_stderr = """
     attempted update of read-only column TEST.hozzáadása
 """
 
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 42000
+    attempted update of read-only column "PUBLIC"."TEST"."hozzáadása"
+
+    Statement failed, SQLSTATE = 42000
+    attempted update of read-only column "PUBLIC"."TEST"."hozzáadása"
+
+    Statement failed, SQLSTATE = 42000
+    attempted update of read-only column "PUBLIC"."TEST"."hozzáadása"
+"""
+
 @pytest.mark.version('>=4.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
