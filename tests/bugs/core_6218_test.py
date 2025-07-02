ad7eaeffae98264460d42d12e47be4aee59ecc16
pavel-zotov <p519446@yandex.ru>
2025-07-03 00:21:41 +0300

    Added/Updated tests\bugs\core_6218_test.py: Removed 'set plan on' because it has no matter for this test. Difference of transactions before and after queries must be checked to be sure that there was no crash. Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214.

--- tests/bugs/core_6218_test.py
+++ tests/bugs/core_6218_test.py
@@ -7,6 +7,10 @@ TITLE:       COUNT(DISTINCT <DECFLOAT_FIELD>) leads FB to crash when there are d
 DESCRIPTION:
 JIRA:        CORE-6218
 FBTEST:      bugs.core_6218
+NOTES:
+    [03.07.2025] pzotov
+    Difference of transactions before and after queries must be checked to be sure that there was no crash.
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
@@ -15,6 +19,7 @@ from firebird.qa import *
 db = db_factory()
 
 test_script = """
+    set list on;
     recreate table test(n decfloat);
     commit;
 
@@ -22,43 +27,32 @@ test_script = """
     insert into test values( 0 );
     commit;
 
-    set list on;
-    set explain on;
+    set term ^;
+    execute block as
+    begin
+        rdb$set_context('USER_TRANSACTION', 'INIT_TX', current_transaction);
+    end ^
+    set term ;^
 
     select n as n_grouped_from_test0 from test group by 1; --- [ 1 ]
     select distinct n as n_uniq_from_test0 from test; -- [ 2 ]
     select count(distinct n) as count_uniq_from_test0 from test; -- [ 3 ]
+    select current_transaction - cast( rdb$get_context('USER_TRANSACTION', 'INIT_TX') as int) as tx_diff from rdb$database;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-    Select Expression
-        -> Aggregate
-            -> Sort (record length: 68, key length: 24)
-                -> Table "TEST" Full Scan
-
-    N_GROUPED_FROM_TEST0                                                     0
-
-
-
-    Select Expression
-        -> Unique Sort (record length: 68, key length: 24)
-            -> Table "TEST" Full Scan
-
-    N_UNIQ_FROM_TEST0                                                        0
-
-
-
-    Select Expression
-        -> Aggregate
-            -> Table "TEST" Full Scan
-
-    COUNT_UNIQ_FROM_TEST0           1
+    N_GROUPED_FROM_TEST0 0
+    N_UNIQ_FROM_TEST0 0
+    COUNT_UNIQ_FROM_TEST0 1
+    TX_DIFF 0
 """
 
 @pytest.mark.version('>=4.0')
 def test_1(act: Action):
+
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
