586565703bb47b72c222146df74e660b2b7f14b3
pavel-zotov <p519446@yandex.ru>
2025-07-02 14:45:58 +0300

    Added/Updated tests\bugs\core_5756_test.py: Confirmed bug  (crash) on 3.0.3.32900. Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_5756_test.py
+++ tests/bugs/core_5756_test.py
@@ -7,6 +7,13 @@ TITLE:       Regression: FB crashes when trying to recreate table that is in use
 DESCRIPTION:
 JIRA:        CORE-5756
 FBTEST:      bugs.core_5756
+    [02.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Difference between current_connection values must be checked to be sure that there was no crash, see 'ATT_DIFF'
+
+    Confirmed bug  (crash) on 3.0.3.32900.
+    Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -19,28 +26,42 @@ test_script = """
     recreate table test(x int);
     insert into test values(1);
     select * from test;
+    set term ^;
+    execute block as
+    begin
+        rdb$set_context('USER_SESSION', 'INIT_ATT', current_connection);
+    end ^
+    set term ;^
+
     recreate table test(x int, y int); -- this led to crash
-    commit;
+
     select * from test;
+    select current_connection - cast( rdb$get_context('USER_SESSION', 'INIT_ATT') as int) as att_diff from rdb$database;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-    X                               1
-    X                               1
-"""
-
-expected_stderr = """
+expected_stdout_5x = """
+    X 1
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -object TABLE "TEST" is in use
+    X 1
+    ATT_DIFF 0
+"""
+
+expected_stdout_6x = """
+    X 1
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -object TABLE "PUBLIC"."TEST" is in use
+    X 1
+    ATT_DIFF 0
 """
 
 @pytest.mark.version('>=3.0.4')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
