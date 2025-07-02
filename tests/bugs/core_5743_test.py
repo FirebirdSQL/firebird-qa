2392df93539559a149efb30da5890b5b9cfa53e2
pavel-zotov <p519446@yandex.ru>
2025-07-02 12:47:26 +0300

    Added/Updated tests\bugs\core_5743_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_5743_test.py
+++ tests/bugs/core_5743_test.py
@@ -32,24 +32,31 @@ test_script = """
         (periode = ?)
     group by 1
     ;
+    set planonly;
 
     -- sample from CORE-5749:
     select 'my constant ' as dsc, count( * )
     from rdb$relations a
-    where a.rdb$system_flag = 99
+    where a.rdb$system_flag = -2147483648
     group by 1
     ;
 """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
+expected_stdout_5x = """
     PLAN SORT (JOURNAL_CAISSE INDEX (JOURNAL_CAISSE_IDX))
     PLAN SORT (A NATURAL)
 """
 
+expected_stdout_6x = """
+    PLAN SORT ("PUBLIC"."JOURNAL_CAISSE" INDEX ("PUBLIC"."JOURNAL_CAISSE_IDX"))
+    PLAN SORT ("A" NATURAL)
+"""
+
+
 @pytest.mark.version('>=3.0.4')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
