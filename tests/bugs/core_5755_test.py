14610d3e34487650d5785c01df5eee1a4f6a784d
pavel-zotov <p519446@yandex.ru>
2025-07-02 14:32:28 +0300

    Added/Updated tests\bugs\core_5755_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_5755_test.py
+++ tests/bugs/core_5755_test.py
@@ -5,10 +5,15 @@ ID:          issue-6018
 ISSUE:       6018
 TITLE:       No error if the GRANT target object does not exist
 DESCRIPTION:
-  grant execute on proc|func|package and grant usage on sequence|exception -- still does NOT produce error/warning.
-  These statements temply disabled until some additional comments in tracker.
+    grant execute on proc|func|package and grant usage on sequence|exception -- still does NOT produce error/warning.
+    These statements temply disabled until some additional comments in tracker.
 JIRA:        CORE-5755
 FBTEST:      bugs.core_5755
+NOTES:
+    [02.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -53,11 +58,6 @@ test_script = """
     grant select on table_test to function wrong_func;
 
 
-    /************
-
-    TEMPLY DISABLED, SEE ISSUE IN THE TICKET, 02-JUN-2018 08:20
-    ===============
-
     grant execute on procedure sp_test to wrong_func;
 
     grant execute on function fn_test to wrong_func;
@@ -68,24 +68,36 @@ test_script = """
 
     grant usage on exception x_test to wrong_func;
 
-    **************/
 """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -GRANT failed
     -Function WRONG_FUNC does not exist
+
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -GRANT failed
     -Function WRONG_FUNC does not exist
 """
 
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -GRANT failed
+    -Function "PUBLIC"."WRONG_FUNC" does not exist
+    
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -GRANT failed
+    -Function "PUBLIC"."WRONG_FUNC" does not exist
+"""
+
 @pytest.mark.version('>=3.0.4')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
