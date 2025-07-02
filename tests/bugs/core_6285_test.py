5b71031de13ca13f060067adacb6c038d8d67f1f
pavel-zotov <p519446@yandex.ru>
2025-07-03 00:34:46 +0300

    Added/Updated tests\bugs\core_6285_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214.

--- tests/bugs/core_6285_test.py
+++ tests/bugs/core_6285_test.py
@@ -12,6 +12,11 @@ NOTES:
   'INCLUDE ...' and 'EXCLUDE ...' respectively (after reply from dimitr).
 JIRA:        CORE-6285
 FBTEST:      bugs.core_6285
+NOTES:
+    [03.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
@@ -58,9 +63,10 @@ test_script = """
      select 'rdb$pub after DROP LIST of some tables from publication' as msg, p.* from  rdb$database left join RDB$PUBLICATION_TABLES p on 1=1;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
+expected_stdout_5x = """
     MSG                             rdb$pub: initial content for a NEW database
     RDB$PUBLICATION_NAME            RDB$DEFAULT
     RDB$OWNER_NAME                  SYSDBA
@@ -116,8 +122,64 @@ expected_stdout = """
     Records affected: 1
 """
 
+expected_stdout_6x = """
+    MSG                             rdb$pub: initial content for a NEW database
+    RDB$PUBLICATION_NAME            RDB$DEFAULT
+    RDB$OWNER_NAME                  SYSDBA
+    RDB$SYSTEM_FLAG                 1
+    RDB$ACTIVE_FLAG                 0
+    RDB$AUTO_ENABLE                 0
+    Records affected: 1
+    MSG                             rdb$pub after enable for the WHOLE DATABASE
+    RDB$PUBLICATION_NAME            RDB$DEFAULT
+    RDB$OWNER_NAME                  SYSDBA
+    RDB$SYSTEM_FLAG                 1
+    RDB$ACTIVE_FLAG                 1
+    RDB$AUTO_ENABLE                 0
+    Records affected: 1
+    MSG                             rdb$pub after disable for the WHOLE DATABASE
+    RDB$PUBLICATION_NAME            RDB$DEFAULT
+    RDB$OWNER_NAME                  SYSDBA
+    RDB$SYSTEM_FLAG                 1
+    RDB$ACTIVE_FLAG                 0
+    RDB$AUTO_ENABLE                 0
+    Records affected: 1
+    MSG                             rdb$pub after ADD ALL tables to publication
+    RDB$PUBLICATION_NAME            RDB$DEFAULT
+    RDB$TABLE_NAME                  TEST1
+    RDB$TABLE_SCHEMA_NAME           PUBLIC
+    MSG                             rdb$pub after ADD ALL tables to publication
+    RDB$PUBLICATION_NAME            RDB$DEFAULT
+    RDB$TABLE_NAME                  TEST2
+    RDB$TABLE_SCHEMA_NAME           PUBLIC
+    MSG                             rdb$pub after ADD ALL tables to publication
+    RDB$PUBLICATION_NAME            RDB$DEFAULT
+    RDB$TABLE_NAME                  TEST3
+    RDB$TABLE_SCHEMA_NAME           PUBLIC
+    Records affected: 3
+    MSG                             rdb$pub after DROP ALL tables from publication
+    RDB$PUBLICATION_NAME            <null>
+    RDB$TABLE_NAME                  <null>
+    RDB$TABLE_SCHEMA_NAME           <null>
+    Records affected: 1
+    MSG                             rdb$pub after ADD LIST of some tables to publication
+    RDB$PUBLICATION_NAME            RDB$DEFAULT
+    RDB$TABLE_NAME                  TEST2
+    RDB$TABLE_SCHEMA_NAME           PUBLIC
+    MSG                             rdb$pub after ADD LIST of some tables to publication
+    RDB$PUBLICATION_NAME            RDB$DEFAULT
+    RDB$TABLE_NAME                  TEST3
+    RDB$TABLE_SCHEMA_NAME           PUBLIC
+    Records affected: 2
+    MSG                             rdb$pub after DROP LIST of some tables from publication
+    RDB$PUBLICATION_NAME            <null>
+    RDB$TABLE_NAME                  <null>
+    RDB$TABLE_SCHEMA_NAME           <null>
+    Records affected: 1
+"""
+
 @pytest.mark.version('>=4.0.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
