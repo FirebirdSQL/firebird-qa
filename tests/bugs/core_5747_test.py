1c33679a324a575faa1c01a39e6dcc55735ed56b
pavel-zotov <p519446@yandex.ru>
2025-07-02 14:26:39 +0300

    Added/Updated tests\bugs\core_5747_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_5747_test.py
+++ tests/bugs/core_5747_test.py
@@ -8,14 +8,15 @@ DESCRIPTION:
   beside generator we also have to check the same issue about grant usage on exception.
 JIRA:        CORE-5747
 FBTEST:      bugs.core_5747
+NOTES:
+    [02.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
-
-substitutions = [('no G privilege with grant option on object .*',
-                  'no USAGE privilege with grant option on object'),
-                 ('GEN_FOR_DBA_ONLY', ''), ('EXC_FOR_DBA_ONLY', '')]
 
 db = db_factory()
 
@@ -56,11 +57,13 @@ test_script = """
 
 """
 
+substitutions = [('no G privilege with grant option on object .*',
+                  'no USAGE privilege with grant option on object'),
+                 ('GEN_FOR_DBA_ONLY', ''), ('EXC_FOR_DBA_ONLY', '')]
+
 act = isql_act('db', test_script, substitutions=substitutions)
 
-# version: 3.0
-
-expected_stderr_1 = """
+expected_stdout_3x = """
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -GRANT failed
@@ -75,36 +78,44 @@ expected_stderr_1 = """
     no permission for USAGE access to EXCEPTION
 """
 
-@pytest.mark.version('>=3.0.4,<4.0')
-def test_1(act: Action):
-    act.expected_stderr = expected_stderr_1
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
-
-# version: 4.0
-
-expected_stderr_2 = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -GRANT failed
     -no USAGE privilege with grant option on object
-
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -GRANT failed
     -no USAGE privilege with grant option on object
-
     Statement failed, SQLSTATE = 28000
     no permission for USAGE access to GENERATOR
     -Effective user is TMP$C5747
-
     Statement failed, SQLSTATE = 28000
     no permission for USAGE access to EXCEPTION
     -Effective user is TMP$C5747
 """
 
-@pytest.mark.version('>=4.0')
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -GRANT failed
+    -no USAGE privilege with grant option on object "PUBLIC".""
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -GRANT failed
+    -no USAGE privilege with grant option on object "PUBLIC".""
+    Statement failed, SQLSTATE = 28000
+    no permission for USAGE access to GENERATOR "PUBLIC".""
+    -Effective user is TMP$C5747
+    Statement failed, SQLSTATE = 28000
+    no permission for USAGE access to EXCEPTION "PUBLIC".""
+    -Effective user is TMP$C5747
+"""
+
+
+@pytest.mark.version('>=3.0')
 def test_2(act: Action):
-    act.expected_stderr = expected_stderr_2
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+
+    act.expected_stdout = expected_stdout_3x if act.is_version('<4') else expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
