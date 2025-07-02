787ab191adecdc042c6c0e1b5e620971a119cd7a
pavel-zotov <p519446@yandex.ru>
2025-07-03 00:28:33 +0300

    Added/Updated tests\bugs\core_6252_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_6252_test.py
+++ tests/bugs/core_6252_test.py
@@ -8,6 +8,11 @@ TITLE:       UNIQUE / PRIMARY KEY constraint can be violated when AUTODDL = OFF 
 DESCRIPTION:
 JIRA:        CORE-6252
 FBTEST:      bugs.core_6252
+NOTES:
+    [03.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -69,37 +74,50 @@ test_script = """
     ;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-    A                               1
-    B                               1
-    A                               1
-    B                               2
-
-    U                               1
-    V                               1
-    U                               1
-    V                               1
-
-    IDX_NAME                        <null>
-    IDX_UNIQ                        <null>
-"""
-
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 23000
     violation of PRIMARY or UNIQUE KEY constraint "TEST1_UNQ" on table "TEST1"
     -Problematic key value is ("A" = 1)
-
     Statement failed, SQLSTATE = 23000
     violation of PRIMARY or UNIQUE KEY constraint "TEST2_PK" on table "TEST2"
     -Problematic key value is ("U" = 1)
+    A 1
+    B 1
+    A 1
+    B 2
+    U 1
+    V 1
+    U 1
+    V 1
+    IDX_NAME <null>
+    IDX_UNIQ <null>
+"""
+
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 23000
+    violation of PRIMARY or UNIQUE KEY constraint "TEST1_UNQ" on table "PUBLIC"."TEST1"
+    -Problematic key value is ("A" = 1)
+    Statement failed, SQLSTATE = 23000
+    violation of PRIMARY or UNIQUE KEY constraint "TEST2_PK" on table "PUBLIC"."TEST2"
+    -Problematic key value is ("U" = 1)
+    A 1
+    B 1
+    A 1
+    B 2
+    U 1
+    V 1
+    U 1
+    V 1
+    IDX_NAME <null>
+    IDX_UNIQ <null>
 """
 
 @pytest.mark.version('>=3.0.6')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
