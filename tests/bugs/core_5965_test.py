b32e59a0aeda5fa3fb3b9483b699d84d72186b7d
pavel-zotov <p519446@yandex.ru>
2025-07-02 20:20:41 +0300

    Added/Updated tests\bugs\core_5965_test.py: Separated expected output for FB major versions prior/since 6.x. Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.

--- tests/bugs/core_5965_test.py
+++ tests/bugs/core_5965_test.py
@@ -5,11 +5,16 @@ ID:          issue-6219
 ISSUE:       6219
 TITLE:       FB3 Optimiser chooses less efficient plan than FB2.5 optimiser
 DESCRIPTION:
-  Filling of database with data from ticket can take noticable time.
-  Instead of this it was decided to extract form ZIP archieve .fbk and then to restore it.
-  Instead of actual execution we can only obtain PLAN by querying cursor read-only property "plan"
+    Filling of database with data from ticket can take noticable time.
+    Instead of this it was decided to extract from ZIP archieve .fbk and restore it.
+    Instead of actual execution we can only obtain PLAN by querying cursor read-only property "plan"
 JIRA:        CORE-5965
 FBTEST:      bugs.core_5965
+NOTES:
+    [02.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -21,11 +26,6 @@ db = db_factory()
 db_tmp = db_factory(filename='tmp_core_5965.fdb', do_not_create=True)
 
 act = python_act('db')
-
-expected_stdout = """
-    PLAN SORT (OPT_TEST INDEX (O_CLID_CUSTTY_CUSTID))
-    PLAN SORT (OPT_TEST INDEX (O_CLID_CUSTTY_CUSTID))
-"""
 
 fbk_file = temp_file('core_5965.fbk')
 
@@ -46,7 +46,17 @@ def test_1(act: Action, fbk_file: Path, db_tmp: Database, capsys):
         #
         c2.execute("select 2 from opt_test where sysid = 1 and clid = 23 and cust_type = 1 and cust_id = 73 order by order_no desc")
         print(c2.statement.plan)
-    # Check
-    act.expected_stdout = expected_stdout
+
+    expected_stdout_5x = """
+        PLAN SORT (OPT_TEST INDEX (O_CLID_CUSTTY_CUSTID))
+        PLAN SORT (OPT_TEST INDEX (O_CLID_CUSTTY_CUSTID))
+    """
+
+    expected_stdout_6x = """
+        PLAN SORT ("PUBLIC"."OPT_TEST" INDEX ("PUBLIC"."O_CLID_CUSTTY_CUSTID"))
+        PLAN SORT ("PUBLIC"."OPT_TEST" INDEX ("PUBLIC"."O_CLID_CUSTTY_CUSTID"))
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
