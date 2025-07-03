54f155b3fba0ff123026d9e10fefb15badee4050
pavel-zotov <p519446@yandex.ru>
2025-07-03 15:38:11 +0300

    Added/Updated tests\bugs\core_6411_test.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x. Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214

--- tests/bugs/core_6411_test.py
+++ tests/bugs/core_6411_test.py
@@ -5,50 +5,38 @@ ID:          issue-6649
 ISSUE:       6649
 TITLE:       FB crashes on attempt to create table with number of fields greater than 5460
 DESCRIPTION:
-  It was found that maximal number of fields with type = BIGINT that could fit in a table DDL is 8066.
-  If this limit is exeeded then FB raises "new record size of N bytes is too big" (where N >= 65536).
-  We use for-loop with two iterations, each of them does following:
-    1. Create table with total number of fields = <N> (one for 'ID primary key' plus 8064 for
-       'user-data' fields with names 'F1', 'F2', ..., 'F'<N>-1). All of them have type = BIGINT.
-    2. DO RECONNECT // mandatory! otherwise crash can not be reproduced.
-    3. Run UPDATE OR INSERT statement that is specified in the ticker(insert single record with ID=1).
-    4. Run SELECT statement which calculates total sum on all 'user-data' fields.
-  When N = 8065 then these actions must complete successfully and result of final SELECT must be displayed.
-  When N = 8066 then we have to get exception:
-    Statement failed, SQLSTATE = 54000
-    unsuccessful metadata update
-    -new record size of 65536 bytes is too big
+    It was found that maximal number of fields with type = BIGINT that could fit in a table DDL is 8066.
+    If this limit is exeeded then FB raises "new record size of N bytes is too big" (where N >= 65536).
+    We use for-loop with two iterations, each of them does following:
+      1. Create table with total number of fields = <N> (one for 'ID primary key' plus 8064 for
+         'user-data' fields with names 'F1', 'F2', ..., 'F'<N>-1). All of them have type = BIGINT.
+      2. DO RECONNECT // mandatory! otherwise crash can not be reproduced.
+      3. Run UPDATE OR INSERT statement that is specified in the ticker(insert single record with ID=1).
+      4. Run SELECT statement which calculates total sum on all 'user-data' fields.
+    When N = 8065 then these actions must complete successfully and result of final SELECT must be displayed.
+    When N = 8066 then we have to get exception:
+      Statement failed, SQLSTATE = 54000
+      unsuccessful metadata update
+      -new record size of 65536 bytes is too big
 
-  Confirmed bug on 4.0.0.2204: got crash when N=8065 (but still "new record size of 65536 bytes is too big" when N=8066).
-  Checked on 3.0.7.33368, 4.0.0.2214 - all OK.
-NOTES:
-[08.02.2022] pcisar
-  Fails on Windows 3.0.8 with diff:
-           step: 0, FLD_COUNT: 8064, result: FIELDS_TOTAL 32510016
-           step: 1, FLD_COUNT: 8065, result: Statement failed, SQLSTATE = 54000
-           step: 1, FLD_COUNT: 8065, result: unsuccessful metadata update
-           step: 1, FLD_COUNT: 8065, result: -new record size of 65536 bytes is too big
-         - step: 1, FLD_COUNT: 8065, result: -TABLE TDATA
-         + step: 1, FLD_COUNT: 8065, result: -TABLE TDATA
-         ?                                               +
-         + step: 1, FLD_COUNT: 8065, result: Statement failed, SQLSTATE = 21S01
-         + step: 1, FLD_COUNT: 8065, result: Dynamic SQL Error
-         + step: 1, FLD_COUNT: 8065, result: -SQL error code = -804
-         + step: 1, FLD_COUNT: 8065, result: -Count of read-write columns does not equal count of values
-
-[24.03.2022] pzotov
-  On WINDOWS excessive lines in STDERR (after first error) appear because ISQL ignores 'SET BAIL' command or '-bail' switch
-  in case when it executing commands that come via PIPE mechanism.
-  Discussed with FB-team, letter with subj "[new-qa] ISQL "SET BAIL ON" problem on Windows when commands come from PIPE", 12-mar-2022 16:04.
-  It looks strange but such behaviour was considered as DESIRED feature and requested long ago (in 2011).
-  This means that it is very unlikely that it will be fixed and ISQL will behave like on POSIX (where no such effect occurs).
-  Because of this, it was decided to change test: we create temporary SQL file and provide this file as input script for launching ISQL,
-  see:
-      act.isql(..., input_file=isql_script, ...)
-
-
+    Confirmed bug on 4.0.0.2204: got crash when N=8065 (but still "new record size of 65536 bytes is too big" when N=8066).
+    Checked on 3.0.7.33368, 4.0.0.2214 - all OK.
 JIRA:        CORE-6411
 FBTEST:      bugs.core_6411
+NOTES:
+    [24.03.2022] pzotov
+        On WINDOWS excessive lines in STDERR (after first error) appear because ISQL ignores 'SET BAIL' command or '-bail' switch
+        in case when it executing commands that come via PIPE mechanism.
+        Discussed with FB-team, letter with subj "[new-qa] ISQL "SET BAIL ON" problem on Windows when commands come from PIPE", 12-mar-2022 16:04.
+        It looks strange but such behaviour was considered as DESIRED feature and requested long ago (in 2011).
+        This means that it is very unlikely that it will be fixed and ISQL will behave like on POSIX (where no such effect occurs).
+        Because of this, it was decided to change test: we create temporary SQL file and provide this file as input script for launching ISQL,
+        see:
+            act.isql(..., input_file=isql_script, ...)
+    [03.07.2025] pzotov
+    ::: NB ::: test duration time is ~6 minutes.
+    Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x
+    Checked on 6.0.0.884; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
@@ -61,17 +49,7 @@ db = db_factory()
 
 act = python_act('db', substitutions=[('.*(-)?After line \\d+.*', ''), ('[ \t]+', ' ')])
 
-expected_stdout = """
-    step: 0, FLD_COUNT: 8064, result: FIELDS_TOTAL 32510016
-    step: 1, FLD_COUNT: 8065, result: Statement failed, SQLSTATE = 54000
-    step: 1, FLD_COUNT: 8065, result: unsuccessful metadata update
-    step: 1, FLD_COUNT: 8065, result: -new record size of 65536 bytes is too big
-    step: 1, FLD_COUNT: 8065, result: -TABLE TDATA
-"""
-
-
 isql_script = temp_file('test-script-6411.sql')
-
 
 @pytest.mark.version('>=3.0.7')
 def test_1(act: Action, isql_script: Path, capsys):
@@ -95,7 +73,7 @@ def test_1(act: Action, isql_script: Path, capsys):
             set list on ;
             {sel_expr} ;
             quit ;
-            """
+        """
         isql_script.write_text(sql_expr)
 
         act.reset()
@@ -106,6 +84,17 @@ def test_1(act: Action, isql_script: Path, capsys):
                 print(f'step: {step}, FLD_COUNT: {FLD_COUNT}, result: {line}')
 
     act.reset()
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else  '"PUBLIC".'
+    TABLE_NAME = 'TDATA' if act.is_version('<6') else  '"TDATA"'
+    expected_stdout = f"""
+        step: 0, FLD_COUNT: 8064, result: FIELDS_TOTAL 32510016
+        step: 1, FLD_COUNT: 8065, result: Statement failed, SQLSTATE = 54000
+        step: 1, FLD_COUNT: 8065, result: unsuccessful metadata update
+        step: 1, FLD_COUNT: 8065, result: -new record size of 65536 bytes is too big
+        step: 1, FLD_COUNT: 8065, result: -TABLE {SQL_SCHEMA_PREFIX}{TABLE_NAME}
+    """
+
     act.expected_stdout = expected_stdout
     act.stdout = capsys.readouterr().out
 
