1064e9ce5e28a568c28ae2eb5eeecc925e4ea4e0
pavel-zotov <p519446@yandex.ru>
2025-07-03 15:52:29 +0300

    Added/Updated tests\bugs\core_6489_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/core_6489_test.py
+++ tests/bugs/core_6489_test.py
@@ -7,6 +7,10 @@ TITLE:       User without ALTER ANY ROLE privilege can use COMMENT ON ROLE
 DESCRIPTION:
 JIRA:        CORE-6489
 FBTEST:      bugs.core_6489
+    [03.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
@@ -37,26 +41,32 @@ test_script = """
     commit;
 """
 
-act = isql_act('db', test_script,
-               substitutions=[('ROLE_DESCR_BLOB_ID .*', ''), ('[\t ]+', ' '),
-                              ('(-)?Effective user is.*', '')])
+substitutions = [
+    ('[\t ]+', ' '),
+    ('ROLE_DESCR_BLOB_ID .*', ''),
+    ('(-)?Effective user is.*', ''),
+]
 
-expected_stdout = """
-    Comment by tmp$c6489_senior
-"""
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 28000
     unsuccessful metadata update
     -COMMENT ON RDB$ADMIN failed
     -no permission for ALTER access to ROLE RDB$ADMIN
-    -Effective user is TMP$C6489_JUNIOR
+    Comment by tmp$c6489_senior
+"""
+
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 28000
+    unsuccessful metadata update
+    -COMMENT ON "RDB$ADMIN" failed
+    -no permission for ALTER access to ROLE "RDB$ADMIN"
+    Comment by tmp$c6489_senior
 """
 
 @pytest.mark.version('>=3.0.8')
 def test_1(act: Action, user_sr, user_jr):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
