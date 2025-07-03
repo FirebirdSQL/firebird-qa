9878b03e49bf9e51a479aee581cf979f1b88a702
pavel-zotov <p519446@yandex.ru>
2025-07-03 20:18:45 +0300

    Added/Updated tests\bugs\core_5606_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/core_5606_test.py
+++ tests/bugs/core_5606_test.py
@@ -5,16 +5,20 @@ ID:          issue-5872
 ISSUE:       5872
 TITLE:       Add expression index name to exception message if computation failed
 DESCRIPTION:
-NOTES:
-[24.08.2020]
-  changed sequence of actions: one statement must violate requirements of only ONE index.
-  Before this statement:
-    insert into test(id, x, y, s) values( 4, 3, -7, 'qwerty' );
-  -- did violate TWO indices: test_eval3 and test_eval5.
-  The order of which of them will raise first is undefined, so this test could fail because of appearance
-  "wrong" index name in its STDERR. Detected on 4.0.0.2173. Discussed with hvlad, letter 23.08.2020 16:58.
 JIRA:        CORE-5606
 FBTEST:      bugs.core_5606
+NOTES:
+    [24.08.2020]
+        Changed sequence of actions: one statement must violate requirements of only ONE index.
+        Before this statement:
+          insert into test(id, x, y, s) values( 4, 3, -7, 'qwerty' );
+        -- did violate TWO indices: test_eval3 and test_eval5.
+        The order of which of them will raise first is undefined, so this test could fail because of appearance
+        "wrong" index name in its STDERR. Detected on 4.0.0.2173. Discussed with hvlad, letter 23.08.2020 16:58.
+    [30.06.2025] pzotov
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -61,39 +65,53 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-    Records affected: 0
-"""
-
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 22012
     Expression evaluation error for index "TEST_EVAL1" on table "TEST"
     -arithmetic exception, numeric overflow, or string truncation
     -Integer divide by zero.  The code attempted to divide an integer value by an integer divisor of zero.
-
     Statement failed, SQLSTATE = 42000
     Expression evaluation error for index "TEST_EVAL2" on table "TEST"
     -expression evaluation not supported
     -Argument for LOG10 must be positive
-
     Statement failed, SQLSTATE = HY000
     Expression evaluation error for index "TEST_EVAL3" on table "TEST"
     -Context variable 'FOO_&_BAR' is not found in namespace 'SYSTEM'
     Statement failed, SQLSTATE = 22012
-
     Expression evaluation error for index "TEST_EVAL4" on table "TEST"
     -arithmetic exception, numeric overflow, or string truncation
     -Integer divide by zero.  The code attempted to divide an integer value by an integer divisor of zero.
-
     Statement failed, SQLSTATE = 22011
     Expression evaluation error for index "TEST_EVAL5" on table "TEST"
     -Invalid length parameter -3 to SUBSTRING. Negative integers are not allowed.
+    Records affected: 0
+"""
+
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 22012
+    Expression evaluation error for index "PUBLIC"."TEST_EVAL1" on table "PUBLIC"."TEST"
+    -arithmetic exception, numeric overflow, or string truncation
+    -Integer divide by zero.  The code attempted to divide an integer value by an integer divisor of zero.
+    Statement failed, SQLSTATE = 42000
+    Expression evaluation error for index "PUBLIC"."TEST_EVAL2" on table "PUBLIC"."TEST"
+    -expression evaluation not supported
+    -Argument for LOG10 must be positive
+    Statement failed, SQLSTATE = HY000
+    Expression evaluation error for index "PUBLIC"."TEST_EVAL3" on table "PUBLIC"."TEST"
+    -Context variable 'FOO_&_BAR' is not found in namespace 'SYSTEM'
+    Statement failed, SQLSTATE = 22012
+    Expression evaluation error for index "PUBLIC"."TEST_EVAL4" on table "PUBLIC"."TEST"
+    -arithmetic exception, numeric overflow, or string truncation
+    -Integer divide by zero.  The code attempted to divide an integer value by an integer divisor of zero.
+    Statement failed, SQLSTATE = 22011
+    Expression evaluation error for index "PUBLIC"."TEST_EVAL5" on table "PUBLIC"."TEST"
+    -Invalid length parameter -3 to SUBSTRING. Negative integers are not allowed.
+    Records affected: 0
 """
 
 @pytest.mark.version('>=4.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+    
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
