02f847b1d05d2158a1fb9889d846ff90715c4142
pavel-zotov <p519446@yandex.ru>
2025-07-04 23:52:45 +0300

    Added/Updated tests\bugs\gh_7257_misc_datatypes_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/gh_7257_misc_datatypes_test.py
+++ tests/bugs/gh_7257_misc_datatypes_test.py
@@ -8,13 +8,16 @@ DESCRIPTION:
     Additional test to check misc datatypes in partial indices.
 NOTES:
     [18.01.2025] pzotov
-    Resultset of cursor that executes using instance of selectable PreparedStatement must be stored
-    in some variable in order to have ability close it EXPLICITLY (before PS will be freed).
-    Otherwise access violation raises during Python GC and pytest hangs at final point (does not return control to OS).
-    This occurs at least for: Python 3.11.2 / pytest: 7.4.4 / firebird.driver: 1.10.6 / Firebird.Qa: 0.19.3
-    The reason of that was explained by Vlad, 26.10.24 17:42 ("oddities when use instances of selective statements").
-    
-    Checked on 6.0.0.409, 5.0.1.1469
+        Resultset of cursor that executes using instance of selectable PreparedStatement must be stored
+        in some variable in order to have ability close it EXPLICITLY (before PS will be freed).
+        Otherwise access violation raises during Python GC and pytest hangs at final point (does not return control to OS).
+        This occurs at least for: Python 3.11.2 / pytest: 7.4.4 / firebird.driver: 1.10.6 / Firebird.Qa: 0.19.3
+        The reason of that was explained by Vlad, 26.10.24 17:42 ("oddities when use instances of selective statements").
+        Checked on 6.0.0.409, 5.0.1.1469
+    [04.07.2025] pzotov
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.863; 5.0.3.1668.
 """
 
 import pytest
@@ -113,7 +116,7 @@ def run_ddl_dml(act, capsys, dtype, v_chk, v_max, use_rand = True):
                 if ps:
                     ps.free()
 
-        act.expected_stdout = f"""
+        expected_stdout_5x = f"""
             Select Expression
             ....-> Aggregate
             ........-> Filter
@@ -129,6 +132,25 @@ def run_ddl_dml(act, capsys, dtype, v_chk, v_max, use_rand = True):
             ....................-> Index "{idx_common_name}" Range Scan (full match)
             COUNT : 10
         """
+
+        expected_stdout_6x = f"""
+            Select Expression
+            ....-> Aggregate
+            ........-> Filter
+            ............-> Table "PUBLIC"."TEST" Access By ID
+            ................-> Bitmap
+            ....................-> Index "PUBLIC"."{idx_partial_name}" Full Scan
+            COUNT : 10
+            Select Expression
+            ....-> Aggregate
+            ........-> Filter
+            ............-> Table "PUBLIC"."TEST" Access By ID
+            ................-> Bitmap
+            ....................-> Index "PUBLIC"."{idx_common_name}" Range Scan (full match)
+            COUNT : 10
+        """
+
+        act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x        
         act.stdout = capsys.readouterr().out
         assert act.clean_stdout == act.clean_expected_stdout
         act.reset()
