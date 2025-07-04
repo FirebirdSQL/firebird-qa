1d837366fffc73c0e0dc5253b9a2ef5a460af5ab
pavel-zotov <p519446@yandex.ru>
2025-07-04 23:58:10 +0300

    Added/Updated tests\bugs\gh_7269_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/gh_7269_test.py
+++ tests/bugs/gh_7269_test.py
@@ -12,7 +12,11 @@ DESCRIPTION:
         gbak:adjusting the ONLINE and FORCED WRITES flags
 NOTES:
     [02.11.2024] pzotov
-    Checked on 5.0.2.1551, 6.0.0.415.
+        Checked on 5.0.2.1551, 6.0.0.415.
+    [04.07.2025] pzotov
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.863; 5.0.3.1668.
 """
 import subprocess
 from pathlib import Path
@@ -47,7 +51,7 @@ def test_1(act: Action, tmp_fbk: Path, tmp_fdb: Path, capsys):
             if act.match_any(line.strip(), allowed_patterns):
                 print(line)
 
-    expected_stdout = """
+    expected_stdout_5x = """
         gbak: ERROR:violation of PRIMARY or UNIQUE KEY constraint "PK_A3" on table "A3"
         gbak: ERROR:    Problematic key value is ("ID" = 9)
         gbak: ERROR:violation of PRIMARY or UNIQUE KEY constraint "PK_A1" on table "A1"
@@ -63,6 +67,22 @@ def test_1(act: Action, tmp_fbk: Path, tmp_fdb: Path, capsys):
         gbak: ERROR:    Run gfix -online to bring database online without active indices.
     """
 
-    act.expected_stdout = expected_stdout
+    expected_stdout_6x = """
+        gbak: ERROR:violation of PRIMARY or UNIQUE KEY constraint "PK_A3" on table "PUBLIC"."A3"
+        gbak: ERROR:    Problematic key value is ("ID" = 9)
+        gbak: ERROR:violation of PRIMARY or UNIQUE KEY constraint "PK_A1" on table "PUBLIC"."A1"
+        gbak: ERROR:    Problematic key value is ("ID" = 5)
+        gbak: ERROR:Cannot create foreign key constraint "FK_A1". Partner index does not exist or is inactive.
+        gbak: ERROR:violation of FOREIGN KEY constraint "FK_A2" on table "PUBLIC"."B2"
+        gbak: ERROR:    Foreign key reference target does not exist
+        gbak: ERROR:    Problematic key value is ("A2_ID" = 5)
+        gbak: ERROR:Cannot create foreign key constraint "FK_A3". Partner index does not exist or is inactive.
+        gbak:finishing, closing, and going home
+        gbak:adjusting the ONLINE and FORCED WRITES flags
+        gbak: ERROR:Database is not online due to failure to activate one or more indices.
+        gbak: ERROR:    Run gfix -online to bring database online without active indices.
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
