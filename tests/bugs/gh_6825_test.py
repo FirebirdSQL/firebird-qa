2f553cf1ae5761236694f58e550dde11dda3b946
pavel-zotov <p519446@yandex.ru>
2025-07-04 17:31:42 +0300

    Added/Updated tests\bugs\gh_6825_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/gh_6825_test.py
+++ tests/bugs/gh_6825_test.py
@@ -6,6 +6,11 @@ ISSUE:       6825
 TITLE:       Correct error message for DROP VIEW
 DESCRIPTION:
 FBTEST:      bugs.gh_6825
+NOTES:
+    [04.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -27,7 +32,7 @@ test_script = """
 
 act = isql_act('db', test_script, substitutions=[('(-)?Effective user is.*', '')])
 
-expected_stderr = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 28000
     unsuccessful metadata update
     -DROP VIEW V1 failed
@@ -35,8 +40,16 @@ expected_stderr = """
     -Effective user is TMP$GH_6825
 """
 
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 28000
+    unsuccessful metadata update
+    -DROP VIEW "PUBLIC"."V1" failed
+    -no permission for DROP access to VIEW "PUBLIC"."V1"
+"""
+
 @pytest.mark.version('>=3.0.8')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
