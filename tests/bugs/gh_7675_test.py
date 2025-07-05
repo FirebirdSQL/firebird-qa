cf396effbfba95c7931458d8d40ac89fe2105e1e
pavel-zotov <p519446@yandex.ru>
2025-07-05 22:04:15 +0300

    Added/Updated tests\bugs\gh_7675_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/gh_7675_test.py
+++ tests/bugs/gh_7675_test.py
@@ -8,13 +8,14 @@ DESCRIPTION:
     Test only ckecks ability to use RDB$SQL package as it is described in the doc.
     More complex tests will be implemented later.
 NOTES:
-    [02.10.2023] pzotov
-    Checked on 6.0.0.65.
-
     [17.11.2024] pzotov
-    Removed output of concrete data for checked query.
-    It is enough only to display content of SQLDA (lines with 'sqltype:' and 'name:') and SQLSTATE (if some error occurs).
-    Checked on 6.0.0.532.
+        Removed output of concrete data for checked query.
+        It is enough only to display content of SQLDA (lines with 'sqltype:' and 'name:') and SQLSTATE (if some error occurs).
+        Checked on 6.0.0.532.
+    [04.07.2025] pzotov
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.909.
 """
 import os
 
@@ -50,46 +51,76 @@ test_script = """
 
 act = isql_act('db', test_script, substitutions=[('^((?!SQLSTATE|sqltype:|name:).)*$',''),('[ \t]+',' ')])
 
-expected_stdout = """
-    01: sqltype: 496 LONG scale: 0 subtype: 0 len: 4
-    : name: PLAN_LINE alias: PLAN_LINE
-
-    02: sqltype: 580 INT64 scale: 0 subtype: 0 len: 8
-    : name: RECORD_SOURCE_ID alias: RECORD_SOURCE_ID
-
-    03: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
-    : name: PARENT_RECORD_SOURCE_ID alias: PARENT_RECORD_SOURCE_ID
-
-    04: sqltype: 496 LONG scale: 0 subtype: 0 len: 4
-    : name: LEVEL alias: LEVEL
-
-    05: sqltype: 500 SHORT Nullable scale: 0 subtype: 0 len: 2
-    : name: OBJECT_TYPE alias: OBJECT_TYPE
-
-    06: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 UTF8
-    : name: PACKAGE_NAME alias: PACKAGE_NAME
-
-    07: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 UTF8
-    : name: OBJECT_NAME alias: OBJECT_NAME
-
-    08: sqltype: 448 VARYING Nullable scale: 0 subtype: 0 len: 1020 charset: 4 UTF8 
-    : name: ALIAS alias: ALIAS
-
-    09: sqltype: 480 DOUBLE Nullable scale: 0 subtype: 0 len: 8
-    : name: CARDINALITY alias: CARDINALITY
-
-    10: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
-    : name: RECORD_LENGTH alias: RECORD_LENGTH
-
-    11: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
-    : name: KEY_LENGTH alias: KEY_LENGTH
-
-    12: sqltype: 520 BLOB scale: 0 subtype: 1 len: 8 charset: 4 UTF8
-    : name: ACCESS_PATH alias: ACCESS_PATH
-"""
-
 @pytest.mark.version('>=6.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
+
+    expected_stdout_5x = """
+        01: sqltype: 496 LONG scale: 0 subtype: 0 len: 4
+        : name: PLAN_LINE alias: PLAN_LINE
+
+        02: sqltype: 580 INT64 scale: 0 subtype: 0 len: 8
+        : name: RECORD_SOURCE_ID alias: RECORD_SOURCE_ID
+
+        03: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+        : name: PARENT_RECORD_SOURCE_ID alias: PARENT_RECORD_SOURCE_ID
+
+        04: sqltype: 496 LONG scale: 0 subtype: 0 len: 4
+        : name: LEVEL alias: LEVEL
+
+        05: sqltype: 500 SHORT Nullable scale: 0 subtype: 0 len: 2
+        : name: OBJECT_TYPE alias: OBJECT_TYPE
+
+        06: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 UTF8
+        : name: PACKAGE_NAME alias: PACKAGE_NAME
+
+        07: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 UTF8
+        : name: OBJECT_NAME alias: OBJECT_NAME
+
+        08: sqltype: 448 VARYING Nullable scale: 0 subtype: 0 len: 1020 charset: 4 UTF8 
+        : name: ALIAS alias: ALIAS
+
+        09: sqltype: 480 DOUBLE Nullable scale: 0 subtype: 0 len: 8
+        : name: CARDINALITY alias: CARDINALITY
+
+        10: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
+        : name: RECORD_LENGTH alias: RECORD_LENGTH
+
+        11: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
+        : name: KEY_LENGTH alias: KEY_LENGTH
+
+        12: sqltype: 520 BLOB scale: 0 subtype: 1 len: 8 charset: 4 UTF8
+        : name: ACCESS_PATH alias: ACCESS_PATH
+    """
+
+    expected_stdout_6x = """
+        01: sqltype: 496 LONG scale: 0 subtype: 0 len: 4
+        : name: PLAN_LINE alias: PLAN_LINE
+        02: sqltype: 580 INT64 scale: 0 subtype: 0 len: 8
+        : name: RECORD_SOURCE_ID alias: RECORD_SOURCE_ID
+        03: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+        : name: PARENT_RECORD_SOURCE_ID alias: PARENT_RECORD_SOURCE_ID
+        04: sqltype: 496 LONG scale: 0 subtype: 0 len: 4
+        : name: LEVEL alias: LEVEL
+        05: sqltype: 500 SHORT Nullable scale: 0 subtype: 0 len: 2
+        : name: OBJECT_TYPE alias: OBJECT_TYPE
+        06: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 SYSTEM.UTF8
+        : name: SCHEMA_NAME alias: SCHEMA_NAME
+        07: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 SYSTEM.UTF8
+        : name: PACKAGE_NAME alias: PACKAGE_NAME
+        08: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 SYSTEM.UTF8
+        : name: OBJECT_NAME alias: OBJECT_NAME
+        09: sqltype: 448 VARYING Nullable scale: 0 subtype: 0 len: 1020 charset: 4 SYSTEM.UTF8
+        : name: ALIAS alias: ALIAS
+        10: sqltype: 480 DOUBLE Nullable scale: 0 subtype: 0 len: 8
+        : name: CARDINALITY alias: CARDINALITY
+        11: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
+        : name: RECORD_LENGTH alias: RECORD_LENGTH
+        12: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
+        : name: KEY_LENGTH alias: KEY_LENGTH
+        13: sqltype: 520 BLOB scale: 0 subtype: 1 len: 8 charset: 4 SYSTEM.UTF8
+        : name: ACCESS_PATH alias: ACCESS_PATH
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
