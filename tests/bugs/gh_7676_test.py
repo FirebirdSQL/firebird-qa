2d1741071758dfcf8b684f8d639f71e9224091c0
pavel-zotov <p519446@yandex.ru>
2025-07-05 22:06:30 +0300

    Added/Updated tests\bugs\gh_7676_test.py: Added 'SQL_SCHEMA_PREFIX' and variables - to be substituted in expected_* on FB 6.x

--- tests/bugs/gh_7676_test.py
+++ tests/bugs/gh_7676_test.py
@@ -7,8 +7,11 @@ TITLE:       Invalid message when violating unique constraint ("Attempt to evalu
 DESCRIPTION:
 NOTES:
     [28.05.2024] pzotov
-    Confirmed bug on 5.0.0.1111: got SQLSTATE = HY000 / Attempt to evaluate index expression recursively
-    Checked on 5.0.0.1121.
+        Confirmed bug on 5.0.0.1111: got SQLSTATE = HY000 / Attempt to evaluate index expression recursively
+        Checked on 5.0.0.1121.
+    [04.07.2025] pzotov
+        Added 'SQL_SCHEMA_PREFIX' and variables - to be substituted in expected_* on FB 6.x
+        Checked on 6.0.0.909; 5.0.3.1668.
 """
 
 import pytest
@@ -26,14 +29,15 @@ test_script = """
 
 act = isql_act('db', test_script, substitutions=[('[ \t]+', ' ')])
 
-expected_stdout = """
-    Statement failed, SQLSTATE = 23000
-    attempt to store duplicate value (visible to active transactions) in unique index "TEST_UNQ"
-    -Problematic key value is (<expression> = 1)
-"""
-
 @pytest.mark.version('>=5.0.0')
 def test_1(act: Action):
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    expected_stdout = f"""
+        Statement failed, SQLSTATE = 23000
+        attempt to store duplicate value (visible to active transactions) in unique index {SQL_SCHEMA_PREFIX}"TEST_UNQ"
+        -Problematic key value is (<expression> = 1)
+    """
     act.expected_stdout = expected_stdout
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
