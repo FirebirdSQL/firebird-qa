4a9b65185e83cd41a97e6012b7b1631e6ffc7376
pavel-zotov <p519446@yandex.ru>
2025-07-05 23:16:43 +0300

    Added/Updated tests\bugs\gh_7899_test.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x

--- tests/bugs/gh_7899_test.py
+++ tests/bugs/gh_7899_test.py
@@ -11,6 +11,9 @@ NOTES:
          6.0.0.186,  commit 305c40a05b1d64c14dbf5f25f36c42c44c6392d9
          5.0.1.1307, commit e35437e00687db9ed6add279cecb816dcdf8b07a
          4.0.5.3042, commit f7b090043e8886ab6286f8d626dd1684dc09e3b8
+    [05.07.2025] pzotov
+        Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x
+        Checked on 6.0.0.909; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
@@ -19,18 +22,6 @@ from firebird.qa import *
 db = db_factory()
 
 act = python_act('db')
-
-expected_stdout = """
-    Statement failed, SQLSTATE = 23000
-    violation of FOREIGN KEY constraint "TDETL_FK" on table "PERSISTENT_DETL"
-    -Foreign key reference target does not exist
-    -Problematic key value is ("ID" = 1)
-
-    Statement failed, SQLSTATE = 23000
-    violation of FOREIGN KEY constraint "TDETL_FK" on table "PERSISTENT_DETL"
-    -Foreign key reference target does not exist
-    -Problematic key value is ("ID" = 2)
-"""
 
 @pytest.mark.version('>=4.0.5')
 def test_1(act: Action):
@@ -65,6 +56,19 @@ def test_1(act: Action):
         select d.id as orphan_child_id
         from persistent_detl d
         where not exists(select * from persistent_main m where m.id = d.id);
+    """
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    expected_stdout = f"""
+        Statement failed, SQLSTATE = 23000
+        violation of FOREIGN KEY constraint "TDETL_FK" on table {SQL_SCHEMA_PREFIX}"PERSISTENT_DETL"
+        -Foreign key reference target does not exist
+        -Problematic key value is ("ID" = 1)
+
+        Statement failed, SQLSTATE = 23000
+        violation of FOREIGN KEY constraint "TDETL_FK" on table {SQL_SCHEMA_PREFIX}"PERSISTENT_DETL"
+        -Foreign key reference target does not exist
+        -Problematic key value is ("ID" = 2)
     """
 
     act.expected_stdout = expected_stdout
