4a227084c59c4e3290265952b006876a4b6280f8
pavel-zotov <p519446@yandex.ru>
2025-07-05 17:51:45 +0300

    Added/Updated tests\bugs\gh_7461_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/gh_7461_test.py
+++ tests/bugs/gh_7461_test.py
@@ -6,36 +6,33 @@ ISSUE:       https://github.com/FirebirdSQL/firebird/issues/7461
 TITLE:       Differences in field metadata descriptions between Firebird 2.5 and Firebird 4
 NOTES:
     [21.01.2024] pzotov
+        NB: original title of ticket was changed. Commits refer to pull reuest rather than to this ticket number:
+            FB 4.x (16.11.2023): https://github.com/FirebirdSQL/firebird/commit/2886cd78991209842ee7e3065bde83ab75571af4
+            FB 5.x (17.11.2023): https://github.com/FirebirdSQL/firebird/commit/1ed7f81f168b643a29357fce2e1f49156e9f5a1f
+            FB 6.x (17.11.2023): https://github.com/FirebirdSQL/firebird/commit/ab6aced05723dc1b2e6bb96bfdaa86cb3090daf2
+        Before fix:
+            01: sqltype: 580 INT64 Nullable scale: -2 subtype: 1 len: 8
+              :  name:   alias: SALARY                                       ---
+              : table:   owner: 
+            02: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
+              :  name:   alias: EMP_NO                                       ---
+              : table:   owner:                                              ---
 
-    NB: original title of ticket was changed. Commits refer to pull reuest rather than to this ticket number:
-        FB 4.x (16.11.2023): https://github.com/FirebirdSQL/firebird/commit/2886cd78991209842ee7e3065bde83ab75571af4
-        FB 5.x (17.11.2023): https://github.com/FirebirdSQL/firebird/commit/1ed7f81f168b643a29357fce2e1f49156e9f5a1f
-        FB 6.x (17.11.2023): https://github.com/FirebirdSQL/firebird/commit/ab6aced05723dc1b2e6bb96bfdaa86cb3090daf2
-    Actual text of commits:
-    FB 4.x:
-        Merge pull request #7848 from medi6/4.0.2
-        Differencies in queries results between Firebird 2.5 and Firebird 4
-    FB 5.x and 6.x:
-        correction metaData
+        After fix:
+            01: sqltype: 580 INT64 Nullable scale: -2 subtype: 1 len: 8
+              :  name: MAX  alias: SALARY                                    +++
+              : table:   owner: 
+            02: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
+              :  name: EMP_NO  alias: EMP_NO                                 +++
+              : table: SALARY_HISTORY  owner: SYSDBA                         +++
 
-    Before fix:
-        01: sqltype: 580 INT64 Nullable scale: -2 subtype: 1 len: 8
-          :  name:   alias: SALARY                                       ---
-          : table:   owner: 
-        02: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
-          :  name:   alias: EMP_NO                                       ---
-          : table:   owner:                                              ---
+        Confirmed bug on 4.0.4.3016, 5.0.0.1268
+        Checked on 4.0.4.3021 (build date: 17-nov-2023), 5.0.0.1271 (build date: 18-nov-2023); 6.0.0.219
 
-    After fix:
-        01: sqltype: 580 INT64 Nullable scale: -2 subtype: 1 len: 8
-          :  name: MAX  alias: SALARY                                    +++
-          : table:   owner: 
-        02: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
-          :  name: EMP_NO  alias: EMP_NO                                 +++
-          : table: SALARY_HISTORY  owner: SYSDBA                         +++
-
-    Confirmed bug on 4.0.4.3016, 5.0.0.1268
-    Checked on 4.0.4.3021 (build date: 17-nov-2023), 5.0.0.1271 (build date: 18-nov-2023); 6.0.0.219
+    [05.07.2025] pzotov
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.909; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
@@ -66,13 +63,19 @@ act = isql_act('db', test_script, substitutions=[('^((?!name:|table:).)*$', ''),
 @pytest.mark.version('>=4.0.5')
 def test_1(act: Action):
 
-    expected_stdout = f"""
+    expected_stdout_5x = f"""
         :  name: MAX  alias: SALARY
         : table:   owner:
         :  name: EMP_NO  alias: EMP_NO
         : table: SALARY_HISTORY  owner: {act.db.user}
     """
+    expected_stdout_6x = f"""
+        : name: MAX alias: SALARY
+        : table: schema: owner:
+        : name: EMP_NO alias: EMP_NO
+        : table: SALARY_HISTORY schema: PUBLIC owner: SYSDBA
+    """
 
-    act.expected_stdout = expected_stdout
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
