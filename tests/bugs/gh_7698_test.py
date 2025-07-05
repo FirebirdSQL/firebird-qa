26c6778551ba126f0f2c50ff8af4009c1dc270fd
pavel-zotov <p519446@yandex.ru>
2025-07-05 22:09:43 +0300

    Added/Updated tests\bugs\gh_7698_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/gh_7698_test.py
+++ tests/bugs/gh_7698_test.py
@@ -6,8 +6,12 @@ ISSUE:       https://github.com/FirebirdSQL/firebird/issues/7698
 TITLE:       The legacy plan with window functions is broken
 DESCRIPTION:
 NOTES:
-    Confirmed bug on 5.0.0.1149
-    Checked on 5.0.0.1155 -- all OK.
+        Confirmed bug on 5.0.0.1149
+        Checked on 5.0.0.1155 -- all OK.
+    [05.07.2025] pzotov
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.909; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
@@ -23,12 +27,16 @@ test_script = f"""
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-    PLAN (RDB$RELATIONS NATURAL)
-"""
-
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
+
+    expected_stdout_5x = """
+        PLAN (RDB$RELATIONS NATURAL)
+    """
+    expected_stdout_6x = """
+        PLAN ("SYSTEM"."RDB$RELATIONS" NATURAL)
+    """
+    
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
