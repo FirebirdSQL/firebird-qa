b1d54805f559a91af11696860af00eea9374c248
pavel-zotov <p519446@yandex.ru>
2025-07-06 17:33:07 +0300

    Added/Updated tests\bugs\gh_8123_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/gh_8123_test.py
+++ tests/bugs/gh_8123_test.py
@@ -13,8 +13,12 @@ DESCRIPTION:
         "SQLSTATE = 42000 / ... / -cannot delete ... / -there are 1 dependencies"
 NOTES:
     [21.05.2024] pzotov
-    Confirmed bug on 6.0.0.357-bf6c467 (regular daily snapshot, 18-may-2024).
-    Checked on intermediate snapshots 6.0.0.357-f94343e, 5.0.1.1404-88bf561.
+        Confirmed bug on 6.0.0.357-bf6c467 (regular daily snapshot, 18-may-2024).
+        Checked on intermediate snapshots 6.0.0.357-f94343e, 5.0.1.1404-88bf561.
+    [06.07.2025] pzotov
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.914; 5.0.3.1668.
 """
 
 import pytest
@@ -116,48 +120,87 @@ test_script = """
 
 act = isql_act('db', test_script, substitutions=[('[ \t]+', ' ')])
 
-expected_stdout = """
-    RDB$DEPENDED_ON_NAME TB_TEST1
-    RDB$FIELD_NAME N1
-    RDB$DEPENDENT_TYPE 3
-    RDB$DEPENDED_ON_TYPE 0
-    Records affected: 1 
-
-    Statement failed, SQLSTATE = 42000
-    unsuccessful metadata update
-    -cannot delete
-    -COLUMN TB_TEST1.N1
-    -there are 1 dependencies
-
-
-    RDB$DEPENDED_ON_NAME TB_TEST2
-    RDB$FIELD_NAME N1
-    RDB$DEPENDENT_TYPE 3
-    RDB$DEPENDED_ON_TYPE 0
-    Records affected: 1
-
-    Statement failed, SQLSTATE = 42000
-    unsuccessful metadata update
-    -cannot delete
-    -COLUMN TB_TEST2.N1
-    -there are 1 dependencies
-
-
-    RDB$DEPENDED_ON_NAME TB_TEST3
-    RDB$FIELD_NAME N1
-    RDB$DEPENDENT_TYPE 3
-    RDB$DEPENDED_ON_TYPE 0
-    Records affected: 1
-
-    Statement failed, SQLSTATE = 42000
-    unsuccessful metadata update
-    -cannot delete
-    -COLUMN TB_TEST3.N1
-    -there are 1 dependencies
-"""
-
 @pytest.mark.version('>=5.0.1')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
+
+    expected_stdout_5x = """
+        RDB$DEPENDED_ON_NAME TB_TEST1
+        RDB$FIELD_NAME N1
+        RDB$DEPENDENT_TYPE 3
+        RDB$DEPENDED_ON_TYPE 0
+        Records affected: 1 
+
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -cannot delete
+        -COLUMN TB_TEST1.N1
+        -there are 1 dependencies
+
+
+        RDB$DEPENDED_ON_NAME TB_TEST2
+        RDB$FIELD_NAME N1
+        RDB$DEPENDENT_TYPE 3
+        RDB$DEPENDED_ON_TYPE 0
+        Records affected: 1
+
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -cannot delete
+        -COLUMN TB_TEST2.N1
+        -there are 1 dependencies
+
+
+        RDB$DEPENDED_ON_NAME TB_TEST3
+        RDB$FIELD_NAME N1
+        RDB$DEPENDENT_TYPE 3
+        RDB$DEPENDED_ON_TYPE 0
+        Records affected: 1
+
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -cannot delete
+        -COLUMN TB_TEST3.N1
+        -there are 1 dependencies
+    """
+
+    expected_stdout_6x = """
+        RDB$DEPENDED_ON_NAME TB_TEST1
+        RDB$FIELD_NAME N1
+        RDB$DEPENDENT_TYPE 3
+        RDB$DEPENDED_ON_TYPE 0
+        Records affected: 1
+
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -cannot delete
+        -COLUMN "PUBLIC"."TB_TEST1"."N1"
+        -there are 1 dependencies
+
+        RDB$DEPENDED_ON_NAME TB_TEST2
+        RDB$FIELD_NAME N1
+        RDB$DEPENDENT_TYPE 3
+        RDB$DEPENDED_ON_TYPE 0
+        Records affected: 1
+
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -cannot delete
+        -COLUMN "PUBLIC"."TB_TEST2"."N1"
+        -there are 1 dependencies
+
+        RDB$DEPENDED_ON_NAME TB_TEST3
+        RDB$FIELD_NAME N1
+        RDB$DEPENDENT_TYPE 3
+        RDB$DEPENDED_ON_TYPE 0
+        Records affected: 1
+
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -cannot delete
+        -COLUMN "PUBLIC"."TB_TEST3"."N1"
+        -there are 1 dependencies
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
