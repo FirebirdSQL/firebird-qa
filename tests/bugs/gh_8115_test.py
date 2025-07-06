51e9cde7c82f13ad4c26bd5b124507e0456f267b
pavel-zotov <p519446@yandex.ru>
2025-07-06 17:27:39 +0300

    Added/Updated tests\bugs\gh_8115_test.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x

--- tests/bugs/gh_8115_test.py
+++ tests/bugs/gh_8115_test.py
@@ -8,28 +8,22 @@ DESCRIPTION:
     Original title: "FB 5.0.0.1306 - unexpected results using LEFT JOIN with When "
 NOTES:
     [16.09.2024] pzotov
-    Confirmed bug in 5.0.1.1369-8c31082 (17.03.2024)
-    Bug was fixed in 5.0.1.1369-bbd35ab (20.03.2024)
-    Commit:
-        https://github.com/FirebirdSQL/firebird/commit/bbd35ab07c129e9735f081fcd29172a8187aa8ab
-        Avoid reading/hashing the inner stream(s) if the leader stream is empty
-    Checked on 6.0.0.457, 5.0.2.1499
-
-    [18.01.2025] pzotov
-    Resultset of cursor that executes using instance of selectable PreparedStatement must be stored
-    in some variable in order to have ability close it EXPLICITLY (before PS will be freed).
-    Otherwise access violation raises during Python GC and pytest hangs at final point (does not return control to OS).
-    This occurs at least for: Python 3.11.2 / pytest: 7.4.4 / firebird.driver: 1.10.6 / Firebird.Qa: 0.19.3
-    The reason of that was explained by Vlad, 26.10.24 17:42 ("oddities when use instances of selective statements").
-
+        Confirmed bug in 5.0.1.1369-8c31082 (17.03.2024)
+        Bug was fixed in 5.0.1.1369-bbd35ab (20.03.2024)
+        Commit:
+            https://github.com/FirebirdSQL/firebird/commit/bbd35ab07c129e9735f081fcd29172a8187aa8ab
+            Avoid reading/hashing the inner stream(s) if the leader stream is empty
+        Checked on 6.0.0.457, 5.0.2.1499
     [27.03.2025] pzotov
-    Explained plan for 6.x became the same as for 5.x, see commit:
-    https://github.com/FirebirdSQL/firebird/commit/6c21404c6ef800ceb7d3bb9c97dc8249431dbc5b
-    Comparison of actual output must be done with single expected_out in both 5.x and 6.x.
-    Plan for 5.x (with TWO 'Filter' clauses) must be considered as more effective.
-    Disussed with dimitr, letter 16.09.2024 17:55.
-
-    Checked on 6.0.0.698-6c21404.
+        Explained plan for 6.x became the same as for 5.x, see commit:
+        https://github.com/FirebirdSQL/firebird/commit/6c21404c6ef800ceb7d3bb9c97dc8249431dbc5b
+        Comparison of actual output must be done with single expected_out in both 5.x and 6.x.
+        Plan for 5.x (with TWO 'Filter' clauses) must be considered as more effective.
+        Disussed with dimitr, letter 16.09.2024 17:55.
+        Checked on 6.0.0.698-6c21404.
+    [06.07.2025] pzotov
+        Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x
+        Checked on 6.0.0.914; 5.0.3.1668.
 """
 import zipfile
 from pathlib import Path
@@ -98,13 +92,15 @@ def test_1(act: Action, tmp_fbk: Path, capsys):
             if ps:
                 ps.free()
 
-    expected_stdout = """
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else  '"PUBLIC".'
+    expected_stdout = f"""
         Select Expression
         ....-> Nested Loop Join (inner)
-        ........-> Procedure "SAL_INPERIOADA2" as "AA" Scan
+        ........-> Procedure {SQL_SCHEMA_PREFIX}"SAL_INPERIOADA2" as "AA" Scan
         ........-> Filter
         ............-> Filter
-        ................-> Procedure "USER_CNP" as "AB" Scan
+        ................-> Procedure {SQL_SCHEMA_PREFIX}"USER_CNP" as "AB" Scan
 
         000DD4E1-B4D0-4D6E-9D9F-DE9A7D0D6492
         E574F734-CECB-4A8F-B9BE-FAF51BC61FAD
