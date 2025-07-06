ff26ec46f1d852694baa1780119f34d1f6726cb4
pavel-zotov <p519446@yandex.ru>
2025-07-06 17:25:20 +0300

    Added/Updated tests\bugs\gh_8109_test.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/bugs/gh_8109_test.py
+++ tests/bugs/gh_8109_test.py
@@ -7,8 +7,12 @@ TITLE:       Plan/Performance regression when using special construct for IN in 
 DESCRIPTION:
 NOTES:
     [03.02.2025] pzotov
-    Confirmed problem (regression) on 6.0.0.595-2c5b146, 5.0.2.1601-f094936
-    Checked on 6.0.0.601-5dee439, 5.0.2.1606-fd31e52 (intermediate snapshots).
+        Confirmed problem (regression) on 6.0.0.595-2c5b146, 5.0.2.1601-f094936
+        Checked on 6.0.0.601-5dee439, 5.0.2.1606-fd31e52 (intermediate snapshots).
+    [06.07.2025] pzotov
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.914; 5.0.3.1668.
 """
 
 import pytest
@@ -78,7 +82,7 @@ def test_1(act: Action, capsys):
                     ps.free()
 
 
-    expected_stdout = f"""
+    expected_stdout_5x = f"""
         {queries_map[ 0 ]}
         Select Expression
         ....-> Filter
@@ -150,6 +154,78 @@ def test_1(act: Action, capsys):
         ....................-> Index "TEST_Q_DEC" Range Scan (full match)
     """
 
-    act.expected_stdout = expected_stdout
+    expected_stdout_6x = f"""
+        {queries_map[ 0 ]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "PUBLIC"."TEST" Access By ID
+        ............-> Bitmap Or
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_X_ASC" Range Scan (full match)
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_Y_ASC" Range Scan (full match)
+
+        {queries_map[ 1 ]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "PUBLIC"."TEST" Access By ID
+        ............-> Bitmap Or
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_U_DEC" Range Scan (full match)
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_V_DEC" Range Scan (full match)
+
+        {queries_map[ 2 ]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "PUBLIC"."TEST" Access By ID
+        ............-> Bitmap Or
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_X_ASC" Range Scan (full match)
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_U_DEC" Range Scan (full match)
+
+        {queries_map[ 3 ]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "PUBLIC"."TEST" Access By ID
+        ............-> Bitmap Or
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_V_DEC" Range Scan (full match)
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_Y_ASC" Range Scan (full match)
+
+        {queries_map[ 4 ]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "PUBLIC"."TEST" Access By ID
+        ............-> Bitmap Or
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_C_ASC" Range Scan (full match)
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_C_DEC" Range Scan (full match)
+
+        {queries_map[ 5 ]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "PUBLIC"."TEST" Access By ID
+        ............-> Bitmap Or
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_P_ASC" Range Scan (full match)
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_Q_ASC" Range Scan (full match)
+
+        {queries_map[ 6 ]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "PUBLIC"."TEST" Access By ID
+        ............-> Bitmap Or
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_P_DEC" Range Scan (full match)
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."TEST_Q_DEC" Range Scan (full match)
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
