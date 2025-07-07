dcdf94d9a517f502e8bd64d3d9330e4b0ca0fb4e
pavel-zotov <p519446@yandex.ru>
2025-07-07 14:44:56 +0300

    Added/Updated tests\functional\arno\indices\test_upper_bound_asc_01_segments_02.py: Added 'SQL_SCHEMA_PREFIX' and variables to be substituted in expected_* on FB 6.x

--- tests/functional/arno/indices/test_upper_bound_asc_01_segments_02.py
+++ tests/functional/arno/indices/test_upper_bound_asc_01_segments_02.py
@@ -10,100 +10,94 @@ FBTEST:      functional.arno.indices.upper_bound_asc_01_segments_02
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE Table_66 (
-  ID INTEGER
-);
-
-SET TERM ^^ ;
-CREATE PROCEDURE PR_FillTable_66
-AS
-DECLARE VARIABLE FillID INTEGER;
-BEGIN
-  FillID = 2147483647;
-  WHILE (FillID > 0) DO
-  BEGIN
-    INSERT INTO Table_66 (ID) VALUES (:FillID);
-    FillID = FillID / 2;
-  END
-  INSERT INTO Table_66 (ID) VALUES (NULL);
-  INSERT INTO Table_66 (ID) VALUES (0);
-  INSERT INTO Table_66 (ID) VALUES (NULL);
-  FillID = -2147483648;
-  WHILE (FillID < 0) DO
-  BEGIN
-    INSERT INTO Table_66 (ID) VALUES (:FillID);
-    FillID = FillID / 2;
-  END
-END
-^^
-SET TERM ; ^^
-
-COMMIT;
-
-EXECUTE PROCEDURE PR_FillTable_66;
-
-COMMIT;
-
-CREATE ASC INDEX I_Table_66_ASC ON Table_66 (ID);
-
-COMMIT;
+init_script = """
+    create table table_66 (
+      id integer
+    );
+    
+    set term ^ ;
+    create procedure pr_filltable_66
+    as
+    declare variable fillid integer;
+    begin
+      fillid = 2147483647;
+      while (fillid > 0) do
+      begin
+        insert into table_66 (id) values (:fillid);
+        fillid = fillid / 2;
+      end
+      insert into table_66 (id) values (null);
+      insert into table_66 (id) values (0);
+      insert into table_66 (id) values (null);
+      fillid = -2147483648;
+      while (fillid < 0) do
+      begin
+        insert into table_66 (id) values (:fillid);
+        fillid = fillid / 2;
+      end
+    end
+    ^
+    set term ; ^
+    commit;
+    
+    execute procedure pr_filltable_66;
+    commit;
+    
+    create asc index i_table_66_asc on table_66 (id);
 """
 
 db = db_factory(init=init_script)
 
-test_script = """SET PLAN ON;
-SELECT
-  ID
-FROM
-  Table_66 t66
-WHERE
-t66.ID < 0;"""
+test_script = """
+    set list on;
+    set plan on;
+    select id from table_66 t66 where t66.id < 0;
+"""
 
-act = isql_act('db', test_script)
-
-expected_stdout = """PLAN (T66 INDEX (I_TABLE_66_ASC))
-
-          ID
-============
-
- -2147483648
- -1073741824
-  -536870912
-  -268435456
-  -134217728
-   -67108864
-   -33554432
-   -16777216
-    -8388608
-    -4194304
-    -2097152
-    -1048576
-     -524288
-     -262144
-     -131072
-      -65536
-      -32768
-      -16384
-       -8192
-       -4096
-
-          ID
-============
-       -2048
-       -1024
-        -512
-        -256
-        -128
-         -64
-         -32
-         -16
-          -8
-          -4
-          -2
--1"""
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TABLE_TEST_NAME = 'T66' if act.is_version('<6') else '"T66"'
+    INDEX_TEST_NAME = 'I_TABLE_66_ASC' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"I_TABLE_66_ASC"'
+    expected_stdout = f"""
+        PLAN ({TABLE_TEST_NAME} INDEX ({INDEX_TEST_NAME}))
+        ID -2147483648
+        ID -1073741824
+        ID -536870912
+        ID -268435456
+        ID -134217728
+        ID -67108864
+        ID -33554432
+        ID -16777216
+        ID -8388608
+        ID -4194304
+        ID -2097152
+        ID -1048576
+        ID -524288
+        ID -262144
+        ID -131072
+        ID -65536
+        ID -32768
+        ID -16384
+        ID -8192
+        ID -4096
+        ID -2048
+        ID -1024
+        ID -512
+        ID -256
+        ID -128
+        ID -64
+        ID -32
+        ID -16
+        ID -8
+        ID -4
+        ID -2
+        ID -1
+    """
+
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
