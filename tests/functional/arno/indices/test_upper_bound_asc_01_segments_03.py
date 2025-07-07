4212038710da8909b54509fe05790f01c90bc01c
pavel-zotov <p519446@yandex.ru>
2025-07-07 14:49:12 +0300

    Added/Updated tests\functional\arno\indices\test_upper_bound_asc_01_segments_03.py: Added 'SQL_SCHEMA_PREFIX' and variables to be substituted in expected_* on FB 6.x

--- tests/functional/arno/indices/test_upper_bound_asc_01_segments_03.py
+++ tests/functional/arno/indices/test_upper_bound_asc_01_segments_03.py
@@ -10,58 +10,56 @@ FBTEST:      functional.arno.indices.upper_bound_asc_01_segments_03
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE Table_A15 (
-  ID VARCHAR(15)
-);
+init_script = """
+    CREATE TABLE TEST (
+      ID VARCHAR(15)
+    );
 
-INSERT INTO Table_A15 (ID) VALUES (NULL);
-INSERT INTO Table_A15 (ID) VALUES ('A');
-INSERT INTO Table_A15 (ID) VALUES ('AA');
-INSERT INTO Table_A15 (ID) VALUES ('AAA');
-INSERT INTO Table_A15 (ID) VALUES ('AAAA');
-INSERT INTO Table_A15 (ID) VALUES ('AAAAB');
-INSERT INTO Table_A15 (ID) VALUES ('AAAB');
-INSERT INTO Table_A15 (ID) VALUES ('AAB');
-INSERT INTO Table_A15 (ID) VALUES ('AB');
-INSERT INTO Table_A15 (ID) VALUES ('B');
-INSERT INTO Table_A15 (ID) VALUES ('BA');
-INSERT INTO Table_A15 (ID) VALUES ('BAA');
-INSERT INTO Table_A15 (ID) VALUES ('BAAA');
-INSERT INTO Table_A15 (ID) VALUES ('BAAAA');
-INSERT INTO Table_A15 (ID) VALUES ('BAAAB');
+    INSERT INTO TEST (ID) VALUES (NULL);
+    INSERT INTO TEST (ID) VALUES ('A');
+    INSERT INTO TEST (ID) VALUES ('AA');
+    INSERT INTO TEST (ID) VALUES ('AAA');
+    INSERT INTO TEST (ID) VALUES ('AAAA');
+    INSERT INTO TEST (ID) VALUES ('AAAAB');
+    INSERT INTO TEST (ID) VALUES ('AAAB');
+    INSERT INTO TEST (ID) VALUES ('AAB');
+    INSERT INTO TEST (ID) VALUES ('AB');
+    INSERT INTO TEST (ID) VALUES ('B');
+    INSERT INTO TEST (ID) VALUES ('BA');
+    INSERT INTO TEST (ID) VALUES ('BAA');
+    INSERT INTO TEST (ID) VALUES ('BAAA');
+    INSERT INTO TEST (ID) VALUES ('BAAAA');
+    INSERT INTO TEST (ID) VALUES ('BAAAB');
+    COMMIT;
 
-COMMIT;
-
-CREATE ASC INDEX I_Table_A15_ASC ON Table_A15 (ID);
-
-COMMIT;
+    CREATE ASC INDEX TEST_IDX ON TEST (ID);
 """
 
 db = db_factory(init=init_script)
 
-test_script = """SET PLAN ON;
-SELECT
-  ID
-FROM
-  Table_A15 a15
-WHERE
-a15.ID <= 'AAAAB';"""
+test_script = """
+    set list on;
+    set plan on;
+    select id from test t where t.id <= 'AAAAB';
+"""
 
-act = isql_act('db', test_script)
-
-expected_stdout = """PLAN (A15 INDEX (I_TABLE_A15_ASC))
-
-ID
-===============
-
-A
-AA
-AAA
-AAAA
-AAAAB"""
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TABLE_TEST_NAME = 'T' if act.is_version('<6') else '"T"'
+    INDEX_TEST_NAME = 'TEST_IDX' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"TEST_IDX"'
+    expected_stdout = f"""
+        PLAN ({TABLE_TEST_NAME} INDEX ({INDEX_TEST_NAME}))
+        ID A
+        ID AA
+        ID AAA
+        ID AAAA
+        ID AAAAB
+    """
+
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
