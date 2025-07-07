84ae37a08985545ad5730c0889384cd59ec655e1
pavel-zotov <p519446@yandex.ru>
2025-07-07 15:11:34 +0300

    Added/Updated tests\functional\arno\indices\test_lower_bound_desc_02_segments_01.py: Added 'SQL_SCHEMA_PREFIX' and variables to be substituted in expected_* on FB 6.x

--- tests/functional/arno/indices/test_lower_bound_desc_02_segments_01.py
+++ tests/functional/arno/indices/test_lower_bound_desc_02_segments_01.py
@@ -13,67 +13,67 @@ FBTEST:      functional.arno.indices.lower_bound_desc_02_segments_01
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE Table_2_10 (
-  F1 INTEGER,
-  F2 INTEGER
-);
+init_script = """
+    create table test (
+      f1 integer,
+      f2 integer
+    );
 
-COMMIT;
+    insert into test (f1, f2) values (1, 1);
+    insert into test (f1, f2) values (1, 2);
+    insert into test (f1, f2) values (1, 3);
+    insert into test (f1, f2) values (1, 4);
+    insert into test (f1, f2) values (1, 5);
+    insert into test (f1, f2) values (1, 6);
+    insert into test (f1, f2) values (1, 7);
+    insert into test (f1, f2) values (1, 8);
+    insert into test (f1, f2) values (1, 9);
+    insert into test (f1, f2) values (1, 10);
+    insert into test (f1, f2) values (2, 1);
+    insert into test (f1, f2) values (2, 2);
+    insert into test (f1, f2) values (2, 3);
+    insert into test (f1, f2) values (2, 4);
+    insert into test (f1, f2) values (2, 5);
+    insert into test (f1, f2) values (2, 6);
+    insert into test (f1, f2) values (2, 7);
+    insert into test (f1, f2) values (2, 8);
+    insert into test (f1, f2) values (2, 9);
+    insert into test (f1, f2) values (2, 10);
+    commit;
 
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 1);
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 2);
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 3);
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 4);
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 5);
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 6);
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 7);
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 8);
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 9);
-INSERT INTO Table_2_10 (F1, F2) VALUES (1, 10);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 1);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 2);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 3);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 4);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 5);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 6);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 7);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 8);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 9);
-INSERT INTO Table_2_10 (F1, F2) VALUES (2, 10);
-
-COMMIT;
-
-CREATE DESC INDEX I_Table_2_10_DESC ON Table_2_10 (F1, F2);
-
-COMMIT;
+    create desc index test_idx on test (f1, f2);
 """
 
 db = db_factory(init=init_script)
 
-test_script = """SET PLAN ON;
-SELECT
-  t.F1,
-  t.F2
-FROM
-  Table_2_10 t
-WHERE
-t.F1 = 2 and t.F2 <= 5;"""
+test_script = """
+    set list on;
+    set plan on;
+    select t.f1, t.f2 from test t where t.f1 = 2 and t.f2 <= 5;
+"""
 
-act = isql_act('db', test_script)
-
-expected_stdout = """PLAN (T INDEX (I_TABLE_2_10_DESC))
-
-          F1           F2
-============ ============
-
-           2            1
-           2            2
-           2            3
-           2            4
-2            5"""
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TABLE_TEST_NAME = 'T' if act.is_version('<6') else '"T"'
+    INDEX_TEST_NAME = 'TEST_IDX' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"TEST_IDX"'
+    expected_stdout = f"""
+        PLAN ({TABLE_TEST_NAME} INDEX ({INDEX_TEST_NAME}))
+        F1 2
+        F2 1
+        F1 2
+        F2 2
+        F1 2
+        F2 3
+        F1 2
+        F2 4
+        F1 2
+        F2 5
+    """
+
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
