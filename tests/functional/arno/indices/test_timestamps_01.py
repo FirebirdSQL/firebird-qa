9797a3808cdb9a6609977522cb4ad7fb4ddab0b1
pavel-zotov <p519446@yandex.ru>
2025-07-07 14:29:56 +0300

    Added/Updated tests\functional\arno\indices\test_timestamps_01.py: Added 'SQL_SCHEMA_PREFIX' and variables (if needed) to be substituted in expected_* on FB 6.x

--- tests/functional/arno/indices/test_timestamps_01.py
+++ tests/functional/arno/indices/test_timestamps_01.py
@@ -39,6 +39,7 @@ test_script = """
     -- clause (index navigation) without bitmap building.
     -- See: http://tracker.firebirdsql.org/browse/CORE-1550
     -- ("the same index should never appear in both ORDER and INDEX parts of the same plan item")
+    set list on;
     set plan on;
     select
       e.begindatetime,
@@ -51,21 +52,29 @@ test_script = """
       begindatetime asc;
 """
 
-act = isql_act('db', test_script, substitutions=[('=.*', '')])
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-    PLAN (E ORDER PK_BEGINDATETIME)
-                BEGINDATETIME               ENDDATETIME
-    ========================= =========================
-    1858-11-15 18:00:00.0000  1858-11-15 20:00:00.0000
-    1858-11-16 12:00:00.0000  1858-11-16 13:00:00.0000
-    1858-11-17 00:00:00.0000  1858-11-17 00:00:00.0000
-    1858-11-18 16:00:00.0000  1858-11-18 17:00:00.0000
-    2004-04-08 02:00:00.0000  2004-04-08 02:09:00.0000
-"""
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TABLE_TEST_NAME = 'E' if act.is_version('<6') else '"E"'
+    INDEX_TEST_NAME = 'PK_BEGINDATETIME' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"PK_BEGINDATETIME"'
+    expected_stdout = f"""
+        PLAN ({TABLE_TEST_NAME} ORDER {INDEX_TEST_NAME})
+        BEGINDATETIME                   1858-11-15 18:00:00.0000
+        ENDDATETIME                     1858-11-15 20:00:00.0000
+        BEGINDATETIME                   1858-11-16 12:00:00.0000
+        ENDDATETIME                     1858-11-16 13:00:00.0000
+        BEGINDATETIME                   1858-11-17 00:00:00.0000
+        ENDDATETIME                     1858-11-17 00:00:00.0000
+        BEGINDATETIME                   1858-11-18 16:00:00.0000
+        ENDDATETIME                     1858-11-18 17:00:00.0000
+        BEGINDATETIME                   2004-04-08 02:00:00.0000
+        ENDDATETIME                     2004-04-08 02:09:00.0000
+    """
+
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
