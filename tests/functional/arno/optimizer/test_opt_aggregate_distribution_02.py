23559ae53be34e10e1b7e731babbcdd626968b7a
pavel-zotov <p519446@yandex.ru>
2025-07-07 15:48:58 +0300

    Added/Updated tests\functional\arno\optimizer\test_opt_aggregate_distribution_02.py: Refactored: explained plan is used to be checked in expected_out. See notes.

--- tests/functional/arno/optimizer/test_opt_aggregate_distribution_02.py
+++ tests/functional/arno/optimizer/test_opt_aggregate_distribution_02.py
@@ -8,6 +8,13 @@ DESCRIPTION:
   should be delivered to the where clause. The underlying aggregate stream could possible
   use it for a index and speed it up.
 FBTEST:      functional.arno.optimizer.opt_aggregate_distribution_02
+NOTES:
+    [07.07.2025] pzotov
+    Refactored: explained plan is used to be checked in expected_out.
+    Added ability to use several queries for check - see 'qry_list' tuple.
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.914; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813
 """
 
 import pytest
@@ -60,26 +67,95 @@ COMMIT;
 
 db = db_factory(init=init_script)
 
-test_script = """SET PLAN ON;
-SELECT
-  f.ColorID,
-  c.ColorName,
-  Count(*)
-FROM
-  Flowers f
-  LEFT JOIN Colors c ON (c.ColorID = f.ColorID)
-GROUP BY
-  f.ColorID, c.ColorName
-HAVING
-  Count(*) >= 2 and
-  MIN(f.FlowerID) >= 1 and
-  MAX(f.FlowerID) >= 1 and
-  AVG(f.FlowerID) >= 1 and
-  Count(DISTINCT f.FlowerID) >= 2 and
-  MIN(DISTINCT f.FlowerID) >= 1 and
-  MAX(DISTINCT f.FlowerID) >= 1 and
-AVG(DISTINCT f.FlowerID) >= 1;"""
+qry_list = (
+    """
+    select
+        f.colorid,
+        c.colorname,
+        count(*)
+    from flowers f
+    left join colors c on (c.colorid = f.colorid)
+    group by
+        f.colorid, c.colorname
+    having
+        count(*) >= 2 and
+        min(f.flowerid) >= 1 and
+        max(f.flowerid) >= 1 and
+        avg(f.flowerid) >= 1 and
+        count(distinct f.flowerid) >= 2 and
+        min(distinct f.flowerid) >= 1 and
+        max(distinct f.flowerid) >= 1 and
+        avg(distinct f.flowerid) >= 1;
+    """,
+)
 
+substitutions = [ ( r'\(record length: \d+, key length: \d+\)', 'record length: N, key length: M' ) ]
+act = python_act('db', substitutions = substitutions)
+
+#-----------------------------------------------------------
+
+def replace_leading(source, char="."):
+    stripped = source.lstrip()
+    return char * (len(source) - len(stripped)) + stripped
+
+#-----------------------------------------------------------
+
+@pytest.mark.version('>=3.0')
+def test_1(act: Action, capsys):
+    with act.db.connect() as con:
+        cur = con.cursor()
+        for test_sql in qry_list:
+            ps, rs =  None, None
+            try:
+                cur = con.cursor()
+                ps = cur.prepare(test_sql)
+                print(test_sql)
+                # Print explained plan with padding eash line by dots in order to see indentations:
+                print( '\n'.join([replace_leading(s) for s in ps.detailed_plan.split('\n')]) )
+            except DatabaseError as e:
+                print(e.__str__())
+                print(e.gds_codes)
+            finally:
+                if rs:
+                    rs.close() # <<< EXPLICITLY CLOSING CURSOR RESULTS
+                if ps:
+                    ps.free()
+
+    expected_out_5x = f"""
+        {qry_list[0]}
+        Select Expression
+        ....-> Filter
+        ........-> Aggregate
+        ............-> Sort (record length: 86, key length: 36)
+        ................-> Nested Loop Join (outer)
+        ....................-> Table "FLOWERS" as "F" Full Scan
+        ....................-> Filter
+        ........................-> Table "COLORS" as "C" Access By ID
+        ............................-> Bitmap
+        ................................-> Index "PK_COLORS" Unique Scan
+    """
+
+    expected_out_6x = f"""
+        {qry_list[0]}
+        Select Expression
+        ....-> Filter
+        ........-> Aggregate
+        ............-> Sort (record length: 86, key length: 36)
+        ................-> Nested Loop Join (outer)
+        ....................-> Table "PUBLIC"."FLOWERS" as "F" Full Scan
+        ....................-> Filter
+        ........................-> Table "PUBLIC"."COLORS" as "C" Access By ID
+        ............................-> Bitmap
+        ................................-> Index "PUBLIC"."PK_COLORS" Unique Scan
+    """
+
+    act.expected_stdout = expected_out_5x if act.is_version('<6') else expected_out_6x
+    act.stdout = capsys.readouterr().out
+    assert act.clean_stdout == act.clean_expected_stdout
+
+
+
+'''
 act = isql_act('db', test_script)
 
 expected_stdout = """PLAN SORT (JOIN (F NATURAL, C INDEX (PK_COLORS)))
@@ -97,3 +173,4 @@ def test_1(act: Action):
     act.expected_stdout = expected_stdout
     act.execute()
     assert act.clean_stdout == act.clean_expected_stdout
+'''
