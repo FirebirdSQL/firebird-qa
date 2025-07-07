50e27cdcc88d741a971811d0d1f06b98548572ea
pavel-zotov <p519446@yandex.ru>
2025-07-07 15:01:17 +0300

    Added/Updated tests\functional\arno\indices\test_upper_bound_desc_01_segments_02.py: Added 'SQL_SCHEMA_PREFIX' and variables to be substituted in expected_* on FB 6.x

--- tests/functional/arno/indices/test_upper_bound_desc_01_segments_02.py
+++ tests/functional/arno/indices/test_upper_bound_desc_01_segments_02.py
@@ -10,79 +10,77 @@ FBTEST:      functional.arno.indices.upper_bound_desc_01_segments_02
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE Table_66 (
-  ID INTEGER
-);
+init_script = """
+    CREATE TABLE TEST (
+      ID INTEGER
+    );
 
-SET TERM ^^ ;
-CREATE PROCEDURE PR_FillTable_66
-AS
-DECLARE VARIABLE FillID INTEGER;
-BEGIN
-  FillID = 2147483647;
-  WHILE (FillID > 0) DO
-  BEGIN
-    INSERT INTO Table_66 (ID) VALUES (:FillID);
-    FillID = FillID / 2;
-  END
-  INSERT INTO Table_66 (ID) VALUES (NULL);
-  INSERT INTO Table_66 (ID) VALUES (0);
-  INSERT INTO Table_66 (ID) VALUES (NULL);
-  FillID = -2147483648;
-  WHILE (FillID < 0) DO
-  BEGIN
-    INSERT INTO Table_66 (ID) VALUES (:FillID);
-    FillID = FillID / 2;
-  END
-END
-^^
-SET TERM ; ^^
+    SET TERM ^ ;
+    CREATE PROCEDURE PR_FillTable_66
+    AS
+    DECLARE VARIABLE FillID INTEGER;
+    BEGIN
+      FillID = 2147483647;
+      WHILE (FillID > 0) DO
+      BEGIN
+        INSERT INTO TEST (ID) VALUES (:FillID);
+        FillID = FillID / 2;
+      END
+      INSERT INTO TEST (ID) VALUES (NULL);
+      INSERT INTO TEST (ID) VALUES (0);
+      INSERT INTO TEST (ID) VALUES (NULL);
+      FillID = -2147483648;
+      WHILE (FillID < 0) DO
+      BEGIN
+        INSERT INTO TEST (ID) VALUES (:FillID);
+        FillID = FillID / 2;
+      END
+    END
+    ^
+    SET TERM ; ^
+    COMMIT;
 
-COMMIT;
+    EXECUTE PROCEDURE PR_FillTable_66;
+    COMMIT;
 
-EXECUTE PROCEDURE PR_FillTable_66;
-
-COMMIT;
-
-CREATE DESC INDEX I_Table_66_DESC ON Table_66 (ID);
-
-COMMIT;
+    CREATE DESC INDEX test_idx ON TEST (ID);
 """
 
 db = db_factory(init=init_script)
 
-test_script = """SET PLAN ON;
-SELECT
-  ID
-FROM
-  Table_66 t66
-WHERE
-t66.ID > 131071;"""
+test_script = """
+    set list on;
+    set plan on;
+    select id from test t where t.id > 131071;
+"""
 
-act = isql_act('db', test_script)
-
-expected_stdout = """PLAN (T66 INDEX (I_TABLE_66_DESC))
-
-          ID
-============
-
-  2147483647
-  1073741823
-   536870911
-   268435455
-   134217727
-    67108863
-    33554431
-    16777215
-     8388607
-     4194303
-     2097151
-     1048575
-      524287
-262143"""
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TABLE_TEST_NAME = 'T' if act.is_version('<6') else '"T"'
+    INDEX_TEST_NAME = 'TEST_IDX' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"TEST_IDX"'
+    expected_stdout = f"""
+        PLAN ({TABLE_TEST_NAME} INDEX ({INDEX_TEST_NAME}))
+        ID 2147483647
+        ID 1073741823
+        ID 536870911
+        ID 268435455
+        ID 134217727
+        ID 67108863
+        ID 33554431
+        ID 16777215
+        ID 8388607
+        ID 4194303
+        ID 2097151
+        ID 1048575
+        ID 524287
+        ID 262143
+    """
+
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
