4dae57a2cad92cecd99c97e9b8378c710e36ceca
pavel-zotov <p519446@yandex.ru>
2025-07-08 15:51:09 +0300

    Added/Updated tests\functional\arno\optimizer\test_opt_sort_by_index_18.py: Refactored: explained plan is used to be checked in expected_out. See notes.

--- tests/functional/arno/optimizer/test_opt_sort_by_index_18.py
+++ tests/functional/arno/optimizer/test_opt_sort_by_index_18.py
@@ -9,13 +9,19 @@ DESCRIPTION:
 FBTEST:      functional.arno.optimizer.opt_sort_by_index_18
 NOTES:
     [17.11.2024] pzotov
-    Query text was replaced after https://github.com/FirebirdSQL/firebird/commit/26e64e9c08f635d55ac7a111469498b3f0c7fe81
-    ( Cost-based decision between ORDER and SORT plans (#8316) ): 'OPTIMIZE FOR FIRST ROWS' is used for 6.x
-    Suggested by dimitr, letter 16.11.2024 15:15
-
-    Checked on 6.0.0.532; 5.0.2.1567; 4.0.6.3168; 3.0.13.33794.
+        Query text was replaced after https://github.com/FirebirdSQL/firebird/commit/26e64e9c08f635d55ac7a111469498b3f0c7fe81
+        ( Cost-based decision between ORDER and SORT plans (#8316) ): 'OPTIMIZE FOR FIRST ROWS' is used for 6.x
+        Suggested by dimitr, letter 16.11.2024 15:15
+        Checked on 6.0.0.532; 5.0.2.1567; 4.0.6.3168; 3.0.13.33794.
+    [08.07.2025] pzotov
+        Refactored: explained plan is used to be checked in expected_out.
+        Added ability to use several queries and their datasets for check - see 'qry_list' and 'qry_data' tuples.
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.930; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813
 """
 
+from firebird.driver import DatabaseError
 import pytest
 from firebird.qa import *
 
@@ -58,7 +64,8 @@ init_sql = """
 
 db = db_factory(init = init_sql)
 
-act = python_act('db', substitutions = [(r'record length: \d+, key length: \d+', 'record length: NN, key length: MM')])
+substitutions = [ ( r'\(record length: \d+, key length: \d+\)', 'record length: N, key length: M' ) ]
+act = python_act('db', substitutions = substitutions)
 
 #-----------------------------------------------------------
 
@@ -68,59 +75,113 @@ def replace_leading(source, char="."):
 
 #-----------------------------------------------------------
 
-@pytest.mark.version('>=3')
+@pytest.mark.version('>=3.0')
 def test_1(act: Action, capsys):
 
     OPT_CLAUSE = '' if act.is_version('<6') else 'optimize for first rows'
-
-    test_sql = f"""
-        select
-            t53.id2,
-            t53.id1
-        from table_53 t53
-        where
-            t53.id1 = 30
-        order by
-            t53.id2 asc
-        {OPT_CLAUSE}
-    """
+    qry_list = (
+        f"""
+            select
+                t53.id2,
+                t53.id1
+            from table_53 t53
+            where
+                t53.id1 = 30
+            order by
+                t53.id2 asc
+            {OPT_CLAUSE}
+        """,
+    )
+    data_list = (
+        """
+        ID2 : 0
+        ID1 : 30
+        ID2 : 1
+        ID1 : 30
+        ID2 : 2
+        ID1 : 30
+        ID2 : 3
+        ID1 : 30
+        ID2 : 4
+        ID1 : 30
+        ID2 : 5
+        ID1 : 30
+        ID2 : 6
+        ID1 : 30
+        ID2 : 7
+        ID1 : 30
+        ID2 : 8
+        ID1 : 30
+        ID2 : 9
+        ID1 : 30
+        """,
+    )
 
     with act.db.connect() as con:
         cur = con.cursor()
-        ps = None
-        try:
-            ps = cur.prepare(test_sql)
+        for test_sql in qry_list:
+            ps, rs =  None, None
+            try:
+                cur = con.cursor()
+                ps = cur.prepare(test_sql)
+                print(test_sql)
+                # Print explained plan with padding eash line by dots in order to see indentations:
+                print( '\n'.join([replace_leading(s) for s in ps.detailed_plan.split('\n')]) )
 
-            # Print explained plan with padding eash line by dots in order to see indentations:
-            # print(test_sql)
-            print( '\n'.join([replace_leading(s) for s in ps.detailed_plan.split('\n')]) )
-            print('')
-        except DatabaseError as e:
-            print(e.__str__())
-            print(e.gds_codes)
-        finally:
-            if ps:
-                ps.free()
+                # ::: NB ::: 'ps' returns data, i.e. this is SELECTABLE expression.
+                # We have to store result of cur.execute(<psInstance>) in order to
+                # close it explicitly.
+                # Otherwise AV can occur during Python garbage collection and this
+                # causes pytest to hang on its final point.
+                # Explained by hvlad, email 26.10.24 17:42
+                rs = cur.execute(ps)
+                cur_cols = cur.description
+                for r in rs:
+                    for i in range(0,len(cur_cols)):
+                        print( cur_cols[i][0], ':', r[i] )
 
-    expected_stdout_5x = """
+            except DatabaseError as e:
+                print(e.__str__())
+                print(e.gds_codes)
+            finally:
+                if rs:
+                    rs.close() # <<< EXPLICITLY CLOSING CURSOR RESULTS
+                if ps:
+                    ps.free()
+
+    expected_out_4x = f"""
+        {qry_list[0]}
         Select Expression
         ....-> Filter
         ........-> Table "TABLE_53" as "T53" Access By ID
         ............-> Index "I_TABLE_53_ID2_ASC" Full Scan
         ................-> Bitmap
         ....................-> Index "I_TABLE_53_ID1_ASC" Range Scan (full match)
+        {data_list[0]}
     """
 
-    expected_stdout_6x = """
+    expected_out_5x = f"""
+        {qry_list[0]}
         Select Expression
         ....-> Filter
         ........-> Table "TABLE_53" as "T53" Access By ID
         ............-> Index "I_TABLE_53_ID2_ASC" Full Scan
         ................-> Bitmap
         ....................-> Index "I_TABLE_53_ID1_ASC" Range Scan (full match)
+        {data_list[0]}
     """
 
-    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    expected_out_6x = f"""
+        {qry_list[0]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "PUBLIC"."TABLE_53" as "T53" Access By ID
+        ............-> Index "PUBLIC"."I_TABLE_53_ID2_ASC" Full Scan
+        ................-> Bitmap
+        ....................-> Index "PUBLIC"."I_TABLE_53_ID1_ASC" Range Scan (full match)
+        {data_list[0]}
+    """
 
+    act.expected_stdout = expected_out_4x if act.is_version('<5') else expected_out_5x if act.is_version('<6') else expected_out_6x
     act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
