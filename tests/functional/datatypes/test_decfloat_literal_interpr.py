f11b2e699805a93bee911af0428aa1a31e1d8fef
pavel-zotov <p519446@yandex.ru>
2025-07-08 21:38:00 +0300

    Added/Updated tests\functional\datatypes\test_decfloat_literal_interpr.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/functional/datatypes/test_decfloat_literal_interpr.py
+++ tests/functional/datatypes/test_decfloat_literal_interpr.py
@@ -13,6 +13,11 @@ DESCRIPTION:
   Currently only double precision form of literals is checked.
   Literals with value out bigint scope are not checked - waiting for reply from Alex, letter 24.05.2017 21:16
 FBTEST:      functional.datatypes.decfloat_literal_interpr
+NOTES:
+    [08.07.2025] pzotov
+    Separated expected output for FB major versions prior/since 6.x.
+    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+    Checked on 6.0.0.930; 5.0.3.1668; 4.0.6.3214.
 """
 
 import pytest
@@ -41,25 +46,29 @@ test_script = """
     */
 """
 
-act = isql_act('db', test_script)
+substitutions = [ ('^((?!SQLSTATE|sqltype|ALMOST_ZERO).)*$', ''), ('[ \t]+', ' ') ]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-    INPUT message field count: 0
-    OUTPUT message field count: 1
+expected_stdout_5x = """
     01: sqltype: 480 DOUBLE scale: 0 subtype: 0 len: 8
-    :  name: CONSTANT  alias: ALMOST_ZERO_DOUBLE_PRECISION
-    : table:   owner:
-    ALMOST_ZERO_DOUBLE_PRECISION    9.999999999999999e-309
-    INPUT message field count: 0
-    OUTPUT message field count: 1
+    : name: CONSTANT alias: ALMOST_ZERO_DOUBLE_PRECISION
+    ALMOST_ZERO_DOUBLE_PRECISION 9.999999999999999e-309
     01: sqltype: 32762 DECFLOAT(34) scale: 0 subtype: 0 len: 16
-    :  name: CONSTANT  alias: ALMOST_ZERO_DECFLOAT_34
-    : table:   owner:
-    ALMOST_ZERO_DECFLOAT_34                                             1E-309
+    : name: CONSTANT alias: ALMOST_ZERO_DECFLOAT_34
+    ALMOST_ZERO_DECFLOAT_34 1E-309
+"""
+
+expected_stdout_6x = """
+    01: sqltype: 480 DOUBLE scale: 0 subtype: 0 len: 8
+    : name: CONSTANT alias: ALMOST_ZERO_DOUBLE_PRECISION
+    ALMOST_ZERO_DOUBLE_PRECISION 9.999999999999999e-309
+    01: sqltype: 32762 DECFLOAT(34) scale: 0 subtype: 0 len: 16
+    : name: CONSTANT alias: ALMOST_ZERO_DECFLOAT_34
+    ALMOST_ZERO_DECFLOAT_34 1E-309
 """
 
 @pytest.mark.version('>=4.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
