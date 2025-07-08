d96be5b96e7254f3e4cee96f685c7d350fb75dea
pavel-zotov <p519446@yandex.ru>
2025-07-08 10:53:25 +0300

    Added/Updated tests\functional\arno\optimizer\test_opt_multi_index_selection_06.py: Refactored: explained plan is used to be checked in expected_out. See notes.

--- tests/functional/arno/optimizer/test_opt_multi_index_selection_06.py
+++ tests/functional/arno/optimizer/test_opt_multi_index_selection_06.py
@@ -9,9 +9,15 @@ DESCRIPTION:
 FBTEST:      functional.arno.optimizer.opt_multi_index_selection_06
 NOTES:
     [09.03.2023] pzotov
-    Adjusted allowed versions: FB 3.x and 4.x issue same execution plan.
-    Plan in FB 5.x differs: PLAN (ST INDEX (I_F3_F1_ASC)).
-    Version 5.x is not checked currently. Wait for resolution.
+        Adjusted allowed versions: FB 3.x and 4.x issue same execution plan.
+        Plan in FB 5.x differs: PLAN (ST INDEX (I_F3_F1_ASC)).
+        Version 5.x is not checked currently. Wait for resolution.
+    [08.07.2025] pzotov
+        Refactored: explained plan is used to be checked in expected_out.
+        Added ability to use several queries and their datasets for check - see 'qry_list' and 'qry_data' tuples.
+        Separated expected output for FB major versions prior/since 6.x.
+        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
+        Checked on 6.0.0.930; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813
 """
 
 import pytest
@@ -71,8 +77,8 @@ init_script = """
 
 db = db_factory(init=init_script)
 
-test_script = """
-    set planonly;
+qry_list = (
+    """
     select
       st.f1, st.f2, st.f3
     from
@@ -80,17 +86,92 @@ test_script = """
     where
       st.f1 >= 99 and st.f1 <= 101 and
       st.f2 >= 99 and
-      st.f3 = 100;
-"""
+      st.f3 = 100
+    """,
+)
+data_list = (
+    """
+    F1 : 100
+    F2 : 100
+    F3 : 100
+    """,
+)
 
-act = isql_act('db', test_script)
+substitutions = [ ( r'\(record length: \d+, key length: \d+\)', 'record length: N, key length: M' ) ]
+act = python_act('db', substitutions = substitutions)
 
-expected_stdout = """
-    PLAN (ST INDEX (I_F3_F2_ASC))
-"""
+#-----------------------------------------------------------
 
-@pytest.mark.version('>=3,<5')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+def replace_leading(source, char="."):
+    stripped = source.lstrip()
+    return char * (len(source) - len(stripped)) + stripped
+
+#-----------------------------------------------------------
+
+@pytest.mark.version('>=3.0')
+def test_1(act: Action, capsys):
+    with act.db.connect() as con:
+        cur = con.cursor()
+        for test_sql in qry_list:
+            ps, rs =  None, None
+            try:
+                cur = con.cursor()
+                ps = cur.prepare(test_sql)
+                print(test_sql)
+                # Print explained plan with padding eash line by dots in order to see indentations:
+                print( '\n'.join([replace_leading(s) for s in ps.detailed_plan.split('\n')]) )
+
+                # ::: NB ::: 'ps' returns data, i.e. this is SELECTABLE expression.
+                # We have to store result of cur.execute(<psInstance>) in order to
+                # close it explicitly.
+                # Otherwise AV can occur during Python garbage collection and this
+                # causes pytest to hang on its final point.
+                # Explained by hvlad, email 26.10.24 17:42
+                rs = cur.execute(ps)
+                cur_cols = cur.description
+                for r in rs:
+                    for i in range(0,len(cur_cols)):
+                        print( cur_cols[i][0], ':', r[i] )
+
+            except DatabaseError as e:
+                print(e.__str__())
+                print(e.gds_codes)
+            finally:
+                if rs:
+                    rs.close() # <<< EXPLICITLY CLOSING CURSOR RESULTS
+                if ps:
+                    ps.free()
+
+    expected_out_4x = f"""
+        {qry_list[0]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "SELECTIONTEST" as "ST" Access By ID
+        ............-> Bitmap
+        ................-> Index "I_F3_F2_ASC" Range Scan (lower bound: 2/2, upper bound: 1/2)
+        {data_list[0]}
+    """
+
+    expected_out_5x = f"""
+        {qry_list[0]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "SELECTIONTEST" as "ST" Access By ID
+        ............-> Bitmap
+        ................-> Index "I_F3_F1_ASC" Range Scan (lower bound: 2/2, upper bound: 2/2)
+        {data_list[0]}
+    """
+
+    expected_out_6x = f"""
+        {qry_list[0]}
+        Select Expression
+        ....-> Filter
+        ........-> Table "PUBLIC"."SELECTIONTEST" as "ST" Access By ID
+        ............-> Bitmap
+        ................-> Index "PUBLIC"."I_F3_F1_ASC" Range Scan (lower bound: 2/2, upper bound: 2/2)
+        {data_list[0]}
+    """
+
+    act.expected_stdout = expected_out_4x if act.is_version('<5') else expected_out_5x if act.is_version('<6') else expected_out_6x
+    act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
