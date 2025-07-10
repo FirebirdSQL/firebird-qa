e40cef9813a038c4671f52f623eb382405a27a81
pavel-zotov <p519446@yandex.ru>
2025-07-10 14:53:46 +0300

    Added/Updated tests\functional\domain\create\test_37.py: Marked as SKIPPED: covered by 'tests/functional/domain/create/test_all_datatypes_basic.py'

--- tests/functional/domain/create/test_37.py
+++ tests/functional/domain/create/test_37.py
@@ -108,6 +108,7 @@ expected_stdout = f"""
     validation error for column "TEST"."S", value "*** null ***"
 """
 
+@pytest.mark.skip("Covered by 'test_all_datatypes_basic.py'")
 @pytest.mark.version('>=3')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
