b70b2a3971e43c876e90dd5e452f64a03c38df54
pavel-zotov <p519446@yandex.ru>
2025-07-10 23:12:38 +0300

    Added/Updated tests\functional\exception\create\test_02.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x

--- tests/functional/exception/create/test_02.py
+++ tests/functional/exception/create/test_02.py
@@ -10,21 +10,26 @@ DESCRIPTION:
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE EXCEPTION test 'A1';
-commit;"""
+init_script = """
+    CREATE EXCEPTION test 'A1';
+    commit;
+"""
 
 db = db_factory(sql_dialect=3, init=init_script)
 
 act = isql_act('db', "CREATE EXCEPTION test 'message to show';")
 
-expected_stderr = """Statement failed, SQLSTATE = 42000
-unsuccessful metadata update
--CREATE EXCEPTION TEST failed
--Exception TEST already exists
-"""
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TEST_EXC_NAME = 'TEST' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"TEST"'
+    expected_stdout = f"""
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -CREATE EXCEPTION {TEST_EXC_NAME} failed
+        -Exception {TEST_EXC_NAME} already exists
+    """
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
