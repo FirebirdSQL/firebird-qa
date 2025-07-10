337a89e11a7023c49ac70097f75575dbf147945a
pavel-zotov <p519446@yandex.ru>
2025-07-10 16:26:56 +0300

    Updated tests\functional\domain\drop\test_02.py: adjust expected stdout/stderr to current FB version.

--- tests/functional/domain/drop/test_02.py
+++ tests/functional/domain/drop/test_02.py
@@ -10,21 +10,33 @@ DESCRIPTION:
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE DOMAIN test SMALLINT;
-CREATE TABLE tb( id test);"""
+db = db_factory()
 
-db = db_factory(init=init_script)
-
-act = isql_act('db', "DROP DOMAIN test;")
-
-expected_stderr = """Statement failed, SQLSTATE = 42000
-unsuccessful metadata update
--DROP DOMAIN TEST failed
--Domain TEST is used in table TB (local name ID) and cannot be dropped
+test_script = """
+    CREATE DOMAIN test SMALLINT;
+    CREATE TABLE tb( id test);
+    DROP DOMAIN test;
 """
+
+act = isql_act('db', test_script)
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+
+    expected_stdout_5x = """
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -DROP DOMAIN TEST failed
+        -Domain TEST is used in table TB (local name ID) and cannot be dropped
+    """
+
+    expected_stdout_6x = """
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -DROP DOMAIN "PUBLIC"."TEST" failed
+        -Domain "PUBLIC"."TEST" is used in table "PUBLIC"."TB" (local name "ID") and cannot be dropped
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
