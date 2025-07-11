4a4743df484d83eafd42c070dcd14989e9101591
pavel-zotov <p519446@yandex.ru>
2025-07-11 12:31:07 +0300

    Added/Updated tests\functional\gtcs\test_ref_integ_drop_fk_index.py: Increased the 'subsitutions' list to suppress PUBLIC schema prefix and remove single/double quotes from object names

--- tests/functional/gtcs/test_ref_integ_drop_fk_index.py
+++ tests/functional/gtcs/test_ref_integ_drop_fk_index.py
@@ -22,6 +22,18 @@ NOTES:
     See https://github.com/FirebirdSQL/firebird/pull/8202
     Commit (05-aug-2024 13"45):
     https://github.com/FirebirdSQL/firebird/commit/0cc8de396a3c2bbe13b161ecbfffa8055e7b4929
+
+    [11.07.2025] pzotov
+    Increased the 'subsitutions' list to suppress "PUBLIC" schema prefix and remove single/double quotes from object names. Need since 6.0.0.834.
+    ::: NB :::
+    File act.files_dir/'test_config.ini' must contain section:
+        [schema_n_quotes_suppress]
+        addi_subst="PUBLIC". " '
+    (thi file is used in qa/plugin.py, see QA_GLOBALS dictionary).
+
+    Value of parameter 'addi_subst' is splitted on tokens using space character and we add every token to 'substitutions' list which
+    eventually will be like this:
+        substitutions = [ ( <previous tuple(s)>, ('"PUBLIC".', ''), ('"', ''), ("'", '') ]
 """
 
 import pytest
@@ -29,8 +41,6 @@ import os
 from firebird.qa import *
 
 db = db_factory()
-
-act = python_act('db')
 
 test_expected_stdout = """
     Records affected: 0
@@ -60,6 +70,18 @@ expected_stderr_6x = """
     -Foreign key reference target does not exist
     -Problematic key value is ("DEPT_NO" = -1)
 """
+
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
+
+substitutions=[('[ \t]+', ' '), ('DM_FDEFAULT_BLOB_ID.*', ''), ('DM_FVALID_BLOB_ID.*', '')]
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
+
+act = python_act('db', substitutions = substitutions)
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
