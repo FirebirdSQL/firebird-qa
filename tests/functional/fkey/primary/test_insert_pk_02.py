ec524dcb0dfe79791c8d7068280587b3612a7fe4
pavel-zotov <p519446@yandex.ru>
2025-07-11 09:36:24 +0300

    Added/Updated tests\functional\fkey\primary\test_insert_pk_02.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x

--- tests/functional/fkey/primary/test_insert_pk_02.py
+++ tests/functional/fkey/primary/test_insert_pk_02.py
@@ -5,48 +5,62 @@ ID:          fkey.primary.insert-02
 FBTEST:      functional.fkey.primary.insert_pk_02
 TITLE:       Check correct work fix with foreign key
 DESCRIPTION:
-  Check foreign key work.
-  Master transaction modifies primary key.
-  Detail transaction inserts record in detail_table.
-  Expected: error - primary key in master table has been changed
+    Master transaction modifies primary key.
+    Detail transaction inserts record in detail_table.
+    Expected: error - primary key in master table has been changed
 """
 
 import pytest
 from firebird.qa import *
 from firebird.driver import DatabaseError, tpb, Isolation
 
-init_script = """CREATE TABLE MASTER_TABLE (
-    ID     INTEGER PRIMARY KEY,
-    INT_F  INTEGER
-);
+init_script = """
+    create table master_table (
+        id     integer primary key,
+        int_f  integer
+    );
 
-CREATE TABLE DETAIL_TABLE (
-    ID    INTEGER PRIMARY KEY,
-    FKEY  INTEGER
-);
+    create table detail_table (
+        id    integer primary key,
+        fkey  integer
+    );
 
-ALTER TABLE DETAIL_TABLE ADD CONSTRAINT FK_DETAIL_TABLE FOREIGN KEY (FKEY) REFERENCES MASTER_TABLE (ID);
-COMMIT;
-INSERT INTO MASTER_TABLE (ID, INT_F) VALUES (1, 10);
-commit;"""
+    alter table detail_table add constraint fk_detail_table foreign key (fkey) references master_table (id);
+    commit;
+    insert into master_table (id, int_f) values (1, 10);
+    commit;
+"""
 
 db = db_factory(init=init_script)
 
 act = python_act('db')
 
 @pytest.mark.version('>3')
-def test_1(act: Action):
-    cust_tpb = tpb(isolation=Isolation.READ_COMMITTED_RECORD_VERSION, lock_timeout=0)
+def test_1(act: Action, capsys):
+    custom_tpb = tpb(isolation=Isolation.READ_COMMITTED_RECORD_VERSION, lock_timeout=0)
     with act.db.connect() as con:
-        con.begin(cust_tpb)
-        with con.cursor() as c:
-            c.execute("UPDATE MASTER_TABLE SET ID = 2 WHERE ID=1")
+        con.begin(custom_tpb)
+        with con.cursor() as cur_main:
+            cur_main.execute("update master_table set id = 2 where id=1")
+
             #Create second connection for change detail table
             with act.db.connect() as con_detail:
-                con_detail.begin(cust_tpb)
-                with con_detail.cursor() as cd:
-                    with pytest.raises(DatabaseError,
-                                       match='.*violation of FOREIGN KEY constraint "FK_DETAIL_TABLE" on table "DETAIL_TABLE".*'):
-                        cd.execute("INSERT INTO DETAIL_TABLE (ID, FKEY) VALUES (1,1)")
-                con_detail.commit()
-    # Passed.
+                con_detail.begin(custom_tpb)
+                with con_detail.cursor() as cur_detl:
+                    try:
+                        cur_detl.execute('insert into detail_table (id, fkey) values (1,1)')
+                    except DatabaseError as e:
+                        print(e.__str__())
+                        print(e.gds_codes)
+
+    
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    act.expected_stdout = f"""
+        violation of FOREIGN KEY constraint "FK_DETAIL_TABLE" on table {SQL_SCHEMA_PREFIX}"DETAIL_TABLE"
+        -Foreign key reference target does not exist
+        -Problematic key value is ("FKEY" = 1)
+        (335544466, 335544838, 335545072)
+    """
+    act.stdout = capsys.readouterr().out
+    assert act.clean_stdout == act.clean_expected_stdout
+
