bab18cb8c0383f9e82beeeb13e0ca7ee545326e4
pavel-zotov <p519446@yandex.ru>
2025-07-11 11:20:15 +0300

    Added/Updated tests\functional\generator\drop\test_03.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x

--- tests/functional/generator/drop/test_03.py
+++ tests/functional/generator/drop/test_03.py
@@ -11,17 +11,19 @@ import pytest
 from firebird.qa import *
 
 db = db_factory()
-
-act = isql_act('db', "DROP GENERATOR test;")
-
-expected_stderr = """Statement failed, SQLSTATE = 42000
-unsuccessful metadata update
--DROP SEQUENCE TEST failed
--generator TEST is not defined
-"""
+act = isql_act('db', "DROP GENERATOR NO_SUCH_GEN;")
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TEST_GEN_NAME = 'NO_SUCH_GEN' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"NO_SUCH_GEN"'
+    expected_stdout = f"""
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -DROP SEQUENCE {TEST_GEN_NAME} failed
+        -generator {TEST_GEN_NAME} is not defined
+    """
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
