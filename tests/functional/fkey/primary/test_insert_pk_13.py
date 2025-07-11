b42deabec605404d4950e5c0d7cbd44e0c155c24
pavel-zotov <p519446@yandex.ru>
2025-07-11 10:38:57 +0300

    Added/Updated tests\functional\fkey\primary\test_insert_pk_13.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x

--- tests/functional/fkey/primary/test_insert_pk_13.py
+++ tests/functional/fkey/primary/test_insert_pk_13.py
@@ -5,10 +5,10 @@ ID:          fkey.primary.insert-13
 FBTEST:      functional.fkey.primary.insert_pk_13
 TITLE:       Check correct work fix with foreign key
 DESCRIPTION:
-  Check foreign key work.
-  Master transaction deletes record from master_table without commit.
-  Detail transaction inserts record in detail_table
-  Expected: error primary key field in master_table has been changed.
+    Check foreign key work.
+    Master transaction deletes record from master_table without commit.
+    Detail transaction inserts record in detail_table
+    Expected: error primary key field in master_table has been changed.
 """
 
 import pytest
@@ -28,7 +28,7 @@ init_script = """
     commit;
     insert into t_main(id) values(1);
     commit;
-  """
+"""
 
 db = db_factory(init=init_script)
 
@@ -49,16 +49,17 @@ test_script = """
 act = isql_act('db', test_script, substitutions=[('-At block line: [\\d]+, col: [\\d]+',
                                                   '-At block line')])
 
-expected_stderr = """
-    Statement failed, SQLSTATE = 23000
-    violation of FOREIGN KEY constraint "FK_TDETL_TMAIN" on table "T_DETL"
-    -Foreign key reference target does not exist
-    -Problematic key value is ("MASTER_PK_ID" = 1)
-    -At block line: 5, col: 9
-"""
-
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    expected_stdout = f"""
+        Statement failed, SQLSTATE = 23000
+        violation of FOREIGN KEY constraint "FK_TDETL_TMAIN" on table {SQL_SCHEMA_PREFIX}"T_DETL"
+        -Foreign key reference target does not exist
+        -Problematic key value is ("MASTER_PK_ID" = 1)
+        -At block line: 5, col: 9
+    """
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
