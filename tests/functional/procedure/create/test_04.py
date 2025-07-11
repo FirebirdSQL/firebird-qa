aed02e3622738c20b00bf094d273d933ab1cb927
pavel-zotov <p519446@yandex.ru>
2025-07-11 17:54:31 +0300

    Added/Updated tests\functional\procedure\create\test_04.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/functional/procedure/create/test_04.py
+++ tests/functional/procedure/create/test_04.py
@@ -12,73 +12,115 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """SET TERM ^;
-CREATE PROCEDURE test
-AS
-DECLARE VARIABLE p1 SMALLINT;
-DECLARE VARIABLE p2 INTEGER;
-DECLARE VARIABLE p3 FLOAT;
-DECLARE VARIABLE p4 DOUBLE PRECISION;
-DECLARE VARIABLE p5 DECIMAL(9,3);
-DECLARE VARIABLE p6 NUMERIC(10,4);
-DECLARE VARIABLE p7 DATE;
-DECLARE VARIABLE p8 TIME;
-DECLARE VARIABLE p9 TIMESTAMP;
-DECLARE VARIABLE p10 CHAR(40);
-DECLARE VARIABLE p11 VARCHAR(60);
-DECLARE VARIABLE p12 NCHAR(70);
-BEGIN
-  p1=1;
-  p2=2;
-  p3=3.4;
-  p4=4.5;
-  p5=5.6;
-  p6=6.7;
-  p7='31.8.1995';
-  p8='13:45:57.1';
-  p9='29.2.200 14:46:59.9';
-  p10='Text p10';
-  p11='Text p11';
-  p12='Text p13';
-END ^
-SET TERM ;^
-commit;
-SHOW PROCEDURE test;"""
+SP_BODY = """
+        declare variable p1 smallint;
+        declare variable p2 integer;
+        declare variable p3 float;
+        declare variable p4 double precision;
+        declare variable p5 decimal(9,3);
+        declare variable p6 numeric(10,4);
+        declare variable p7 date;
+        declare variable p8 time;
+        declare variable p9 timestamp;
+        declare variable p10 char(40);
+        declare variable p11 varchar(60);
+        declare variable p12 nchar(70);
+    begin
+        p1=1;
+        p2=2;
+        p3=3.4;
+        p4=4.5;
+        p5=5.6;
+        p6=6.7;
+        p7='31.8.1995';
+        p8='13:45:57.1';
+        p9='29.2.200 14:46:59.9';
+        p10='text p10';
+        p11='text p11';
+        p12='text p13';
+    end
+"""
 
-act = isql_act('db', test_script)
+test_script = f"""
+    set term ^;
+    create procedure sp_test as
+    {SP_BODY}
+    ^
+    set term ;^
+    commit;
+    show procedure sp_test;
+"""
 
-expected_stdout = """Procedure text:
-=============================================================================
-DECLARE VARIABLE p1 SMALLINT;
-DECLARE VARIABLE p2 INTEGER;
-DECLARE VARIABLE p3 FLOAT;
-DECLARE VARIABLE p4 DOUBLE PRECISION;
-DECLARE VARIABLE p5 DECIMAL(9,3);
-DECLARE VARIABLE p6 NUMERIC(10,4);
-DECLARE VARIABLE p7 DATE;
-DECLARE VARIABLE p8 TIME;
-DECLARE VARIABLE p9 TIMESTAMP;
-DECLARE VARIABLE p10 CHAR(40);
-DECLARE VARIABLE p11 VARCHAR(60);
-DECLARE VARIABLE p12 NCHAR(70);
-BEGIN
-  p1=1;
-  p2=2;
-  p3=3.4;
-  p4=4.5;
-  p5=5.6;
-  p6=6.7;
-  p7='31.8.1995';
-  p8='13:45:57.1';
-  p9='29.2.200 14:46:59.9';
-  p10='Text p10';
-  p11='Text p11';
-  p12='Text p13';
-END
-============================================================================="""
+act = isql_act('db', test_script, substitutions = [('=====*','')])
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+
+    expected_stdout_5x = """
+        Procedure text:
+        =============================================================================
+        declare variable p1 smallint;
+        declare variable p2 integer;
+        declare variable p3 float;
+        declare variable p4 double precision;
+        declare variable p5 decimal(9,3);
+        declare variable p6 numeric(10,4);
+        declare variable p7 date;
+        declare variable p8 time;
+        declare variable p9 timestamp;
+        declare variable p10 char(40);
+        declare variable p11 varchar(60);
+        declare variable p12 nchar(70);
+        begin
+        p1=1;
+        p2=2;
+        p3=3.4;
+        p4=4.5;
+        p5=5.6;
+        p6=6.7;
+        p7='31.8.1995';
+        p8='13:45:57.1';
+        p9='29.2.200 14:46:59.9';
+        p10='text p10';
+        p11='text p11';
+        p12='text p13';
+        end
+        =============================================================================
+    """
+
+    expected_stdout_6x = """
+        Procedure: PUBLIC.SP_TEST
+        Procedure text:
+        =============================================================================
+        declare variable p1 smallint;
+        declare variable p2 integer;
+        declare variable p3 float;
+        declare variable p4 double precision;
+        declare variable p5 decimal(9,3);
+        declare variable p6 numeric(10,4);
+        declare variable p7 date;
+        declare variable p8 time;
+        declare variable p9 timestamp;
+        declare variable p10 char(40);
+        declare variable p11 varchar(60);
+        declare variable p12 nchar(70);
+        begin
+        p1=1;
+        p2=2;
+        p3=3.4;
+        p4=4.5;
+        p5=5.6;
+        p6=6.7;
+        p7='31.8.1995';
+        p8='13:45:57.1';
+        p9='29.2.200 14:46:59.9';
+        p10='text p10';
+        p11='text p11';
+        p12='text p13';
+        end
+        =============================================================================
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
