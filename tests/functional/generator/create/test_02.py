85e92a4d5559e33cad1b12ce6818039f02a7425b
pavel-zotov <p519446@yandex.ru>
2025-07-11 11:15:15 +0300

    Added/Updated tests\functional\generator\create\test_02.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x

--- tests/functional/generator/create/test_02.py
+++ tests/functional/generator/create/test_02.py
@@ -10,26 +10,29 @@ import pytest
 from firebird.qa import *
 
 init_script = """
-    CREATE GENERATOR test;
+    create generator gen_test;
     commit;
 """
 
 db = db_factory(init=init_script)
 
 test_script = """
-    CREATE GENERATOR test;
+    create generator gen_test;
 """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """Statement failed, SQLSTATE = 42000
-    unsuccessful metadata update
-    -CREATE SEQUENCE TEST failed
-    -Sequence TEST already exists
-"""
-
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TEST_GEN_NAME = 'GEN_TEST' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"GEN_TEST"'
+    expected_stdout = f"""
+        Statement failed, SQLSTATE = 42000
+        unsuccessful metadata update
+        -CREATE SEQUENCE {TEST_GEN_NAME} failed
+        -Sequence {TEST_GEN_NAME} already exists
+    """
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
