a66288f798d1e6766858b60272ec03c45957c31f
pavel-zotov <p519446@yandex.ru>
2025-07-11 16:10:39 +0300

    Added/Updated tests\functional\monitoring\test_03.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/functional/monitoring/test_03.py
+++ tests/functional/monitoring/test_03.py
@@ -29,45 +29,77 @@ test_script = """
     select mon$call_stack.mon$compiled_statement_id from mon$call_stack;
 """
 
-substitutions = [('^((?!SQLSTATE|sqltype|name:|table:).)*$', ''), ('.*owner:.*', ''), ('.*alias:.*', ''), ('[ \t]+', ' ')]
-
+substitutions = [('^((?!SQLSTATE|sqltype|name:|table:).)*$', ''), ('[ \t]+', ' ')]
 act = isql_act('db', test_script, substitutions = substitutions)
-
-expected_stdout = """
-    01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
-      :  name: MON$COMPILED_STATEMENT_ID  alias: MON$COMPILED_STATEMENT_ID
-      : table: MON$COMPILED_STATEMENTS  owner: SYSDBA
-    02: sqltype: 520 BLOB Nullable scale: 0 subtype: 1 len: 8 charset: 4 UTF8
-      :  name: MON$SQL_TEXT  alias: MON$SQL_TEXT
-      : table: MON$COMPILED_STATEMENTS  owner: SYSDBA
-    03: sqltype: 520 BLOB Nullable scale: 0 subtype: 1 len: 8 charset: 4 UTF8
-      :  name: MON$EXPLAINED_PLAN  alias: MON$EXPLAINED_PLAN
-      : table: MON$COMPILED_STATEMENTS  owner: SYSDBA
-    04: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 UTF8
-      :  name: MON$OBJECT_NAME  alias: MON$OBJECT_NAME
-      : table: MON$COMPILED_STATEMENTS  owner: SYSDBA
-    05: sqltype: 500 SHORT Nullable scale: 0 subtype: 0 len: 2
-      :  name: MON$OBJECT_TYPE  alias: MON$OBJECT_TYPE
-      : table: MON$COMPILED_STATEMENTS  owner: SYSDBA
-    06: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 UTF8
-      :  name: MON$PACKAGE_NAME  alias: MON$PACKAGE_NAME
-      : table: MON$COMPILED_STATEMENTS  owner: SYSDBA
-    07: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
-      :  name: MON$STAT_ID  alias: MON$STAT_ID
-      : table: MON$COMPILED_STATEMENTS  owner: SYSDBA
-
-
-    01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
-      :  name: MON$COMPILED_STATEMENT_ID  alias: MON$COMPILED_STATEMENT_ID
-      : table: MON$STATEMENTS  owner: SYSDBA
-
-    01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
-      :  name: MON$COMPILED_STATEMENT_ID  alias: MON$COMPILED_STATEMENT_ID
-      : table: MON$CALL_STACK  owner: SYSDBA
-"""
 
 @pytest.mark.version('>=5.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
+
+    expected_stdout_5x = """
+        01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+        : name: MON$COMPILED_STATEMENT_ID alias: MON$COMPILED_STATEMENT_ID
+        : table: MON$COMPILED_STATEMENTS owner: SYSDBA
+        02: sqltype: 520 BLOB Nullable scale: 0 subtype: 1 len: 8 charset: 4 UTF8
+        : name: MON$SQL_TEXT alias: MON$SQL_TEXT
+        : table: MON$COMPILED_STATEMENTS owner: SYSDBA
+        03: sqltype: 520 BLOB Nullable scale: 0 subtype: 1 len: 8 charset: 4 UTF8
+        : name: MON$EXPLAINED_PLAN alias: MON$EXPLAINED_PLAN
+        : table: MON$COMPILED_STATEMENTS owner: SYSDBA
+        04: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 UTF8
+        : name: MON$OBJECT_NAME alias: MON$OBJECT_NAME
+        : table: MON$COMPILED_STATEMENTS owner: SYSDBA
+        05: sqltype: 500 SHORT Nullable scale: 0 subtype: 0 len: 2
+        : name: MON$OBJECT_TYPE alias: MON$OBJECT_TYPE
+        : table: MON$COMPILED_STATEMENTS owner: SYSDBA
+        06: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 UTF8
+        : name: MON$PACKAGE_NAME alias: MON$PACKAGE_NAME
+        : table: MON$COMPILED_STATEMENTS owner: SYSDBA
+        07: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
+        : name: MON$STAT_ID alias: MON$STAT_ID
+        : table: MON$COMPILED_STATEMENTS owner: SYSDBA
+
+        01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+        : name: MON$COMPILED_STATEMENT_ID alias: MON$COMPILED_STATEMENT_ID
+        : table: MON$STATEMENTS owner: SYSDBA
+        01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+        : name: MON$COMPILED_STATEMENT_ID alias: MON$COMPILED_STATEMENT_ID
+        : table: MON$CALL_STACK owner: SYSDBA
+    """
+
+    expected_stdout_6x = """
+        01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+        : name: MON$COMPILED_STATEMENT_ID alias: MON$COMPILED_STATEMENT_ID
+        : table: MON$COMPILED_STATEMENTS schema: SYSTEM owner: SYSDBA
+        02: sqltype: 520 BLOB Nullable scale: 0 subtype: 1 len: 8 charset: 4 SYSTEM.UTF8
+        : name: MON$SQL_TEXT alias: MON$SQL_TEXT
+        : table: MON$COMPILED_STATEMENTS schema: SYSTEM owner: SYSDBA
+        03: sqltype: 520 BLOB Nullable scale: 0 subtype: 1 len: 8 charset: 4 SYSTEM.UTF8
+        : name: MON$EXPLAINED_PLAN alias: MON$EXPLAINED_PLAN
+        : table: MON$COMPILED_STATEMENTS schema: SYSTEM owner: SYSDBA
+        04: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 SYSTEM.UTF8
+        : name: MON$OBJECT_NAME alias: MON$OBJECT_NAME
+        : table: MON$COMPILED_STATEMENTS schema: SYSTEM owner: SYSDBA
+        05: sqltype: 500 SHORT Nullable scale: 0 subtype: 0 len: 2
+        : name: MON$OBJECT_TYPE alias: MON$OBJECT_TYPE
+        : table: MON$COMPILED_STATEMENTS schema: SYSTEM owner: SYSDBA
+        06: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 SYSTEM.UTF8
+        : name: MON$PACKAGE_NAME alias: MON$PACKAGE_NAME
+        : table: MON$COMPILED_STATEMENTS schema: SYSTEM owner: SYSDBA
+        07: sqltype: 496 LONG Nullable scale: 0 subtype: 0 len: 4
+        : name: MON$STAT_ID alias: MON$STAT_ID
+        : table: MON$COMPILED_STATEMENTS schema: SYSTEM owner: SYSDBA
+        08: sqltype: 452 TEXT Nullable scale: 0 subtype: 0 len: 252 charset: 4 SYSTEM.UTF8
+        : name: MON$SCHEMA_NAME alias: MON$SCHEMA_NAME
+        : table: MON$COMPILED_STATEMENTS schema: SYSTEM owner: SYSDBA
+
+        01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+        : name: MON$COMPILED_STATEMENT_ID alias: MON$COMPILED_STATEMENT_ID
+        : table: MON$STATEMENTS schema: SYSTEM owner: SYSDBA
+        01: sqltype: 580 INT64 Nullable scale: 0 subtype: 0 len: 8
+        : name: MON$COMPILED_STATEMENT_ID alias: MON$COMPILED_STATEMENT_ID
+        : table: MON$CALL_STACK schema: SYSTEM owner: SYSDBA    
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
