c6e65e6df575fc10ad8c153067def9213ffd8cb8
pavel-zotov <p519446@yandex.ru>
2025-07-11 18:14:20 +0300

    Added/Updated tests\functional\procedure\create\test_04.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/functional/procedure/create/test_04.py
+++ tests/functional/procedure/create/test_04.py
@@ -56,69 +56,15 @@ act = isql_act('db', test_script, substitutions = [('=====*','')])
 @pytest.mark.version('>=3')
 def test_1(act: Action):
 
-    expected_stdout_5x = """
+    expected_stdout_5x = f"""
         Procedure text:
-        =============================================================================
-        declare variable p1 smallint;
-        declare variable p2 integer;
-        declare variable p3 float;
-        declare variable p4 double precision;
-        declare variable p5 decimal(9,3);
-        declare variable p6 numeric(10,4);
-        declare variable p7 date;
-        declare variable p8 time;
-        declare variable p9 timestamp;
-        declare variable p10 char(40);
-        declare variable p11 varchar(60);
-        declare variable p12 nchar(70);
-        begin
-        p1=1;
-        p2=2;
-        p3=3.4;
-        p4=4.5;
-        p5=5.6;
-        p6=6.7;
-        p7='31.8.1995';
-        p8='13:45:57.1';
-        p9='29.2.200 14:46:59.9';
-        p10='text p10';
-        p11='text p11';
-        p12='text p13';
-        end
-        =============================================================================
+        {SP_BODY}
     """
 
-    expected_stdout_6x = """
+    expected_stdout_6x = f"""
         Procedure: PUBLIC.SP_TEST
         Procedure text:
-        =============================================================================
-        declare variable p1 smallint;
-        declare variable p2 integer;
-        declare variable p3 float;
-        declare variable p4 double precision;
-        declare variable p5 decimal(9,3);
-        declare variable p6 numeric(10,4);
-        declare variable p7 date;
-        declare variable p8 time;
-        declare variable p9 timestamp;
-        declare variable p10 char(40);
-        declare variable p11 varchar(60);
-        declare variable p12 nchar(70);
-        begin
-        p1=1;
-        p2=2;
-        p3=3.4;
-        p4=4.5;
-        p5=5.6;
-        p6=6.7;
-        p7='31.8.1995';
-        p8='13:45:57.1';
-        p9='29.2.200 14:46:59.9';
-        p10='text p10';
-        p11='text p11';
-        p12='text p13';
-        end
-        =============================================================================
+        {SP_BODY}
     """
 
     act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
