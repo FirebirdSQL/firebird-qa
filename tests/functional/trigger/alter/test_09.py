130e3322640bf4f218facdd102521670737307e9
pavel-zotov <p519446@yandex.ru>
2025-07-12 21:39:12 +0300

    Added/Updated tests\functional\trigger\alter\test_09.py: Marked as SKIPPED: covered by 'tests/functional/trigger/alter/test_alter_dml_basic.py'

--- tests/functional/trigger/alter/test_09.py
+++ tests/functional/trigger/alter/test_09.py
@@ -45,6 +45,7 @@ expected_stdout = """TEXT
 tg2 tg1
 """
 
+@pytest.mark.skip("Covered by 'test_alter_dml_basic.py'")
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
