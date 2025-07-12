aa4b7a9d1f517f9f32606e4b7aa46ac98c1a59be
pavel-zotov <p519446@yandex.ru>
2025-07-12 15:40:13 +0300

    Added/Updated tests\functional\tabloid\test_cc183f59.py: adapted to run on 6.x

--- tests/functional/tabloid/test_cc183f59.py
+++ tests/functional/tabloid/test_cc183f59.py
@@ -56,7 +56,17 @@ substitutions =  [  ('[ \t]+', ' ')
                    ,('record length: \\d+, key length: \\d+', 'record length, key length')
                  ]
 
+# QA_GLOBALS -- dict, is defined in qa/plugin.py, obtain settings
+# from act.files_dir/'test_config.ini':
+#
+addi_subst_settings = QA_GLOBALS['schema_n_quotes_suppress']
+addi_subst_tokens = addi_subst_settings['addi_subst']
+
+for p in addi_subst_tokens.split(' '):
+    substitutions.append( (p, '') )
+
 act = python_act('db', substitutions = substitutions)
+
 
 
 #-----------------------------------------------------------
