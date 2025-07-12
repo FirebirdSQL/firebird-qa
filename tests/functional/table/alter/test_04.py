52a9a179dc9f89a2e36f78b056b6f502360e0b9b
pavel-zotov <p519446@yandex.ru>
2025-07-12 12:35:50 +0300

    Added/Updated tests\functional\table\alter\test_04.py: Removed 'SHOW' command. Statement 'ALTER TABLE...' followed by commit must allow further actions related to changed DDL. See notes.

--- tests/functional/table/alter/test_04.py
+++ tests/functional/table/alter/test_04.py
@@ -5,27 +5,60 @@ ID:          table.alter-04
 TITLE:       ALTER TABLE - ADD CONSTRAINT - UNIQUE
 DESCRIPTION:
 FBTEST:      functional.table.alter.04
+NOTES:
+    [12.07.2025] pzotov
+    Removed 'SHOW' command.
+    Statement 'ALTER TABLE...' followed by commit must allow further actions related to changed DDL.
+    Added 'SQL_SCHEMA_PREFIX' and variable to be substituted in expected_* on FB 6.x
+    Checked on 6.0.0.949; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE test( id INTEGER NOT NULL);
-commit;"""
+db = db_factory()
 
-db = db_factory(init=init_script)
+test_script = """
+    set list on;
+    create table test( id int);
+    insert into test(id) values(null);
+    insert into test(id) values(-2);
+    insert into test(id) values(-2);
+    insert into test(id) values(null);
+    commit;
+    alter table test add constraint test_unq unique(id); -- must fail because duplicates exist
+    commit;
+    delete from test where id is not null;
+    commit;
+    alter table test add constraint test_unq unique(id); -- must pass
+    commit;
+    insert into test(id) values(1);
+    insert into test(id) values(1); -- must fail
+    select id, count(*) from test group by id order by id; -- must issue null:2, 1:1
+"""
 
-test_script = """ALTER TABLE test ADD CONSTRAINT unq UNIQUE(id);
-SHOW TABLE test;"""
-
-act = isql_act('db', test_script)
-
-expected_stdout = """ID                              INTEGER Not Null
-CONSTRAINT UNQ:
-Unique key (ID)"""
+substitutions = [] # [('[ \t]+', ' '), ('(-)?Problematic key value is.*', 'Problematic key value is')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TEST_TABLE_NAME = '"TEST"' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"TEST"'
+    expected_stdout = f"""
+        Statement failed, SQLSTATE = 23000
+        violation of PRIMARY or UNIQUE KEY constraint "TEST_UNQ" on table {TEST_TABLE_NAME}
+        -Problematic key value is ("ID" = -2)
+
+        Statement failed, SQLSTATE = 23000
+        violation of PRIMARY or UNIQUE KEY constraint "TEST_UNQ" on table {TEST_TABLE_NAME}
+        -Problematic key value is ("ID" = 1)
+
+        ID                              <null>
+        COUNT                           2
+        ID                              1
+        COUNT                           1
+    """
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
