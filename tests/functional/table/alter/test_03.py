2bdfd5b9f38af82aa87dc3eacaac63898afbf6d1
pavel-zotov <p519446@yandex.ru>
2025-07-12 12:30:34 +0300

    Added/Updated tests\functional\table\alter\test_03.py: Removed 'SHOW' command. Statement 'ALTER TABLE...' followed by commit must allow further actions related to changed DDL. See notes.

--- tests/functional/table/alter/test_03.py
+++ tests/functional/table/alter/test_03.py
@@ -5,27 +5,55 @@ ID:          table.alter-03
 TITLE:       ALTER TABLE - ADD CONSTRAINT - PRIMARY KEY
 DESCRIPTION:
 FBTEST:      functional.table.alter.03
+NOTES:
+    [12.07.2025] pzotov
+    Removed 'SHOW' command.
+    Statement 'ALTER TABLE...' followed by commit must allow further actions related to changed DDL.
+    Added 'SQL_SCHEMA_PREFIX' and variable to be substituted in expected_* on FB 6.x
+    Checked on 6.0.0.949; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE test( id INTEGER NOT NULL);
-commit;"""
+db = db_factory()
 
-db = db_factory(init=init_script)
-
-test_script = """ALTER TABLE test ADD CONSTRAINT pk PRIMARY KEY(id);
-SHOW TABLE test;"""
-
-act = isql_act('db', test_script)
-
-expected_stdout = """ID                              INTEGER Not Null
-CONSTRAINT PK:
-Primary key (ID)"""
+test_script = """
+    set list on;
+    create table test( id integer not null);
+    insert into test(id) values(-1);
+    insert into test(id) values(-2);
+    insert into test(id) values(-1);
+    commit;
+    alter table test add constraint pk primary key(id); -- must fail because duplicates exist
+    commit;
+    delete from test;
+    commit;
+    alter table test add constraint pk primary key(id); -- must pass
+    commit;
+    insert into test(id) values(1);
+    insert into test(id) values(1); -- must fail
+    select * from test; -- must issue one record
+"""
+substitutions = [('[ \t]+', ' '), ('(-)?Problematic key value is.*', 'Problematic key value is')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TEST_TABLE_NAME = '"TEST"' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"TEST"'
+    expected_stdout = f"""
+        Statement failed, SQLSTATE = 23000
+        violation of PRIMARY or UNIQUE KEY constraint "PK" on table {TEST_TABLE_NAME}
+        -Problematic key value is ("ID" = -1)
+
+        Statement failed, SQLSTATE = 23000
+        violation of PRIMARY or UNIQUE KEY constraint "PK" on table {TEST_TABLE_NAME}
+        -Problematic key value is ("ID" = 1)
+
+        ID                              1
+    """
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
