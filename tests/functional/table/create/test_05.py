a5da6f1f71c2753249a4e90b72f99dbad0e2cb5d
pavel-zotov <p519446@yandex.ru>
2025-07-12 14:49:56 +0300

    Added/Updated tests\functional\table\create\test_05.py: Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x

--- tests/functional/table/create/test_05.py
+++ tests/functional/table/create/test_05.py
@@ -5,35 +5,42 @@ ID:          table.create-05
 TITLE:       CREATE TABLE - create table with same name
 DESCRIPTION:
 FBTEST:      functional.table.create.05
+NOTES:
+    [12.07.2025] pzotov
+    Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x
+    Checked on 6.0.0.949; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
 """
 
 import pytest
 from firebird.qa import *
 
-init_script = """CREATE TABLE test(
- c1 SMALLINT
-);
-commit;
-"""
+db = db_factory()
 
-db = db_factory(init=init_script)
+test_script = """
+    create table test(
+     c1 smallint
+    );
+    commit;
 
-test_script = """CREATE TABLE test(
- c1 SMALLINT,
- c2 INTEGER
-);
+    create table test(
+     c1 smallint,
+     c2 integer
+    );
 """
 
 act = isql_act('db', test_script)
 
-expected_stderr = """Statement failed, SQLSTATE = 42S01
-unsuccessful metadata update
--CREATE TABLE TEST failed
--Table TEST already exists
-"""
-
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert act.clean_stderr == act.clean_expected_stderr
+
+    SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
+    TEST_TABLE_NAME = 'TEST' if act.is_version('<6') else f'{SQL_SCHEMA_PREFIX}"TEST"'
+    expected_stdout = f"""
+        Statement failed, SQLSTATE = 42S01
+        unsuccessful metadata update
+        -CREATE TABLE {TEST_TABLE_NAME} failed
+        -Table {TEST_TABLE_NAME} already exists
+    """
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
