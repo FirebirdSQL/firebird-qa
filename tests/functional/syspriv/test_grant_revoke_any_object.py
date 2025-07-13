0257099585ae278e94bac6a34f8001e78a06d4ff
pavel-zotov <p519446@yandex.ru>
2025-07-13 09:53:52 +0300

    Added/Updated tests\functional\syspriv\test_grant_revoke_any_object.py: Separated expected output for FB major versions prior/since 6.x.

--- tests/functional/syspriv/test_grant_revoke_any_object.py
+++ tests/functional/syspriv/test_grant_revoke_any_object.py
@@ -20,112 +20,116 @@ import pytest
 from firebird.qa import *
 
 db = db_factory()
-user_01 = user_factory('db', name='u01', do_not_create=True)
-user_02 = user_factory('db', name='u02', do_not_create=True)
-role_revoke = role_factory('db', name='role_for_grant_revoke_any_object', do_not_create=True)
+user_01 = user_factory('db', name='u01', password = '123')
+user_02 = user_factory('db', name='u02', password = '456')
+role_revoke = role_factory('db', name='role_for_grant_revoke_any_object')
 
-test_script = """
-    set wng off;
-    set bail on;
-    set list on;
-
-
-    create or alter user u01 password '123' revoke admin role;
-    create or alter user u02 password '456' revoke admin role;
-    revoke all on all from u01;
-    revoke all on all from u02;
-    grant create table to u01;
-    commit;
-/*
-    set term ^;
-    execute block as
-    begin
-      execute statement 'drop role role_for_grant_revoke_any_object';
-      when any do begin end
-    end^
-    set term ;^
-    commit;
-*/
-    -- Add/change/delete non-system records in RDB$TYPES
-    create role role_for_grant_revoke_any_object set system privileges to GRANT_REVOKE_ON_ANY_OBJECT;
-    commit;
-    grant default role_for_grant_revoke_any_object to user u01;
-    commit;
-
-    connect '$(DSN)' user u01 password '123';
-    select current_user as who_am_i,r.rdb$role_name,rdb$role_in_use(r.rdb$role_name),r.rdb$system_privileges
-    from mon$database m cross join rdb$roles r;
-    commit;
-
-    recreate table test_u01(id int, who_is_author varchar(31) default current_user);
-    commit;
-    insert into test_u01(id) values(1);
-    commit;
-
-    grant select on table test_u01 to u02; -- nb: do NOT add here "granted by sysdba"!
-    commit;
-
-    -- this should give output with rdb$grantor = 'SYSDBA' despite that actual grantor was 'U01':
-    select * from rdb$user_privileges where rdb$relation_name=upper('test_u01') and rdb$user=upper('u02');
-    commit;
-
-    connect '$(DSN)' user u02 password '456';
-    select current_user as who_am_i, u.* from test_u01 u;
-    commit;
-
-    connect '$(DSN)' user u01 password '123';
-    revoke select on test_u01 from u02;
-    commit;
-
-    set bail off;
-    connect '$(DSN)' user u02 password '456';
-    select current_user as who_am_i, u.* from test_u01 u; -- this should FAIL
-    commit;
-    set bail on;
-
-    -- connect '$(DSN)' user sysdba password 'masterkey';
-    -- drop user u01;
-    -- drop user u02;
-    -- commit;
-"""
-
-act = isql_act('db', test_script)
-
-expected_stdout = """
-    WHO_AM_I                        U01
-    RDB$ROLE_NAME                   RDB$ADMIN
-    RDB$ROLE_IN_USE                 <false>
-    RDB$SYSTEM_PRIVILEGES           FFFFFFFFFFFFFFFF
-
-    WHO_AM_I                        U01
-    RDB$ROLE_NAME                   ROLE_FOR_GRANT_REVOKE_ANY_OBJECT
-    RDB$ROLE_IN_USE                 <true>
-    RDB$SYSTEM_PRIVILEGES           0000200000000000
-
-    RDB$USER                        U02
-    RDB$GRANTOR                     U01
-    RDB$PRIVILEGE                   S
-    RDB$GRANT_OPTION                0
-    RDB$RELATION_NAME               TEST_U01
-    RDB$FIELD_NAME                  <null>
-    RDB$USER_TYPE                   8
-    RDB$OBJECT_TYPE                 0
-
-    WHO_AM_I                        U02
-    ID                              1
-    WHO_IS_AUTHOR                   U01
-"""
-
-expected_stderr = """
-    Statement failed, SQLSTATE = 28000
-    no permission for SELECT access to TABLE TEST_U01
-    -Effective user is U02
-"""
+act = isql_act('db')
 
 @pytest.mark.version('>=4.0')
-def test_1(act: Action, user_01, user_02, role_revoke):
-    act.expected_stdout = expected_stdout
-    act.expected_stderr = expected_stderr
-    act.execute()
-    assert (act.clean_stderr == act.clean_expected_stderr and
-            act.clean_stdout == act.clean_expected_stdout)
+def test_1(act: Action, user_01: User, user_02: User, role_revoke: Role):
+
+    test_script = f"""
+        set wng off;
+        set bail on;
+        set list on;
+
+        alter user {user_01.name} revoke admin role;
+        alter user {user_02.name} revoke admin role;
+        grant create table to {user_01.name};
+        commit;
+
+        -- Add/change/delete non-system records in RDB$TYPES
+        alter role {role_revoke.name} set system privileges to GRANT_REVOKE_ON_ANY_OBJECT;
+        commit;
+        grant default {role_revoke.name} to user {user_01.name};
+        commit;
+
+        connect '{act.db.dsn}' user {user_01.name} password '{user_01.password}';
+        select current_user as who_am_i,r.rdb$role_name,rdb$role_in_use(r.rdb$role_name),r.rdb$system_privileges
+        from mon$database m cross join rdb$roles r;
+        commit;
+
+        recreate table test_u01(id int, who_is_author varchar(31) default current_user);
+        commit;
+        insert into test_u01(id) values(1);
+        commit;
+
+        grant select on table test_u01 to {user_02.name}; -- nb: do NOT add here "granted by sysdba"!
+        commit;
+
+        -- this should give output with rdb$grantor = 'SYSDBA' despite that actual grantor was '{user_01.name}':
+        select * from rdb$user_privileges where rdb$relation_name=upper('test_u01') and rdb$user=upper('{user_02.name}');
+        commit;
+
+        connect '{act.db.dsn}' user {user_02.name} password '{user_02.password}';
+        select current_user as who_am_i, u.* from test_u01 u;
+        commit;
+
+        connect '{act.db.dsn}' user {user_01.name} password '{user_01.password}';
+        revoke select on test_u01 from {user_02.name};
+        commit;
+
+        set bail off;
+        connect '{act.db.dsn}' user {user_02.name} password '{user_02.password}';
+        select current_user as who_am_i, u.* from test_u01 u; -- this should FAIL
+        commit;
+        set bail on;
+    """
+
+
+    expected_stdout_5x = f"""
+        WHO_AM_I                        {user_01.name.upper()}
+        RDB$ROLE_NAME                   RDB$ADMIN
+        RDB$ROLE_IN_USE                 <false>
+        RDB$SYSTEM_PRIVILEGES           FFFFFFFFFFFFFFFF
+        WHO_AM_I                        {user_01.name.upper()}
+        RDB$ROLE_NAME                   {role_revoke.name.upper()}
+        RDB$ROLE_IN_USE                 <true>
+        RDB$SYSTEM_PRIVILEGES           0000200000000000
+        RDB$USER                        {user_02.name.upper()}
+        RDB$GRANTOR                     {user_01.name.upper()}
+        RDB$PRIVILEGE                   S
+        RDB$GRANT_OPTION                0
+        RDB$RELATION_NAME               TEST_U01
+        RDB$FIELD_NAME                  <null>
+        RDB$USER_TYPE                   8
+        RDB$OBJECT_TYPE                 0
+        WHO_AM_I                        {user_02.name.upper()}
+        ID                              1
+        WHO_IS_AUTHOR                   {user_01.name.upper()}
+        Statement failed, SQLSTATE = 28000
+        no permission for SELECT access to TABLE TEST_U01
+        -Effective user is {user_02.name.upper()}
+    """
+
+    expected_stdout_6x = f"""
+        WHO_AM_I                        {user_01.name.upper()}
+        RDB$ROLE_NAME                   RDB$ADMIN
+        RDB$ROLE_IN_USE                 <false>
+        RDB$SYSTEM_PRIVILEGES           FFFFFFFFFFFFFFFF
+        WHO_AM_I                        {user_01.name.upper()}
+        RDB$ROLE_NAME                   {role_revoke.name.upper()}
+        RDB$ROLE_IN_USE                 <true>
+        RDB$SYSTEM_PRIVILEGES           0000200000000000
+        RDB$USER                        {user_02.name.upper()}
+        RDB$GRANTOR                     {user_01.name.upper()}
+        RDB$PRIVILEGE                   S
+        RDB$GRANT_OPTION                0
+        RDB$RELATION_NAME               TEST_U01
+        RDB$FIELD_NAME                  <null>
+        RDB$USER_TYPE                   8
+        RDB$OBJECT_TYPE                 0
+        RDB$RELATION_SCHEMA_NAME        PUBLIC
+        RDB$USER_SCHEMA_NAME            <null>
+        WHO_AM_I                        {user_02.name.upper()}
+        ID                              1
+        WHO_IS_AUTHOR                   {user_01.name.upper()}
+        Statement failed, SQLSTATE = 28000
+        no permission for SELECT access to TABLE "PUBLIC"."TEST_U01"
+        -Effective user is {user_02.name.upper()}
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.isql(switches = ['-q'], input = test_script, combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
