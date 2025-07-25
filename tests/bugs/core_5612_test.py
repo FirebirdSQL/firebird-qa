318b111706fff9e0b347f3d32c7bc374807e2f4b
pavel-zotov <p519446@yandex.ru>
2025-07-25 11:50:56 +0300

    Added/Updated tests\bugs\core_5612_test.py: Separated expected output for check on versions prior/since 6.x. Checked on 6.0.0.1061; 5.0.3.1686; 4.0.6.3223.

--- tests/bugs/core_5612_test.py
+++ tests/bugs/core_5612_test.py
@@ -7,6 +7,10 @@ TITLE:       Gradual slowdown compilation (create, recreate or drop) of views
 DESCRIPTION:
 JIRA:        CORE-5612
 FBTEST:      bugs.core_5612
+NOTES:
+    [25.07.2025] pzotov
+    Separated expected output for check on versions prior/since 6.x.
+    Checked on 6.0.0.1061; 5.0.3.1686; 4.0.6.3223.
 """
 
 import pytest
@@ -26,29 +30,56 @@ test_script = """
 
 act = isql_act('db', test_script)
 
-expected_stdout = """
-    IDX_ID                          1
-    POS                             0
-    KEY                             RDB$DEPENDENT_NAME
-    IDX_ID                          1
-    POS                             1
-    KEY                             RDB$DEPENDENT_TYPE
-    IDX_ID                          2
-    POS                             0
-    KEY                             RDB$DEPENDED_ON_NAME
-
-    IDX_ID                          2
-    POS                             1
-    KEY                             RDB$DEPENDED_ON_TYPE
-    IDX_ID                          2
-    POS                             2
-    KEY                             RDB$FIELD_NAME
-
-    Records affected: 5
-"""
-
 @pytest.mark.version('>=4.0')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
+
+    expected_stdout_5x = """
+        IDX_ID                          1
+        POS                             0
+        KEY                             RDB$DEPENDENT_NAME
+        IDX_ID                          1
+        POS                             1
+        KEY                             RDB$DEPENDENT_TYPE
+        IDX_ID                          2
+        POS                             0
+        KEY                             RDB$DEPENDED_ON_NAME
+
+        IDX_ID                          2
+        POS                             1
+        KEY                             RDB$DEPENDED_ON_TYPE
+        IDX_ID                          2
+        POS                             2
+        KEY                             RDB$FIELD_NAME
+
+        Records affected: 5
+    """
+
+    expected_stdout_6x = """
+        IDX_ID                          1
+        POS                             0
+        KEY                             RDB$DEPENDENT_SCHEMA_NAME
+        IDX_ID                          1
+        POS                             1
+        KEY                             RDB$DEPENDENT_NAME
+        IDX_ID                          1
+        POS                             2
+        KEY                             RDB$DEPENDENT_TYPE
+        IDX_ID                          2
+        POS                             0
+        KEY                             RDB$DEPENDED_ON_SCHEMA_NAME
+        IDX_ID                          2
+        POS                             1
+        KEY                             RDB$DEPENDED_ON_NAME
+        IDX_ID                          2
+        POS                             2
+        KEY                             RDB$DEPENDED_ON_TYPE
+        IDX_ID                          2
+        POS                             3
+        KEY                             RDB$FIELD_NAME
+
+        Records affected: 7
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
