d66ebfb99b2a7ed7b935ab9b030318b3cfb26aca
pavel-zotov <p519446@yandex.ru>
2025-07-26 17:04:46 +0300

    Added/Updated tests\bugs\core_2341_test.py: Explicit actions with instancses of cursor and prepared statement.

--- tests/bugs/core_2341_test.py
+++ tests/bugs/core_2341_test.py
@@ -8,6 +8,7 @@ DESCRIPTION:
 JIRA:        CORE-2341
 FBTEST:      bugs.core_2341
 """
+from firebird.driver import DatabaseError
 
 import pytest
 from firebird.qa import *
@@ -16,25 +17,41 @@ db = db_factory()
 
 act = python_act('db')
 
-expected_stdout = """O
-----------
-asd
+test_sql = """
+    execute block (i varchar(10) = ?) returns (out_arg varchar(10)) as
+    begin
+      out_arg = coalesce(cast(out_arg as date), current_date);
+      out_arg = i;
+      suspend;
+    end
 """
+INPUT_ARG = 'QweRty'
 
 @pytest.mark.version('>=3')
 def test_1(act: Action, capsys):
     with act.db.connect() as con:
-        c = con.cursor()
-        cmd = c.prepare("""execute block (i varchar(10) = ?) returns (o varchar(10))
-        as
-        begin
-          o = coalesce(cast(o as date), current_date);
-          o = i;
-          suspend;
-        end""")
-        c.execute(cmd, ['asd'])
-        act.print_data(c)
-        #
+        cur = con.cursor()
+        ps, rs = None, None
+        try:
+            ps = cur.prepare(test_sql)
+            rs = cur.execute(ps, (INPUT_ARG,))
+
+            cur_cols = cur.description
+            for r in cur:
+                for i in range(0,len(cur_cols)):
+                    print( cur_cols[i][0], ':', r[i] )
+        except DatabaseError as e:
+            print( e.__str__() )
+            print(e.gds_codes)
+        finally:
+            if rs:
+                rs.close()
+            if ps:
+                ps.free()
+
+        expected_stdout = f"""
+            OUT_ARG : {INPUT_ARG}
+        """
         act.expected_stdout = expected_stdout
         act.stdout = capsys.readouterr().out
         assert act.clean_stdout == act.clean_expected_stdout
