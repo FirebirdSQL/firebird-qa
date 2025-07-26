77d625369fd962799aef71b74e27fd63e356bf63
pavel-zotov <p519446@yandex.ru>
2025-07-26 16:07:10 +0300

    Added/Updated tests\functional\services\test_user_management.py: Explicit actions with instancses of cursor and prepared statement.

--- tests/functional/services/test_user_management.py
+++ tests/functional/services/test_user_management.py
@@ -19,10 +19,11 @@ DESCRIPTION:
 FBTEST:      functional.services.user_management
 """
 import sys
+import firebird.driver
+from firebird.driver import TPB, core as fb_core, DatabaseError, tpb, Isolation
+
 import pytest
 from firebird.qa import *
-import firebird.driver
-from firebird.driver import TPB, Isolation, core as fb_core, DatabaseError
 
 #sys.stdout.reconfigure(encoding='utf-8')
 
@@ -34,21 +35,40 @@ act = python_act('db')
 def test_1(act: Action, capsys):
     
     #----------------------------------------------------------
-    def print_user_data(con, cur, prepared_sttm, mode, uname):
-        con.commit()
-        con.begin()
-        cur.execute(prepared_sttm, (uname,))
-        col_hdr=cur.description
-        for r in cur:
-            for i in range(0,len(col_hdr)):
-                print( (col_hdr[i][0] +':').ljust(32), r[i] )
-        if mode != 'del':
-            if cur.affected_rows == 0:
-                cur.execute('select sec$user_name from sec$users')
-                existing_users = '\n'.join( [r[0].rstrip() for r in cur.fetchall()] )
-                assert False, f"Problem with security.db for {mode=}: could not find expected record with sec$user_name = '{uname}' using charset = {con.charset}, {existing_users=}"
-        else:
-            assert cur.affected_rows == 0, f"Problem with security.db for {mode=}: UNEXPECTED record encountered with sec$user_name = '{uname}', {cur.affected_rows=}"
+    def print_user_data(con, mode, uname):
+
+        sttm = 'select gen_id(g,1) as point, sec$user_name, sec$first_name, sec$last_name, sec$admin from rdb$database join sec$users on sec$user_name = ?'
+    
+        custom_tpb = tpb(isolation = Isolation.READ_COMMITTED_READ_CONSISTENCY, lock_timeout = 0)
+        tx1 = con.transaction_manager(custom_tpb)
+        tx1.begin()
+        cur = tx1.cursor()
+        ps, rs = None, None
+        try:
+            ps = cur.prepare(sttm)
+            rs = cur.execute(ps, (uname,))
+            col_hdr=cur.description
+            for r in cur:
+                for i in range(0,len(col_hdr)):
+                    print( (col_hdr[i][0] +':').ljust(32), r[i] )
+            if mode.upper() != 'del'.upper():
+                if cur.affected_rows == 0:
+                    cur.execute('select sec$user_name from sec$users')
+                    existing_users = '\n'.join( [r[0].rstrip() for r in cur.fetchall()] )
+                    assert False, f"Problem with security.db for {mode=}: could not find expected record with sec$user_name = '{uname}' using charset = {con.charset}, {existing_users=}"
+            else:
+                assert cur.affected_rows == 0, f"Problem with security.db for {mode=}: UNEXPECTED record encountered with sec$user_name = '{uname}', {cur.affected_rows=}"
+
+        except DatabaseError as e:
+            print(e.__str__())
+            for x in e.gds_codes:
+                print(x)
+        finally:
+            if rs:
+                rs.close()
+            if ps:
+                ps.free()
+        tx1.rollback()
 
     #----------------------------------------------------------
 
@@ -60,14 +80,7 @@ def test_1(act: Action, capsys):
     # UnicodeEncodeError: 'ascii' codec can't encode character '\xe1' in position 5: ordinal not in range(128)
     #TMP_USER_NAME = '"tmp_Václav"'
 
-    sttm = 'select gen_id(g,1) as point, sec$user_name, sec$first_name, sec$last_name, sec$admin from rdb$database join sec$users on sec$user_name = ?'
-
     with act.db.connect(charset = 'utf8') as con:
-        con.begin()
-        cur = con.cursor()
-        
-        prepared_sttm = cur.prepare(sttm)
-
         with act.connect_server() as srv:
             svc = fb_core.ServerUserServices(srv)
     
@@ -79,17 +92,17 @@ def test_1(act: Action, capsys):
                 svc.delete(user_name = TMP_USER_NAME)
 
             svc.add( user_name = TMP_USER_NAME, password = '123', first_name = 'John', last_name = 'Smith', admin = True)
-            print_user_data(con, cur, prepared_sttm, 'add', TMP_USER_NAME)
+            print_user_data(con, 'add', TMP_USER_NAME)
 
             # Here we make sure that user actually exists and can make connecttion:
-            with act.db.connect(user = TMP_USER_NAME, password = '123', charset = con.charset) as con_check:
-                pass
+            with act.db.connect(user = TMP_USER_NAME, password = '123', charset = 'win1257') as con_check:
+                print(con_check.charset.lower())
 
             svc.update( user_name = TMP_USER_NAME, last_name = 'Jackson', admin = False, first_name = 'Robert')
-            print_user_data(con, cur, prepared_sttm, 'upd', TMP_USER_NAME)
+            print_user_data(con, 'upd', TMP_USER_NAME)
 
             svc.delete(user_name = TMP_USER_NAME)
-            print_user_data(con, cur, prepared_sttm, 'del', TMP_USER_NAME)
+            print_user_data(con, 'del', TMP_USER_NAME)
             try:
                 with act.db.connect(user = TMP_USER_NAME, password = '123') as con_check:
                     print('UNEXPECTED: user must not exist at this point!')
@@ -104,6 +117,8 @@ def test_1(act: Action, capsys):
         SEC$FIRST_NAME:                  John
         SEC$LAST_NAME:                   Smith
         SEC$ADMIN:                       True
+
+        win1257
 
         POINT:                           2
         SEC$USER_NAME:                   {TMP_USER_NAME}
