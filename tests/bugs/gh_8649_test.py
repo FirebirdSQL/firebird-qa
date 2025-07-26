cf1877b3c2f1694e925e972e46f146d08c19054a
pavel-zotov <p519446@yandex.ru>
2025-07-26 22:33:17 +0300

    Added/Updated tests\bugs\gh_8649_test.py: removed wrong code which did attampt to connect to empty file instead of usual (auxilizary) database.

--- tests/bugs/gh_8649_test.py
+++ tests/bugs/gh_8649_test.py
@@ -5,14 +5,10 @@ ID:          n/a
 ISSUE:       8649
 TITLE:       AV when ON CONNECT triggers uses EXECUTE STATEMENT ON EXTERNAL
 DESCRIPTION:
+    Auxiliary database is created here ('db2') in order to make ES/EDS.
 NOTES:
-    [13.07.2025] pzotov
-    1. On localized Windows non-ascii message appears in output:
-         335544734 : Error while trying to open file
-         85767113 : <file not found> // localized message here
-         Data source : Firebird::localhost:
-       We have to filter out all such messages, see substitutions.
-    2. By default, any test database is created with flag 'do_not_drop = False' which means that such DB
+    [26.07.2025] pzotov
+    1. By default, any test database is created with flag 'do_not_drop = False' which means that such DB
        will be dropped at teardown phase of pytest.
        Appropriate method of Database class does following:
            1.   Changes test DB linger to 0 (using services API);
@@ -22,28 +18,37 @@ NOTES:
            3.   If DB file still exists - calls self.db_path.unlink(missing_ok=True)
        This means that on step "2." DB-level triggers will fire, even if they are invalid or cause problems.
        We *ourselves* have to drop such triggers, BEFORE teardown - see 'con_kill_db_level_trigger'.
-    3. Name of trigger must be adjusted on FB 6.x because of SQL schemas introduction since 6.0.0.834
+    2. Name of trigger must be adjusted on FB 6.x because of SQL schemas introduction since 6.0.0.834
 
     Confirmed bug (crash) on 6.0.0.949; 5.0.3.1668; 4.0.6.3214.
     Checked on 6.0.0.967; 5.0.3.1683; 4.0.6.3221
 """
+import os
 import locale
 from pathlib import Path
 import pytest
 from firebird.qa import *
 
 db = db_factory()
-tmp_fdb = temp_file('tmp_gh_8649.fdb')
+db2 = db_factory(filename = 'tmp_gh_8649_aux.fdb')
 
-substitutions = [  ('^((?!(SQLSTATE|error|Error|TRG_CONNECT|source)).)*$', '')
+for v in ('ISC_USER','ISC_PASSWORD'):
+    try:
+        del os.environ[ v ]
+    except KeyError as e:
+        pass
+
+substitutions = [  ('^((?!(SQLSTATE|error|Error|TRG_CONNECT|attach|security|password|source)).)*$', '')
                   ,('operation for file .*', 'operation for file')
+                  ,('Missing security context for .*', 'Missing security context')
                   ,('Data source : Firebird::.*', 'Data source : Firebird::')
                   ,(r'line(:)?\s+\d+.*', '')
                 ]
 act = python_act('db', substitutions = substitutions)
+act2 = python_act('db2')
 
 @pytest.mark.version('>=4.0.6')
-def test_1(act: Action, tmp_fdb: Path):
+def test_1(act: Action, act2: Action):
 
     test_script = f"""
         set term ^;
@@ -51,7 +56,7 @@ def test_1(act: Action, tmp_fdb: Path):
             declare id int;
         begin
             execute statement 'select 1 from rdb$database'
-            on external 'localhost:{str(tmp_fdb)}'
+            on external '{act2.db.dsn}'
         	into :id;
         end
         ^
@@ -64,8 +69,7 @@ def test_1(act: Action, tmp_fdb: Path):
     act.expected_stdout = f"""
         Statement failed, SQLSTATE = 42000
         Execute statement error at attach :
-        335544344 : I/O error during "CreateFile (open)" operation for file
-        335544734 : Error while trying to open file
+        335545060 : Missing security context
         Data source : Firebird::
         -At trigger {TEST_TRIGGER_NAME}
     """
