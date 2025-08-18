8bd19fb76de8a9f20e1fc48f55922e2cb35204db
pavel-zotov <p519446@yandex.ru>
2025-08-18 13:45:24 +0300

    Added/Updated tests\functional\sqlite\test_ce8717f088.py: See description and notes. Checked on 6.0.0.1204, 5.0.4.1701, 4.0.7.3231, 3.0.14.33824

---
+++ tests/functional/sqlite/test_ce8717f088.py
@@ -0,0 +1,42 @@
+#coding:utf-8
+
+"""
+ID:          ce8717f088
+ISSUE:       https://www.sqlite.org/src/tktview/ce8717f088
+TITLE:       LIKE malfunctions for UNIQUE COLLATE NOCASE column
+DESCRIPTION:
+NOTES:
+    [18.08.2025] pzotov
+    Checked on 6.0.0.1204, 5.0.4.1701, 4.0.7.3231, 3.0.14.33824
+"""
+
+import pytest
+from firebird.qa import *
+
+db = db_factory()
+
+test_script = """
+    set list on;
+    create collation name_coll for utf8 from unicode case insensitive;
+    create domain dm_test varchar(3) character set utf8 collate name_coll;
+
+    create table t0(c0 dm_test unique);
+    insert into t0(c0) values ('.1%');
+
+    set count on;
+    select * from t0 where t0.c0 like '.1%';
+"""
+
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
+
+expected_stdout = """
+    C0 .1%
+    Records affected: 1
+"""
+
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
