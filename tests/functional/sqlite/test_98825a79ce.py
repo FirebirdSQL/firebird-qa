d40dc9e2bf612c6aaa53658b3aa9847704f81463
pavel-zotov <p519446@yandex.ru>
2025-08-21 18:52:12 +0300

    Added/Updated tests\functional\sqlite\test_98825a79ce.py: See description and notes. Checked on 6.0.0.1232, 5.0.4.1701, 4.0.7.3231, 3.0.14.33824

---
+++ tests/functional/sqlite/test_98825a79ce.py
@@ -0,0 +1,43 @@
+#coding:utf-8
+
+"""
+ID:          98825a79ce
+ISSUE:       https://www.sqlite.org/src/tktview/98825a79ce
+TITLE:       Incorrect result from a DISTINCT + GROUP BY + ORDER BY query
+DESCRIPTION:
+NOTES:
+    [21.08.2025] pzotov
+    Checked on 6.0.0.1232, 5.0.4.1701, 4.0.7.3231, 3.0.14.33824
+"""
+
+import pytest
+from firebird.qa import *
+
+db = db_factory()
+
+test_script = """
+    set list on;
+    create table t1(x char(10));
+    insert into t1 values('right');
+    insert into t1 values('wrong');
+
+    set count on;
+    select distinct x
+    from (select x from t1 group by x)
+    where x='right'
+    order by x;
+"""
+
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
+
+expected_stdout = """
+    X right
+    Records affected: 1
+"""
+
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+    act.expected_stdout = expected_stdout
+    act.execute(combine_output = True)
+    assert act.clean_stdout == act.clean_expected_stdout
