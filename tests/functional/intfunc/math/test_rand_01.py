c5ef1c6978524f376f085fc5b2635119f7efc71d
pavel-zotov <p519446@yandex.ru>
2025-09-09 20:05:42 +0300

    Added/Updated tests\functional\intfunc\math\test_rand_01.py: changed output format to 'SET LIST ON', added substitution to remove excessive spaces in order to remove dependency on estimated column width

--- tests/functional/intfunc/math/test_rand_01.py
+++ tests/functional/intfunc/math/test_rand_01.py
@@ -2,7 +2,7 @@
 
 """
 ID:          intfunc.math.rand
-TITLE:       RAND()
+TITLE:       Result of cast(RAND() as <textual_type>) must not loss fractional part
 DESCRIPTION: Returns a random number between 0 and 1.
 FBTEST:      functional.intfunc.math.rand_01
 """
@@ -12,46 +12,39 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """create table test( id char(30) );
+N_COUNT = 50;
 
---on verrifie qu'il y en a pas deux identique
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
-insert into test values(CAST(rand() AS VARCHAR(255)) );
+test_script = f"""
+    set list on;
+    create table test( s char(30), b blob );
+    set term ^;
+    execute block as
+        declare i int  = 0;
+    begin
+        while (i < {N_COUNT}) do
+        begin
+            insert into test(s, b) values(rand(), rand());
+            i = i + 1;
+        end
+    end
+    ^
+    set term ;^
+    commit;
 
+    select count(distinct s) as count_uniq_char from test;
+    select count(distinct b) as count_uniq_blob from test;
+"""
 
-select count(id)  from test group by id;"""
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-act = isql_act('db', test_script)
-
-expected_stdout = """
-COUNT
-=====================
-                    1
-                    1
-                    1
-                    1
-                    1
-                    1
-                    1
-                    1
-                    1
-                    1
-                    1
-                    1
+expected_stdout = f"""
+    COUNT_UNIQ_CHAR {N_COUNT}
+    COUNT_UNIQ_BLOB {N_COUNT}
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
