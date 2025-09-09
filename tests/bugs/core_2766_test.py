66c6725c0e8b3760bc0a7a44464bc134e3662506
pavel-zotov <p519446@yandex.ru>
2025-09-09 17:40:05 +0300

    Added/Updated tests\bugs\core_2766_test.py: Changed output format to 'SET LIST ON', added substitution to remove excessive spaces in order to remove dependency on estimated column width

--- tests/bugs/core_2766_test.py
+++ tests/bugs/core_2766_test.py
@@ -12,43 +12,46 @@ FBTEST:      bugs.core_2766
 import pytest
 from firebird.qa import *
 
-init_script = """create table t_master (id int not null, name varchar(64));
-alter table t_master add constraint PK_master primary key (id);
+init_script = """
+    create table t_master (id int not null, name varchar(64));
+    alter table t_master add constraint PK_master primary key (id);
 
-create table t_detail (id_master int not null, name varchar(64));
-alter table t_detail add constraint FK_detail foreign key (id_master) references t_master (id);
+    create table t_detail (id_master int not null, name varchar(64));
+    alter table t_detail add constraint FK_detail foreign key (id_master) references t_master (id);
 
-commit;
+    commit;
 
-insert into t_master values (1, '1');
-insert into t_detail values (1, 'a');
-commit;
+    insert into t_master values (1, '1');
+    insert into t_detail values (1, 'a');
+    commit;
 """
 
 db = db_factory(charset='UTF8', init=init_script)
 
-test_script = """insert into t_master values (3, '2');
-delete from t_master where id = 3;
-commit;
+test_script = """
+    insert into t_master values (3, '2');
+    delete from t_master where id = 3;
+    commit;
 
-drop table t_detail;
-commit;
+    drop table t_detail;
+    commit;
 
-delete from t_master;
-select count(*) from t_master;
+    delete from t_master;
+
+    set list on;
+    select count(*) from t_master;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-                COUNT
-=====================
-                    0
+    COUNT 0
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
