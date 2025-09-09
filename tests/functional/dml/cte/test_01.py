6bf84978d4b4513f106de33202d11da1a14745e9
pavel-zotov <p519446@yandex.ru>
2025-09-09 19:29:37 +0300

    Added/Updated tests\functional\dml\cte\test_01.py: changed output format to 'SET LIST ON', added substitution to remove excessive spaces in order to remove dependency on estimated column width

--- tests/functional/dml/cte/test_01.py
+++ tests/functional/dml/cte/test_01.py
@@ -5,74 +5,86 @@ ID:          dml.cte-01
 TITLE:       Non-Recursive CTEs
 FBTEST:      functional.dml.cte.01
 DESCRIPTION:
-  Rules for Non-Recursive CTEs :
-  - Multiple table expressions can be defined in one query
-  - Any clause legal in a SELECT specification is legal in table expressions
-  - Table expressions can reference one another
-  - References between expressions should not have loops
-  - Table expressions can be used within any part of the main query or another table expression
-  - The same table expression can be used more than once in the main query
-  - Table expressions (as subqueries) can be used in INSERT, UPDATE and DELETE statements
-  - Table expressions are legal in PSQL code
-  - WITH statements can not be nested
+    Rules for Non-Recursive CTEs :
+    * Multiple table expressions can be defined in one query
+    * Any clause legal in a SELECT specification is legal in table expressions
+    * Table expressions can reference one another
+    * References between expressions should not have loops
+    * Table expressions can be used within any part of the main query or another table expression
+    * The same table expression can be used more than once in the main query
+    * Table expressions (as subqueries) can be used in INSERT, UPDATE and DELETE statements
+    * Table expressions are legal in PSQL code
+    * WITH statements can not be nested
 """
 
 import pytest
 from firebird.qa import *
 
 init_script = """
-		CREATE TABLE employee( id_employee INTEGER , prenom VARCHAR(20) ,id_department INTEGER,age INTEGER ,  PRIMARY KEY(id_employee));
+    create table employee( id_employee integer , prenom varchar(20) ,id_department integer,age integer ,  primary key(id_employee));
+    create table department(id_department integer, name varchar(20));
 
-		CREATE TABLE department(id_department INTEGER, name VARCHAR(20));
-
-		INSERT INTO department(id_department, name) values(1,'service compta');
-		INSERT INTO department(id_department, name) values(2,'production');
-		INSERT INTO employee(id_employee, prenom,id_department,age) VALUES (1,'benoit',1 , 30 );
-		INSERT INTO employee(id_employee, prenom,id_department,age) VALUES (2,'ludivine',1 , 30 );
-		INSERT INTO employee(id_employee, prenom,id_department,age) VALUES (3,'michel',1 , 27 );
-		INSERT INTO employee(id_employee, prenom,id_department,age) VALUES (4,'Gilbert',1 , 42 );
-		INSERT INTO employee(id_employee, prenom,id_department,age) VALUES (5,'tom',2 ,23);
-		INSERT INTO employee(id_employee, prenom,id_department,age) VALUES (6,'jacque',2,44 );
-		INSERT INTO employee(id_employee, prenom,id_department,age) VALUES (7,'justine',2,30 );
-		INSERT INTO employee(id_employee, prenom,id_department,age) VALUES (8,'martine',2,31 );
-INSERT INTO employee(id_employee, prenom,id_department,age) VALUES (9,'noemie',2,39 );
-
+    insert into department(id_department, name) values(1,'service compta');
+    insert into department(id_department, name) values(2,'production');
+    insert into employee(id_employee, prenom,id_department,age) values (1,'benoit',1 , 30 );
+    insert into employee(id_employee, prenom,id_department,age) values (2,'ludivine',1 , 30 );
+    insert into employee(id_employee, prenom,id_department,age) values (3,'michel',1 , 27 );
+    insert into employee(id_employee, prenom,id_department,age) values (4,'gilbert',1 , 42 );
+    insert into employee(id_employee, prenom,id_department,age) values (5,'tom',2 ,23);
+    insert into employee(id_employee, prenom,id_department,age) values (6,'jacque',2,44 );
+    insert into employee(id_employee, prenom,id_department,age) values (7,'justine',2,30 );
+    insert into employee(id_employee, prenom,id_department,age) values (8,'martine',2,31 );
+    insert into employee(id_employee, prenom,id_department,age) values (9,'noemie',2,39 );
 """
 
 db = db_factory(init=init_script)
 
-test_script = """WITH
-  repartition_by_age AS (
-SELECT age/10 as trancheage , id_department,
-        COUNT(1) AS nombre
-      FROM employee
-    GROUP BY age/10, id_department
-)
-select d.name , jeune.nombre as jeune , trentenaire.nombre as trentenaire, quarentenaire.nombre as quantenaire
-from department d
-left join repartition_by_age jeune
-on d.id_department = jeune.id_department
-and jeune.trancheage = 2
-left join repartition_by_age trentenaire
-on d.id_department = trentenaire.id_department
-and trentenaire.trancheage = 3
-left join repartition_by_age quarentenaire
-on d.id_department = quarentenaire.id_department
-and quarentenaire.trancheage = 4 ;
+test_script = """
+    set list on;
+    set count on;
+    with
+    repartition_by_age as (
+        select
+            age/10 as trancheage
+           ,id_department
+           ,count(1) as nombre
+        from employee
+        group by age/10, id_department
+    )
+    select
+        d.name
+       ,jeune.nombre as jeune
+       ,trentenaire.nombre as trentenaire
+       ,quarentenaire.nombre as quantenaire
+    from department d
+    left join repartition_by_age jeune
+        on d.id_department = jeune.id_department and jeune.trancheage = 2
+    left join repartition_by_age trentenaire
+        on d.id_department = trentenaire.id_department and trentenaire.trancheage = 3
+    left join repartition_by_age quarentenaire
+        on d.id_department = quarentenaire.id_department and quarentenaire.trancheage = 4 ;
 
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-NAME                                 JEUNE           TRENTENAIRE           QUANTENAIRE
-==================== ===================== ===================== =====================
-service compta                           1                     2                     1
-production                               1                     3                     1
+    NAME service compta
+    JEUNE 1
+    TRENTENAIRE 2
+    QUANTENAIRE 1
+    
+    NAME production
+    JEUNE 1
+    TRENTENAIRE 3
+    QUANTENAIRE 1
+
+    Records affected: 2
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
