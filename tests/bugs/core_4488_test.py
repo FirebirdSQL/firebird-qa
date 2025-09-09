096e99e209bbcbd4aa34c54fecb78a500ede6453
pavel-zotov <p519446@yandex.ru>
2025-09-09 18:02:38 +0300

    Added/Updated tests\bugs\core_4488_test.py: Changed output format to 'SET LIST ON', added substitution to remove excessive spaces in order to remove dependency on estimated column width

--- tests/bugs/core_4488_test.py
+++ tests/bugs/core_4488_test.py
@@ -3,13 +3,12 @@
 """
 ID:          issue-4808
 ISSUE:       4808
-TITLE:       Wrong results of FOR SELECT <L> FROM <T> AS CURSOR <C> and table <T> is
-  modified inside cursor's begin...end block
+TITLE:       Wrong results of FOR SELECT <L> FROM <T> AS CURSOR <C> and table <T> is modified inside cursor's begin...end block
 DESCRIPTION:
-  See doc\\sql.extensions\\README.cursor_variables.txt:
-  7) Reading from a cursor variable returns the current field values. That means an UPDATE (with
-     WHERE CURRENT OF) also updates the fields for subsequent reads. And DELETE (with WHERE
-     CURRENT OF) makes subsequent reads to return NULL.
+    See doc\\sql.extensions\\README.cursor_variables.txt:
+    7) Reading from a cursor variable returns the current field values. That means an UPDATE (with
+       WHERE CURRENT OF) also updates the fields for subsequent reads. And DELETE (with WHERE
+       CURRENT OF) makes subsequent reads to return NULL.
 JIRA:        CORE-4488
 FBTEST:      bugs.core_4488
 """
@@ -29,6 +28,7 @@ init_script = """
 db = db_factory(init=init_script)
 
 test_script = """
+    set list on;
     --------------- 1 -----------------
     select '' as "old data of t_source:", (select count(*) from t_source) "rows in t_source", s.*
     from rdb$database b left join t_source s on 1=1;
@@ -111,57 +111,117 @@ test_script = """
     rollback;
 """
 
-act = isql_act('db', test_script, substitutions=[('=.*', '')])
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-    old data of t_source:      rows in t_source           ID            X            Y            Z
-    ===================== ===================== ============ ============ ============ ============
-                                              3            1           10          100         1000
-                                              3            2           20          200         2000
-                                              3            3           30          300         3000
-
-
-    1. New data of t_source:      rows in t_source           ID            X            Y            Z
-    ======================== ===================== ============ ============ ============ ============
-                                                 0       <null>       <null>       <null>       <null>
-
-
-    1. New data of t_target:      rows in t_target           ID            X            Y            Z
-    ======================== ===================== ============ ============ ============ ============
-                                                 3       <null>       <null>       <null>       <null>
-                                                 3       <null>       <null>       <null>       <null>
-                                                 3       <null>       <null>       <null>       <null>
-
-
-    2. New data of t_source:      rows in t_source           ID            X            Y            Z
-    ======================== ===================== ============ ============ ============ ============
-                                                 0       <null>       <null>       <null>       <null>
-
-
-    2. New data of t_target:      rows in t_target           ID            X            Y            Z
-    ======================== ===================== ============ ============ ============ ============
-                                                 3       <null>       <null>       <null>       <null>
-                                                 3       <null>       <null>       <null>       <null>
-                                                 3       <null>       <null>       <null>       <null>
-
-
-    3. New data of t_source:      rows in t_source           ID            X            Y            Z
-    ======================== ===================== ============ ============ ============ ============
-                                                 3            1         1000           10          100
-                                                 3            2         2000           20          200
-                                                 3            3         3000           30          300
-
-
-    3. New data of t_target:      rows in t_target           ID            X            Y            Z
-    ======================== ===================== ============ ============ ============ ============
-                                                 3            1         1000           10          101
-                                                 3            2         2000           20          201
-                                                 3            3         3000           30          301
+    old data of t_source:
+    rows in t_source 3
+    ID 1
+    X 10
+    Y 100
+    Z 1000
+    old data of t_source:
+    rows in t_source 3
+    ID 2
+    X 20
+    Y 200
+    Z 2000
+    old data of t_source:
+    rows in t_source 3
+    ID 3
+    X 30
+    Y 300
+    Z 3000
+    1. New data of t_source:
+    rows in t_source 0
+    ID <null>
+    X <null>
+    Y <null>
+    Z <null>
+    1. New data of t_target:
+    rows in t_target 3
+    ID <null>
+    X <null>
+    Y <null>
+    Z <null>
+    1. New data of t_target:
+    rows in t_target 3
+    ID <null>
+    X <null>
+    Y <null>
+    Z <null>
+    1. New data of t_target:
+    rows in t_target 3
+    ID <null>
+    X <null>
+    Y <null>
+    Z <null>
+    2. New data of t_source:
+    rows in t_source 0
+    ID <null>
+    X <null>
+    Y <null>
+    Z <null>
+    2. New data of t_target:
+    rows in t_target 3
+    ID <null>
+    X <null>
+    Y <null>
+    Z <null>
+    2. New data of t_target:
+    rows in t_target 3
+    ID <null>
+    X <null>
+    Y <null>
+    Z <null>
+    2. New data of t_target:
+    rows in t_target 3
+    ID <null>
+    X <null>
+    Y <null>
+    Z <null>
+    3. New data of t_source:
+    rows in t_source 3
+    ID 1
+    X 1000
+    Y 10
+    Z 100
+    3. New data of t_source:
+    rows in t_source 3
+    ID 2
+    X 2000
+    Y 20
+    Z 200
+    3. New data of t_source:
+    rows in t_source 3
+    ID 3
+    X 3000
+    Y 30
+    Z 300
+    3. New data of t_target:
+    rows in t_target 3
+    ID 1
+    X 1000
+    Y 10
+    Z 101
+    3. New data of t_target:
+    rows in t_target 3
+    ID 2
+    X 2000
+    Y 20
+    Z 201
+    3. New data of t_target:
+    rows in t_target 3
+    ID 3
+    X 3000
+    Y 30
+    Z 301
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
