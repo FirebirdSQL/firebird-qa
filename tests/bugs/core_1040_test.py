6aa8360a266b7c4cb3e84e5bfa770412c2dde193
pavel-zotov <p519446@yandex.ru>
2025-09-09 17:06:30 +0300

    Added/Updated tests\bugs\core_1040_test.py: Changed output format to 'SET LIST ON', added substitution to remove excessive spaces in order to remove dependency on estimated column width

--- tests/bugs/core_1040_test.py
+++ tests/bugs/core_1040_test.py
@@ -12,32 +12,35 @@ FBTEST:      bugs.core_1040
 import pytest
 from firebird.qa import *
 
-init_script = """recreate table t (str varchar(10));
-commit;
+init_script = """
+    recreate table test (x varchar(10));
+    commit;
+    insert into test select '' from rdb$types,(select 1 x from rdb$types rows 10);
+    insert into test values (null);
+    commit;
 
-insert into t values ('');
-insert into t values (null);
-commit;
-
-create index t_i on t (str);
-commit;
+    create index test_x on test (x);
+    commit;
 """
 
 db = db_factory(init=init_script)
 
-test_script = """select count(*) from t where str is null;"""
+test_script = """
+    set list on;
+    -- set plan on;
+    select count(*) from test where x is null;
+"""
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' '), ('=', '')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """                COUNT
-=====================
-                    1
-
+expected_stdout = """
+    COUNT 1
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
