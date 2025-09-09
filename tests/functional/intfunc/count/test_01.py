884695e9296d9a0f0858b95a24ade449f427a6cf
pavel-zotov <p519446@yandex.ru>
2025-09-09 19:50:53 +0300

    Added/Updated tests\functional\intfunc\count\test_01.py: changed output format to 'SET LIST ON', added substitution to remove excessive spaces in order to remove dependency on estimated column width

--- tests/functional/intfunc/count/test_01.py
+++ tests/functional/intfunc/count/test_01.py
@@ -10,18 +10,24 @@ FBTEST:      functional.intfunc.count.01
 import pytest
 from firebird.qa import *
 
-db = db_factory(init="CREATE TABLE test( id INTEGER);")
+db = db_factory(init="create table test(id int);")
 
-act = isql_act('db', "SELECT COUNT(*) FROM test;")
+test_script = """
+    set list on;
+    select count(*) from test;
+    commit;
+"""
+
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-COUNT
-=====================
-                    0
+    COUNT 0
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
+
