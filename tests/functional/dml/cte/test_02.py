9b634301401fcd98b42b5be1c0d6f65cdbed399a
pavel-zotov <p519446@yandex.ru>
2025-09-09 19:34:08 +0300

    Added/Updated tests\functional\dml\cte\test_02.py: changed output format to 'SET LIST ON', added substitution to remove excessive spaces in order to remove dependency on estimated column width

--- tests/functional/dml/cte/test_02.py
+++ tests/functional/dml/cte/test_02.py
@@ -5,21 +5,21 @@ ID:          dml.cte-02
 TITLE:       Recursive CTEs
 FBTEST:      functional.dml.cte.02
 DESCRIPTION:
-  Rules for Recursive CTEs
-    A recursive CTE is self-referencing (has a reference to itself)
-    A recursive CTE is a UNION of recursive and non-recursive members:
-    At least one non-recursive member (anchor) must be present
-    Non-recursive members are placed first in the UNION
-    Recursive members are separated from anchor members and from one another with UNION ALL clauses, i.e.,
-       non-recursive member (anchor)
-        UNION [ALL | DISTINCT]
-        non-recursive member (anchor)
-        UNION [ALL | DISTINCT]
-        non-recursive member (anchor)
-        UNION ALL
-        recursive member
-        UNION ALL
-        recursive member
+    Rules for Recursive CTEs
+    * A recursive CTE is self-referencing (has a reference to itself)
+    * A recursive CTE is a UNION of recursive and non-recursive members:
+    * At least one non-recursive member (anchor) must be present
+    * Non-recursive members are placed first in the UNION
+    * Recursive members are separated from anchor members and from one another with UNION ALL clauses, i.e.,
+         non-recursive member (anchor)
+          UNION [ALL | DISTINCT]
+          non-recursive member (anchor)
+          UNION [ALL | DISTINCT]
+          non-recursive member (anchor)
+          UNION ALL
+          recursive member
+          UNION ALL
+          recursive member
 
     References between CTEs should not have loops
     Aggregates (DISTINCT, GROUP BY, HAVING) and aggregate functions (SUM, COUNT, MAX etc) are not allowed in recursive members
@@ -31,65 +31,85 @@ import pytest
 from firebird.qa import *
 
 init_script = """
-    CREATE TABLE product( id_product INTEGER , name VARCHAR(20) ,id_type_product INTEGER,  PRIMARY KEY(id_product));
-    CREATE TABLE type_product(id_type_product INTEGER, name VARCHAR(20),id_sub_type INTEGER);
-    INSERT INTO type_product(id_type_product,name,id_sub_type) values(1,'DVD',NULL);
-    INSERT INTO type_product(id_type_product,name,id_sub_type) values(2,'BOOK',NULL);
-    INSERT INTO type_product(id_type_product,name,id_sub_type) values(3,'FILM SF',1);
-    INSERT INTO type_product(id_type_product,name,id_sub_type) values(4,'FILM ACTION',1);
-    INSERT INTO type_product(id_type_product,name,id_sub_type) values(5,'FILM ROMANCE',1);
-    INSERT INTO product(id_product, name,id_type_product) VALUES (1,'Harry Potter 8',3  );
-    INSERT INTO product(id_product, name,id_type_product) VALUES (2,'Total Recall',3  );
-    INSERT INTO product(id_product, name,id_type_product) VALUES (3,'Kingdom of Heaven',3  );
-    INSERT INTO product(id_product, name,id_type_product) VALUES (4,'Desperate Housewives',5  );
-    INSERT INTO product(id_product, name,id_type_product) VALUES (5,'Reign over me',5  );
-    INSERT INTO product(id_product, name,id_type_product) VALUES (6,'Prison Break',4  );
+    create table product( id_product integer , name varchar(20) ,id_type_product integer,  primary key(id_product));
+    create table type_product(id_type_product integer, name varchar(20),id_sub_type integer);
+    insert into type_product(id_type_product,name,id_sub_type) values(1,'dvd',null);
+    insert into type_product(id_type_product,name,id_sub_type) values(2,'book',null);
+    insert into type_product(id_type_product,name,id_sub_type) values(3,'film sf',1);
+    insert into type_product(id_type_product,name,id_sub_type) values(4,'film action',1);
+    insert into type_product(id_type_product,name,id_sub_type) values(5,'film romance',1);
+    insert into product(id_product, name,id_type_product) values (1,'harry potter 8',3  );
+    insert into product(id_product, name,id_type_product) values (2,'total recall',3  );
+    insert into product(id_product, name,id_type_product) values (3,'kingdom of heaven',3  );
+    insert into product(id_product, name,id_type_product) values (4,'desperate housewives',5  );
+    insert into product(id_product, name,id_type_product) values (5,'reign over me',5  );
+    insert into product(id_product, name,id_type_product) values (6,'prison break',4  );
 """
 
 db = db_factory(init=init_script)
 
-test_script = """WITH RECURSIVE
-TYPE_PRODUCT_RECUR (id_type_product,name,father) AS
-(
-SELECT id_type_product ,'+ ' || name as name  , id_type_product as father
-FROM type_product
-WHERE type_product.id_sub_type is null
-UNION ALL
-SELECT T.id_type_product ,' - ' || T.name , TR.id_type_product as father
-FROM type_product T
-JOIN TYPE_PRODUCT_RECUR TR on TR.id_type_product = T.id_sub_type
-),
-COUNT_BY_TYPE AS
-(
-SELECT P.ID_TYPE_PRODUCT,count(ID_PRODUCT) as count_p from PRODUCT P
-group by P.ID_TYPE_PRODUCT
-union
-SELECT TP.FATHER,count(ID_PRODUCT) as count_p from
-TYPE_PRODUCT_RECUR TP , PRODUCT P
-where TP.ID_TYPE_PRODUCT = P.id_type_product
-group by TP.FATHER
-)
-SELECT  T.id_type_product , T.name ,C.count_p
-FROM TYPE_PRODUCT_RECUR T
-left join  COUNT_BY_TYPE C
-on C.ID_TYPE_PRODUCT = T.id_type_product;
+test_script = """
+    set list on;
+    set count on;
+    with recursive
+    type_product_recur (id_type_product,name,father) as (
+        select id_type_product, '+ ' || name as name, id_type_product as father
+        from type_product
+        where type_product.id_sub_type is null
+        
+        UNION ALL
+        
+        select t.id_type_product, ' - ' || t.name, tr.id_type_product as father
+        from type_product t
+        join type_product_recur tr on tr.id_type_product = t.id_sub_type
+    ),
+    count_by_type as (
+        select p.id_type_product,count(id_product) as count_p from product p
+        group by p.id_type_product
+        
+        UNION
 
+        select tp.father, count(id_product) as count_p from
+        type_product_recur tp, product p
+        where tp.id_type_product = p.id_type_product
+        group by tp.father
+    )
+    select  t.id_type_product, t.name, c.count_p
+    from type_product_recur t
+    left join  count_by_type c
+    on c.id_type_product = t.id_type_product
+    ;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-ID_TYPE_PRODUCT NAME                                 COUNT_P
-=============== ====================== =====================
-              1 + DVD                                      6
-              3  - FILM SF                                 3
-              4  - FILM ACTION                             1
-              5  - FILM ROMANCE                            2
-              2 + BOOK                                <null>
+    ID_TYPE_PRODUCT 1
+    NAME + dvd
+    COUNT_P 6
+    
+    ID_TYPE_PRODUCT 3
+    NAME - film sf
+    COUNT_P 3
+    
+    ID_TYPE_PRODUCT 4
+    NAME - film action
+    COUNT_P 1
+    
+    ID_TYPE_PRODUCT 5
+    NAME - film romance
+    COUNT_P 2
+    
+    ID_TYPE_PRODUCT 2
+    NAME + book
+    COUNT_P <null>
+
+    Records affected: 5
 """
 
 @pytest.mark.version('>=3.0')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
