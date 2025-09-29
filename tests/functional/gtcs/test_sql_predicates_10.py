ec1f7b857db008a172c995bbadd6e001a1d29635
pavel-zotov <p519446@yandex.ru>
2025-09-29 14:50:45 +0300

    Added/Updated tests\functional\gtcs\test_sql_predicates_10.py: Checked on 6.0.0.1282; 5.0.4.1715; 4.0.7.3235; 3.0.14.33826.

---
+++ tests/functional/gtcs/test_sql_predicates_10.py
@@ -0,0 +1,115 @@
+#coding:utf-8
+
+"""
+ID:          n/a
+TITLE:       Check result of DISTINCT when applying SINGULAR to result of query with aggregate function
+DESCRIPTION:
+    Original test see in:
+    https://github.com/FirebirdSQL/fbtcs/blob/master/GTCS/tests/C_SQL_PRED_10.script
+NOTES:
+    [29.09.2025] pzotov
+    Checked on 6.0.0.1282; 5.0.4.1715; 4.0.7.3235; 3.0.14.33826.
+"""
+import os
+import pytest
+from firebird.qa import *
+
+db = db_factory()
+
+substitutions = [ ('[ \t]+', ' '), ]
+act = python_act('db', substitutions = substitutions)
+
+@pytest.mark.version('>=3')
+def test_1(act: Action):
+
+    sql_init = (act.files_dir / 'gtcs-shtest-ddl-and-data.sql').read_text()
+    sql_addi = """
+        set list on;
+        select 'point-00' as msg from rdb$database;
+        set count on;
+
+        select distinct t1.sales_rep,t1.total_value
+        from sales t1
+        where singular
+        (
+            select *
+            from sales t2
+            where
+                t2.sales_rep = t1.sales_rep
+                and t2.total_value >
+                (
+                    select avg(t3.total_value)
+                    from sales t3
+                    where t3.sales_rep = 137
+                )
+        )
+        order by 1,2
+        ;
+
+        set count off;
+        select 'point-01' as msg from rdb$database;
+        set count on;
+
+        select distinct t1.sales_rep,t1.total_value
+        from sales t1
+        where 1 =
+        (
+            select count(*)
+            from sales t2
+            where
+                t2.sales_rep = t1.sales_rep
+                and t2.total_value >
+                (
+                    select avg(t3.total_value)
+                    from sales t3
+                    where t3.sales_rep = 137
+                )
+        )
+        order by 1,2
+        ;
+
+        set count off;
+        select 'point-02' as msg from rdb$database;
+    """
+    
+    act.expected_stdout = """
+        MSG point-00
+        SALES_REP 125
+        TOTAL_VALUE 15500
+        SALES_REP 125
+        TOTAL_VALUE 18500
+        SALES_REP 125
+        TOTAL_VALUE 25500
+        SALES_REP 125
+        TOTAL_VALUE 43500
+        SALES_REP 125
+        TOTAL_VALUE 48500
+        SALES_REP 137
+        TOTAL_VALUE 18000
+        SALES_REP 137
+        TOTAL_VALUE 38000
+        SALES_REP 137
+        TOTAL_VALUE 85000
+        Records affected: 8
+        MSG point-01
+        SALES_REP 125
+        TOTAL_VALUE 15500
+        SALES_REP 125
+        TOTAL_VALUE 18500
+        SALES_REP 125
+        TOTAL_VALUE 25500
+        SALES_REP 125
+        TOTAL_VALUE 43500
+        SALES_REP 125
+        TOTAL_VALUE 48500
+        SALES_REP 137
+        TOTAL_VALUE 18000
+        SALES_REP 137
+        TOTAL_VALUE 38000
+        SALES_REP 137
+        TOTAL_VALUE 85000
+        Records affected: 8
+        MSG point-02
+    """
+    act.isql(switches=['-q'], input = os.linesep.join( (sql_init, sql_addi) ), combine_output = True )
+    assert act.clean_stdout == act.clean_expected_stdout
