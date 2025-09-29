58a5277552ec0cea84a5256b8939b0e93ea6a291
pavel-zotov <p519446@yandex.ru>
2025-09-29 12:49:35 +0300

    Added/Updated tests\functional\gtcs\test_sql_predicates_03.py: Changed code to use $QA_ROOT/files/gtcs-shtest-ddl-and-data.sql

--- tests/functional/gtcs/test_sql_predicates_03.py
+++ tests/functional/gtcs/test_sql_predicates_03.py
@@ -7,49 +7,50 @@ DESCRIPTION:
   Original test see in:
       https://github.com/FirebirdSQL/fbtcs/blob/master/GTCS/tests/C_SQL_PRED_3.script
 NOTES:
-    [27.09.2025] pzotov
+    [29.09.2025] pzotov
+    Test uses pre-created script $QA_ROOT/files/gtcs-shtest-ddl-and-data.sql 
+    Original database ($QA_ROOT/backups/gtcs_sh_test.fbk) has problems in RDB$ tables:
+    relation 'sales' after restore will have duplicates in rdb$relation_fields.rdb$field_position.
+    Query "select * from sales ..." will produce columns in DIFFERENT ORDER on 3.x vs 4.x+.
+    Also, this DB was created in dialect 1 which is not actual nowadays.
+
     Checked on 6.0.0.1282; 5.0.4.1715; 4.0.7.3235; 3.0.14.33826.
 """
-
+import os
 import pytest
 from firebird.qa import *
 
-db = db_factory(from_backup = 'gtcs_sh_test.fbk')
-
-test_script = """
-    set list on;
-    select 'point-00' as msg from rdb$database;
-    set count on;
-    select * from customers c1
-    where not singular (select * from sales s1 where s1.custno = c1.custno)
-    ;
-    set count off;
-    select 'point-01' as msg from rdb$database;
-    set count on;
-    select * from customers c1
-    where 0 = (select count(*) from sales s1 where s1.custno = c1.custno)
-    or 1 < (select count(*) from sales s2 where s2.custno = c1.custno)
-    ;
-    set count off;
-    select 'point-02' as msg from rdb$database;
-"""
+db = db_factory()
 
 substitutions = [ ('[ \t]+', ' '), ]
-act = isql_act('db', test_script, substitutions = substitutions)
+act = python_act('db', substitutions = substitutions)
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
+
+    sql_init = (act.files_dir / 'gtcs-shtest-ddl-and-data.sql').read_text()
+    sql_addi = """
+        set list on;
+        select 'point-00' as msg from rdb$database;
+        set count on;
+        select * from customers c1
+        where not singular (select * from sales s1 where s1.custno = c1.custno)
+        order by c1.custno
+        ;
+        set count off;
+        select 'point-01' as msg from rdb$database;
+        set count on;
+        select * from customers c1
+        where 0 = (select count(*) from sales s1 where s1.custno = c1.custno)
+        or 1 < (select count(*) from sales s2 where s2.custno = c1.custno)
+        order by c1.custno
+        ;
+        set count off;
+        select 'point-02' as msg from rdb$database;
+    """
+    
     act.expected_stdout = """
         MSG point-00
-        CUSTNO 114
-        CUSTOMER 1st Canine Bank
-        CONTACT George McClellan
-        ADDRESS 1342 Massachusetts Avenue
-        CITY Boston
-        STATE MA
-        ZIP_CODE 02113
-        PHONE_NO 6179291622
-        ON_HOLD <null>
         CUSTNO 103
         CUSTOMER The Groton Company
         CONTACT Phil Sheridan
@@ -67,18 +68,18 @@ def test_1(act: Action):
         STATE WI
         ZIP_CODE 54305
         PHONE_NO 4142983288
+        ON_HOLD <null>
+        CUSTNO 114
+        CUSTOMER 1st Canine Bank
+        CONTACT George McClellan
+        ADDRESS 1342 Massachusetts Avenue
+        CITY Boston
+        STATE MA
+        ZIP_CODE 02113
+        PHONE_NO 6179291622
         ON_HOLD <null>
         Records affected: 3
         MSG point-01
-        CUSTNO 114
-        CUSTOMER 1st Canine Bank
-        CONTACT George McClellan
-        ADDRESS 1342 Massachusetts Avenue
-        CITY Boston
-        STATE MA
-        ZIP_CODE 02113
-        PHONE_NO 6179291622
-        ON_HOLD <null>
         CUSTNO 103
         CUSTOMER The Groton Company
         CONTACT Phil Sheridan
@@ -96,9 +97,18 @@ def test_1(act: Action):
         STATE WI
         ZIP_CODE 54305
         PHONE_NO 4142983288
+        ON_HOLD <null>
+        CUSTNO 114
+        CUSTOMER 1st Canine Bank
+        CONTACT George McClellan
+        ADDRESS 1342 Massachusetts Avenue
+        CITY Boston
+        STATE MA
+        ZIP_CODE 02113
+        PHONE_NO 6179291622
         ON_HOLD <null>
         Records affected: 3
         MSG point-02
     """
-    act.execute(combine_output = True)
+    act.isql(switches=['-q'], input = os.linesep.join( (sql_init, sql_addi) ), combine_output = True )
     assert act.clean_stdout == act.clean_expected_stdout
