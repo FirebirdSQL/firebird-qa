11e16bcb1ba7237705b4d8a71570f4897d310d82
pavel-zotov <p519446@yandex.ru>
2025-10-07 13:44:05 +0300

    Added/Updated tests\bugs\core_4382_test.py: Separated expected output for 6.x after commit 1214b460, see #8542. Checked on: 6.0.0.1295; 5.0.4.1711; 4.0.7.3235; 3.0.14.33826.

--- tests/bugs/core_4382_test.py
+++ tests/bugs/core_4382_test.py
@@ -5,81 +5,74 @@ ID:          issue-4704
 ISSUE:       4704
 TITLE:       User savepoints are not released on commit
 DESCRIPTION:
-  Added separate code for 4.0: one need to be sure that all changes have been flushed on disk before we launch gstat.
-  See letter from hvlad, 02.02.2019 22:30.
-  ::: NOTE :::
-  !! It looks strange but if we put preparing statement in 'init_script' section than result of 'gstat -i' will be WRONG,
-  even if we do db_conn.close() before runProgram('gstat' ...) !!
+    Added separate code for 4.0: one need to be sure that all changes have been flushed on disk before we launch gstat.
+    See letter from hvlad, 02.02.2019 22:30.
+    ::: NOTE :::
+    !! It looks strange but if we put preparing statement in 'init_script' section than result of 'gstat -i' will be WRONG,
+    even if we do db_conn.close() before runProgram('gstat' ...) !!
 JIRA:        CORE-4382
-FBTEST:      bugs.core_4382
+NOTES:
+    [07.10.2025] pzotov
+    Separated expected output for 6.x after commit 1214b460 ("Remove the check in loadInventoryPage ...", see #8542):
+    number of nodes become 1 (as it was in 3.x).
+
+    Checked on: 6.0.0.1295; 5.0.4.1711; 4.0.7.3235; 3.0.14.33826.
 """
 
 import pytest
 from firebird.qa import *
 
-substitutions = [('^((?!nodes).)*$', ''), ('Root page: [0-9]+,', ''), ('Depth', 'depth')]
+substitutions = [('^((?!nodes|Error|SQLSTATE).)*$', ''), ('Root page: \\d+,', 'Root page: N'), ('Depth', 'depth')]
 
-init_script = """
-    create table g_test (f integer);
-    create index g_ind on g_test (f);
-    insert into g_test values (1);
-    commit;
-    update g_test set f=2;
-    savepoint a;
-    update g_test set f=3;
-    savepoint b;
-    update g_test set f=4;
-    savepoint c;
-    update g_test set f=5;
-    savepoint d;
-    update g_test set f=6;
-    savepoint e;
-    update g_test set f=7;
-    commit;
-    select * from g_test;
-    COMMIT;
-    -- Confirmed result of "gstat -i"
-    -- 1) for 3.0 Alpha1 & Alpha2:
-    -- Root page: 203, depth: 1, leaf buckets: 1, nodes: 6
-    --                                                   ^- orphans, must be: 1
-    -- 2) for 2.5.3:
-    -- Depth: 1, leaf buckets: 1, nodes: 6
-    -- ^- upper case!                    ^- orphans, must be: 1
-"""
+db = db_factory()
+act = python_act('db', substitutions = substitutions)
 
-# version: 3.0
+@pytest.mark.version('>=3')
+def test_1(act: Action):
 
-db_1 = db_factory(init=init_script)
+    test_script = """
+        create table g_test (f integer);
+        create index g_ind on g_test (f);
+        insert into g_test values (1);
+        commit;
+        update g_test set f=2;
+        savepoint a;
+        update g_test set f=3;
+        savepoint b;
+        update g_test set f=4;
+        savepoint c;
+        update g_test set f=5;
+        savepoint d;
+        update g_test set f=6;
+        savepoint e;
+        update g_test set f=7;
+        commit;
+        select * from g_test;
+        COMMIT;
+        -- Confirmed result of "gstat -i"
+        -- 1) for 3.0 Alpha1 & Alpha2:
+        -- Root page: 203, depth: 1, leaf buckets: 1, nodes: 6
+        --                                                   ^- orphans, must be: 1
+        -- 2) for 2.5.3:
+        -- Depth: 1, leaf buckets: 1, nodes: 6
+        -- ^- upper case!                    ^- orphans, must be: 1
+    """
+    act.isql(switches=['-q'], input = test_script, combine_output = True)
+    assert act.clean_stdout == '', f'Error in test script:\n{act.clean_stdout}'
+    act.reset()
 
-act_1 = python_act('db_1', substitutions=substitutions)
+    
+    expected_stdout_3x = """
+        Root page: 203, depth: 1, leaf buckets: 1, nodes: 1
+    """
+    expected_stdout_4x = """
+        Root page: 203, depth: 1, leaf buckets: 1, nodes: 2
+    """
 
-expected_stdout_1 = """
-    Root page: 203, depth: 1, leaf buckets: 1, nodes: 1
-"""
+    expected_stdout_6x = """
+        Root page: 203, depth: 1, leaf buckets: 1, nodes: 1
+    """
 
-@pytest.mark.version('>=3,<4.0')
-def test_1(act_1: Action):
-    act_1.expected_stdout = expected_stdout_1
-    act_1.gstat(switches=['-i'])
-    assert act_1.clean_stdout == act_1.clean_expected_stdout
-
-# version: 4.0
-
-db_2 = db_factory()
-
-act_2 = python_act('db_2', substitutions=substitutions)
-
-expected_stdout_2 = """
-    Root page: 203, depth: 1, leaf buckets: 1, nodes: 2
-"""
-
-@pytest.mark.version('>=4.0')
-def test_2(act_2: Action):
-    act_2.isql(switches=['-q'], input=init_script)
-    act_2.reset()
-    act_2.expected_stdout = expected_stdout_2
-    act_2.gstat(switches=['-i'])
-    assert act_2.clean_stdout == act_2.clean_expected_stdout
-
-
-
+    act.expected_stdout = expected_stdout_3x if act.is_version('<4') else expected_stdout_4x if act.is_version('<6') else expected_stdout_6x
+    act.gstat(switches=['-i'])
+    assert act.clean_stdout == act.clean_expected_stdout
