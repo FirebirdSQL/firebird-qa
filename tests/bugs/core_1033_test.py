6eab51fd7414e1e7a42043caf478e23f71f143eb
pavel-zotov <p519446@yandex.ru>
2025-10-07 12:16:25 +0300

    Added/Updated tests\bugs\core_1033_test.py: Separated expected output after #8701 was fixed by 4e3a0026. See notes.

--- tests/bugs/core_1033_test.py
+++ tests/bugs/core_1033_test.py
@@ -7,9 +7,15 @@ TITLE:       LIKE doesn't work for computed values (at least in a view)
 DESCRIPTION:
 JIRA:        CORE-1033
 NOTES:
-    [29.09.2025] pzotov
-    Refactored: show SQLDA, changed table output to the list, added substitutions.
-    Checked on 6.0.0.1280; 5.0.4.1715; 4.0.7.3235; 3.0.14.33826.
+    [07.10.2025] pzotov
+    Added SQLDA_DISPLAY, changed table output to the list, added substitutions.
+    Separated expected output after #8701 was fixed by 4e3a0026: length of
+    column 'part_descr' on 6.x increased from 280 to 292:
+        02: sqltype: 448 VARYING Nullable scale: 0 subtype: 0 len: 292
+    (previous value = 280 must be considered as wrong; discussed with Dm. Sibiryakov, 30.09.2025 19:44).
+    See also: https://github.com/FirebirdSQL/firebird/pull/8757
+
+    Checked on 6.0.0.1295-b14254f; 5.0.4.1715; 4.0.7.3235; 3.0.14.33826.
 """
 
 import pytest
@@ -47,31 +53,55 @@ substitutions = [ ('^((?!(SQLSTATE|sqltype|PART_ID|PART_DESCR|Records)).)*$', ''
 
 act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
-    01: sqltype: 580 INT64 Nullable scale: 0 subtype: 1 len: 8
-    : name: PART_ID alias: PART_ID
-    02: sqltype: 448 VARYING Nullable scale: 0 subtype: 0 len: 280
-    : name: PART_DESCR alias: PART_DESCR
-
-    PART_ID 1
-    PART_DESCR xyz (1)
-    Records affected: 1
-    
-    PART_ID 1
-    PART_DESCR xyz (1)
-    Records affected: 1
-    
-    PART_ID 1
-    PART_DESCR xyz (1)
-    PART_ID 2
-    PART_DESCR xyzxyz (2)
-    PART_ID 3
-    PART_DESCR xyz012 (3)
-    Records affected: 3
-"""
-
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
+
+    expected_stdout_5x = """
+        01: sqltype: 580 INT64 Nullable scale: 0 subtype: 1 len: 8
+        : name: PART_ID alias: PART_ID
+        02: sqltype: 448 VARYING Nullable scale: 0 subtype: 0 len: 280
+        : name: PART_DESCR alias: PART_DESCR
+
+        PART_ID 1
+        PART_DESCR xyz (1)
+        Records affected: 1
+        
+        PART_ID 1
+        PART_DESCR xyz (1)
+        Records affected: 1
+        
+        PART_ID 1
+        PART_DESCR xyz (1)
+        PART_ID 2
+        PART_DESCR xyzxyz (2)
+        PART_ID 3
+        PART_DESCR xyz012 (3)
+        Records affected: 3
+    """
+
+    expected_stdout_6x = """
+        01: sqltype: 580 INT64 Nullable scale: 0 subtype: 1 len: 8
+        : name: PART_ID alias: PART_ID
+        02: sqltype: 448 VARYING Nullable scale: 0 subtype: 0 len: 292
+        : name: PART_DESCR alias: PART_DESCR
+
+        PART_ID 1
+        PART_DESCR xyz (1)
+        Records affected: 1
+        
+        PART_ID 1
+        PART_DESCR xyz (1)
+        Records affected: 1
+        
+        PART_ID 1
+        PART_DESCR xyz (1)
+        PART_ID 2
+        PART_DESCR xyzxyz (2)
+        PART_ID 3
+        PART_DESCR xyz012 (3)
+        Records affected: 3
+    """
+    
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
