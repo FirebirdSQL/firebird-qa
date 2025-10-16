324ed41f1afc53207420eec0299f6dd027760a8a
pavel-zotov <p519446@yandex.ru>
2025-10-16 12:27:48 +0300

    Added/Updated tests\bugs\core_6499_test.py: added comments

--- tests/bugs/core_6499_test.py
+++ tests/bugs/core_6499_test.py
@@ -18,9 +18,35 @@ NOTES:
         3.x (backported): 10-mar-2021,
             https://github.com/FirebirdSQL/firebird/commit/f0c193def839069459d5b12ba3dcaf65deec36a1
 
-    Probably, the bug could be reproduced in some old (initial) version of firebird-driver when it
+    Probably, the bug could be reproduced in versions of firebird-driver before 1.3.4 when it
     did not use isc_spb_sts_table tag during creation of SPB, as noted by Alex:
     https://github.com/FirebirdSQL/firebird/issues/6729#issuecomment-826248689
+
+    Following dirrerence exists between versions 1.3.3 (08-oct-2021) and 1.3.4 (30-nov-2021)
+    of firebird-driver (file core.py, class class ServerDbServices3, def get_statistics):
+        index 2ecd6ea..0f74e60 100644
+        --- a/core.py-1.3.3
+        +++ b/core.py-1.3.4
+        @@ -13,15 +13,13 @@
+                 self._srv()._reset_output()
+                 with a.get_api().util.get_xpb_builder(XpbKind.SPB_START) as spb:
+                     spb.insert_tag(ServerAction.DB_STATS)
+        -            spb.insert_string(SPBItem.DBNAME, database)
+        +            spb.insert_string(SPBItem.DBNAME, database, encoding=self._srv().encoding)
+                     spb.insert_int(SPBItem.OPTIONS, flags)
+                     if role is not None:
+        -                spb.insert_string(SPBItem.SQL_ROLE_NAME, role)
+        +                spb.insert_string(SPBItem.SQL_ROLE_NAME, role, encoding=self._srv().encoding)
+                     if tables is not None:
+        -                cmdline = ['-t']
+        -                cmdline.extend(tables)
+        -                for item in cmdline:
+        -                    spb.insert_string(SPBItem.COMMAND_LINE, item)
+        +                for table in tables:
+        +                    spb.insert_string(64, table, encoding=self._srv().encoding) # isc_spb_sts_table = 64
+                     self._srv()._svc.start(spb.get_buffer())
+                 if callback:
+                     for line in self._srv():
     
     Bug can NOT be reproduced using current version of firebird-driver (2.0.2), checked on:
         4.0.0.2382 Release Candidate 1 // 21-feb-2021
@@ -28,6 +54,8 @@ NOTES:
         3.0.7.33388 // 07-nov-2020
         3.0.8.33423 // 04-mar-2021
     Because of that, i've decided to DISABLE this test at all.
+    Separate test for check gstat output will be implemented (including test of presense of SQL schemas that have been
+    introduced in 6.0.0.813).
 """
 
 import pytest
