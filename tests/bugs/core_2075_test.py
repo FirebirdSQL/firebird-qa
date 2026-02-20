3ad8ef4a83bcb3c09eb44556cec442a33965ee91
pavel-zotov <p519446@yandex.ru>
2026-02-20 14:35:44 +0300

    Added/Updated tests\bugs\core_2075_test.py: Re-implemented, see notes. Checked on 6.0.0.1461-5e98812; 6.0.0.1454-b45aa4e; 5.0.4.1767-52823f5; 4.0.7.3243; 3.0.14.33838

--- tests/bugs/core_2075_test.py
+++ tests/bugs/core_2075_test.py
@@ -1,64 +1,86 @@
 #coding:utf-8
 
 """
-ID:          issue-2510
-ISSUE:       2510
-TITLE:       Parts of RDB$DB_KEY of views may be inverted when using outer joins
+ID:          n/a
+ISSUE:       https://github.com/FirebirdSQL/firebird/issues/2510
+TITLE:       Parts of RDB$DB_KEY of views may be inverted when using FULL JOINs.
 DESCRIPTION:
-JIRA:        CORE-2075
-FBTEST:      bugs.core_2075
-"""
+    Test verifies that rdb$db_key of the VIEW that is declared as FULL JOIN of two tables (t1 and t2)
+    meets requirement: view_db_key == coalesce(t2.db_key, '0'*16) || coalesce(t1.db_key, '0'*16).
+    If any record does not pass this check, we add appropriate db_key values to the special list 'failed_chk'
+    which content will be displayed at final point.
+    Test can be considered as 'passed' if list 'failed_chk' is empty (i.e. output is empty).
+NOTES:
+    [20.02.2026] pzotov
+    Re-implemented: FULL JOIN execution plan has changed since 19.02.2026 6.0.0.1458,
+    commit: "6a76c1da Better index usage in full outer joins...".
+    This caused to changed order of output lines.
 
+    We have to check that 'top-level' view rdb$db_key equals to result of concatenation of rdb$db_key
+    columns of underlying tables. But concrete values of rdb$db_key NO matter for this test.
+
+    Checked on 6.0.0.1461-5e98812; 6.0.0.1454-b45aa4e; 5.0.4.1767-52823f5; 4.0.7.3243; 3.0.14.33838
+"""
 import pytest
 from firebird.qa import *
 
-init_script = """create table t1 (n integer);
-create table t2 (n integer);
+init_script = """
+    create table t1 (n integer);
+    create table t2 (n integer);
 
-insert into t1 values (1);
-insert into t1 values (2);
-insert into t1 values (3);
-insert into t1 values (4);
-insert into t1 values (5);
-insert into t1 values (6);
+    insert into t1 values (1);
+    insert into t1 values (2);
+    insert into t1 values (3);
+    insert into t1 values (4);
+    insert into t1 values (5);
+    insert into t1 values (6);
 
-insert into t2 values (2);
-insert into t2 values (4);
-insert into t2 values (5);
-insert into t2 values (8);
-commit;
+    insert into t2 values (2);
+    insert into t2 values (4);
+    insert into t2 values (5);
+    insert into t2 values (8);
+    commit;
 
-create view v (t1, t2) as
-  select t1.rdb$db_key, t2.rdb$db_key
+    create view v (t1, t2) as
+    select t1.rdb$db_key, t2.rdb$db_key
     from t1
-    full join t2
-      on t2.n = t1.n;
+    full join t2 on t2.n = t1.n
+    ;
 """
 
 db = db_factory(init=init_script)
 
-test_script = """select v.rdb$db_key, v.*
-  from v;
+test_script = """
+    set heading off;
+    select v.rdb$db_key, v.*
+    from v;
 """
 
-act = isql_act('db', test_script)
-
-expected_stdout = """
-DB_KEY                           T1               T2
-================================ ================ ================
-81000000010000008000000002000000 8000000002000000 8100000001000000
-81000000020000008000000004000000 8000000004000000 8100000002000000
-81000000030000008000000005000000 8000000005000000 8100000003000000
-81000000040000000000000000000000 <null>           8100000004000000
-00000000000000008000000001000000 8000000001000000 <null>
-00000000000000008000000003000000 8000000003000000 <null>
-00000000000000008000000006000000 8000000006000000 <null>
-
-"""
+act = python_act('db')
 
 @pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
+def test_1(act: Action, capsys):
+    
+    act.isql(switches = ['-q'], input = test_script, combine_output = True)
+    
+    failed_chk = []
+    for i, line in enumerate(act.clean_stdout.splitlines()):
+        if (s := line.strip()):
+            f01, f02, f03 = s.split()[:3]
+            k02 = '0' * 16 if f02 == '<null>'else f02
+            k03 = '0' * 16 if f03 == '<null>'else f03
+            if f01 == k03 + k02:
+                pass
+            else:
+                failed_chk.append( f"line {i:3d}: FAILED. {f01} <> {k03} || {k02}" )
 
+    act.reset()
+
+    # failed_chk must contain only lines which did NOT pass check:
+    for x in failed_chk:
+        print(x)
+
+    act.expected_stdout = """
+    """
+    act.stdout = capsys.readouterr().out
+    assert act.clean_stdout == act.clean_expected_stdout
