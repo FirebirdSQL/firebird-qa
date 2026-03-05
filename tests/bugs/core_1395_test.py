2e375b19f75e3eec64c3c603fc73af13730c3c84
pavel-zotov <p519446@yandex.ru>
2026-03-05 18:33:58 +0300

    Added/Updated tests\bugs\core_1395_test.py: Adjusted expected output which has changed since #b38046e1 'Encapsulation of metadata cache'. Checked on 6.0.0.1807-46797ab.

--- tests/bugs/core_1395_test.py
+++ tests/bugs/core_1395_test.py
@@ -9,11 +9,13 @@ JIRA:        CORE-1395
 FBTEST:      bugs.core_1395
 NOTES:
     [24.06.2025] pzotov
-    ::: NB :::
-    SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
+    ::: NB ::: SQL schema name (introduced since 6.0.0.834), single and double quotes are suppressed in the output.
     See $QA_HOME/README.substitutions.md or https://github.com/FirebirdSQL/firebird-qa/blob/master/README.substitutions.md
-
     Checked on 6.0.0.858; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
+
+    [05.03.2026] pzotov
+    Adjusted expected output which has changed since #b38046e1 ('Encapsulation of metadata cache'; 24-feb-2026 17:31:04 +0000).
+    Checked on 6.0.0.1807-46797ab.
 """
 
 import pytest
@@ -44,7 +46,7 @@ for p in addi_subst_tokens.split(' '):
 
 act = isql_act('db', test_script, substitutions = substitutions)
 
-expected_stdout = """
+expected_stdout_5x = """
     Statement failed, SQLSTATE = 42000
     unsuccessful metadata update
     -cannot delete
@@ -52,9 +54,17 @@ expected_stdout = """
     -there are 1 dependencies
 """
 
+expected_stdout_6x = """
+    Statement failed, SQLSTATE = 42000
+    unsuccessful metadata update
+    -cannot delete
+    -TABLE TEST
+    -there are 1 dependencies
+"""
+
 @pytest.mark.version('>=3')
 def test_1(act: Action):
-    act.expected_stdout = expected_stdout
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
