c4fa4a1699a6b43d993fdc33b0c0f22a34c4ae74
pavel-zotov <p519446@yandex.ru>
2026-03-05 22:27:36 +0300

    Added/Updated tests\bugs\core_3925_test.py: Adjusted expected output which has changed since #b38046e1 'Encapsulation of metadata cache'.

--- tests/bugs/core_3925_test.py
+++ tests/bugs/core_3925_test.py
@@ -5,15 +5,17 @@ ID:          issue-4260
 ISSUE:       4260
 TITLE:       Creating self-referential FK crashes database (bug-check) whether constraint violation had place
 DESCRIPTION:
-  Confirmed on WI-V3.0.5.33118, WI-T4.0.0.1496: "SQLSTATE = 08006 / Error reading..." on last DELETE statement
-  Checked on WI-V3.0.5.33123, WI-T4.0.0.1501 (both SS an CS): works OK, got only SQLSTATE = 23000 when try to add FK.
-  DELETE statement does not raise error.
+    Confirmed on WI-V3.0.5.33118, WI-T4.0.0.1496: "SQLSTATE = 08006 / Error reading..." on last DELETE statement
+    Checked on WI-V3.0.5.33123, WI-T4.0.0.1501 (both SS an CS): works OK, got only SQLSTATE = 23000 when try to add FK.
+    DELETE statement does not raise error.
 JIRA:        CORE-3925
-FBTEST:      bugs.core_3925
 NOTES:
     [27.06.2025] pzotov
     Added 'SQL_SCHEMA_PREFIX' to be substituted in expected_* on FB 6.x
     Checked on 6.0.0.876; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
+    [05.03.2026] pzotov
+    Adjusted expected output which has changed since #b38046e1 ('Encapsulation of metadata cache'; 24-feb-2026 17:31:04 +0000).
+    Checked on 6.0.0.1807-46797ab; 5.0.4.1780-2040071.
 """
 
 import pytest
@@ -36,14 +38,24 @@ act = isql_act('db', test_script)
 def test_1(act: Action):
     
     SQL_SCHEMA_PREFIX = '' if act.is_version('<6') else '"PUBLIC".'
-    expected_stdout = f"""
+
+    expected_stdout_5x = f"""
         Statement failed, SQLSTATE = 23000
         violation of FOREIGN KEY constraint "FK_KEY_REF" on table {SQL_SCHEMA_PREFIX}"TEST"
         -Foreign key reference target does not exist
         -Problematic key value is ("REF" = -1)
     """
 
-    act.expected_stdout = expected_stdout
+    expected_stdout_6x = f"""
+        Statement failed, SQLSTATE = 23000
+        unsuccessful metadata update
+        -ALTER TABLE {SQL_SCHEMA_PREFIX}"TEST" failed
+        -violation of FOREIGN KEY constraint "FK_KEY_REF" on table {SQL_SCHEMA_PREFIX}"TEST"
+        -Foreign key reference target does not exist
+        -Problematic key value is ("REF" = -1)
+    """
+
+    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
     act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
 
