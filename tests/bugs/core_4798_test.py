d6e41e030d805c4a98723ead62158dd284ea2bc0
pavel-zotov <p519446@yandex.ru>
2026-03-16 21:12:04 +0300

    Added/Updated tests\bugs\core_4798_test.py: Re-implemented after note by dimitr: con.info.get_table_access_stats() is invoked to obtain NR and IR values.

--- tests/bugs/core_4798_test.py
+++ tests/bugs/core_4798_test.py
@@ -2,89 +2,76 @@
 
 """
 ID:          issue-5096
-ISSUE:       5096
+ISSUE:       https://github.com/FirebirdSQL/firebird/issues/5096
 TITLE:       Regression: MIN/MAX with a join ignores possible index navigation
 DESCRIPTION:
 JIRA:        CORE-4798
-FBTEST:      bugs.core_4798
+NOTES:
+    [16.03.2026] pzotov
+    Re-implemented after note by dimitr: con.info.get_table_access_stats() is invoked to obtain NR and IR values.
+    Small threshold for max allowed indexed reads remains if optimizer and statistics will be changed in the future.
+    Checked on 6.0.0.1824, 5.0.4.1780, 4.0.7.3256, 3.0.14.33845.
 """
 
 import pytest
 from firebird.qa import *
 
-db = db_factory(from_backup='mon-stat-gathering-3_0.fbk')
+NAT_READS_MAX_THRESHOLD = 0
+IDX_READS_MAX_THRESHOLD = 5
 
-test_script = """
+init_script = """
     recreate table test(x int);
     commit;
-
-    insert into test select rand()*100 from (select 1 i from rdb$types rows 50) a, (select 1 i from rdb$types rows 50) b;
+    set term ^;
+    execute block as
+        declare n int = 100000;
+    begin
+        while (n > 0) do
+        begin
+            insert into test(x) values(mod(:n, 123));
+            n = n - 1;
+        end
+    end
+    ^
+    set term ;^
     commit;
     create index test_x on test(x);
-    commit;
-    set term ^;
-    execute block as
-    begin
-        -- ####################################
-        -- S E T T I N G    T H R E S H O L D S
-        -- ####################################
-        rdb$set_context('USER_SESSION','NAT_READS_MAX_THRESHOLD', '0');
-        rdb$set_context('USER_SESSION','IDX_READS_MAX_THRESHOLD', '20'); -- increased 27.07.2016
-    end
-    ^
-    set term ;^
-
-    alter sequence g_gather_stat restart with 0;
-    execute procedure sp_truncate_stat;
-    commit;
-
-    execute procedure sp_gather_stat; ------- catch statistics BEFORE measured statement(s)
-    commit;
-
-    set term ^;
-    execute block as
-        declare v_dummy int;
-    begin
-        select min(a.x) from test a join test b on a.x = b.x join test c on b.x = c.x into v_dummy;
-    end
-    ^
-    set term ;^
-    execute procedure sp_gather_stat;  ------- catch statistics AFTER measured statement(s)
-    commit;
-
-    set list on;
-        -- 3.0.0.31374 (Beta 1):
-        --    natural_reads                   2500
-        --    indexed_reads                   1839808
-        -- 3.0.0.31852:
-        --    natural_reads                   0
-        --    indexed_reads                   6
-        -- 4.0.0.313:
-        --    natural_reads                   0
-        --    indexed_reads                   18
-    select
-         iif( natural_reads <= nat_max, 'acceptable, <= ' || nat_max, 'abnormal, ' || natural_reads || ' > ' || nat_max ) as natural_reads
-        ,iif( indexed_reads <= idx_max, 'acceptable, <= ' || idx_max, 'abnormal, ' || indexed_reads || ' > ' || idx_max ) as indexed_reads
-    from (
-        select
-             v.natural_reads
-            ,v.indexed_reads
-            ,cast(rdb$get_context('USER_SESSION','NAT_READS_MAX_THRESHOLD') as int) nat_max
-            ,cast(rdb$get_context('USER_SESSION','IDX_READS_MAX_THRESHOLD') as int) idx_max
-        from v_agg_stat v
-    );
 """
+db = db_factory(init = init_script)
 
-act = isql_act('db', test_script)
-
-expected_stdout = """
-    NATURAL_READS                   acceptable, <= 0
-    INDEXED_READS                   acceptable, <= 20
-"""
+act = python_act('db')
+EXPECTED_MSG = 'Expected.'
 
 @pytest.mark.version('>=3.0')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout
-    act.execute()
-    assert act.clean_stdout == act.clean_expected_stdout
+def test_1(act: Action, capsys):
+    nat_reads = idx_reads = 0
+    with act.db.connect() as con:
+        cur=con.cursor()
+        cur.execute("select rdb$relation_id from rdb$relations where rdb$relation_name = upper('test')")
+        test_rel_id = int(cur.fetchone()[0])
 
+        for x_table in con.info.get_table_access_stats():
+            if x_table.table_id == test_rel_id:
+                nat_reads = -x_table.sequential if x_table.sequential else 0
+                idx_reads = -x_table.indexed if x_table.indexed else 0
+
+        cur.execute(f"select /* trace_me */ min(a.x) from test a join test b on a.x = b.x join test c on b.x = c.x")
+        data = cur.fetchall()
+
+        for x_table in con.info.get_table_access_stats():
+            if x_table.table_id == test_rel_id:
+                nat_reads += x_table.sequential if x_table.sequential else 0
+                idx_reads += x_table.indexed if x_table.indexed else 0
+    
+    if nat_reads <= NAT_READS_MAX_THRESHOLD and idx_reads <= IDX_READS_MAX_THRESHOLD:
+        print(EXPECTED_MSG)
+    else:
+        print('Unexpected values of NR and/or IR:')
+        print(f'{nat_reads=}')
+        print(f'{idx_reads=}')
+
+    expected_stdout = EXPECTED_MSG
+
+    act.expected_stdout = expected_stdout
+    act.stdout = capsys.readouterr().out
+    assert act.clean_stdout == act.clean_expected_stdout
