a8fa1ce2cb10983fe2efdb4328a27042aa1ac9f0
pavel-zotov <p519446@yandex.ru>
2026-04-14 17:29:19 +0300

    Added/Updated tests\bugs\core_5756_test.py: refactored, see notes. Checked on 6.0.0.1891; 5.0.4.1808; 4.0.7.3269; 3.0.14.33855.

--- tests/bugs/core_5756_test.py
+++ tests/bugs/core_5756_test.py
@@ -1,19 +1,23 @@
 #coding:utf-8
 
 """
-ID:          issue-6019-A
-ISSUE:       6019
+ID:          n/a
+ISSUE:       https://github.com/FirebirdSQL/firebird/issues/6019
 TITLE:       Regression: FB crashes when trying to recreate table that is in use by DML (3.0.3; 3.0.4; 4.0.0)
 DESCRIPTION:
 JIRA:        CORE-5756
 FBTEST:      bugs.core_5756
-    [02.07.2025] pzotov
-    Separated expected output for FB major versions prior/since 6.x.
-    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
-    Difference between current_connection values must be checked to be sure that there was no crash, see 'ATT_DIFF'
+    [14.04.2026] pzotov
+    Refactored: we have to check only that  script finished w/o fatal error that can not be catched in 'when any' block.
+    Errors like 'object in use' will be suppressed.
+    Test can be considered as passed if output will contain 'MSG OK' message.
 
-    Confirmed bug  (crash) on 3.0.3.32900.
-    Checked on 6.0.0.889; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
+    Confirmed crash on 3.0.3.32900 SS/CS; following messages appeared in the firebird.log:
+        Access violation. The code attempted to access a virtual address without privilege to do so.
+        This exception will cause the Firebird server to terminate abnormally.
+        ...
+        Error writing data to the connection.
+    Checked on 6.0.0.1891; 5.0.4.1808; 4.0.7.3269; 3.0.14.33855.
 """
 
 import pytest
@@ -21,47 +25,51 @@ from firebird.qa import *
 
 db = db_factory()
 
-test_script = """
-    set list on;
-    recreate table test(x int);
-    insert into test values(1);
-    select * from test;
-    set term ^;
-    execute block as
-    begin
-        rdb$set_context('USER_SESSION', 'INIT_ATT', current_connection);
-    end ^
-    set term ;^
-
-    recreate table test(x int, y int); -- this led to crash
-
-    select * from test;
-    select current_connection - cast( rdb$get_context('USER_SESSION', 'INIT_ATT') as int) as att_diff from rdb$database;
-"""
-
 substitutions = [('[ \t]+', ' ')]
-act = isql_act('db', test_script, substitutions = substitutions)
+act = python_act('db', substitutions = substitutions)
 
-expected_stdout_5x = """
-    X 1
-    Statement failed, SQLSTATE = 42000
-    unsuccessful metadata update
-    -object TABLE "TEST" is in use
-    X 1
-    ATT_DIFF 0
-"""
+@pytest.mark.version('>=3.0.3')
+def test_1(act: Action, capsys):
+    
+    test_script = """
+        set bail on;
+        set list on;
+        recreate table test(x int);
+        commit;
+        set transaction no wait;
+        insert into test values(1);
+        select * from test;
+        set term ^;
+        execute block as
+        begin
+            begin
+                execute statement 'recreate table test(x int, y int)'
+                with autonomous transaction -- this is needed for crash
+                ; 
+            when any do
+                begin
+                    -- nop --
+                end
+            end
+        end ^
+        set term ;^
+        commit;
+        select 'OK' as msg from rdb$database;
+    """
+    act.isql(switches = ['-q'], input = test_script, combine_output = False)
 
-expected_stdout_6x = """
-    X 1
-    Statement failed, SQLSTATE = 42000
-    unsuccessful metadata update
-    -object TABLE "PUBLIC"."TEST" is in use
-    X 1
-    ATT_DIFF 0
-"""
+    if act.stdout:
+        for line in act.clean_stdout.splitlines():
+                print(line)
 
-@pytest.mark.version('>=3.0.4')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
-    act.execute(combine_output = True)
+    if act.stderr:
+        for line in act.clean_stderr.splitlines():
+                print(line)
+    act.reset()
+
+    act.expected_stdout = """
+        X   1
+        MSG OK
+    """
+    act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
