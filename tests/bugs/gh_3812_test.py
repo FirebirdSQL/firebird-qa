d99b0c624e7aaa2bebb7401e815da95565ec8595
pavel-zotov <p519446@yandex.ru>
2026-04-16 14:40:56 +0300

    Added/Updated tests\bugs\gh_3812_test.py: Adjusted output in 6.x to current (letter from dimitr, 13.04.2026 0855).

--- tests/bugs/gh_3812_test.py
+++ tests/bugs/gh_3812_test.py
@@ -9,45 +9,43 @@ NOTES:
         Confirmed problem on 5.0.0.743, got:
             Statement failed, SQLSTATE = 42S02
             -Invalid command
-            -there is no alias or table named TMP_SP1 at this scope level
+            -there is no alias or table named sp_test at this scope level
         Checked on 5.0.0.745 - all OK.
-    [04.07.2025] pzotov
-        Separated expected output for FB major versions prior/since 6.x.
-        No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
-        Checked on 6.0.0.876; 5.0.3.1668.
+    [13.04.2026] pzotov
+        Adjusted output in 6.x to current (letter from dimitr, 13.04.2026 0855).
+        Checked on 6.0.0.1891; 5.0.4.1808.
 """
+from firebird.driver import DatabaseError
 
 import pytest
 from firebird.qa import *
 
-db = db_factory()
-
-test_script = """
+init_script = """
     set bail on;
-    recreate table tmp_tbl1(
+    recreate table test1(
        fld1 int
        ,fld2 int
        ,fld3 int
     );
-    create index tmp_tbl1_fld2 on tmp_tbl1(fld2);
+    create index tmp_tbl1_fld2 on test1(fld2);
 
-    recreate table tmp_tbl2(
+    recreate table test2(
        fld1 int
        ,fld2 int
        ,fld3 int
     );
-    create index tmp_tbl2_fld1 on tmp_tbl2(fld1);
+    create index tmp_tbl2_fld1 on test2(fld1);
 
 
-    recreate table tmp_tbl3(
+    recreate table test3(
        fld1 int
        ,fld2 int
        ,fld3 int
     );
-    create index tmp_tbl3_fld1 on tmp_tbl3(fld1);
+    create index tmp_tbl3_fld1 on test3(fld1);
 
     set term ^;
-    create or alter procedure tmp_sp1 returns(fld1 int) as
+    create or alter procedure sp_test returns(fld1 int) as
     begin
         fld1 = 2;
         suspend;
@@ -55,30 +53,56 @@ test_script = """
     ^
     set term ;^
     commit;
-
-    set plan on;
-    select t2.fld1
-    from tmp_tbl2 t2
-    join tmp_tbl1 t1 on t1.fld1=t2.fld1
-    join tmp_sp1 on tmp_sp1.fld1=t1.fld2
-    join tmp_tbl3 t3 on t3.fld1=t1.fld3
-    where t2.fld2=2
-    PLAN JOIN (JOIN (TMP_SP1 NATURAL, T1 INDEX (TMP_TBL1_FLD2)), T2 INDEX (TMP_TBL2_FLD1), T3 INDEX (TMP_TBL3_FLD1))
-    ;
 """
 
-act = isql_act('db', test_script)
+db = db_factory(init = init_script)
 
-expected_stdout_5x = """
-    PLAN JOIN (JOIN (TMP_SP1 NATURAL, T1 INDEX (TMP_TBL1_FLD2)), T2 INDEX (TMP_TBL2_FLD1), T3 INDEX (TMP_TBL3_FLD1))
-"""
-
-expected_stdout_6x = """
-    PLAN JOIN (JOIN ("PUBLIC"."TMP_SP1" NATURAL, "T1" INDEX ("PUBLIC"."TMP_TBL1_FLD2")), "T2" INDEX ("PUBLIC"."TMP_TBL2_FLD1"), "T3" INDEX ("PUBLIC"."TMP_TBL3_FLD1"))
-"""
+act = python_act('db')
 
 @pytest.mark.version('>=5.0')
-def test_1(act: Action):
-    act.expected_stdout = expected_stdout_5x if act.is_version('<6') else expected_stdout_6x
-    act.execute(combine_output = True)
+def test_1(act: Action, capsys):
+
+    if act.is_version('<6'):
+        desired_plan = 'PLAN JOIN (SP_TEST NATURAL, T1 INDEX (TMP_TBL1_FLD2), T2 INDEX (TMP_TBL2_FLD1), T3 INDEX (TMP_TBL3_FLD1))'
+    else:
+        desired_plan = 'PLAN JOIN ("PUBLIC"."SP_TEST" NATURAL, "T1" INDEX ("PUBLIC"."TMP_TBL1_FLD2"), "T2" INDEX ("PUBLIC"."TMP_TBL2_FLD1"), "T3" INDEX ("PUBLIC"."TMP_TBL3_FLD1"))'
+
+    test_sql = f"""
+        select t2.fld1
+        from test2 t2
+        join test1 t1 on t1.fld1=t2.fld1
+        join SP_TEST on sp_test.fld1=t1.fld2
+        join test3 t3 on t3.fld1=t1.fld3
+        where t2.fld2=2
+        {desired_plan}
+        ;
+    """
+
+    with act.db.connect() as con:
+        cur = con.cursor()
+        ps, rs = None, None
+        try:
+            cur = con.cursor()
+            ps = cur.prepare(test_sql)
+
+            print(ps.plan)
+
+            # Print explained plan with padding eash line by dots in order to see indentations:
+            #print( '\n'.join([replace_leading(s) for s in ps.detailed_plan.split('\n')]) )
+
+        except DatabaseError as e:
+            print(e.__str__())
+            print(e.gds_codes)
+        finally:
+            if rs:
+                rs.close() # <<< EXPLICITLY CLOSING CURSOR RESULTS
+            if ps:
+                ps.free()
+    
+    expected_stdout = f"""
+        {desired_plan}
+    """
+
+    act.expected_stdout = expected_stdout
+    act.stdout = capsys.readouterr().out
     assert act.clean_stdout == act.clean_expected_stdout
