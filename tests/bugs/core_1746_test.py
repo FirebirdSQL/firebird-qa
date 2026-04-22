ca77684f37e3be83cc2841744a83f122ea2c356f
pavel-zotov <p519446@yandex.ru>
2026-04-22 23:03:54 +0300

    Added/Updated tests\bugs\core_1746_test.py: Reverted to be executed on 6.x. Adjusted expected output and comments. Checked on 6.0.0.1914; 5.0.4.1813; 4.0.7.3271; 3.0.14.33855.

--- tests/bugs/core_1746_test.py
+++ tests/bugs/core_1746_test.py
@@ -15,11 +15,12 @@ NOTES:
         On 2.5.27020 and 3.0.1 such attempt leads to exception "-901 / object ... in use" - and this is expected.
         See also core_4386_test.py.
         Checked on 3.0.8.33535 (SS/CS), 4.0.1.2692 (SS/CS), 5.0.0.730 (SS/CS)
-    [11.04.2026] pzotov
-        Disabled run on 6.x+ because of shared metadata cache introduction since 6.0.0.1771-f73321c (25.02.2026).
-        Outcome of actions described in this ticket must be opposite to expected.
-        Re-implemented version of this test will appear in $QA_ROOT/tests/functional/metacache/ folder.
-        Checked on 5.0.4.1808; 4.0.7.3269; 3.0.14.33855.
+    [22.04.2026] pzotov
+        Adjusted expected_output on 6.x+ because of shared metadata cache (introduced 6.0.0.1771-f73321c, 25.02.2026).
+        Error 'object in use' still MAY occur in 6.x when index is created but concurrent transaction with DML exists.
+        Explained by Alex, letter 24.04.2026 1753.
+        Additional version of this test that exploits shared metacache features see in functional/metacache/ folder.
+        Checked on 6.0.0.1914; 5.0.4.1813; 4.0.7.3271; 3.0.14.33855
 """
 
 import pytest
@@ -30,9 +31,10 @@ from firebird.driver import tpb, Isolation
 
 db = db_factory(init='create table test(id int);')
 
+substitutions = [('[ \t]+', ' '), ('^(-)?', '')]
 act = python_act('db')
 
-@pytest.mark.version('>=3,<6')
+@pytest.mark.version('>=3')
 def test_1(act: Action, capsys):
 
     tx_isol_lst = [ Isolation.READ_COMMITTED_NO_RECORD_VERSION,
@@ -42,6 +44,9 @@ def test_1(act: Action, capsys):
                   ]
     if act.is_version('>=4'):
        tx_isol_lst.append(Isolation.READ_COMMITTED_READ_CONSISTENCY)
+
+    # tmp
+    tx_isol_lst = [ Isolation.SNAPSHOT, ]
 
     # for any isolation mode attempt to run DDL for table that is in use by another Tx must fail
     # with the same error message. We check all possible Tx isolation modes for that:
@@ -63,14 +68,23 @@ def test_1(act: Action, capsys):
                     print(e.__str__())
                     print(e.gds_codes)
 
-        expected_out = """
+        expected_out_5x = """
             lock conflict on no wait transaction
             -unsuccessful metadata update
             -object TABLE "TEST" is in use
             (335544345, 335544351, 335544453)
         """
 
-        act.expected_stdout = expected_out
+        expected_out_6x = """
+            unsuccessful metadata update
+            -CREATE INDEX "PUBLIC"."TEST_CALC_IDX" failed
+            -lock conflict on no wait transaction
+            -unsuccessful metadata update
+            -object TABLE "PUBLIC"."TEST" is in use
+            (335544351, 336397316, 335544345, 335544351, 335544453)
+        """
+
+        act.expected_stdout = expected_out_5x if act.is_version('<6') else expected_out_6x
         act.stdout = capsys.readouterr().out
         assert act.clean_stdout == act.clean_expected_stdout
         act.reset()
