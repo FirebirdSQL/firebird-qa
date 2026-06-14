ef7035c971c4ffd4ee84fe2de28f9dfbdf59b820
pavel-zotov <p519446@yandex.ru>
2026-06-14 18:51:24 +0300

    Added/Updated tests\bugs\core_2783_test.py: Changed query to reproduce problem -- see notes. Confirmed bug on 2.1.3.18185. Checked on 2.1.7.18553 -- all fine. Checked on 6.0.0.2002; 5.0.5.1826; 4.0.8.3279; 3.0.14.33855.

--- tests/bugs/core_2783_test.py
+++ tests/bugs/core_2783_test.py
@@ -6,61 +6,73 @@ ISSUE:       3174
 TITLE:       AV using recursive query as subquery in SELECT list and ORDER'ing by them
 DESCRIPTION:
 JIRA:        CORE-2783
-FBTEST:      bugs.core_2783
+NOTES:
+    [14.06.2026] pzotov
+    Changed query to reproduce problem: on 6.x we have to avoid referring of RDB$DATABASE.RDB$RELATION_ID
+    because GENERATOR is used to store generated relation_id instead of this field, see #bb280120.
+
+    Confirmed bug on 2.1.3.18185 (check possible only using command prompt and ISQL; not in this QA).
+    Checked on 2.1.7.18553 -- all fine.
+
+    Checked on 6.0.0.2002; 5.0.5.1826; 4.0.8.3279; 3.0.14.33855.
 """
 
 import pytest
 from firebird.qa import *
 
-db = db_factory(charset='UTF8')
+db = db_factory()
 
-test_script = """SELECT RDB$RELATION_ID,
-       (WITH RECURSIVE
-         NUM (ID) AS
-         (
-           SELECT 1 FROM RDB$DATABASE
+test_script = """
+    set list on;
+    recreate table test(id int);
+    insert into test(id) values(128);
+    commit;
 
-           UNION ALL
+    select
+        1 as qry
+        ,(
+             with recursive
+             num (id) as
+             (
+                 select 1 from test
+                 UNION ALL
+                 select id + 1
+                 from num
+                 where id < 10
+             )
+            select max(id) from num
+        ) as nnn
+    from test
+    order by nnn;
 
-           SELECT ID + 1
-             FROM NUM
-            WHERE ID < 10
-         )
-        SELECT MAX(ID) FROM NUM
-       ) AS NNN
-  FROM RDB$DATABASE
-ORDER BY NNN;
-WITH RECURSIVE
-  NUM (ID) AS
-  (
-           SELECT 1 FROM RDB$DATABASE
+    with recursive
+      num (id) as
+      (
+               select 1 from test
 
-           UNION ALL
+               union all
 
-           SELECT ID + 1
-             FROM NUM
-            WHERE ID < 10
-  )
-SELECT RDB$RELATION_ID, (SELECT MAX(ID) FROM NUM) AS NNN
-  FROM RDB$DATABASE
-ORDER BY NNN;
+               select id + 1
+                 from num
+                where id < 10
+      )
+    select 2 as qry, (select max(id) from num) as nnn
+    from test
+    order by nnn;
 """
 
-act = isql_act('db', test_script)
+substitutions = [('[ \t]+', ' ')]
+act = isql_act('db', test_script, substitutions = substitutions)
 
 expected_stdout = """
-RDB$RELATION_ID          NNN
-=============== ============
-            128           10
-
-RDB$RELATION_ID          NNN
-=============== ============
-            128           10
+    QRY 1
+    NNN 10
+    QRY 2
+    NNN 10
 """
 
 @pytest.mark.version('>=3')
 def test_1(act: Action):
     act.expected_stdout = expected_stdout
-    act.execute()
+    act.execute(combine_output = True)
     assert act.clean_stdout == act.clean_expected_stdout
-
