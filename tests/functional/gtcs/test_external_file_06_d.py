077b690d5f4e9b57df173a1bd964a22a9a3e5ce4
pavel-zotov <p519446@yandex.ru>
2026-09-12 01:42:40 +0300

    Added/Updated tests\functional\gtcs\test_external_file_06_d.py: Test has been replaced with functional/table/external/test_external_file_all_datatypes.py. Execution DISABLED.

--- tests/functional/gtcs/test_external_file_06_d.py
+++ tests/functional/gtcs/test_external_file_06_d.py
@@ -5,18 +5,15 @@ ID:          gtcs.external-file-06
 FBTEST:      functional.gtcs.external_file_06_d
 TITLE:       Test for external table with field of DOUBLE PRECISION datatype
 DESCRIPTION:
-  Original test see in:
-  https://github.com/FirebirdSQL/fbtcs/blob/master/GTCS/tests/EXT_REL_0_6_D.script
+    Original test see in:
+    https://github.com/FirebirdSQL/fbtcs/blob/master/GTCS/tests/EXT_REL_0_6_D.script
 NOTES:
-[03.03.2021] pzotov
-  Added substitution for zero value ('F02') as result of evaluating exp(-745.1332192):
-  on Windows number of digits in decimal representation more than on Linux for 1.
-NOTES:
-  [31.07.2022] pzotov
-  1. FB config must allow creation of external tables, check parameter 'ExternalFileAccess'.
-     Otherwise: SQLSTATE = 28000 / Use of external file at location ... is not allowed ...
-  2. REMOVED evaluation of exp(-745.***) because results depends on CPU (intel vs AMD).
-  Checked on 3.0.8.33535, 4.0.1.2692, 5.0.0.591
+    [03.03.2021] pzotov
+    Added substitution for zero value ('F02') as result of evaluating exp(-745.1332192):
+    on Windows number of digits in decimal representation more than on Linux for 1.
+    [11.09.2026] pzotov
+    Test has been replaced with functional/table/external/test_external_file_all_datatypes.py
+    Execution DISABLED.
 """
 
 
@@ -41,6 +38,7 @@ expected_stdout = """
     Records affected: 1
 """
 
+@pytest.mark.skip('Not needed anymore. See functional/table/external/test_external_file_all_datatypes.py')
 @pytest.mark.version('>=3.0')
 def test_1(act: Action, tmp_ext_file: Path):
     
