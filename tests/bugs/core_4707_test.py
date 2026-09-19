e5acbb648a431a4498825889712ac145aaff7639
pavel-zotov <p519446@yandex.ru>
2026-09-19 14:47:00 +0300

    Added/Updated tests\bugs\core_4707_test.py: significantly reworked after receiving notes from ZCODE.AI. Use `terminate_sync()` function from QA-plugin in order to avoid PermissionError at terdown stage with fixtures created by temp_file(). See notes.

--- tests/bugs/core_4707_test.py
+++ tests/bugs/core_4707_test.py
@@ -2,46 +2,73 @@
 
 """
 ID:          issue-5015
-ISSUE:       5015
+ISSUE:       https://github.com/FirebirdSQL/firebird/issues/5015
 TITLE:       Implement ability to validate tables and indices online (without exclusive access to database)
 DESCRIPTION:
+    Test verifies ability to run online validation during 'static' state of database with one connection
+    which has done several incomplete DML and stands in endless pause (because of `WAIT` transaction and attempt
+    to change via ES/EDS some record that has been already locked).
 JIRA:        CORE-4707
 FBTEST:      bugs.core_4707
 NOTES:
-    [30.06.2025] pzotov
-    Separated expected output for FB major versions prior/since 6.x.
-    No substitutions are used to suppress schema and quotes. Discussed with dimitr, 24.06.2025 12:39.
-    Checked on 6.0.0.881; 5.0.3.1668; 4.0.6.3214; 3.0.13.33813.
+    [19.09.2026] pzotov
+    Test has been significantly reworked after receiving notes from ZCODE.AI about waiting via `time.sleep(2)`.
+    This pause starts just after async launch os ISQL in order to let this process to load in memory and complete code
+    that eventually falls into infinite waiting. Online validation should start only after this pause completed.
+    This way is unreliable: under heavy concurrent workload ISQL may not complete its work (or even does not load in memory)
+    and online validation will not check what it should because some table(s) either not exist or one of them not yet locked.
+    
+    To avoid this, we have to run auxiliary connect (`con_monitoring`) which has to perform loop and check presence of
+    ISQL process in mon$attachments and, moreover, check that value of generator `g` is greater than 0 (this will prove that
+    ISQL has started `WAIT` transaction and stands now in endless waiting for record that is locked).
 
-    [02.11.2025] pzotov
-    Commented out 'request.addfinalizer(drop_connections)' because QA plugin already has similar functionality
-    for teardown phase - see class Database, func drop().
-    Checked on Windows 6.0.0.1335; 5.0.4.1725; 4.0.7.3237; 3.0.14.33827 (SS and CS)
+    After this connect will detect such record and verify value of generator, we can break from monitoring loop and start
+    online validation. After validation finish, we have to terminate async ISQL process and check validation output.
+    If this connect could not be found during some limit (see `MAX_WAIT_FOR_WORKER_START_MS`) then we
+    have to terminate async ISQL, break from loop and run assertion with showing ISQL log.
+
+    ### CRITICAL ISSUE-1 ###
+    Asynchronously launched ISQL (see `p_hang_sql`) keeps opened .sql and .log files created using fixture `temp_file()`.
+    It was found that after call its p_hang_sql.terminate() these logs may live BEYOND this process is gone. Main reason:
+    https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-terminateprocess
+    ("TerminateProcess is asynchronous; it initiates termination and returns immediately.")
+    This caused PermissionError (32) at teardown stage: one of both of them could not be deleted because they still were
+    opened by OS (though for a short time).
+    This was explained by zcode.ai (together with suggested fix - see call of `terminate_sync()` function from QA-plugin).
+
+    ### CRITICAL ISSUE-2 ###
+    We have to be sure that while async ISQL stands in endless waiting its connection will not be cancelled because of
+    too small value of per-database `StatementTimeout` parameter which may present in the firebird.conf (though there is
+    completely no sense to set this limit to such small values like 1..2 seconds). It order to prevent it, parameter
+    is CHANGED by replacing existing databases.conf with temporary content, see ConfigManager usage.
+    This can be done since 4.0.0, see: 2c49e6fc / hvlad / 22.02.2017 14:30:57 +0200
+    ("New feature CORE-5488 : Timeouts for running SQL statements and idle connections")
+
+    Checked on 6.0.0.2176; 5.0.5.1886; 4.0.8.3320; 3.0.15.33885.
 """
-
 import pytest
 import subprocess
 import time
+import datetime as py_dt
 from pathlib import Path
 from firebird.qa import *
 
+MAX_WAIT_FOR_WORKER_START_MS = 20000
+
 init_script = """
-    set term ^;
-    execute block as
-    begin
-        execute statement 'drop sequence g';
-    when any do begin end
-    end^
-    set term ;^
+    recreate sequence g;
     commit;
-    create sequence g;
-    commit;
+    -- this table will NOT be validated because of incompleted `UPDATE` against it:
     recreate table test1(id int, s varchar(1000));
+
+    -- this table MUST be validated:
     recreate table test2(id int primary key using index test2_pk, s varchar(1000), t computed by (s) );
+
+    -- this table will NOT be validated because of incompleted `INSERT` against it:
     recreate table test3(id int);
     commit;
 
-    insert into test1(id, s) select gen_id(g,1), rpad('', 1000, gen_id(g,0) ) from rdb$types rows 100;
+    insert into test1(id, s) select row_number()over(), rpad('', 1000, uuid_to_char(gen_uuid())) from rdb$types rows 100;
     insert into test2(id, s) select id, s from test1;
     commit;
 
@@ -51,52 +78,45 @@ init_script = """
     commit;
 """
 
+# Name of alias that will be written into temporary replacement of databases.conf:
+CONNECT_TO_ALIAS = 'tmp_core_4707_alias'
+
 db = db_factory(init=init_script)
 
-substitutions=[ ('\\d{2}:\\d{2}:\\d{2}.\\d{2}', ''), ('Relation \\d{3,4}', 'Relation') ]
+substitutions=[ ('\\d{2}:\\d{2}:\\d{2}.\\d{2}', ''), ('Relation \\d{3,4}', 'Relation'), ('[ \t]+', ' ') ]
 act = python_act('db', substitutions = substitutions)
 
-hang_script_file = temp_file('hang_script.sql')
-hang_output = temp_file('hang_script.out')
+tmp_hang_sql = temp_file('tmp_hanging_4707.sql')
+tmp_hang_log = temp_file('tmp_hanging_4707.log')
 
+@pytest.mark.ai
 @pytest.mark.es_eds
 @pytest.mark.version('>=3.0')
-def test_1(act: Action, hang_script_file: Path, hang_output: Path, capsys, request):
-    
-    ## Fializer for FB4
-    #def drop_connections():
-    #    with act.db.connect() as con4cleanup:
-    #        con4cleanup.execute_immediate('delete from mon$attachments where mon$attachment_id != current_connection')
-    #        con4cleanup.commit()
-    #
-    #request.addfinalizer(drop_connections)
+def test_1(act: Action, tmp_hang_sql: Path, tmp_hang_log: Path, store_config: ConfigManager, capsys):
 
-    # Following script will hang for sevral seconds (see 'lock timeout' argument - and this will serve as pause
-    # during which we can launch fbsvcmgr to validate database:
-    hang_script_file.write_text(
+    statement_timeout_sql = "select * from rdb$config where lower(rdb$config_name) = lower('StatementTimeout')"
+    if act.is_version('<4'):
+        statement_timeout_sql = ''
+
+    # Following script will hang for endless waiting ==> we can start online validation
+    # when mon$attachment will contain record of appriate async ISQL:
+    tmp_hang_sql.write_text(
         f"""
-            set term ^;
-            execute block as
-            begin
-                execute statement 'drop role tmp$r4707';
-                when any do begin end
-            end ^
-            set term ;^
+            set echo on;
+            set list on;
+            set count on;
+            {statement_timeout_sql};
             commit;
-
             set transaction wait;
-
             delete from test1;
             insert into test3(id) values(1);
-            set list on;
             select 'Starting EB with infinite pause.' as isql_msg from rdb$database;
             set term ^;
             execute block as
             begin
-                execute statement 'update test1 set id=-id'
+                execute statement ('update test1 set id = -id where id = ?') ( gen_id(g,1) )
                 on external 'localhost:' || rdb$get_context('SYSTEM','DB_NAME')
                 as user '{act.db.user}' password '{act.db.password}'
-                   role 'TMP$R4707' -- this will force to create new attachment, and its Tx will be paused on INFINITE time.
                 ;
                 -- Discussed with Vlad, letters 29.03.2025 ... 09.04.2025
                 -- subj: "WI-6.0.0.707-4bd4f5f0, Classic. CORE-4707 (online validation): ..."
@@ -108,26 +128,121 @@ def test_1(act: Action, hang_script_file: Path, hang_output: Path, capsys, reque
         """
     )
 
-    # Make asynchronous call of ISQL which will stay several seconds in pause due to row-level lock
-    with open(hang_output, mode='w') as hang_out:
-        p_hang_sql = subprocess.Popen([act.vars['isql'], '-i', str(hang_script_file),
-                                       '-user', act.db.user,
-                                       '-password', act.db.password, act.db.dsn],
-                                       stdout=hang_out, stderr=subprocess.STDOUT)
-        try:
-            time.sleep(2)
-            # Make SYNC. call of fbsvcmgr in order to validate database which has locks on some relations
-            act.svcmgr(switches=['action_validate', 'dbname', str(act.db.db_path),
-                                   'val_lock_timeout', '1'])
-        finally:
-            p_hang_sql.terminate()
-    #
-    print(hang_output.read_text())
-    print(act.stdout)
+    if act.is_version('<4'):
+        # There was no parameter 'StatementTimeout' in FB 3.x. SKIP replacing of database.conf.
+        pass
+    else:
+        # Obtain the physical database location.
+        # NOTE: we must NOT use 'act.db.db_path' for ALIASED databases! It will return '.' rather than full path+filename.
+        # Use only con.info.name for that:
+        TEMP_DB_CONF_CONTENT = ''
+        with act.db.connect() as con:
+            test_db_file = con.info.name
+            test_db_path = Path(test_db_file).parent
+            TEMP_DB_CONF_CONTENT = f"""
+                {CONNECT_TO_ALIAS} = {test_db_file}
+                {{
+                    StatementTimeout = 0
+                }}
+            """
+        assert CONNECT_TO_ALIAS in TEMP_DB_CONF_CONTENT and 'StatementTimeout' in TEMP_DB_CONF_CONTENT
 
+        # REPLACE databases.conf: put there `StatementTimeout = 0` in order to prevent cancellation
+        # of endless waiting in async ISQL:
+        store_config.replace('databases.conf', TEMP_DB_CONF_CONTENT)
+
+    worker_attach_id = 0
+    in_locked_state = False
+    p_hang_sql = None
+    validation_output = ''
+    with open(tmp_hang_log, mode='w') as f_hang_log:
+        try:
+            # Create connection for watching mon$attachments: we are waiting appearance
+            # of ISQL process which has to hang on lock conflict in EDS statement.
+            # Each iteration opens and completes its own transaction, see core_5685_test.py:
+            #
+            with act.db.connect() as con_monitoring:
+                cur_monitoring = con_monitoring.cursor()
+
+                sql_watched = """
+                    select /* trace_me */ mon$attachment_id from mon$attachments
+                    where
+                        mon$attachment_id != current_connection
+                        and trim(lower(mon$remote_process)) similar to '%[\\/]isql(.exe)?'
+                """
+
+                iter = 0
+                t1 = py_dt.datetime.now()
+                while True:
+                    if iter == 0:
+                        #########################
+                        ###   'w o r k e r'   ###
+                        #########################
+                        # Asynchronous launch ISQL which will be blocked infinitely in pause due to row-level lock
+                        p_hang_sql = subprocess.Popen([act.vars['isql'], '-i', str(tmp_hang_sql),
+                                                       '-user', act.db.user,
+                                                       '-password', act.db.password, act.db.dsn],
+                                                      stdout=f_hang_log, stderr=subprocess.STDOUT)
+
+                    con_monitoring.begin()
+                    try:
+                        cur_monitoring.execute(sql_watched)
+                        worker_attach_id = cur_monitoring.fetchone()
+                        if worker_attach_id:
+                            try:
+                                cur_monitoring.execute('select gen_id(g,0) from rdb$database')
+                                in_locked_state = cur_monitoring.fetchone()[0] > 0
+                            except DatabaseError:
+                                in_locked_state = False
+                    finally:
+                        con_monitoring.rollback()
+
+                    if worker_attach_id and in_locked_state:
+
+                        ###############################################
+                        ###    o n l i n e    v a l i d a t i o n   ###
+                        ###############################################
+                        # Validate database which has connection that infinitely waits for on some resource:
+                        act.svcmgr(switches=['action_validate', 'dbname', str(act.db.db_path), 'val_lock_timeout', '1'])
+                        validation_output = act.stdout
+                        act.reset() 
+
+                        break
+
+                    if p_hang_sql.poll() is not None:
+                        # ISQL has completed: nothing to wait for more (this is possible
+                        # only if its script failed to hang; assert below will show its log).
+                        break
+
+                    t2 = py_dt.datetime.now()
+                    d1 = t2 - t1
+                    if d1.seconds * 1000 + d1.microseconds // 1000 >= MAX_WAIT_FOR_WORKER_START_MS:
+                        break
+                    else:
+                        time.sleep(0.2)
+
+                    iter += 1
+                # < while True
+
+            #< with act.db.connect() as con_monitoring
+
+        except Exception as e:
+            print(f'{e.__class__=}')
+            print(e.__str__())
+
+        finally:
+            # suggested by ZCODE.AI, added to QA plugin 19.09.2026. Source code see in QA-plugin:
+            terminate_sync(p_hang_sql)
+
+    # < with open(tmp_hang_log, mode='w')
+
+    if not (worker_attach_id and in_locked_state):
+        isql_log = '\n'.join( [ x for x in tmp_hang_log.read_text().splitlines() if x.strip() ] )
+        print(f"Could not detect ISQL process with established connection and locked resource for {MAX_WAIT_FOR_WORKER_START_MS} ms. Check its log:\n" + isql_log)
+
+    print(validation_output)
 
     expected_stdout_5x = """
-        ISQL_MSG                        Starting EB with infinite pause.
         Validation started
         Relation (TEST1)
         Acquire relation lock failed
@@ -146,7 +261,6 @@ def test_1(act: Action, hang_script_file: Path, hang_output: Path, capsys, reque
     """
 
     expected_stdout_6x = """
-        ISQL_MSG                        Starting EB with infinite pause.
         Validation started
         Relation ("PUBLIC"."TEST1")
         Acquire relation lock failed
